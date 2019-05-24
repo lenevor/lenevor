6160e54c2d7453ca8cfd78da7f6b497b659ea7cb
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-05-24 15:58:16 -0500

    Added menu iconList

--- syscode/classes/Debug/Exceptions/Resources/views/frame_description.php
+++ syscode/classes/Debug/Exceptions/Resources/views/frame_description.php
@@ -1,5 +1,20 @@
 <div class="frame-description frame-description-application">
 
-	<span href="#"><?= e(__('exception.stackFrames')) ?> (<?=count($frames)?>)</span>:
+	<span href="#"><?= e(__('exception.stackFrames')) ?> (<?=count($frames)?>)</span>
+
+	<div class="iconlist">
+		<div class="icon-holder icon-print" onclick="javascript:window.print()">
+			<div class="tooltip tooltip-print">
+				<?= e(__('exception.print'))?>
+			</div>
+			<i class="icofont-print"></i>
+		</div>      
+		<div class="icon-holder icon-pdf">
+			<div class="tooltip tooltip-pdf">
+				<?= e(__('exception.openReaderPDF'))?>
+			</div>
+			<i class="icofont-file-pdf"></i>
+		</div>  
+	</div>
 
 </div>
