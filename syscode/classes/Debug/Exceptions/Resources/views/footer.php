33ba7192212529c1f2477046fa345672100ded81
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-06-10 11:41:16 -0500

    Added classes of span tag: 'status-code' and 'php-version'

--- syscode/classes/Debug/Exceptions/Resources/views/footer.php
+++ syscode/classes/Debug/Exceptions/Resources/views/footer.php
@@ -4,11 +4,13 @@
 
 		<div class="name">
 			<?= e($brand) ?>
+			<span class="status-code" title="Status Code"><?= (new \Syscode\Debug\GDebug)->sendHttpCode() ?></span>
+			<span class="php-version" title="php version"><?= phpversion() ?></span>
+			
 		</div>
-
+		
 		<div class="copy">
-			Displayed at <?= date('H:i:sa') ?> &mdash;
-			PHP: <?= phpversion() ?>  &mdash;
+			Displayed at <?= date('H:i:sa') ?> &mdash;			
 			<?= \Syscode\Version::shortVersion() ?>
 		</div>
 
