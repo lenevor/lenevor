10dc119ee0c24e6d3f991d39b132e6a1811c5e60
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-06-12 12:17:49 -0500

    Added '{elapsed_time}' tag for show benchmark

--- resources/views/partials/footer.plaze.php
+++ resources/views/partials/footer.plaze.php
@@ -1,5 +1,5 @@
 		<footer>
 			<p>Version: <b><?= \Syscode\Version::RELEASE ?>&#45;<?= \Syscode\Version::STATUS ?></b></p>
-			<p>Rendered: <b></b></p>
+			<p>Rendered: <b>{elapsed_time}</b></p>
 			<p>Environment: <b><?= ENVIRONMENT ?></b></p>			
 		</footer>
