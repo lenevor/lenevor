960301e4f571ac1a2e0c9ab6018e92b15db22345
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-07-20 00:44:47 -0500

    Added new language files for HTTP

---
+++ resources/lang/es/http.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Http de Líneas De Idioma                                                       
+    |------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma contienen los mensajes de error 
+    | predeterminados por la respuest HTTP. Usted es libre de cambiar 
+    | cualquier valor que desee personalizar en estas líneas que mejor se 
+    | adapte a su solicitud.
+    |
+    */
+    
+    'cannotParseURI' => 'No se puede analizar URI: {0}',
+    'invalidPort' => 'Los puertos deben estar entre 0 y 65535. Dado: {0}',
+
+];
