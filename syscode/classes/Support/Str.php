834fb4892c97a1fee6a830c4cd834b1ee42100ef
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-07-26 22:57:15 -0500

    Added new method

--- syscode/classes/Support/Str.php
+++ syscode/classes/Support/Str.php
@@ -58,6 +58,27 @@ class Str
     }
 
     /**
+     * Determine if a given string contains a given substring.
+     * 
+     * @param  string        $haystack
+     * @param  string|array  $needles
+     * 
+     * @return bool
+     */
+    public static function contains($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle)
+        {
+            if ($needle !== '' && mb_strpos($haystack, $needle) !== false)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Determine if a given string ends with a given substring.
      *
      * @param  string        $haystack
