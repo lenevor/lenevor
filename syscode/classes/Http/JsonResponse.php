982629d251f9c67db9e1ea847496111e367d3381
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-07-29 00:23:47 -0500

    Added main method __construct

--- syscode/classes/Http/JsonResponse.php
+++ syscode/classes/Http/JsonResponse.php
@@ -24,6 +24,7 @@
 
 namespace Syscode\Http;
 
+use ArrayObject;
 use InvalidArgumentException;
 
 /**
@@ -33,5 +34,33 @@ use InvalidArgumentException;
  */
 class JsonResponse extends Response
 {
-    
+    /**
+     * The JSON encoding options.
+     * 
+     * @var int $jsonEncodingOptions
+     */
+    protected $jsonEncodingOptions;
+
+    /**
+     * Constructor. The JsonReponse classs instance.
+     * 
+     * @param  mixed|null  $data     (null by default)
+     * @param  int         $status   (200 by default)
+     * @param  array       $headers  
+     * @param  int         $options  (0 by default)
+     * @param  bool        $json     (false by default)
+     * 
+     * @return void
+     */
+    public function __construct($data = null, int $status = 200, array $headers = [], int $options = 0, bool $json = false)
+    {
+        $this->jsonEncodingOptions = $options;
+
+        parent::__construct($data, $status, $headers);
+
+        if (null === $data)
+        {
+            $data = new ArrayObject;
+        }
+    }
 }
