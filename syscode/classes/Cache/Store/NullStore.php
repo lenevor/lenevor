77d9fdf2af04e6e0779211f0dc2664233db5baaf
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-08-24 11:43:34 -0500

    Added new methods

--- syscode/classes/Cache/Store/NullStore.php
+++ syscode/classes/Cache/Store/NullStore.php
@@ -33,5 +33,107 @@ use Syscode\Contracts\Cache\Store;
  */
 class NullStore implements Store
 {
-    
+    /**
+     * The array storaged value.
+     * 
+     * @var array $storage
+     */
+    protected $storage = [];
+
+    /**
+     * Gets an item from the cache by key.
+     * 
+     * @param  string|array  $key
+     * 
+     * @return mixed
+     */
+    public function get($key)
+    {
+        //
+    }
+
+    /**
+     * Store an item in the cache for a given number of seconds.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * @param  int     $seconds
+     * 
+     * @return bool
+     */
+    public function put($key, $value, $seconds)
+    {
+        return false;
+    }
+
+    /**
+     * Increment the value of an item in the cache.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value  (1 by default)
+     * 
+     * @return int
+     */
+    public function increment($key, $value = 1)
+    {
+        return false;
+    }
+
+    /**
+     * Decrement the value of an item in the cache.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value  (1 by default)
+     * 
+     * @return int
+     */
+    protected function decrement($key, $value = 1)
+    {
+        return false;
+    }
+
+    /**
+     * Remove a specific item from the cache store.
+     * 
+     * @param  string  $key
+     * 
+     * @return bool
+     */
+    public function delete($key)
+    {
+        return true;
+    }
+
+    /**
+     * Stores an item in the cache indefinitely.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * 
+     * @return bool
+     */
+    public function forever($key, $value)
+    {
+        return false;
+    }
+
+    /**
+     * Remove all items from the cache.
+     * 
+     * @return bool
+     */
+    public function flush()
+    {
+        return true;
+    }
+
+    /**
+     * Gets the cache key prefix.
+     * 
+     * @return string
+     */
+    public function getPrefix()
+    {
+        return '';
+    }
 }
