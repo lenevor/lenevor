7aea34f04823507ab600975ce7aea7aac2dece2b
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-08-25 11:09:13 -0500

    Added new methods

--- syscode/classes/Cache/Store/MemcachedStore.php
+++ syscode/classes/Cache/Store/MemcachedStore.php
@@ -65,6 +65,132 @@ class MemcachedStore implements Store
     }
 
     /**
+     * Gets an item from the cache by key.
+     * 
+     * @param  string|array  $key
+     * 
+     * @return mixed
+     */
+    public function get($key)
+    {
+        $value = $this->memcached->get($this->prefix.$key);
+
+        if ($this->memcached->getResultCode() == 0)
+        {
+            return $value;
+        }
+    }
+
+    /**
+     * Store an item in the cache if the key doesn't exist.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * @param  int     $seconds
+     * 
+     * @return bool
+     */
+    public function add($key, $value, $seconds)
+    {
+        return $this->memcached->add($this->prefix.$key, $value, $this->calcExpiration($seconds));
+    }
+
+    /**
+     * Store an item in the cache for a given number of seconds.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * @param  int     $seconds
+     * 
+     * @return bool
+     */
+    public function put($key, $value, $seconds)
+    {
+        return $this->memcached->set($this->prefix.$key, $value, $this->calcExpiration($seconds));
+    }
+
+    /**
+     * Increment the value of an item in the cache.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value  (1 by default)
+     * 
+     * @return int
+     */
+    public function increment($key, $value = 1)
+    {
+        return $this->memcached->increment($this->prefix.$key, $value);
+    }
+
+    /**
+     * Decrement the value of an item in the cache.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value  (1 by default)
+     * 
+     * @return int
+     */
+    public function decrement($key, $value = 1)
+    {
+        return $this->memcached->decrement($this->prefix.$key, $value = 1);
+    }
+
+     /**
+     * Remove a specific item from the cache store.
+     * 
+     * @param  string  $key
+     * 
+     * @return bool
+     */
+    public function delete($key)
+    {
+        return $this->memcached->delete($this->prefix.$key);
+    }
+
+    /**
+     * Stores an item in the cache indefinitely.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * 
+     * @return bool
+     */
+    public function forever($key, $value)
+    {
+        return $this->put($this->prefix.$key, $value, 0);
+    }
+
+    /**
+     * Remove all items from the cache.
+     * 
+     * @return bool
+     */
+    public function flush()
+    {
+        return $this->memcached->flush();
+    }
+
+    /**
+     * Gets the Memcached connection.
+     * 
+     * @return \Memcached
+     */
+    public function getMemcached()
+    {
+        return $this->memcached;
+    }
+
+    /**
+     * Gets the cache key prefix.
+     * 
+     * @return string
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+    
+    /**
      * Set the cache key prefix.
      * 
      * @param  string  $prefix
@@ -74,5 +200,29 @@ class MemcachedStore implements Store
     public function setPrefix($prefix)
     {
         $this->prefix = ! empty($prefix) ? $prefix.':' : '';
+    }
+
+    /**
+     * Gets the expiration time of the key.
+     * 
+     * @param  int  $seconds
+     * 
+     * @return int
+     */
+    protected function calcExpiration($seconds)
+    {
+        return $this->toTimestamp($seconds);
+    }
+
+    /**
+     * Gets the UNIX timestamp for the given number of seconds.
+     * 
+     * @param  int  $seconds
+     * 
+     * @return int
+     */
+    protected function toTimestamp($seconds)
+    {
+        return $seconds > 0 ? $this->availableAt($seconds) : 0;
     }
 }
