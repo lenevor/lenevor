7f5107b32f80632a495a6d7fc25fa2b8b0147685
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-08-30 23:31:09 -0500

    Added new methods

--- syscode/classes/Cache/Store/RedisStore.php
+++ syscode/classes/Cache/Store/RedisStore.php
@@ -24,6 +24,7 @@
 
 namespace Syscode\Cache\Store;
 
+use Syscode\Redis\Redis;
 use Syscode\Contracts\Cache\Store;
 
 /**
@@ -33,5 +34,213 @@ use Syscode\Contracts\Cache\Store;
  */
 class RedisStore implements Store
 {
+    /**
+     * The Redis connection that should be used.
+     * 
+     * @var string $connection
+     */
+    protected $connection;
 
+    /**
+     * A string that should be prepended to keys.
+     * 
+     * @var string $prefix
+     */
+    protected $prefix;
+
+    /**
+     * The Redis database connection.
+     * 
+     * @var \Syscode\Redis\Redis $redis 
+     */
+    protected $redis;
+
+    /**
+     * Constructor. Create a new Redis store.
+     * 
+     * @param  \Syscode\Redis\Redis  $redis 
+     * @param  string                $prefix
+     * @param  string                $connection  
+     * 
+     * @return void
+     */
+    public function __construct(Redis $redis, $prefix = '', $connection = 'default')
+    {
+        $this->redis = $redis;
+        $this->setPrefix($prefix);
+        $this->setConnection($connection);
+    }
+
+    /**
+     * Gets an item from the cache by key.
+     * 
+     * @param  string|array  $key
+     * 
+     * @return mixed
+     */
+    public function get($key)
+    {
+        $value = $this->connection()->get($this->prefix.$key);
+
+        return ! is_null($value) ? $this->unserialize($value) : null;
+    }
+
+    /**
+     * Store an item in the cache for a given number of seconds.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * @param  int     $seconds
+     * 
+     * @return bool
+     */
+    public function put($key, $value, $seconds)
+    {
+        return (bool) $this->connection()->setex(
+                $this->prefix.$key,
+                (int) max(1, $seconds),
+                $this->serialize($value)
+        );
+    }
+
+    /**
+     * Increment the value of an item in the cache.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value  (1 by default)
+     * 
+     * @return int
+     */
+    public function increment($key, $value = 1)
+    {
+        return $this->connection()->incrby($this->prefix.$key, $value);
+    }
+
+    /**
+     * Decrement the value of an item in the cache.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value  (1 by default)
+     * 
+     * @return int
+     */
+    public function decrement($key, $value = 1)
+    {
+        return $this->connection()->decrby($this->prefix.$key, $value);
+    }
+
+    /**
+     * Remove a specific item from the cache store.
+     * 
+     * @param  string  $key
+     * 
+     * @return bool
+     */
+    public function delete($key)
+    {
+        return (bool) $this->connection()->del($this->prefix.$key);
+    }
+
+    /**
+     * Stores an item in the cache indefinitely.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * 
+     * @return bool
+     */
+    public function forever($key, $value)
+    {
+        return (bool) $this->connection()->set($this->prefix.$key, $this->serialize($value));
+    }
+
+    /**
+     * Remove all items from the cache.
+     * 
+     * @return bool
+     */
+    public function flush()
+    {
+        $this->connection()->flushdb();
+
+        return true; 
+    }
+
+    /**
+     * Get the Redis database instance.
+     * 
+     * @return \Syscode\Redis\Redis
+     */
+    public function getRedis()
+    {
+        return $this->redis;
+    }
+
+    /**
+     * Get the Redis connection instance.
+     * 
+     * @return \Predis\ClientInterface
+     */
+    public function connection()
+    {
+        return $this->redis->connection($this->connection);
+    }
+
+    /**
+     * Set the connection name to be used.
+     * 
+     * @param  string  $connection
+     * 
+     * @return void
+     */
+    public function setConnection($connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Get the cache key prefix.
+     * 
+     * @return string
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * Set the cache key prefix.
+     * 
+     * @param  string  $prefix
+     * 
+     * @return void
+     */
+    public function setPrefix($prefix)
+    {
+        return $this->prefix = (strlen($prefix) > 0) ? $prefix.':' : '';
+    }
+
+    /**
+     * Serialize the value.
+     * 
+     * @param  mixed  $value
+     * 
+     * @return mixed
+     */
+    public function serialize($value)
+    {
+        return is_numeric($value) ? $value : serialize($value);
+    }
+
+    /**
+     * Unserialize the value.
+     * 
+     * @param  mixed  $value
+     * 
+     * @return mixed
+     */
+    public function unserialize($value)
+    {
+        return is_numeric($value) ? $value : unserialize($value);
+    }
 }
