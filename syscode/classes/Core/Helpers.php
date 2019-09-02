a7e17afd25920caaeb098082f2eda54c2d2793be
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-09-01 22:54:46 -0500

    Update file

--- syscode/classes/Core/Helpers.php
+++ syscode/classes/Core/Helpers.php
@@ -102,6 +102,46 @@ if ( ! function_exists('back'))
     }
 }
 
+if ( ! function_exists('cache'))
+{
+    /**
+     * Get / set the specified cache value.
+     *
+     * If an array is passed, we'll assume you want to put to the cache.
+     *
+     * @param  dynamic  key|key,default|data,expiration|null
+     * @return mixed|\Illuminate\Cache\CacheManager
+     *
+     * @throws \Exception
+     */
+    function cache()
+    {
+        $arguments = func_get_args();
+        
+        if (empty($arguments))
+        {
+            return app('cache');
+        }
+        
+        if (is_string($arguments[0]))
+        {
+            return app('cache')->get(...$arguments);
+        }
+        
+        if ( ! is_array($arguments[0]))
+        {
+            throw new Exception('When setting a value in the cache, you must pass an array of key / value pairs.');
+        }
+        
+        if ( ! isset($arguments[1]))
+        {
+            throw new Exception('You must specify an expiration time when setting a value in the cache.');
+        }
+        
+        return app('cache')->put(key($arguments[0]), reset($arguments[0]), $arguments[1]);
+    }
+}
+
 if ( ! function_exists('config'))
 {
     /**
