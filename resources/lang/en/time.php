7a78ee2004602a37713a3aeff4f2f1d00fc4ed20
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-09-20 15:23:15 -0500

    Added new language filec called 'time'

---
+++ resources/lang/en/time.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+     /*
+    |------------------------------------------------------------------------
+    | DateTime Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the Chronos class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+
+    'invalidMonth' => 'Months must be between 1 and 12. You have erroneously placed: {0}'
+
+];
