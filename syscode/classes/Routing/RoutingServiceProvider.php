943ca04d289963361c45c560eb4c1c9e5d2de7fb
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-10-11 23:25:39 -0500

    Changed number version and added new methods

--- syscode/classes/Routing/RoutingServiceProvider.php
+++ syscode/classes/Routing/RoutingServiceProvider.php
@@ -19,14 +19,17 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.1.0
+ * @since       0.4.0
  */
 
 namespace Syscode\Routing;
 
 use Syscode\Support\ServiceProvider;
+use Syscode\Contracts\Routing\RouteResponse as ResponseContract;
 
 /**
+ * For loading the classes from the container of services.
+ * 
  * @author Javier Alexander Campo M. <jalexcam@gmail.com>
  */
 class RoutingServiceProvider extends ServiceProvider
@@ -38,6 +41,83 @@ class RoutingServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        
+        $this->registerRouter();
+        $this->registerRouteGroup();
+        $this->registerRouteResolver();
+        $this->registerRouteResponse();
+        $this->registerUrlGenerator();
+        $this->registerRedirector();
+    }
+
+    /**
+     * Register the router instance.
+     * 
+     * @return void
+     */
+    protected function registerRouter()
+    {
+        $this->app->singleton('router', function ($app) {
+            return new Router(null, $app['routeGroup'], $app['routeResolver']);
+        });
+    }
+
+    /**
+     * Register the route group service.
+     * 
+     * @return void
+     */
+    protected function registerRouteGroup()
+    {
+        $this->app->singleton('routeGroup', function () {
+            return new RouteGroup;
+        });
+    }
+
+    /**
+     * Register the route resolver service.
+     * 
+     * @return void
+     */
+    protected function registerRouteResolver()
+    {
+        $this->app->singleton('routeResolver', function () {
+            return new RouteResolver;
+        });
+    }
+
+    /**
+     * Register the route response implementation.
+     * 
+     * @return void
+     */
+    protected function registerRouteResponse()
+    {
+        $this->app->singleton(ResponseContract::class, function($app) {
+            return new RouteResponse($app['view'], $app['redirect']);
+        });
+    }
+
+    /**
+     * Register the URL generator service.
+     * 
+     * @return void
+     */
+    protected function registerUrlGenerator()
+    {
+        $this->app->singleton('url', function ($app) {
+            return new UrlGenerator($app['request']);
+        });
+    }
+
+    /**
+     * Register the URL generator service.
+     * 
+     * @return void
+     */
+    protected function registerRedirector()
+    {
+        $this->app->singleton('redirect', function ($app) {
+            return new Redirector($app['url']);
+        });
     }
 }
