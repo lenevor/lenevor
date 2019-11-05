87c0f32eb8ecb1ae0a232694f77bb272e090b9f5
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-11-05 12:25:28 -0500

    Added new keys

--- resources/lang/es/log.php
+++ resources/lang/es/log.php
@@ -8,12 +8,14 @@ return [
     |------------------------------------------------------------------------
     |
     | Las siguientes líneas de idioma contienen los mensajes de error 
-    | predeterminados por la clase Logger. Usted es libre de cambiar 
+    | predeterminados por la clase LogManager. Usted es libre de cambiar 
     | cualquier valor que desee personalizar en estas líneas que mejor se 
     | adapte a su aplicación.
     |
     */
     
+    'driverNotSupported' => 'El controlador [{config}] no es compatible.',
     'invalidLogLevel' => '{0} es un nivel de registro no válido.',
+    'LogNotDefined' => 'El registrador [{name}] no está definido.',
 
 ];
