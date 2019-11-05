304aa5455b697132b44bdf8f057e47ebb7935c0f
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-11-05 12:25:14 -0500

    Added new keys

--- resources/lang/en/log.php
+++ resources/lang/en/log.php
@@ -8,11 +8,13 @@ return [
     |------------------------------------------------------------------------
     |
     | The following language lines contain the error messages pre-determined 
-    | by the Log class. You are free to change any value you want to customize 
+    | by the LogManager class. You are free to change any value you want to customize 
     | in these lines that best match your application.
     |
     */
     
+    'driverNotSupported' => 'Driver [{config}] is not supported.',
     'invalidLogLevel' => '{0} is an invalid log level.',
+    'LogNotDefined' => 'Log [{name}] is not defined.',
 
 ];
