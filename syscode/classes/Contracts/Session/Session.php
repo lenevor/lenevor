f047904696988f6b8744e0cc42900788f03381ce
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-11-16 01:26:24 -0500

    Added functions

--- syscode/classes/Contracts/Session/Session.php
+++ syscode/classes/Contracts/Session/Session.php
@@ -32,6 +32,123 @@ namespace Syscode\Contracts\Session;
 interface Session
 {
     /**
+     * Get the name of the session.
      * 
+     * @return string
      */
+    public function getName();
+
+    /**
+     * Start the session.
+     * 
+     * @return bool
+     */
+    public function start();
+
+    /**
+     * Get all of the session data.
+     * 
+     * @return array
+     */
+    public function all();
+
+    /**
+     * Get the current session ID.
+     * 
+     * @return string
+     */
+    public function getId();
+
+    /**
+     * Set the session ID.
+     * 
+     * @param  string  $id
+     * 
+     * @return void
+     */
+    public function setId($id);
+
+    /**
+     * Save the session data to storage.
+     * 
+     * @return void
+     */
+    public function save();
+
+    /**
+     * Checks if a key exists.
+     * 
+     * @param  string|array  $key
+     * 
+     * @return void
+     */
+    public function exists($key);
+
+    /**
+     * Checks if an a key is present and not null.
+     * 
+     * @param  string|array  $key
+     * 
+     * @return void
+     */
+    public function has($key);
+
+    /**
+     * Get an key from the session.
+     * 
+     * @param  string  $key
+     * @param  mixed   $default
+     * 
+     * @return mixed
+     */
+    public function get($key, $default = null);
+
+    /**
+     * Put a key / value pair or array of key / value pairs in the session.
+     * 
+     * @param  string|array  $key
+     * @param  mixed         $value
+     * 
+     * @return mixed
+     */
+    public function put($key, $value = null);
+
+    /**
+     * Remove an key from the session.
+     * 
+     * @param  string  $key
+     * 
+     * @return mixed
+     */
+    public function remove($key);
+
+    /**
+     * Remove all of the keys from the session.
+     * 
+     * @return void
+     */
+    public function flush();
+
+    /**
+     * Get the CSRF token value.
+     * 
+     * @return string
+     */
+    public function token();
+
+    /**
+     * Generate a new session ID for the session.
+     * 
+     * @param  bool  $destroy
+     * 
+     * @return bool
+     */
+    public function migrate($destroy = false);
+
+    /**
+     * Determine if the session has been started.
+     * 
+     * @return bool
+     */
+    public function isStarted();
 }
