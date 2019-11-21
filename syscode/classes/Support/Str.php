ed6d8a9a7d36727b33eb7d1eb9dfaad780d65448
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-11-20 23:57:28 -0500

    Added function static random

--- syscode/classes/Support/Str.php
+++ syscode/classes/Support/Str.php
@@ -183,6 +183,27 @@ class Str
     {
         return mb_strtolower($value);
     }
+    
+    /**
+     * Generate a more truly "random" alpha-numeric string.
+     * 
+     * @param  int  $length
+     * 
+     * @return string
+     */
+    public static function random($length = 16)
+    {
+        $string = '';
+        
+        while (($len = strlen($string)) < $length)
+        {
+            $size = $length - $len;
+            $bytes = random_bytes($size);
+            $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+        }
+        
+        return $string;
+    }
 
     /**
      * Generate a URL friendly "slug" from a given string.
