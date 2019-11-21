3ff2c2f227ab127ea4893e538d659a9c5d7ac10e
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2019-11-20 23:59:25 -0500

    Added new keys

--- config/session.php
+++ config/session.php
@@ -11,10 +11,35 @@ return [
     | requests. By default, we will use the lightweight native driver but
     | you may specify any of the other wonderful drivers provided here.
     |
-    | Supported: "file", "array", "null"
+    | Supported: "array", "file", "null"
     |
     */
     
     'driver' => env('SESSION_DRIVER', 'file'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Session Lifetime
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the number of minutes that you wish the session
+    | to be allowed to remain idle before it expires. If you want them to 
+    | immediately expire on the browser closing, set that option.
+    |
+    */
+
+    'lifetime' => env('SESSION_LIFETIME', 120),
+    
+    /*
+    |---------------------------------------------------------------------------
+    | Session File Location
+    |---------------------------------------------------------------------------
+    | When using the native session driver, we need a location where session 
+    | files may be stored. A default has been set for you but a different 
+    | location may be specified. This is only needed for file sessions.
+    |
+    */
+    
+    'files' => storagePath('sessions'),
 
 ];
