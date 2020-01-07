5a4ecadac477d3b344b8f3edcade598d7627be9b
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2020-01-07 03:11:28 -0500

    Added new tag html

--- syscode/classes/Debug/Exceptions/Resources/views/frame_list.php
+++ syscode/classes/Debug/Exceptions/Resources/views/frame_list.php
@@ -1,20 +1,24 @@
+<div class="frame-container-scroll scrollbar">
 <?php foreach ($frames as $index => $frame) : ?>
-<div class="frame frame-application <?= ($index == 0) ? 'active' : '' ?>" data-index="<?= $index ?>">
-	
-	<div class="frame-index"><?= (count($frames) - $index - 1) + 1 ?></div>
+	<div class="frame frame-application <?= ($index == 0) ? 'active' : '' ?>" data-index="<?= $index ?>">
 		
-	<div class="frame-method-info">
-		<span class="frame-class"><?= e($frame->getClass()) ?: '' ?></span>
-		<span class="frame-function"><?= e($frame->getFunction()) ?: '' ?></span>
-	</div>
-	<div class="frame-file">
+		<div class="frame-main-index">
+			<div class="frame-index"><?= (count($frames) - $index - 1) + 1 ?></div>
+		</div>
+			
+		<div class="frame-method-info">
+			<span class="frame-class"><?= e($frame->getClass()) ?: '' ?></span>
+			<span class="frame-function"><?= e($frame->getFunction()) ?: '' ?></span>
+		</div>
+		<div class="frame-file">
 
-		<div class="delimiter">
-			<?= $frame->getFile() ? $template->cleanPath($frame->getFile()) : '<#unknown>'?>
-		</div>		
-		<span class="frame-line">Line&nbsp;<?= (int) $frame->getLine() ?></span>
+			<div class="delimiter">
+				<?= $frame->getFile() ? $template->cleanPath($frame->getFile()) : '<#unknown>'?>
+			</div>		
+			<span class="frame-line">Line&nbsp;<?= (int) $frame->getLine() ?></span>
 
-	</div>
-	
-</div>		
+		</div>
+		
+	</div>		
 <?php endforeach; ?>	
+</div>
