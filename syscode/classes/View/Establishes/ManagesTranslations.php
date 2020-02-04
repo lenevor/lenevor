d819c1f5b69386dece7dcf37df2dae2df40ddfd1
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2020-02-04 00:36:03 -0500

    Added new trait

---
+++ syscode/classes/View/Establishes/ManagesTranslations.php
@@ -0,0 +1,66 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @author      Javier Alexander Campo M. <jalexcam@gmail.com>
+ * @link        https://lenevor.com 
+ * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
+ * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
+ * @since       0.6.0
+ */
+
+namespace Syscode\View\Establishes;
+
+/**
+ * Trait ManagesTranslations.
+ * 
+ * @author Javier Alexander Campo M. <jalexcam@gmail.com>
+ */
+trait ManagesTranslations
+{
+    /**
+     * Gets the translation replacements.
+     * 
+     * @var array $replacements
+     */
+    protected $replacements = [];
+
+    /**
+     * Begin a translation block.
+     * 
+     * @param  array  $replacements
+     * 
+     * @return void
+     */
+    public function beginTranslation($replacements = [])
+    {
+        ob_start();
+
+        $this->replacements = $replacements;
+    }
+
+    /**
+     * Render the translation.
+     * 
+     * @return string
+     */
+    public function renderTranslation()
+    {
+        return $this->container->make('translator')->getLine(
+            trim(ob_get_clean()), $this->replacements
+        );
+    }
+}
