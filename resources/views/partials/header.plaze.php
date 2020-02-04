f47a4b9c2397de247a9a7569a95fb10f787076cc
Javier Alexander Campo Meneses <kovenenphp@gmail.com>
2020-02-04 00:34:44 -0500

    Changed tags of plaze engine

--- resources/views/partials/header.plaze.php
+++ resources/views/partials/header.plaze.php
@@ -5,9 +5,9 @@
 				<nav>
 				
 					<ul class="menu">
-						<li><a href="#"><span class="icon-file-text2"></span><?= e(__('view.documentation')) ?></a></li>
-						<li><a href="#"><span class="icon-display"></span><?= e(__('lyzercasts')) ?></a></li>
-						<li><a href="#"><span class="icon-books"></span><?= e(__('blog')) ?></a></li>
+						<li><a href="#"><span class="icon-file-text2"></span><@lang('view.documentation')</a></li>
+						<li><a href="#"><span class="icon-display"></span>{{ __('lyzercasts') }}</a></li>
+						<li><a href="#"><span class="icon-books"></span>{{ __('blog') }}</a></li>
 						<li><a href="https://github.com/lenevor"><span class="icon-github"></span>Github</a></li>
 					</ul>
 					
