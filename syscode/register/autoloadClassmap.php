946f9275ba6ed8468f544e0d7ce053cad7ea421d
Alexander Campo <kovenenphp@gmail.com>
2020-02-10 08:47:03 -0500

    Removed namespace

--- syscode/register/autoloadClassmap.php
+++ syscode/register/autoloadClassmap.php
@@ -228,7 +228,6 @@ return [
     'Syscode\\View\\Engines\\FileEngine' => SYS_PATH.'classes/View/Engines/FileEngine.php',
     'Syscode\\View\\Engines\\PhpEngine' => SYS_PATH.'classes/View/Engines/PhpEngine.php',
     'Syscode\\View\\Establishes\\ManagesComponents' => SYS_PATH.'classes/View/Establishes/ManagesComponents.php',
-    'Syscode\\View\\Establishes\\ManagesIncludes' => SYS_PATH.'classes/View/Establishes/ManagesIncludes.php',
     'Syscode\\View\\Establishes\\ManagesLayouts' => SYS_PATH.'classes/View/Establishes/ManagesLayouts.php',
     'Syscode\\View\\Establishes\\ManagesTranslations' => SYS_PATH.'classes/View/Establishes/ManagesTranslations.php',
     'Syscode\\View\\Exceptions\\ViewException' => SYS_PATH.'classes/View/Exceptions/ViewException.php',
