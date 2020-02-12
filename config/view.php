3e55e7e0520bbbe0b10319426a91235162409db6
Alexander Campo <kovenenphp@gmail.com>
2020-02-12 13:52:26 -0500

    Changed name of key

--- config/view.php
+++ config/view.php
@@ -4,16 +4,16 @@ return [
 
     /*
     |------------------------------------------------------------------------
-    | Compiled View Paths
+    | Transpiled View Paths
     |------------------------------------------------------------------------
     |
-    | This option determines where the compiled Plaze templates will be. 
+    | This option determines where the transpiled Plaze templates will be. 
     | However, the directory may vary if you wish.  
     |
     */
 
-    'compiled' => env(
-        'COMPILED_VIEW_PATH', 
+    'transpiled' => env(
+        'TRANSPILED_VIEW_PATH', 
         realpath(storagePath('views'))
     ),
 
