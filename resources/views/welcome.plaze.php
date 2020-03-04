50ddbf498ed2df2993400209cc415eec7feca802
Alexander Campo <kovenenphp@gmail.com>
2020-03-04 02:25:14 -0500

    Added new parameters

--- resources/views/welcome.plaze.php
+++ resources/views/welcome.plaze.php
@@ -4,27 +4,47 @@
 
 <@section('content')
 
-		<div class="content">
-
-			<div id="js-menu">
-				<div class="menu"></div>
-			</div> 
+		<div class="container">
 			
-			<div class="container">
+			<div class="content">
 
 				<div class="info">
-					<h1>Welcome to {{ config('app.name') }} </h1>
-					<img src="{!! asset('svg/logo.svg') !!}">
+					<img src="{!! asset('svg/logo.svg') !!}" />
+					<h1>Welcome to <b>{{ config('app.name') }}</b> </h1>
+					<div class="location">
+						<span><i class="icon-checkmark"></i>{{ basePath() }}</span>
+					</div>
 					<p class="message">
-						You have successfully installed your Lenevor application.
-					</p>
-					<p class="message">
-						If you want to know more about Lenevor and exploring many of its features, I recommend you start by reading the <a href="#">User Guide</a>.
-					</p>
-					
-					<@include('partials::footer')
-					
+						Your have successfully installed this application, you can use it now.
+					</p>					
+					<div class="box">
+						<div class="documents">
+							<span><i class="icon-books"></i></span>
+							<h2>Documents</h2>
+							<span>
+								<a href="#">Guides</a>
+								<a href="#">api</a>
+								<a href="#">references</a>
+							</span>
+						</div>
+						<div class="tutorials">
+							<span><i class="icon-embed"></i></span>
+							<h2>Tutorials</h2>
+							<span>
+								<a href="#">Create your first page</a>
+							</span>
+						</div>
+						<div class="community">
+							<span><i class="icon-users"></i></span>
+							<h2>Community</h2>
+							<span>
+								<a href="#">Blog</a>
+								<a href="#">github</a>
+							</span>
+						</div>
+					</div>
 				</div>
+
 			</div>
 		  	
 		</div>	
