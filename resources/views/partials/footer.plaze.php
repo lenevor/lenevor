de9212fc2a8d8aef1abea4efda7585664af8cd9a
Alexander Campo <kovenenphp@gmail.com>
2020-03-04 02:24:38 -0500

    Delete file

--- resources/views/partials/footer.plaze.php
+++
@@ -1,6 +0,0 @@
-					<footer>
-						<p>Version: <b>{{ \Syscode\Version::RELEASE }}&#45;{{ \Syscode\Version::STATUS }}</b></p>
-						<p>Rendered: <b>{elapsed_time}</b></p>
-						<p>Environment: <b>{{ ENVIRONMENT }}</b></p>
-					</footer>
-					
