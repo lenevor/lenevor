5e731731bc0385512f8ccd8edec8787b822bdb19
Alexander Campo <kovenenphp@gmail.com>
2020-03-17 23:36:14 -0500

    Changed version

--- syscode/classes/Database/Connection.php
+++ syscode/classes/Database/Connection.php
@@ -19,13 +19,12 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor PHP Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.1.0
+ * @since       0.7.0
  */
  
 namespace Syscode\Database;
 
 use PDO;
-
 
 /**
  * Creates a database connection using PDO.
