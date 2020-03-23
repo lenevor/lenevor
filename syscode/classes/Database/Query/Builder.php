27a7e8e3998127c295f8a4771afa8c2076c4866f
Alexander Campo <kovenenphp@gmail.com>
2020-03-23 00:48:34 -0500

    Added new class

---
+++ syscode/classes/Database/Query/Builder.php
@@ -0,0 +1,37 @@
+<?php 
+
+/**
+ * Lenevor PHP Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @author      Javier Alexander Campo M. <jalexcam@gmail.com>
+ * @link        https://lenevor.com 
+ * @copyright   Copyright (c) 2019-2020 Lenevor PHP Framework 
+ * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
+ * @since       0.7.0
+ */
+ 
+namespace Syscode\Database\Query;
+
+/**
+ * Lenevor database query builder provides a convenient, fluent interface 
+ * to creating and running database queries. and works on all supported 
+ * database systems.
+ * 
+ * @author Javier Alexander Campo M. <jalexcam@gmail.com>
+ */
+class Builder
+{
+    
+}
