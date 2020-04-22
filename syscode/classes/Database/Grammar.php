8c73cc62e4e76ccac8eb9628b46fcf543c53a18b
Alexander Campo <kovenenphp@gmail.com>
2020-04-22 01:05:56 -0500

    Added new methods for calls tables and columns

--- syscode/classes/Database/Grammar.php
+++ syscode/classes/Database/Grammar.php
@@ -41,6 +41,140 @@ abstract class Grammar
     protected $tablePrefix = '';
 
     /**
+     * Wrap a table in keyword identifiers.
+     * 
+     * @param  string  $table
+     * 
+     * @return string
+     */
+    public function wrapTable($table)
+    {
+        if ( ! $this->isExpression($table))
+        {
+            return $this->wrap($this->tablePrefix.$table, true);
+        }
+
+        return $this->getValue($table);
+    }
+
+    /**
+     * Wrap a value in keyword identifiers.
+     * 
+     * @param  \Syscode\Database\Query\Expression|string  $value
+     * @param  bool  $prefix
+     * 
+     * @return string
+     */
+    public function wrap($value, $prefix = false)
+    {
+        if ($this->isExpression($value))
+        {
+            return $this->getValue($value);
+        }
+
+        if (strpos($value, ' as ') !== false)
+        {
+            return $this->wrapAliasedValues($value, $prefix);
+        }
+
+        return $this->wrapSegments(explode('.', $value));
+    }
+
+    /**
+     * Wrap a value that has an alias.
+     * 
+     * @param  string  $value
+     * @param  bool  $prefix
+     * 
+     * @return string
+     */
+    protected function wrapAliasedValues($value, $prefix = false)
+    {
+        $segments = explode(' ', $value);
+
+        if ($prefix)
+        {
+            $segments[2] = $this->tablePrefix.$segments[2];
+        }
+
+        return $this->wrap($segments[0].' AS '.$this->wrapValue($segments[2]));
+    }
+
+    /**
+     * Wrap the given value segments.
+     * 
+     * @param  array  $segments
+     * 
+     * @return string
+     */
+    protected function wrapSegments($segments)
+    {
+        $wrapped = [];
+
+        foreach ($segments as $key => $segment)
+        {
+            $wrapped[] = ($key == 0 && count($segment) > 1)
+                        ? $this->wrapTable($segment)
+                        : $this->wrapValue($segment);
+        }
+
+        return implode('.', $wrapped);
+    }
+
+    /**
+     * Wrap a single string in keyword identifiers.
+     * 
+     * @param  string  $value
+     * 
+     * @return string
+     */
+    protected function wrapValue($value)
+    {
+        if ($value !== '*')
+        {
+            return '"'.str_replace('"', '""', $value).'"';
+        }
+
+        return $value;
+    }
+
+    /**
+     * An array of column names.
+     * 
+     * @param  array  $columns
+     * 
+     * @return string
+     */
+    public function columnize(array $columns)
+    {
+        return implode(', ', array_map([$this, 'wrap'], $columns));
+    }
+
+    /**
+     * Create query parameter place-holders for an array.
+     * 
+     * @param  mixed  $values
+     * 
+     * @return string
+     */
+    public function parameterize(array $values)
+    {
+        return implode(', ', array_map([$this, 'parameter'], $values));
+    }
+
+    /**
+     * Get the appropriate query parameter place-holder for a value.
+     * 
+     * @param  mixed  $value
+     * 
+     * @return string
+     */
+    public function parameter($value)
+    {
+        return $this->isExpression($value) ? $this->getValue($value) : '?';
+    }
+
+    /**
      * Get the value of a raw expression.
      * 
      * @param  \Syscode\Database\Query\Expression  $expression
