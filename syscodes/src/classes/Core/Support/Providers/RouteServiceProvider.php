1252aa044676e80f246af09fd4fccf2f1e81d220
Alexander Campo <jalexcam@gmail.com>
2020-06-17 01:02:04 -0500

    Added new a register 'booted' listener

--- syscodes/src/classes/Core/Support/Providers/RouteServiceProvider.php
+++ syscodes/src/classes/Core/Support/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.1.0
+ * @since       0.7.0
  */
 
 namespace Syscodes\Core\Support\Providers;
@@ -42,6 +42,12 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutes();
+
+        $this->app->booted(function ()
+        {
+            $this->app['router']->getRoutes()->refreshNameLookups();
+            $this->app['router']->getRoutes()->refreshActionLookups();
+        });
     }
 
     /**
