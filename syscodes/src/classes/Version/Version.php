7ee5a7146c05abfa6f08e14cf255b1065c78e117
Alexander Campo <jalexcam@gmail.com>
2020-07-02 02:15:45 -0500

    Changed release

--- syscodes/src/classes/Version/Version.php
+++ syscodes/src/classes/Version/Version.php
@@ -19,7 +19,7 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.1.0
+ * @since       0.1.1
  */
 
 namespace Syscodes;
@@ -39,7 +39,7 @@ final class Version
     /** 
      * Version Lenevor.
      */
-    const RELEASE = '0.7.0';
+    const RELEASE = '0.7.1';
 
     /**
      * Release status.
@@ -64,7 +64,7 @@ final class Version
     /**
      * Product copyrighting.
      */
-    const COPY = '&copy';
+    const COPY = '©';
 
     /**
      * Year actual.
