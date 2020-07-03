159c670c081a8c6c9d4766529fcefbb7def65dc8
Alexander Campo <jalexcam@gmail.com>
2020-07-03 03:04:46 -0500

    Added trait in a folder specific

---
+++ syscodes/src/classes/Routing/Traits/RouteResolverTrait.php
@@ -0,0 +1,200 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @author      Javier Alexander Campo M. <jalexcam@gmail.com>
+ * @link        https://lenevor.com 
+ * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
+ * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
+ * @since       0.7.1
+ */
+
+namespace Syscodes\Routing\Traits;
+
+use Closure;
+use JsonSerializable;
+use Syscodes\Http\Response;
+use Syscodes\Http\JsonResponse;
+use Syscodes\Routing\RouteCollection;
+use Syscodes\Contracts\Routing\Routable;
+use Syscodes\Contracts\Support\Renderable;
+use Syscodes\Routing\Exceptions\RouteNotFoundException;
+
+/**
+ * This trait resolve the given route and called the method that belongs to the route.
+ * 
+ * @author Javier Alexander Campo M. <jalexcam@gmail.com>
+ */
+trait RouteResolverTrait
+{
+	/**
+	 * The currently dispatched route instance.
+	 * 
+	 * @var \Syscodes\Routing\Route|null
+	 */
+	protected $current;
+
+	/**
+	 * Resolve the given route and call the method that belongs to the route.
+	 *
+	 * @param  \Syscodes\Http\Request  $request
+	 * @param  \Syscodes\Routing\RouteCollection  $route 
+	 *
+	 * @return \Syscodes\Http\Response
+	 */
+	public function resolve($request, Routecollection $route)
+	{
+		return $this->dispatchToRoute($request, $route);
+	}
+
+	/**
+	 * Dispatch the request to a route and return the response.
+	 * 
+	 * @param  \Syscodes\Http\Request  $request
+	 * @param  \Syscodes\Routing\RouteCollection  $route 
+	 *
+	 * @return \Syscodes\Http\Response
+	 */
+	protected function dispatchToRoute($request, $route)
+	{
+		return $this->runRoute($request, 
+			$this->findRoute($request, $route)
+		);
+	}
+
+	/**
+	 * Return the response for the given route.
+	 * 
+	 * @param  \Syscodes\Http\Request  $request
+	 * @param  \Syscodes\Routing\Route  $route
+	 * 
+	 * @return \Syscodes\Http\Response 
+	 */
+	protected function runRoute($request, $route)
+	{		
+		return $this->callResponse($request, $route->runResolver());
+	}
+
+	/**
+	 * Create a response instance from the given value.
+	 * 
+	 * @param  \Syscodes\Http\Request  $request
+	 * @param  mixed  $response
+	 * 
+	 * @return \Syscodes\Http\Response
+	 */
+	protected function callResponse($request, $response)
+	{
+		if ( ! $response instanceof Response && 
+		      ($response instanceof Jsonserializable || 
+			   is_array($response)))
+		{
+			$response = new JsonResponse($response);
+		}
+		elseif ( ! $response instanceof Response)
+		{
+			if ($response instanceof Renderable)
+			{
+				$response = $response->render();
+			}
+
+			$response = new Response($response, 200, ['Content-Type' => 'text/html']);
+		}
+
+		return $response->prepare($request);
+	}
+
+	/**
+	 * Find the route matching a given request.
+	 * 
+	 * @param  \Syscodes\Http\Request  $request
+	 * @param  \Syscodes\Routing\RouteCollection  $route
+	 * 
+	 * @return \Syscodes\Routing\Route 
+	 * 
+	 * @throws \Syscodes\Routing\Exceptions\RouteNotFoundException
+	 */
+	protected function findRoute($request, $route)
+	{
+		// Get all register routes with the same request method
+		$routes = $route->match($request);
+		
+		$this->container->instance(Route::class, $routes);
+
+		// Remove trailing and leading slash
+		$requestedUri = $request->url();
+
+		// Loop trough the posible routes
+		foreach ($routes as $route) 
+		{
+			if (isset($requestedUri))
+			{
+				$host = $route->getHost();
+				
+				if ($host !== null && $host != $request->getHost())
+				{
+					continue;
+				}
+				
+				$scheme = $route->getScheme();
+				
+				if ($scheme !== null && $scheme !== $request->getScheme())
+				{
+					continue;
+				}
+				
+				$port = $route->getPort();
+				
+				if ($port !== null && $port !== $request->getPort())
+				{
+					continue;
+				}
+			}
+			
+			// If the requested route one of the defined routes
+			if ($route->getRoute() === $requestedUri || preg_match('~^'.$route->getRoute().'$~', $requestedUri)) 
+			{	
+				$this->current = $route;
+
+				return $this->current()->bind($request);
+			}
+		}
+
+		throw new RouteNotFoundException;
+	}
+
+	/**
+	 * Get the currently dispatched route instance.
+	 * 
+	 * @return \Syscodes\Routing\Route|null
+	 */
+	public function current()
+	{
+		return $this->current;
+	}
+
+	/**
+	 * Determine if the current route matches a pattern.
+	 * 
+	 * @param  mixed  ...$patterns
+	 * 
+	 * @return bool
+	 */
+	public function currentRouteNamed(...$patterns)
+	{
+		return $this->current() && $this->current()->named(...$patterns);
+	}
+}
