6fb854e43ca8e33350a02d5f6471320344c95618
Alexander Campo <jalexcam@gmail.com>
2020-08-26 00:43:29 -0500

    Changed method name

--- syscodes/src/classes/Routing/RouteParamBinding.php
+++ syscodes/src/classes/Routing/RouteParamBinding.php
@@ -19,7 +19,7 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.7.1
+ * @since       0.7.2
  */
 
 namespace Syscodes\Routing;
@@ -75,7 +75,7 @@ class RouteParamBinding
      */
     protected function bindParameters($request)
     {
-        $path = '/'.ltrim($request->getUri(), '/');
+        $path = '/'.ltrim($request->get(), '/');
 
         preg_match('#[^/]+$#sDu', $path, $matches);
         
