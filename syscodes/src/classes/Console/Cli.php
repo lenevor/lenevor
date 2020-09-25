69281b006d3a2ed77e00b0ff80dd0d27d0276e0d
Alexander Campo <jalexcam@gmail.com>
2020-09-25 00:48:01 -0500

    Changed namespace and release to 0.7.2

--- syscodes/src/classes/Console/Cli.php
+++ syscodes/src/classes/Console/Cli.php
@@ -19,13 +19,13 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.1.1
+ * @since       0.7.2
  */
 
 namespace Syscodes\Console;
 
 use Exception;
-use Syscodes\Support\Arr;
+use Syscodes\Collections\Arr;
 use Syscodes\Contracts\Core\Lenevor;
 use Syscodes\Core\Http\Exceptions\LenevorException;
 
