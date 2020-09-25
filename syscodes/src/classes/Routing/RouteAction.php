91a9a3f0669121dc4e8fbcc40964be7b19069130
Alexander Campo <jalexcam@gmail.com>
2020-09-25 00:52:19 -0500

    Changed namespace and release to 0.7.2

--- syscodes/src/classes/Routing/RouteAction.php
+++ syscodes/src/classes/Routing/RouteAction.php
@@ -19,15 +19,15 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.5.1
+ * @since       0.7.2
  */
 
 namespace Syscodes\Routing;
 
 use LogicException;
-use Syscodes\Support\Arr;
 use Syscodes\Support\Str;
 use UnexpectedValueException;
+use Syscodes\Collections\Arr;
 
 /**
  * Solve the actions obtained from a route.
