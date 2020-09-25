1210db0823ac00ab473a70129130a1fa692adfd1
Alexander Campo <jalexcam@gmail.com>
2020-09-25 00:57:06 -0500

    Changed namespace

--- syscodes/src/classes/View/Transpilers/Establishes/TranspilesLayouts.php
+++ syscodes/src/classes/View/Transpilers/Establishes/TranspilesLayouts.php
@@ -44,7 +44,7 @@ trait TranspilesLayouts
     {
         $expression = $this->stripParentheses($expression);
 
-        $data = "<?php echo \$__env->make({$expression}, \Syscodes\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
+        $data = "<?php echo \$__env->make({$expression}, \Syscodes\Collections\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
 
         $this->footer[] = $data;
 
