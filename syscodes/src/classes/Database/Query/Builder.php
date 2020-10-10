92d3005b63bf6506dcb2097277c73ba9c57c26c5
Alexander Campo <jalexcam@gmail.com>
2020-10-09 22:56:01 -0500

    Changed release to 0.7.2

--- syscodes/src/classes/Database/Query/Builder.php
+++ syscodes/src/classes/Database/Query/Builder.php
@@ -19,7 +19,7 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor PHP Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.7.0
+ * @since       0.7.2
  */
  
 namespace Syscodes\Database\Query;
