13644b552c18d28f73f20d24d95fbee2bcecfa14
Alexander Campo <jalexcam@gmail.com>
2020-10-31 23:14:51 -0500

    Added new Pipeline class for handle the given exception

---
+++ syscodes/src/classes/Routing/Pipeline.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @author      Javier Alexander Campo M. <jalexcam@gmail.com>
+ * @link        https://lenevor.com 
+ * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
+ * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
+ * @since       0.7.3
+ */
+
+namespace Syscodes\Routing;
+
+use Throwable;
+use Syscodes\Http\Request;
+use Syscodes\Contracts\Debug\ExceptionHandler;
+use Syscodes\Pipeline\Pipeline as BasePipeline;
+
+/**
+ * This extended pipeline catches any exceptions.
+ * 
+ * @author Javier Alexander Campo M. <jalexcam@gmail.com>
+ */
+class Pipeline extends BasePipeline
+{
+    /**
+     * Handle the given exception.
+     * 
+     * @param  mixed  $passable
+     * @param  \Throwable  $e
+     * 
+     * @return mixed
+     * 
+     * @throws \Throwable
+     */
+    protected function handleException($passable, Throwable $e)
+    {
+        if ( ! $passable instanceof Request)
+        {
+            throw $e;
+        }
+        
+        $handler = $this->container->make(ExceptionHandler::class);
+
+        $handler->report($e);
+
+        $response = $handler->render($passable, $e);
+
+        if (is_object($response) && method_exists($response, 'withException'))
+        {
+            $response->withException($e);
+        }
+
+        return $response;
+    }
+}
