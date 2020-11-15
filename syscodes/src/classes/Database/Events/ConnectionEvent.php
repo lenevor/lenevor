0c9f7840c8b19224a0301c42c67fe092ae107a65
Alexander Campo <jalexcam@gmail.com>
2020-11-15 01:32:05 -0500

    Added new database connection event

---
+++ syscodes/src/classes/Database/Events/ConnectionEvent.php
@@ -0,0 +1,60 @@
+<?php 
+
+/**
+ * Lenevor PHP Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @author      Javier Alexander Campo M. <jalexcam@gmail.com>
+ * @link        https://lenevor.com 
+ * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
+ * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
+ * @since       0.7.3
+ */
+ 
+namespace Syscodes\Database\Events;
+
+/**
+ * Get the database connection event.
+ * 
+ * @author Javier Alexander Campo M. <jalexcam@gmail.com>
+ */
+abstract class ConnectionEvent
+{
+    /**
+     * The database connection instance.
+     * 
+     * @var \Syscodes\Database\Connection $connection
+     */
+    public $connection;
+
+    /**
+     * The name of the connection.
+     * 
+     * @var string $ConnectionName
+     */
+    public $connectionName;
+
+    /**
+     * Constructor. Create a new event instance.
+     * 
+     * @param  \Syscodes\Database\Connection  $connection
+     * 
+     * @return void
+     */
+    public function __construct($connection)
+    {
+        $this->connection     = $connection;
+        $this->connectionName = $connection->getName();
+    }
+}
