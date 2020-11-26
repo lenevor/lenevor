9aab09c4c7b7ff2d65e62308293dce602e68d6f5
Alexander Campo <jalexcam@gmail.com>
2020-11-26 01:41:35 -0500

    Updated class

--- syscodes/src/classes/Database/ConnectionFactory.php
+++ syscodes/src/classes/Database/ConnectionFactory.php
@@ -19,15 +19,48 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.7.0
+ * @since       0.7.3
  */
 
 namespace Syscodes\Database;
 
+use PDOException;
+use InvalidArgumentException;
+use Syscodes\Collections\Arr;
+use Syscodes\Contracts\Container\Container;
+use Syscodes\Database\Connectors\MysqlConnectors;
+use Syscodes\Database\Connectors\SQLiteConnectors;
+use Syscodes\Database\Connectors\PostgresConnectors;
+use Syscodes\Database\Connectors\SqlServerConnectors;
+
+use Syscodes\Database\Connections\MysqlConnection;
+use Syscodes\Database\Connections\SQLiteConnection;
+use Syscodes\Database\Connections\PostgresConnection;
+use Syscodes\Database\Connections\SqlServerConnection;
+
 /**
+ * Create a new instance based on the configuration of the database.
+ * 
  * @author Javier Alexander Campo M. <jalexcam@gmail.com>
  */
 class ConnectionFactory
 {
-    
+    /**
+     * The IoC container instance.
+     * 
+     * @var \Syscodes\Contracts\Container\Container $container
+     */
+    protected $container;
+
+    /**
+     * Constructor. Create a new ConnectionFactory class instance.
+     * 
+     * @param  \Syscodes\Contracts\Container\Container  $container
+     * 
+     * @return void
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
 }
