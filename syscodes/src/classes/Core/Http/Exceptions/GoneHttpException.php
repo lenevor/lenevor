9bfd9edf5032fab1375fce3ec7710762b5ea0231
Alexander Campo <jalexcam@gmail.com>
2020-12-02 23:55:42 -0500

    Changed release to 0.7.3 and added variables according to HTTP status code

--- syscodes/src/classes/Core/Http/Exceptions/GoneHttpException.php
+++ syscodes/src/classes/Core/Http/Exceptions/GoneHttpException.php
@@ -19,12 +19,10 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.1.0
+ * @since       0.7.3
  */
 
 namespace Syscodes\Core\Http\Exceptions;
-
-use Throwable;
 
 /**
  * It is activated when the requested content has been deleted from the server.
@@ -34,17 +32,23 @@ use Throwable;
 class GoneHttpException extends HttpException
 {
 	/**
-	 * Initialize constructor.
+	 * Get the HTTP status code.
 	 * 
-	 * @param  string  $message  
-	 * @param  \Throwable  $previous
-	 * @param  int  $code 
-	 * @param  array  $headers
-	 * 
-	 * @return void
+	 * @var int $code
 	 */
-	public function __construct(string $message = null, Throwable $previous = null, int $code = 0, array $headers = [])
-	{		
-		parent::__construct(410, $message, $previous, $headers, $code);
-	}
+	protected $code = 410;
+
+	/**
+	 * Get the HTTP message.
+	 * 
+	 * @var string $message
+	 */
+	protected $message = 'Gone';
+
+	/**
+	 * Get the title page exception.
+	 * 
+	 * @var string $title
+	 */
+	protected $title = 'Gone';
 }
