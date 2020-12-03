2ea43cbbbf3bdf96ab3e8c408eee67618f30b6c7
Alexander Campo <jalexcam@gmail.com>
2020-12-02 23:42:44 -0500

    Added the called methods to from HttpException

--- syscodes/src/classes/Core/Exceptions/views/500.plaze.php
+++ syscodes/src/classes/Core/Exceptions/views/500.plaze.php
@@ -1,5 +1,5 @@
 <@extends('errors::layout')
 
-<@section('title', 'Internal Server Error')
-<@section('code', 500)
-<@section('message', 'Internal Server Error')
+<@section('title', $exception->getTitle())  {{-- Title: Internal Server Error --}}
+<@section('code', $exception->getStatusCode())  {{-- Code: 500 --}} 
+<@section('message', $exception->getMessage())
