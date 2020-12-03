0d080de546654e2230c3850d285f1f6b19eb06f4
Alexander Campo <jalexcam@gmail.com>
2020-12-02 23:40:57 -0500

    Added the called methods to from HttpException

--- syscodes/src/classes/Core/Exceptions/views/409.plaze.php
+++ syscodes/src/classes/Core/Exceptions/views/409.plaze.php
@@ -1,5 +1,5 @@
 <@extends('errors::layout')
 
-<@section('title', 'Conflict')
-<@section('code', 409)
-<@section('message', 'Conflict')
+<@section('title', $exception->getTitle())  {{-- Title: Conflict --}} 
+<@section('code', $exception->getStatusCode())  {{-- Code: 409 --}} 
+<@section('message', $exception->getMessage())
