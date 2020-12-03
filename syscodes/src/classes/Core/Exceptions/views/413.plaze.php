1327902adad5db2880f8626a7ee35aac59b508a1
Alexander Campo <jalexcam@gmail.com>
2020-12-02 23:42:04 -0500

    Added the called methods to from HttpException

--- syscodes/src/classes/Core/Exceptions/views/413.plaze.php
+++ syscodes/src/classes/Core/Exceptions/views/413.plaze.php
@@ -1,5 +1,5 @@
 <@extends('errors::layout')
 
-<@section('title', 'Request Entity Too Large')
-<@section('code', 413)
-<@section('message', 'Request Entity Too Large')
+<@section('title', $exception->getTitle())  {{-- Title: Request Entity Too Large --}}
+<@section('code', $exception->getStatusCode())  {{-- Code: 413 --}} 
+<@section('message', $exception->getMessage())
