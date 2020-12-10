567b08f1b0acc8c52103b0832a3f585466a7b75d
Alexander Campo <jalexcam@gmail.com>
2020-12-09 23:36:23 -0500

    Added methods for allow its database connection instance

--- syscodes/src/components/Database/ConnectionFactory.php
+++ syscodes/src/components/Database/ConnectionFactory.php
@@ -64,10 +64,134 @@ class ConnectionFactory
     {
         $this->container = $container;
     }
+    
+    /**
+     * Establish a PDO connection based on the configuration.
+     * 
+     * @param  array   $config
+     * @param  string  $name
+     * 
+     * @return \Syscodes\Database\Connection
+     */
+    public function make(array $config, $name = null)
+    {
+        $config = $this->parseConfig($config, $name);
+        
+        return isset($config['read'])
+                    ? $this->createReadWriteConnection($config)
+                    : $this->createSingleConnection($config);
+    }
+    
+    /**
+     * Create a read / write database connection instance.
+     * 
+     * @param  array  $config
+     * 
+     * @return \Syscodes\Database\Connection
+     */
+    protected function createReadWriteConnection(array $config)
+    {
+        $connection = $this->createSingleConnection($this->getWriteConfig($config));
+        
+        return $connection->setReadPdo($this->createReadPdo($config));
+    }
+    
+    /**
+     * Create a single database connection instance.
+     * 
+     * @param  array  $config
+     * 
+     * @return \Syscodes\Database\Connection
+     */
+    protected function createSingleConnection(array $config)
+    {
+        $pdo = $this->createConnector($config)->connect($config);
+        
+        return $this->createConnection($config['driver'], $pdo, $config['database'], $config['prefix'], $config);
+    }
 
     /**
+     * Create a new PDO instance for reading.
      * 
+     * @param  array  $config
+     * 
+     * @return \PDO
      */
+    protected function createReadPdo(array $config)
+    {
+        $readConfig = $this->getReadConfig($config);
+        
+        return $this->createConnector($readConfig)->connect($readConfig);
+    }
+    
+    /**
+     * Get the read configuration for a read / write connection.
+     * 
+     * @param  array  $config
+     * 
+     * @return array
+     */
+    protected function getReadConfig(array $config)
+    {
+        $readConfig = $this->getReadWriteConfig($config, 'read');
+        
+        return $this->mergeReadWriteConfig($config, $readConfig);
+    }
+
+    /**
+     * Get the read configuration for a read / write connection.
+     * 
+     * @param  array  $config
+     * 
+     * @return array
+     */
+    protected function getWriteConfig(array $config)
+    {
+        $writeConfig = $this->getReadWriteConfig($config, 'write');
+        
+        return $this->mergeReadWriteConfig($config, $writeConfig);
+    }
+
+    /**
+     * Get a read / write level configuration.
+     * 
+     * @param  array  $config
+     * @param  string  $type
+     * 
+     * @return array
+     */
+    protected function getReadWriteConfig(array $config, $type)
+    {
+        return isset($config[$type][0])
+                    ? $config[$type][array_rand($config[$type])]
+                    : $config[$type];
+    }
+
+    /**
+     * Merge a configuration for a read / write connection.
+     * 
+     * @param  array  $config
+     * @param  array  $merge
+     * 
+     * @return array
+     */
+    protected function mergeReadWriteConfig(array $config, array $merge)
+    {
+        return Arr::except(array_merge($config, $merge), ['read', 'write']);
+    }
+
+    /**
+     * Parse and prepare the database configuration.
+     * 
+     * @param  array  $config
+     * @param  string  $name
+     * 
+     * @return array
+     */
+    protected function parseConfig(array $config, $name)
+    {
+        return Arr::add(Arr::add($config, 'prefix', ''), 'name', $name);
+    }
 
     /**
      * Create a connector instance based on the configuration.
