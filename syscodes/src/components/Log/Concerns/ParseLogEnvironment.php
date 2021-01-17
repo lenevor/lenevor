60c56213ce1413341ef9c5ffc42e3241df383c22
Alexander Campo <jalexcam@gmail.com>
2021-01-16 19:59:50 -0500

    Creates trait to get a log of application environment

---
+++ syscodes/src/components/Log/Concerns/ParseLogEnvironment.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @author      Javier Alexander Campo M. <jalexcam@gmail.com>
+ * @link        https://lenevor.com 
+ * @copyright   Copyright (c) 2019-2021 Lenevor Framework 
+ * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
+ * @since       0.7.4
+ */
+
+namespace Syscodes\Log\Concerns;
+
+/**
+ * Gets log the current application environment.
+ * 
+ * @author Javier Alexander Campo M. <jalexcam@gmail.com>
+ */
+trait ParseLogEnvironment
+{
+    /**
+     * Get log environment name.
+     * 
+     * @return string
+     */
+    protected function getLogEnvironment()
+    {
+        return $this->app->environment() ?? 'production';
+    }
+}
