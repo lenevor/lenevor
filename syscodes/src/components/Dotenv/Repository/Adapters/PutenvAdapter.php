fffdbb570c677d77732b8e6a9af32aaa5e9b7881
Alexander Campo <jalexcam@gmail.com>
2021-01-19 18:01:34 -0500

    Added class for read, write and delete an evironment variables

---
+++ syscodes/src/components/Dotenv/Repository/Adapters/PutenvAdapter.php
@@ -0,0 +1,75 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Dotenv\Repository\Adapters;
+
+use Syscodes\Contracts\Dotenv\Adapter;
+
+/**
+ * Read, write and delete an environment variable for 
+ * process of putenv.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+class PutenvAdapter implements Adapter
+{
+    /**
+     * Read an environment variable.
+     * 
+     * @param  string  $name
+     * 
+     * @return array
+     */
+    public function read(string $name)
+    {
+        return getenv($name);
+    }
+
+     /**
+     * Write to an environment variable.
+     * 
+     * @param  string  $name
+     * @param  string  $value
+     * 
+     * @return bool
+     */
+    public function write(string $name, string $value)
+    {
+        putenv("$name=$value");
+
+        return true;
+    }
+
+    /**
+     * Delete an environment variable.
+     * 
+     * @param  string  $name
+     * 
+     * @return bool
+     */
+    public function delete(string $name)
+    {
+        putenv($name);
+
+        return true;
+    }
+}
---
+++ syscodes/src/components/Dotenv/Repository/Adapters/ServerAdapter.php
@@ -0,0 +1,74 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Dotenv\Repository\Adapters;
+
+use Syscodes\Contracts\Dotenv\Adapter;
+
+/**
+ * Read, write and delete an environment variable for $_SERVER.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+class ServerAdapter implements Adapter
+{
+    /**
+     * Read an environment variable.
+     * 
+     * @param  string  $name
+     * 
+     * @return array
+     */
+    public function read(string $name)
+    {
+        return $_SERVER[$name];
+    }
+
+     /**
+     * Write to an environment variable.
+     * 
+     * @param  string  $name
+     * @param  string  $value
+     * 
+     * @return bool
+     */
+    public function write(string $name, string $value)
+    {
+        $_SERVER[$name] = $value;
+
+        return true;
+    }
+
+    /**
+     * Delete an environment variable.
+     * 
+     * @param  string  $name
+     * 
+     * @return bool
+     */
+    public function delete(string $name)
+    {
+        unset($_SERVER[$name]);
+
+        return true;
+    }
+}
