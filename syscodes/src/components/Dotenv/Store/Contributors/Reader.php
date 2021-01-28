93dc01d1d9da774479d5953cbcd2da3665dd561d
Alexander Campo <jalexcam@gmail.com>
2021-01-28 08:49:58 -0500

    Allows the read files and converting string in array

---
+++ syscodes/src/components/Dotenv/Store/Contributors/Reader.php
@@ -0,0 +1,56 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Dotenv\Store\Contributors;
+
+use InvalidArgumentException;
+
+/**
+ * Allows the read files and converting string in array.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+final class Reader
+{
+    /**
+     * Read the file(s), and return their raw content.
+     * 
+     * @param  string[]  $filePaths
+     * @param  bool  $modeEnabled  (true by default)
+     * 
+     * @return array 
+     */
+    public static function read(array $filePaths, bool $modeEnabled = true)
+    {
+        $output = '';
+
+        foreach ($filePaths as $filePath) {
+            $output = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); 
+            
+            if ($modeEnabled) {
+                break;
+            }
+        }
+
+        return $output;
+    }
+}
