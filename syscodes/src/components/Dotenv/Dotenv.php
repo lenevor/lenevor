f9caa960b18b3ac93e7036d92a93740768a7ac96
Alexander Campo <jalexcam@gmail.com>
2021-02-01 00:17:21 -0500

    Removed methods

--- syscodes/src/components/Dotenv/Dotenv.php
+++ syscodes/src/components/Dotenv/Dotenv.php
@@ -23,6 +23,7 @@
 namespace Syscodes\Dotenv;
 
 use InvalidArgumentException;
+use Syscodes\Dotenv\Loader\Loader;
 use Syscodes\Dotenv\Store\StoreBuilder;
 use Syscodes\Dotenv\Repository\RepositoryCreator;
 
@@ -33,6 +34,13 @@ use Syscodes\Dotenv\Repository\RepositoryCreator;
  */
 final class Dotenv
 {
+    /**
+     * The Loader instance.
+     * 
+     * @var \Syscodes\Dotenv\Loader\Loader $loader
+     */
+    protected $loader;
+
     /**
      * The Repository creator instance.
      * 
@@ -48,23 +56,18 @@ final class Dotenv
     protected $store;
 
     /**
-     * Activate use of putenv, by default is true.
-     * 
-     * @var bool $usePutenv 
-     */
-    protected $usePutenv = true;
-
-    /**
      * Constructor. Create a new Dotenv instance.
      * 
      * @param  \Syscodes\Dotenv\Store\StoreBuilder  $store
-p  * @param  \Syscodes\Dotenv\Repository\RepositoryCreator  $repository
+     * @param  \Syscodes\Dotenv\Loader\Loader  $loader
+     * @param  \Syscodes\Dotenv\Repository\RepositoryCreator  $repository
      * 
      * @return void
      */
-    public function __construct($store, $repository)
+    public function __construct($store, Loader $loader, $repository)
     {
         $this->store      = $store;
+        $this->loader     = $loader;
         $this->repository = $repository;
     }
 
@@ -95,31 +98,7 @@ p  * @param  \Syscodes\Dotenv\Repository\RepositoryCreator  $repository
             $store = $store->modeEnabled();
         }
 
-        return new self($store->make(), $repository);
-    }
-
-    /**
-     * Determine if the line in the file is a comment.
-     * 
-     * @param  string  $line
-     * 
-     * @return bool
-     */
-    protected function isComment(string $line)
-    {
-        return strpos(ltrim($line), '#') === 0;
-    }
-
-    /**
-     * Determine if the given line looks like it's setting a variable.
-     * 
-     * @param  string  $line
-     * 
-     * @return bool
-     */
-    protected function checkedLikeSetter(string $line)
-    {
-        return strpos($line, '=') !== false;
+        return new self($store->make(), new Loader($repository), $repository);
     }
 
     /**
@@ -130,162 +109,6 @@ p  * @param  \Syscodes\Dotenv\Repository\RepositoryCreator  $repository
      */
     public function load()
     {        
-        foreach ($this->store->read() as $line) {
-            // Is it a comment?
-            if ($this->isComment($line)) {
-                continue;
-            }
-
-            // If there is an equal sign, then we know we
-            // are assigning a variable.
-            if ($this->checkedLikeSetter($line)) {
-                list($name, $value) = $this->normaliseEnvironmentVariable($line);
-                $this->setVariable($name, $value);
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Normalise the given environment variable.
-     * 
-     * @param  string  $name
-     * @param  string  $value
-     * 
-     * @return array
-     */
-    public function normaliseEnvironmentVariable(string $name, string $value = '')
-    {
-        // Split our compound string into it's parts.
-        if (strpos($name, '=') !== false) {
-            list($name, $value) = explode('=', $name, 2);
-        }
-        
-        $name  = trim($name);
-        $value = trim($value);
-        
-        // Sanitize the name
-        $name = $this->sanitizeName($name);
-        
-        // Sanitize the value
-        $value = $this->sanitizeValue($value);
-        
-        // Get environment variables
-        $value = $this->getResolverVariables($value);
-        
-        return [$name, $value];
-    }
-
-    /**
-     * Strips quotes and the optional leading "export " from the environment variable name.
-     * 
-     * @param  string  $name
-     * 
-     * @return array
-     */
-    protected function sanitizeName(string $name)
-    {
-        return str_replace(array('export ', '\'', '"'), '', $name);
-    }
-
-    /**
-     * Sets the variable into the environment. 
-     * Will parse the string to look for {name}={value} pattern.
-     * 
-     * @param  string  $name
-     * @param  string|null  $value  (null by default)
-     * 
-     * @return void
-     */
-    protected function setVariable(string $name, $value = null)
-    {        
-        return $this->repository->set($name, $value);
-    }
-
-    /**
-     * Strips quotes from the environment variable value.
-     * 
-     * This was borrowed from the excellent phpdotenv with very few changes.
-     * https://github.com/vlucas/phpdotenv
-     * 
-     * @param  string  $value
-     * 
-     * @return array
-     * 
-     * @throws \InvalidArgumentException
-     */
-    protected function sanitizeValue($value)
-    {
-        if ( ! $value) {
-            return $value;
-        }
-        
-        // Does it begin with a quote?
-        if (strpbrk($value[0], '"\'') !== false) {
-            // value starts with a quote
-            $quote = $value[0];
-
-            $regexPattern = sprintf(
-					'/^
-					%1$s          # match a quote at the start of the value
-					(             # capturing sub-pattern used
-								  (?:          # we do not need to capture this
-								   [^%1$s\\\\] # any character other than a quote or backslash
-								   |\\\\\\\\   # or two backslashes together
-								   |\\\\%1$s   # or an escaped quote e.g \"
-								  )*           # as many characters that match the previous rules
-					)             # end of the capturing sub-pattern
-					%1$s          # and the closing quote
-					.*$           # and discard any string after the closing quote
-					/mx', $quote
-            );
-            
-            $value = preg_replace($regexPattern, '$1', $value);
-            $value = str_replace("\\$quote", $quote, $value);
-            $value = str_replace('\\\\', '\\', $value);
-        } else {
-            $parts = explode(' #', $value, 2);
-            $value = trim($parts[0]);
-            // Unquoted values cannot contain whitespace
-            if (preg_match('/\s+/', $value) > 0) {
-                throw new InvalidArgumentException('.env values containing spaces must be surrounded by quotes.');
-            }
-        }
-        
-        return $value;
-    }
-    
-    /**
-     * Resolve the nested variables.
-     * 
-     * Look for ${varname} patterns in the variable value and replace with an existing
-     * environment variable.
-     * 
-     * This was borrowed from the excellent phpdotenv with very few changes.
-     * https://github.com/vlucas/phpdotenv
-     * 
-     * @param  string  $value
-     * 
-     * @return string
-     */
-    protected function getResolverVariables(string $value)
-    {
-        if (strpos($value, '$') !== false) {
-            $repository = $this->repository;
-
-            $value = preg_replace_callback('~\${([a-zA-Z0-9_]+)}~', function ($pattern) use ($repository) {
-                $nestedVariable = $repository->get($pattern[1]);
-
-                if (is_null($nestedVariable)) {
-                    return $pattern[0];
-                }
-                
-                return $nestedVariable;
-                
-            }, $value);
-        }
-        
-        return $value;
+        return $this->loader->load($this->store->read());
     }
 }
