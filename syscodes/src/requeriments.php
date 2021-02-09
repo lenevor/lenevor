3cafa40ffa1612e39e717b54247449e2a55d5e20
Alexander Campo <jalexcam@gmail.com>
2021-02-09 13:31:23 -0500

    Added conditional for php_cli

--- syscodes/src/requeriments.php
+++ syscodes/src/requeriments.php
@@ -25,13 +25,28 @@
  */
 
 // You can remove this if you are confident that your PHP version is sufficient
-if (version_compare(PHP_VERSION, '7.3.12') < 0) 
+
+$version = '7.3.12';
+
+if (version_compare(PHP_VERSION, $version) < 0) 
 {
-    trigger_error('Your PHP version must be equal or higher than 7.3.12 to use Lenevor.'.PHP_EOL, E_USER_ERROR);
+    if (PHP_SAPI == 'cli') {
+        $string  = "\033[1;36m";
+        $string .= "$version\033[0m";
+        trigger_error("Your PHP version must be equal or higher than {$string} to use Lenevor Framework.".PHP_EOL, E_USER_ERROR);
+    }
+
+    exit("Your PHP version must be equal or higher than <b>{$version}</b> to use Lenevor Framework.");
 }
 
 // You can remove this if you are confident you have mbstring installed
 if ( ! extension_loaded('mbstring')) 
 {
-    trigger_error('You must enable the mbstring extension to use Lenevor.'.PHP_EOL, E_USER_ERROR);
+    if (PHP_SAPI == 'cli') {
+        $string  = "\033[1;36m";
+        $string .= "mbstring\033[0m";
+        trigger_error("You must enable the {$string} extension to use Lenevor Framework.".PHP_EOL, E_USER_ERROR);
+    }
+
+    exit('You must enable the <b>mbstring</b> extension to use Lenevor Framework.');
 }
