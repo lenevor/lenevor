db33d7f79c1055af7ad4f5d92d784445c284023f
Alexander Campo <jalexcam@gmail.com>
2021-03-07 04:31:12 -0500

    Added new methods for the console commands

--- syscodes/src/components/Core/Console/Lenevor.php
+++ syscodes/src/components/Core/Console/Lenevor.php
@@ -23,7 +23,6 @@
 namespace Syscodes\Core\Console;
 
 use Closure;
-use Exception;
 use Throwable;
 use ReflectionClass;
 use Syscodes\Support\Str;
@@ -31,9 +30,9 @@ use Syscodes\Support\Finder;
 use Syscodes\Collections\Arr;
 use Syscodes\Contracts\Core\Application;
 use Syscodes\Contracts\Events\Dispatcher;
+use Syscodes\Console\Application as Prime;
 use Syscodes\Contracts\Debug\ExceptionHandler;
-use Syscodes\Console\Application as LenevorConsole;
-use Syscodes\Debug\FatalExceptions\FatalThrowableError;
+use Syscodes\Contracts\Console\Lenevor as LenevorConsole;
 
 /**
  * The Lenevor class is the heart of the system when use 
@@ -41,7 +40,119 @@ use Syscodes\Debug\FatalExceptions\FatalThrowableError;
  *
  * @author Alexander Campo <jalexcam@gmail.com>
  */
-class Lenevor extends LenevorConsole
+class Lenevor implements LenevorConsole
 {
+    /**
+     * The application implementation.
+     * 
+     * @var \Syscodes\Contracts\Core\Application $app
+     */
+    protected $app;
 
+    /**
+	 * The bootstrap classes for the application.
+	 * 
+	 * @var array $bootstrappers
+	 */
+	protected $bootstrappers = [
+		\Syscodes\Core\Bootstrap\BootDetectEnvironment::class,
+		\Syscodes\Core\Bootstrap\BootConfiguration::class,
+		\Syscodes\Core\Bootstrap\BootHandleExceptions::class,
+		\Syscodes\Core\Bootstrap\BootRegisterFacades::class,
+		\Syscodes\Core\Bootstrap\BootRegisterProviders::class,
+		\Syscodes\Core\Bootstrap\BootProviders::class,
+	];
+
+    /**
+     * Constructor. Create new console Lenevor instance.
+     * 
+     * @param  \Syscodes\Contracts\Core\Application $app
+     * 
+     * @return void
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Handle an incoming console command.
+     * 
+     * @param  \Syscodes\Http\Request  $request
+     * 
+     * @return int
+     */
+    public function handle($request)
+    {
+        try {
+            $this->bootstrap();
+            
+            return $this->getPrime();
+        } catch (Throwable $e) {
+            
+            $this->reportException($e);
+            
+            $this->renderException($request, $e);
+
+            return 1;
+        }
+        
+    }
+
+    /**
+     * Bootstrap the application for artisan commands.
+     * 
+     * @return void
+     */
+    public function bootstrap()
+    {
+        if ( ! $this->app->hasBeenBootstrapped()) {
+			$this->app->bootstrapWith($this->bootstrappers());
+		}
+    }
+
+    /**
+	 * Get the bootstrap classes for the application.
+	 * 
+	 * @return array
+	 */
+	protected function bootstrappers()
+	{
+		return $this->bootstrappers;
+	}
+
+    /**
+     * Get the Prime application instance.
+     * 
+     * @return \Syscodes\Console\Application
+     */
+    protected function getPrime()
+    {
+        return (new Prime($this->app))->showHeader();
+    }
+
+    /**
+	 * Report the exception to the exception handler.
+	 * 
+	 * @param  \Throwable  $e
+	 * 
+	 * @return void
+	 */
+	protected function reportException(Throwable $e)
+	{
+		$this->app[ExceptionHandler::class]->report($e);
+	}
+	
+	/**
+	 * Render the exception to a response.
+	 * 
+	 * @param  \Syscodes\Http\Request  $request
+	 * @param  \Throwable  $e
+	 * 
+	 * @return void
+	 */
+	protected function renderException($request, Throwable $e)
+	{
+		$this->app[ExceptionHandler::class]->render($request, $e);
+	}
 }
