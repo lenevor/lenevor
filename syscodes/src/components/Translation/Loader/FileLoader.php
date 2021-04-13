9c819ec8c4b5d6861927c328f24e9db49c3d4651
Alexander Campo <jalexcam@gmail.com>
2021-04-13 01:02:28 -0500

    Added methods for load from the given JSON or PHP

--- syscodes/src/components/Translation/Loader/FileLoader.php
+++ syscodes/src/components/Translation/Loader/FileLoader.php
@@ -61,4 +61,63 @@ class FileLoader implements LoaderContract
         $this->files = $files;
         $this->path  = $path;
     }
+
+    /**
+     * Load the messages for the given locale.
+     * 
+     * @param  string  $locale
+     * @param  string  $group
+     * 
+     * @return array
+     */
+    public function load($locale, $group = '*')
+    {
+        if (isset($group)) {
+            return $this->loadFilePaths($locale, $group);
+        }
+
+        return $this->loadJsonPaths($locale);
+    }
+
+    /**
+     * Load a locale from the given JSON file path.
+     * 
+     * @param  string  $locale
+     * 
+     * @return array
+     * 
+     * @throws \RuntimeException
+     */
+    protected function loadJsonPaths($locale)
+    {
+        if ($this->files->exists($fullPath = "{$this->path}/{$locale}.json")) {
+            $output = json_decode($this->files->get($fullPath), true);
+
+            if (is_null($output) || json_last_error() !== JSON_ERROR_NONE) {
+                throw new RuntimeException("Translation file [{$fullPath}] contains an invalid JSON structure");
+            }
+
+            return $output;
+        }
+
+        return [];
+    }
+
+    /**
+     * Load a locale from a given path.
+     * 
+     * @param  string  $path
+     * @param  string  $locale
+     * @param  string  $group
+     * 
+     * @return array
+     */
+    protected function loadFilePaths($locale, $group)
+    {
+        if ($this->files->exists($fullPath = "{$this->path}/{$locale}/{$group}.php")) {
+            return $this->files->getRequire($fullPath);
+        }
+
+        return [];
+    }
 }
