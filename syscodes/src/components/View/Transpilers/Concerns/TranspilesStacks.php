fb6b9f1ba1885af5239a744765f5c73abe9afd03
Alexander Campo <jalexcam@gmail.com>
2021-06-09 02:03:40 -0500

    Added new trait for the transpiler

---
+++ syscodes/src/components/View/Transpilers/Concerns/TranspilesStacks.php
@@ -0,0 +1,87 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\View\Transpilers\Concerns;
+
+/**
+ * Trait TranspilesStacks.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+trait TranspilesStacks
+{
+    /**
+     * Transpile the stack statements into valid PHP.
+     * 
+     * @param  string  $expression
+     * 
+     * @return string
+     */
+    protected function transpileStack($expression)
+    {
+        return "<?php echo \$__env->givePushContent{$expression}; ?>";
+    }
+
+    /**
+     * Transpile the push statements into valid PHP.
+     * 
+     * @param  string  $expression
+     * 
+     * @return string
+     */
+    protected function transpilePush($expression)
+    {
+        return "<?php \$__env->startPush{$expression}; ?>";
+    }
+
+    /**
+     * Transpile the end-push statements into valid PHP.
+     * 
+     * @return string
+     */
+    protected function transpileEndPush()
+    {
+        return '<?php $__env->stopPush(); ?>';
+    }
+
+    /**
+     * Transpile the prepend statements into valid PHP.
+     * 
+     * @param  string  $expression
+     * 
+     * @return string
+     */
+    protected function transpilePrepend($expression)
+    {
+        return "<?php \$__env->startPrepend{$expression}; ?>";
+    }
+
+    /**
+     * Transpile the end-prepend statements into valid PHP.
+     * 
+     * @return string
+     */
+    protected function transpileEndPrepend()
+    {
+        return '<?php $__env->stopPrepend(); ?>';
+    }
+}
