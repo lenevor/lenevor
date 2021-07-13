b77629d5ed90a9184cb4440d4ae71daa87799838
Alexander Campo <jalexcam@gmail.com>
2021-07-13 03:03:06 -0500

    Added variables and methods for outputs a string to the CLI command

--- syscodes/src/components/Console/Output/Write.php
+++ syscodes/src/components/Console/Output/Write.php
@@ -27,5 +27,91 @@ namespace Syscodes\Console\Output;
  */
 class Write
 {
-    
+	/**
+	 * Get the Color for the Cli command.
+	 * 
+	 * @var \Syscodes\Console\Output\Color $colorizer
+	 */
+	protected $colorizer;
+
+	/**
+ 	 * The standar STDERR is where the application writes its error messages.
+ 	 *
+ 	 * @var string $stderr 
+ 	 */
+	protected $stderr;
+
+	/**
+ 	 * The estandar STDOUT is where the application records its output messages.
+ 	 *
+ 	 * @var resource $stdout
+ 	 */
+	protected $stdout;
+
+	/**
+	 * Constructor. Create a new Write class instance.
+	 * 
+	 * @param  string|null  $path
+	 * @param  \Syscodes\Console\Output\Color|null
+	 * 
+	 * @return void 
+	 */
+	public function __construct(string $path = null, Color $colorizer = null)
+	{
+		if ($path) {
+			$path = fopen($path, 'w');
+		}
+
+		// Writes with color all messages
+		$this->colorizer = $colorizer ?? new Color;
+
+		// Writes its error messages
+		$this->stderr = $path ?: \STDERR;
+
+		// Records its output messages
+		$this->stdout = $path ?: \STDOUT;
+	}
+	
+    /**
+ 	 * Outputs a string to the cli.	If you send an array it will implode them 
+ 	 * with a line break.
+ 	 * 
+ 	 * @param  string|array  $text  The text to output, or array of lines
+ 	 * @param  string|null  $foreground  The foreground color
+ 	 * @param  string|null  $background  The background color
+ 	 *
+ 	 * @return string
+ 	 */
+ 	public function write(string $text = '', string $foreground = null, string $background = null)
+ 	{
+ 		if (is_array($text)) {
+ 			$text = implode(PHP_EOL, $text);
+ 		}
+
+ 		if ($foreground OR $background) {
+ 			$text = $this->colorizer->line($text, $foreground, $background);
+ 		}
+
+ 		$this->fwrite($this->stdout, $text.PHP_EOL);
+ 	}
+
+    /**
+	 * The library is intended for used on Cli command, 
+	 * this commands can be called from controllers and 
+	 * elsewhere of framework.
+	 * 
+	 * @param  resource  $handle
+	 * @param  string  $text
+	 * 
+	 * @return string
+	 */
+	protected function fwrite($handle, string $text)
+	{
+		if (isCli()) {
+			fwrite($handle, $text);
+			return;
+		}
+
+		echo $text;
+	}                                      
 }
