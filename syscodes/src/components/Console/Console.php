c642b69325813db4820a5c3c57a42ae0e9886afe
Alexander Campo <jalexcam@gmail.com>
2021-07-19 23:55:43 -0500

    Create Console.php

---
+++ syscodes/src/components/Console/Console.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Console;
+
+/**
+ * 
+ */
+class Console
+{
+    /**
+     * Gets the name of the aplication.
+     * 
+     * @var string $name
+     */
+    protected $name;
+
+    /**
+     * Gets the version of the application.
+     * 
+     * @var string $version
+     */
+    protected $version;
+
+    /**
+     * Constructor. Create new Console instance.
+     * 
+     * @param  string  $name
+     * @param  string  $version
+     * 
+     * @return void
+     */
+    public function __construct(string $name = 'UNKNOWN', string $version = 'UNKNOWN')
+    {
+        $this->name    = $name;
+        $this->version = $version;
+    }
+
+    /**
+     * Gets the name of the application.
+     * 
+     * @return string 
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the name of the application.
+     * 
+     * @param  string  $name  The application name
+     * 
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Gets the version of the application.
+     * 
+     * @return string
+     */
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+    /**
+     * Sets the name of the application.
+     * 
+     * @param  string  $version  The application version
+     * 
+     * @return void
+     */
+    public function setVersion(string $version): void
+    {
+        $this->version = $version;
+    }
+}
