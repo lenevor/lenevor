4a69db1c6a1345d3cd1284bf5bb7451d1eb52160
Alexander Campo <jalexcam@gmail.com>
2021-07-20 02:18:34 -0500

    Update Table.php

--- syscodes/src/components/Console/Helper/Table.php
+++ syscodes/src/components/Console/Helper/Table.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Console\Helpers;
+namespace Syscodes\Console\Helper;
 
 /**
  * @author Alexander Campo <jalexcam@gmail.com>
