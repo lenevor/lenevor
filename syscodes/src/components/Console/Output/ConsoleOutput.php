464be1e09fbe767ae5f3ddf8ddf15648b626302a
Alexander Campo <jalexcam@gmail.com>
2021-07-21 02:59:22 -0500

    Create ConsoleOutput.php

---
+++ syscodes/src/components/Console/Output/ConsoleOutput.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Console\Output;
+
+use Syscodes\Contracts\Console\Output as OutputInterface;
+
+/**
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+class ConsoleOutput
+{
+    /**
+     * Gets the Output interface for errors.
+     * 
+     * @return \Syscodes\Contracts\Console\Output
+     */
+    public function getErrorOutput(): Output
+    {
+
+    }
+
+    /**
+     * Sets the Output interface for errors.
+     * 
+     * @return void
+     */
+    public function SetErrorOutput(OutputInterface $error): void
+    {
+        $this->stderr = $error;
+    }
+}
