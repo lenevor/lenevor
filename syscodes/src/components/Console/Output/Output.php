fe22116a08857d814532d9ace6ffff5f444c5136
Alexander Campo <jalexcam@gmail.com>
2021-07-26 23:55:34 -0500

    Changed class to abstract

--- syscodes/src/components/Console/Output/Output.php
+++ syscodes/src/components/Console/Output/Output.php
@@ -23,7 +23,8 @@
 namespace Syscodes\Console\Output;
 
 use Syscodes\Console\Formatter\OutputFormatter;
-use Syscodes\Console\Formatter\OutputFormatter as OutputFormatterInterface;
+use Syscodes\Contracts\Console\Output as OutputInterface;
+use Syscodes\Contracts\Console\OutputFormatter as OutputFormatterInterface;
 
 /**
  * Allows the use of the formatter in the messages to be displayed 
@@ -31,7 +32,7 @@ use Syscodes\Console\Formatter\OutputFormatter as OutputFormatterInterface;
  * 
  * @author Alexander Campo <jalexcam@gmail.com>
  */
-class Output
+abstract class Output implements OutputInterface
 {
 	/**
 	 * Gets formatter for output console.
@@ -43,14 +44,71 @@ class Output
 	/**
 	 * Constructor. Create a new Output instance.
 	 * 
-	 * @param  bool  $decorated
-	 * @param  \Syscodes\Contracts\Console\OutputFormatter|null  $formatter
+	 * @param  bool  $decorated  Whether to decorated messages
+	 * @param  \Syscodes\Contracts\Console\OutputFormatter|null  $formatter  The output formatter instance
 	 * 
 	 * @return void
 	 */
 	public function __construct(bool $decorated = false, OutputFormatterInterface $formatter = null)
 	{
 		$this->formatter = $formatter ?? new OutputFormatter();
+		$this->formatter->setDecorated($decorated);
+	}
+
+	/**
+	 * Gets the decorated flag.
+	 * 
+	 * @return bool
+	 */
+	public function getDecorated(): string
+	{
+		return $this->formatter->getDecorated();
+	}
+
+	/**
+	 * Sets the decorated flag.
+	 * 
+	 * @param  bool  $decorated  Whether to decorated messages
+	 * 
+	 * @return void
+	 */
+	public function setDecorated(bool $decorated): void
+	{
+		$this->formatter->setDecorated($decorated);
+	}
+
+	/**
+	 * Returns a output formatter instance.
+	 * 
+	 * @return \Syscodes\Contracts\Console\OutputFormatter
+	 */
+	public function getFormatter(): OutputFormatterInterface
+	{
+		return $this->formatter;
+	}
+
+	/**
+	 * Sets a output formatter instance.
+	 * 
+	 * @param  \Syscodes\Contracts\Console\OutputFormatter  $formatter;
+	 * 
+	 * @return void
+	 */
+	public function setFormatter(OutputFormatterInterface $formatter): void
+	{
+		$this->formatter = $formatter;
+	}
+
+	/**
+	 * Enter a number of empty lines.
+	 * 
+	 * @param  int  $num  Number of lines to output
+	 * 
+	 * @return string
+	 */
+	public function newline(int $num = 1)
+	{
+		return str_repeat(\PHP_EOL, \max($num, 1));
 	}
 
     /**
@@ -59,27 +117,55 @@ class Output
 	 * 
 	 * @param  string|iterable  $messages  The text to output, or array of lines
 	 * @param  bool  $newline  Add a newline command
+	 * @param  int  $options  A bitmask of options (0 is considered the same as self::OUTPUT_NORMAL)
 	 * 
 	 * @return string
 	 */
-	public function write($messages, bool $newline = false)
+	public function write($messages, bool $newline = false, int $options = self::OUTPUT_NORMAL)
     {
-		if ($newline) {
-			$messages .= \PHP_EOL;
+		if ( ! is_iterable($messages)) {
+			$messages = [$messages];
+		}
+		
+		$types = self::OUTPUT_NORMAL | self::OUTPUT_RAW | self::OUTPUT_PLAIN;
+		$type = $types & $options ?: self::OUTPUT_NORMAL;
+		
+		foreach ($messages as $message) {
+			switch($type) {
+				case OutputInterface::OUTPUT_NORMAL:
+					$message = $this->formatter->format($message);
+					break;
+				case OutputInterface::OUTPUT_RAW:
+					break;
+				case OutputInterface::OUTPUT_PLAIN:
+					$message = strip_tags($this->formatter->format($message));
+					break;
+			}
 		}
 
-		return $this->formatter->format($messages);
+		return $this->toWrite($message ?? '', $newline);
     }
 
     /**
 	 * Writes a message to the output and adds a newline at the end..
 	 * 
 	 * @param  string|iterable  $messages  The message as an iterable of strings or a single string
+	 * @param  int  $options  A bitmask of options (0 is considered the same as self::OUTPUT_NORMAL)
 	 * 
 	 * @return string
 	 */
-	public function writeln($messages)
+	public function writeln($messages, int $options = self::OUTPUT_NORMAL)
     {
-        return $this->write($messages, true);
+        return $this->write($messages, true, $options);
     }
+
+	/**
+     * Writes a message to the output.
+     * 
+     * @param  string  $message  The text to output
+     * @param  bool  $newline  Add a newline command
+     * 
+     * @return mixed
+     */
+    abstract protected function toWrite(string $message, bool $newline);
 }
