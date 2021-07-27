c83ef054e1e9d0b896bf9cff54d4cfa33111709a
Alexander Campo <jalexcam@gmail.com>
2021-07-27 00:00:07 -0500

    Added method for output console

--- syscodes/src/components/Contracts/Console/Output.php
+++ syscodes/src/components/Contracts/Console/Output.php
@@ -22,6 +22,8 @@
 
 namespace Syscodes\Contracts\Console;
 
+use Syscodes\Contracts\Console\OutputFormatter as OutputFormatterInterface;
+
 /**
  * <Output> is the interface implemented by all Output classes.
  * 
@@ -29,23 +31,70 @@ namespace Syscodes\Contracts\Console;
  */
 interface Output
 {
+	public const OUTPUT_NORMAL = 1;
+	public const OUTPUT_RAW = 2;
+	public const OUTPUT_PLAIN = 3;
+
+	/**
+	 * Gets the decorated flag.
+	 * 
+	 * @return bool
+	 */
+	public function getDecorated(): string;
+
+	/**
+	 * Sets the decorated flag.
+	 * 
+	 * @param  bool  $decorated  Whether to decorated messages
+	 * 
+	 * @return void
+	 */
+	public function setDecorated(bool $decorated): void;
+
+	/**
+	 * Returns a output formatter instance.
+	 * 
+	 * @return \Syscodes\Contracts\Console\OutputFormatter
+	 */
+	public function getFormatter(): OutputFormatterInterface;
+
+	/**
+	 * Sets a output formatter instance.
+	 * 
+	 * @param  \Syscodes\Contracts\Console\OutputFormatter  $formatter;
+	 * 
+	 * @return void
+	 */
+	public function setFormatter(OutputFormatterInterface $formatter): void;
+
+	/**
+	 * Enter a number of empty lines.
+	 * 
+	 * @param  int  $num  Number of lines to output
+	 * 
+	 * @return string
+	 */
+	public function newline(int $num = 1);
+
     /**
 	 * Outputs a string to the cli.	If you send an array it will implode them
 	 * with a line break.
 	 * 
 	 * @param  string|iterable  $messages  The text to output, or array of lines
 	 * @param  bool  $newline  Add a newline command
+	 * @param  int  $options  A bitmask of options (0 is considered the same as self::OUTPUT_NORMAL)
 	 * 
 	 * @return string
 	 */
-	public function write($messages, bool $newline = false);
+	public function write($messages, bool $newline = false, int $options = 0);
 
     /**
 	 * Writes a message to the output and adds a newline at the end..
 	 * 
 	 * @param  string|iterable  $messages  The message as an iterable of strings or a single string
+	 * @param  int  $options  A bitmask of options (0 is considered the same as self::OUTPUT_NORMAL)
 	 * 
 	 * @return string
 	 */
-	public function writeln($messages);
+	public function writeln($messages, int $options = 0);
 }
