616a2817951ff6b0478279d8fabe90e19effc575
Alexander Campo <jalexcam@gmail.com>
2021-08-05 02:34:09 -0500

    Added method for parsing of text

--- syscodes/src/components/Console/Style/ColorTag.php
+++ syscodes/src/components/Console/Style/ColorTag.php
@@ -22,10 +22,103 @@
 
 namespace Syscodes\Console\Style;
 
+use Syscodes\Console\Style\Color;
+
 /**
+ * Formats for color tags.
+ * 
  * @author Alexander Campo <jalexcam@gmail.com>
  */
 final class ColorTag
 {
-    
+    // Regex to match tags.
+    public const REGEX_TAG = '/<([a-zA-Z0-9=;_]+)>(.*?)<\/\\1>/s';
+
+    /**
+     * Alias of the wrap()
+     *
+     * @param string $text
+     * @param string $tag
+     *
+     * @return string
+     */
+    public static function add(string $text, string $tag): string
+    {
+        return self::wrap($text, $tag);
+    }
+
+    /**
+     * wrap a color style tag
+     *
+     * @param string $text
+     * @param string $tag
+     *
+     * @return string
+     */
+    public static function wrap(string $text, string $tag): string
+    {
+        if (!$text || !$tag) {
+            return $text;
+        }
+
+        return "<$tag>$text</$tag>";
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return array
+     */
+    public static function matchAll(string $text): array
+    {
+        if ( ! preg_match_all(self::REGEX_TAG, $text, $matches)) {
+            return [];
+        }
+
+        return $matches;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return string
+     */
+    public static function parse(string $text): string
+    {
+        if (!$text || false === strpos($text, '</')) {
+            return $text;
+        }
+
+        // match color tags
+        if ( ! $matches = self::matchAll($text)) {
+            return $text;
+        }
+
+        foreach ((array)$matches[0] as $i => $m) {
+            $key = $matches[1][$i];
+
+            if (Color::STYLES[$key]) {
+                $text = self::replaceColor($text, $key, $matches[2][$i], Color::STYLES[$key]);
+            }
+        }
+
+        return $text;
+    }
+
+    /**
+     * Replace color tags in a string.
+     *
+     * @param string $text
+     * @param string $tag       The matched tag.
+     * @param string $match     The matched text
+     * @param string $colorCode The color style to apply.
+     *
+     * @return  string
+     */
+    public static function replaceColor(string $text, string $tag, string $match, string $colorCode): string
+    {
+        $replace = sprintf("\033[%sm%s\033[0m", $colorCode, $match);
+
+        return str_replace("<$tag>$match</$tag>", $replace, $text);
+    }
 }
