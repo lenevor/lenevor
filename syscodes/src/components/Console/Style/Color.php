0f4a97f0c90a3e539f5c8de06ebdf6e057cd9809
Alexander Campo <jalexcam@gmail.com>
2021-08-09 22:27:48 -0500

    Update Color.php

--- syscodes/src/components/Console/Style/Color.php
+++ syscodes/src/components/Console/Style/Color.php
@@ -32,38 +32,13 @@ use InvalidArgumentException;
  */
 final class Color
 {
-    protected const COLORS = [
-		'black'   => 0,
-		'red'     => 1,
-		'green'   => 2,
-		'yellow'  => 3,
-		'blue'    => 4,
-		'magenta' => 5,
-		'cyan'    => 6,
-		'white'   => 7,
-		'default' => 9,
-	];
+	// Regex to match color tags
+	public const COLOR_TAG = '/<([a-z=;]+)>(.*?)<\/\\1>/s';
+	
+	// CLI color template
+	public const COLOR_TPL = "\033[%sm%s\033[0m";
 
-	protected const LIGHT_COLORS = [
-		'gray'          => 0,
-		'light-red'     => 1,
-		'light-green'   => 2,
-		'light-yellow'  => 3,
-		'light-blue'    => 4,
-		'light-magenta' => 5,
-		'light-cyan'    => 6,
-		'light-white'   => 7,
-	];
-
-	protected const OPTIONS = [
-		'bold' => ['set' => 1, 'unset' => 22],
-		'underline' => ['set' => 4, 'unset' => 24],
-		'blink' => ['set' => 5, 'unset' => 25],
-		'reverse' => ['set' => 7, 'unset' => 27],
-		'conceal' => ['set' => 8, 'unset' => 28],
-	];
-
-	/**
+    /**
      * There are some internal styles
      * custom style: fg;bg;opt
      *
@@ -163,129 +138,7 @@ final class Color
         'light_gray'  => '37',
     ];
 
-
-	/**
-	 * The background color to text or CLI command.
-	 * 
-	 * @var string $background
-	 */
-	protected $background;
-
-	/**
-	 * The foreground color to text or CLI command.
-	 * 
-	 * @var string $foreground
-	 */
-	protected $foreground;
-
-	/**
-	 * Gets options the colors for CLI command.
-	 * 
-	 * @var array $options
-	 */
-	protected $options = [];
-
-	/**
-	 * Constructor. Create a new Color instance.
-	 * 
-	 * @param  string  $foreground
-	 * @param  string  $background
-	 * @param  array  $options
-	 * 
-	 * @return void
-	 */
-	public function __construct(string $foreground = '', string $background = '', array $options = [])
-	{
-		$this->foreground =  $this->parser($foreground);
-		$this->background =  $this->parser($background, true);
-
-		foreach ($options as $option) {
-			if ( ! isset(self::OPTIONS[$option])) {
-				throw new InvalidArgumentException(
-					sprintf('Invalid option specified: "%s". Expected one of (%s).', 
-						$option, 
-						implode(',', array_keys(self::OPTIONS))
-					)
-				);
-			}
-			
-			$this->options[$option] = self::OPTIONS[$option];
-		}
-	}
-
-	/**
-	 * Gets the parse color for capture to the color type that is needed 
-	 * on foreground and background of CLI Commands.
-	 * 
-	 * @param  string  $color
-	 * @param  bool  $background
-	 * 
-	 * @return string
-	 * 
-	 * @throws \InvalidArgumentException
-	 */
-	private function parser(string $color, bool $background = false): string
-	{
-		if ('' === $color) {
-			return '';
-		}
-
-		if (isset(self::COLORS[$color])) {
-			return ($background ? '4' : '3').self::COLORS[$color];
-		}
-
-		if (isset(self::LIGHT_COLORS[$color])) {
-			return ($background ? '10' : '9').self::LIGHT_COLORS[$color];
-		}
-
-		throw new InvalidArgumentException(
-			sprintf('Invalid "%s" color; expected one of (% s). ',
-				$color,
-				implode(', ',array_merge(array_keys(self::COLORS), array_keys(self::LIGHT_COLORS)))
-			)
-		);
-	}
-
-	/**
-     * Create a color style from a parameter string.
-     * 
-     * @param  string  $string  e.g 'fg=white;bg=black;options=bold,underscore;extra=1'
-     * 
-     * @return self
-     * 
-     * @throws \InvalidArgumentException
-     */
-    public static function fromString(string $string): self
-    {
-        $options = [];
-        $parts   = explode(';', str_replace(' ', '', $string));
-        
-        $fg = $bg = '';
-
-        foreach ($parts as $part) {
-            $subParts = explode('=', $part);
-            
-            if (count($subParts) < 2) {
-                continue;
-            }
-            
-            switch ($subParts[0]) {
-                case 'fg':
-                    $foreground = $subParts[1];
-                    break;
-                case 'bg':
-                    $background = $subParts[1];
-                    break;
-                case 'options':
-                    $options = explode(',', $subParts[1]);
-                    break;
-                default:
-                    throw new RuntimeException('Invalid option');
-            }
-        }
-        
-        return new self($foreground, $background, $options);
-    }
+	
 
 	/**
 	 * Gets the result of the string applied to the text in the CLI command.
@@ -294,64 +147,56 @@ final class Color
 	 * 
 	 * @return string
 	 */
-	public function apply(string $text): string
+	public static function apply(string $text): string
 	{
-		return $this->set().$text.$this->unset();
+		return ColorCode::fromString($text)->toStyle();
 	}
 
 	/**
-	 * Gets the set color to CLI command.
-	 * 
-	 * @return string
-	 */
-	public function set(): string
-	{
-		$codes = [];
-		
-		if ('' !== $this->foreground) {
-			$codes[] = $this->foreground;
-		}
-		
-		if ('' !== $this->background) {
-			$codes[] = $this->background;
-		}
-		
-		foreach ($this->options as $option) {
-			$codes[] = $option['set'];
-		}
-		
-		if (0 === \count($codes)) {
-			return '';
-		}
-		
-		return sprintf("\033[%sm", implode(';', $codes));
-	}
+     * Render text, apply color code.
+     *
+     * @param  string             $text
+     * @param  string|array|null  $style
+     *
+     * @return string
+     */
+    public static function render(string $text, $style = null): string
+    {
+        if ( ! $text) {
+            return $text;
+        }
 
-	/**
-	 * Gets the unset color to CLI command.
-	 * 
-	 * @return string
-	 */
-	public function unset(): string
-	{
-		$codes = [];
-		
-		if ('' !== $this->foreground) {
-			$codes[] = 39;
-		}
-		
-		if ('' !== $this->background) {
-			$codes[] = 49;
-		}
-		
-		foreach ($this->options as $option) {
-			$codes[] = $option['unset'];
-		}
-		
-		if (0 === \count($codes)) {
-			return '';
-		}
-		
-		return sprintf("\033[%sm", implode(';', $codes));
-	}
+        $color = '';
+
+        // use defined style: 'green'
+        if (is_string($style)) {
+            $color = self::STYLES[$style] ?? '';
+
+            // custom style: [self::FG_GREEN, self::BG_WHITE, self::UNDERSCORE]
+        } elseif (is_array($style)) {
+            $color = implode(';', $style);
+
+            // user color tag: <info>message</info>
+        } elseif (strpos($text, '</') > 0) {
+            return self::parseTag($text);
+        }
+
+        if (!$color) {
+            return $text;
+        }
+
+        return sprintf(self::COLOR_TPL, $color, $text);
+    }
+
+    /**
+     * Parse color tag e.g: <info>message</info>.
+     *
+     * @param string $text
+     *
+     * @return string
+     */
+    public static function parseTag(string $text): string
+    {
+        return ColorTag::parse($text);
+    }
 }
