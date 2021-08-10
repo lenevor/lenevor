c57477d94e309708660dedee2e1e7be32677ed3d
Alexander Campo <jalexcam@gmail.com>
2021-08-10 01:43:42 -0500

    Create interface for input to CLI arguments

---
+++ syscodes/src/components/Contracts/Console/Input/InputArgument.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Contracts\Console;
+
+/**
+ * InputArgument Interface.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+interface InputArgument
+{
+    public const REQUIRED = 1;
+    public const OPTIONAL = 2;
+    public const IS_ARRAY = 4;
+    
+    /**
+     * Gets the argument name.
+     * 
+     * @return string  The argument name
+     */
+    public function getName(): string;
+    
+    /**
+     * Sets the argument name.
+     * 
+     * @param  string  $name  The argument name
+     * 
+     * @return void
+     */
+    public function setName(string $name): void;
+
+    /**
+     * Gets the argument mode.
+     * 
+     * @return int
+     */
+    public function getMode(): int;
+
+    /**
+     * Sets the argument mode.
+     * 
+     * @param  int  $mode  The argument mode
+     * 
+     * @return void
+     */
+    public function setMode(int $mode): void;
+    
+    /**
+     * Gets the description text.
+     * 
+     * @return string  The description text
+     */
+    public function getDescription(): string;
+
+    /**
+     * Sets the description text.
+     * 
+     * @param  string  $description  The description text
+     * 
+     * @return void
+     */
+    public function setDescription(string $description): void;
+    
+    /**
+     * Gets the default value.
+     * 
+     * @return mixed
+     */
+    public function getDefault();
+    
+    /**
+     * Sets the default value.
+     * 
+     * @param  mixed  $default
+     * 
+     * @return mixed
+     * 
+     * @throws \LogicException
+     */
+    public function setDefault($default = null): void;
+    
+    /**
+     * Gets true if the argument is required.
+     * 
+     * @return bool true if parameter mode is self::REQUIRED, false otherwise
+     */
+    public function isRequired();
+    
+    /**
+     * Gets true if the argument can take multiple values.
+     * 
+     * @return bool true if mode is self::IS_ARRAY, false otherwise
+     */
+    public function isArray();
+}
