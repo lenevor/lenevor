8d8f6e29b54995212115e7827896f348db21048c
Alexander Campo <jalexcam@gmail.com>
2021-08-11 01:35:27 -0500

    Update InputOption.php

--- syscodes/src/components/Console/Input/InputOption.php
+++ syscodes/src/components/Console/Input/InputOption.php
@@ -22,6 +22,268 @@
 
 namespace Syscodes\Console\Input;
 
+use Syscodes\Support\Str;
+use Syscodes\Contracts\Console\InputOption as InputOptionInterface;
+
 /**
+ * This class represents a command line option.
+ * 
  * @author Alexander Campo <jalexcam@gmail.com>
  */
+class InputOption implements InputOptionInterface
+{
+    /**
+     * The default value.
+     * 
+     * @var mixed $default
+     */
+    protected $default;
+
+    /**
+     * The option description.
+     * 
+     * @var string $description
+     */
+    protected $description;
+
+    /**
+     * The option mode.
+     * 
+     * @var int $mode
+     */
+    protected $mode;
+
+    /**
+     * The option name.
+     * 
+     * @var string $name
+     */
+    protected $name;
+
+    /**
+     * The Shortcut of the option.
+     * 
+     * @var string $shortcut
+     */
+    protected $shortcut;
+
+    /**
+     * Constructor. Create a new InputOption instance.
+     * 
+     * @param  string  $name  The argument name
+     * @param  string|array|null  $shortcut  The shortcut of the option
+     * @param  int|null  $mode  The argument mode
+     * @param  string|null  $description  The description text
+     * @param  mixed  $default  The default value
+     * 
+     * @return void
+     * 
+     * @throws \InvalidArgumentException  If option mode is invalid or incompatible
+     */
+    public function __construct(
+        string $name, 
+        $shortcut = null,
+        int $mode = null,
+        string $description = null,
+        $default = null
+    ) {
+        if (Str::startsWith($name, '--')) {
+            $name = substr($name, 2);
+        }
+
+        if (empty($name)) {
+            throw new InvalidArgumentException('An option name cannot be empty');
+        }
+
+        if (empty($shortcut)) {
+            $shortcut = null;
+        }
+
+        if (null !== $shortcut) {
+            if (\is_array($shortcut)) {
+                $shortcut = \implode('|', $shortcut);
+            }
+    
+            $shortcuts = \preg_split('{(\|)-?}', \ltrim($shortcut, '-'));
+            $shortcuts = \array_filter($shortcuts);
+            $shortcut  = \implode('|', $shortcuts);
+            
+            if (empty($shortcut)) {
+                throw new InvalidArgumentException('An option shortcut cannot be empty');
+            }
+        }
+        
+        if (null === $mode) {
+            $mode = self::VALUE_NONE;
+        } elseif ($mode >= (self::VALUE_NEGATABLE << 1) || $mode < 1) {
+            throw new InvalidArgumentException(sprintf('Option mode "%s" is not valid.', $mode));
+        }
+
+        $this->name        = $name;
+        $this->shortcut    = $shortcut;
+        $this->mode        = $mode;
+        $this->description = $description;
+
+        $this->setDefault($default);        
+    }
+    
+    /**
+     * Gets the description text.
+     * 
+     * @return string  The description text
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * Sets the description text.
+     * 
+     * @param  string  $description  The description text
+     * 
+     * @return void
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Gets the default value.
+     * 
+     * @return mixed
+     */
+    public function getDefault()
+    {
+        return $this->default;
+    }
+    
+    /**
+     * Sets the default value.
+     * 
+     * @param  mixed  $default
+     * 
+     * @return mixed
+     * 
+     * @throws \LogicException
+     */
+    public function setDefault($default = null): void
+    {
+        if (InputOptionInterface::VALUE_NONE === (self::VALUE_NONE & $this->mode) && null !== $default) {
+            throw new LogicException('Cannot set a default value when using InputOptionInterface::VALUE_NONE mode');
+        }
+
+        if ($this->isArray()) {
+            if (null === $default) {
+                $default = [];
+            } elseif ( ! \is_array($default)) {
+                throw new LogicException('Should get a default value for an array argument');
+            }
+        }
+
+        $this->default = $this->acceptValue() || $this->isNegatable() ? $default : false;
+    }
+
+    /**
+     * Gets the option name.
+     * 
+     * @return string The name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the option name.
+     * 
+     * @param  string  $name
+     * 
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Gets the option shortcut.
+     * 
+     * @return string|null  The shortcut
+     */
+    public function getShortcut(): string
+    {
+        return $this->shortcut;
+    }
+
+    /**
+     * Sets the option shortcut.
+     * 
+     * @param  string|array|null  $shortcut
+     * 
+     * @return void
+     */
+    public function setShortcut($shortcut): void
+    {
+        if (\is_array($shortcut)) {
+            $shortcut = \implode('|', $shortcut);
+        }
+
+        $shortcuts = \preg_split('{(\|)-?}', \ltrim($shortcut, '-'));
+        $shortcuts = \array_filter($shortcuts);
+        $shortcuts = \implode('|', $shortcuts);
+
+        $this->shortcut = $shortcuts;
+    }
+    
+    /**
+     * Gets true if the option accepts a value.
+     * 
+     * @return bool  True if value mode is not self::VALUE_NONE, false otherwise
+     */
+    public function acceptValue(): bool
+    {
+        return $this->isValueRequired() || $this->isValueOptional();
+    }
+
+    /**
+     * Gets true if the option requires a value.
+     *
+     * @return bool  True if value mode is self::VALUE_REQUIRED, false otherwise
+     */
+    public function isValueRequired(): bool
+    {
+        return self::VALUE_REQUIRED === (self::VALUE_REQUIRED & $this->mode);
+    }
+    
+    /**
+     * Gets true if the option takes an optional value.
+     *
+     * @return bool  True if value mode is self::VALUE_OPTIONAL, false otherwise
+     */
+    public function isValueOptional(): bool
+    {
+        return self::VALUE_OPTIONAL === (self::VALUE_OPTIONAL & $this->mode);
+    }
+    
+    /**
+     * Gets true if the option can take multiple values.
+     * 
+     * @return bool  True if mode is self::VALUE_IS_ARRAY, false otherwise
+     */
+    public function isArray(): bool
+    {
+        return self::VALUE_IS_ARRAY === (self::VALUE_IS_ARRAY & $this->mode);
+    }
+
+    /**
+     * Gets an option have either positive or negative value.
+     * 
+     * @return bool  True if mode is self::VALUE_NEGATABLE, false otherwise
+     */
+    public function isNegatable(): bool
+    {
+        return self::VALUE_NEGATABLE === (self::VALUE_NEGATABLE & $this->mode);
+    }
+}
