18340686bd7f32d198e0cbab39be40636961696c
Alexander Campo <jalexcam@gmail.com>
2021-09-21 02:35:21 -0500

    Create SingleList.php

---
+++ syscodes/src/components/Console/Helper/SingleList.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Console\Helper;
+
+use Syscodes\Support\Str;
+use Syscodes\Console\Style\ColorTag;
+use Syscodes\Console\Util\FormatUtil;
+use Syscodes\Contracts\Console\Output as OutputInterface;
+
+/**
+ * Format and render to a single list of items.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+class SingleList
+{
+    /**
+     * Displays the list of data with options.
+     * 
+     * @param  mixed  $data  The list of data
+     * @param  string  $title  The title of list
+     * @param  array  $options  The options for list of data
+     * @param  \Syscodes\Contracts\Console\Output  $output  The output interface implemented
+     * 
+     * @return int|string
+     */
+    public static function show($data, string $title = 'Information', array $options = [], OutputInterface $output)
+    {
+        $string = '';
+        
+        $options = array_merge([
+            'leftChar'     => '  ', 
+            //'sepChar'      => '  ',
+            'keyStyle'     => 'info',
+            'titleStyle'   => 'hiGreen',
+            'ucFirst'      => false,
+            'returned'     => false,
+            'ucTitleWords' => true,
+        ], $options);
+        
+        // title
+        if ($title) {
+            $title   = $options['ucTitleWords'] ? Str::title(trim($title)) : $title;
+            $string .= ColorTag::wrap($title, $options['titleStyle']).PHP_EOL;
+        }
+        
+        // Handle item list
+        $string .= FormatUtil::spliceKeyValue((array)$data, $options);
+        
+        // Return formatted string
+        if ($options['returned']) {
+            return $string;
+        }
+        
+        return $output->writeln($string);
+    }
+}
