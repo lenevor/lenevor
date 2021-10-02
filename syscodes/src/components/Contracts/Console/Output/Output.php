512565513599933d01f65f97ef1195fb22099c5e
Alexander Campo <jalexcam@gmail.com>
2021-10-01 22:02:31 -0500

    Update Output.php

--- syscodes/src/components/Contracts/Console/Output/Output.php
+++ syscodes/src/components/Contracts/Console/Output/Output.php
@@ -132,7 +132,7 @@ interface Output
 	public function write($messages, bool $newline = false, int $options = 0);
 
     /**
-	 * Writes a message to the output and adds a newline at the end..
+	 * Writes a message to the output and adds a newline at the end.
 	 * 
 	 * @param  string|iterable  $messages  The message as an iterable of strings or a single string
 	 * @param  int  $options  A bitmask of options (0 is considered the same as self::OUTPUT_NORMAL)
