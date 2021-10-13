f1ba8022dbc1d1819df4352bf9f385e400a1996d
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:13:49 -0500

    Changed namespace

--- syscodes/src/components/Contracts/Cache/Key.php
+++ syscodes/src/components/Contracts/Cache/Key.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Contracts\Cache;
+namespace Syscodes\Components\Contracts\Cache;
 
 /**
  * Returns a key name for the cache.
