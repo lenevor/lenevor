b06a51d1a6053a5128d6fe198a33224b1ad6bc09
Alexander Campo <jalexcam@gmail.com>
2021-10-13 01:24:52 -0500

    Changed namespace

--- syscodes/src/components/Autoloader/Autoload.php
+++ syscodes/src/components/Autoloader/Autoload.php
@@ -20,9 +20,9 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes;
+namespace Syscodes\Components\Autoloader;
 
-use Syscodes\Config\AutoloadConfig;
+use Syscodes\Components\Config\AutoloadConfig;
 
 /**
  * Autoload.
