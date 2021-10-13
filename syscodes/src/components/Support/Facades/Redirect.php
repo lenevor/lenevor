e9c3abe912e3f5cd6a110c6d64493810e0b190ae
Alexander Campo <jalexcam@gmail.com>
2021-10-13 05:00:43 -0500

    Changed namespaces

--- syscodes/src/components/Support/Facades/Redirect.php
+++ syscodes/src/components/Support/Facades/Redirect.php
@@ -20,20 +20,20 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Support\Facades;
+namespace Syscodes\Components\Support\Facades;
 
 /**
  * Initialize the Redirect class facade.
  *
  * @author Alexander Campo <jalexcam@gmail.com>
  * 
- * @method static \Syscodes\Http\RedirectResponse back(int $status = 302, array $headers = [], bool $fallback = false)
- * @method static \Syscodes\Http\RedirectResponse refresh(int $status = 302, array $headers = [])
- * @method static \Syscodes\Http\RedirectResponse to(string $path, int $status = 302, array $headers = [], bool $secure = null)
- * @method static \Syscodes\Http\RedirectResponse away(string $path, int $status = 302, array $headers = [])
- * @method static \Syscodes\Http\RedirectResponse secure(string $path, int $status = 302, array $headers = [])
+ * @method static \Syscodes\Components\Http\RedirectResponse back(int $status = 302, array $headers = [], bool $fallback = false)
+ * @method static \Syscodes\Components\Http\RedirectResponse refresh(int $status = 302, array $headers = [])
+ * @method static \Syscodes\Components\Http\RedirectResponse to(string $path, int $status = 302, array $headers = [], bool $secure = null)
+ * @method static \Syscodes\Components\Http\RedirectResponse away(string $path, int $status = 302, array $headers = [])
+ * @method static \Syscodes\Components\Http\RedirectResponse secure(string $path, int $status = 302, array $headers = [])
  * 
- * @see \Syscodes\Routing\Redirector
+ * @see \Syscodes\Components\Routing\Redirector
  */
 class Redirect extends Facade
 {
