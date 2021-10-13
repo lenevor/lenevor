5cfec8aca3782b460fc341393cd318fce9367f2f
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:29:49 -0500

    Changed namespace

--- syscodes/src/components/Debug/Exceptions/Handlers/CallbackHandler.php
+++ syscodes/src/components/Debug/Exceptions/Handlers/CallbackHandler.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Debug\Handlers;
+namespace Syscodes\Components\Debug\Handlers;
 
 use InvalidArgumentException;
 
