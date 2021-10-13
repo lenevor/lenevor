15c0bb29ec4bd77c7c26fde36b384104023e7db6
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:38:16 -0500

    Changed namespaces

--- syscodes/src/components/Events/EventServiceProvider.php
+++ syscodes/src/components/Events/EventServiceProvider.php
@@ -20,9 +20,9 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Events;
+namespace Syscodes\Components\Events;
 
-use Syscodes\Support\ServiceProvider;
+use Syscodes\Components\Support\ServiceProvider;
 
 /**
  * For loading the classes from the container of services.
