28bd2b4964788523ae3d877a46b0b38ba74fc429
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:42:20 -0500

    Changed namespaces

--- syscodes/src/components/Http/Exceptions/RequestErrorException.php
+++ syscodes/src/components/Http/Exceptions/RequestErrorException.php
@@ -20,9 +20,9 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Http\Exceptions;
+namespace Syscodes\Components\Http\Exceptions;
 
-use Syscodes\Core\Http\Exceptions\LenevorException;
+use Syscodes\Components\Core\Http\Exceptions\LenevorException;
 
 /**
  * RequestErrorException.
