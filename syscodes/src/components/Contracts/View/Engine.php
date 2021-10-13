3e512904adb42eb1bdf46bc2441c6d2be73a51e2
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:19:16 -0500

    Changed namespace

--- syscodes/src/components/Contracts/View/Engine.php
+++ syscodes/src/components/Contracts/View/Engine.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Contracts\View;
+namespace Syscodes\Components\Contracts\View;
 
 /**
  * Checks the contents of a view.
