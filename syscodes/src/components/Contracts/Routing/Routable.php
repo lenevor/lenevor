8bb45d290b4d28cd814848b4596a0414b181bb42
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:17:37 -0500

    Changed namespace

--- syscodes/src/components/Contracts/Routing/Routable.php
+++ syscodes/src/components/Contracts/Routing/Routable.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Contracts\Routing;
+namespace Syscodes\Components\Contracts\Routing;
 
 /**
  * All Lenevor routes are defined in your route files, which are located in the routes 
