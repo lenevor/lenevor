77e4b37d359c336c028e3778634d3a7a64e03641
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:14:16 -0500

    Changed namespace

--- syscodes/src/components/Contracts/Cache/Store.php
+++ syscodes/src/components/Contracts/Cache/Store.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Contracts\Cache;
+namespace Syscodes\Components\Contracts\Cache;
 
 /**
  * Sets functions by the item from the cache store.
