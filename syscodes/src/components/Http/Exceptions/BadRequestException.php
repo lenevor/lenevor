728f4d98d83e0d35df1863bafba1cb7d03cb7b33
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:41:15 -0500

    Changed namespace

--- syscodes/src/components/Http/Exceptions/BadRequestException.php
+++ syscodes/src/components/Http/Exceptions/BadRequestException.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Http\Exceptions;
+namespace Syscodes\Components\Http\Exceptions;
 
 use UnexpectedValueException;
 
