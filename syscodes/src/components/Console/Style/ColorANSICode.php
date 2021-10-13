45398d3a8a17dc378342208d828020e89d9f4394
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:12:46 -0500

    Changed namespace

--- syscodes/src/components/Console/Style/ColorANSICode.php
+++ syscodes/src/components/Console/Style/ColorANSICode.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Console\Style;
+namespace Syscodes\Components\Console\Style;
 
 /**
  * The ANSI code for use the colors and options format on CLI command. 
