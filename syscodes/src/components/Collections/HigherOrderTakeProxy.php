8c8977c28319f9e6efa49dd56ba13cb2de6288c9
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:03:08 -0500

    Changed namespace

--- syscodes/src/components/Collections/HigherOrderTakeProxy.php
+++ syscodes/src/components/Collections/HigherOrderTakeProxy.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Collections;
+namespace Syscodes\Components\Collections;
 
 /**
  * Allows a new take proxy instance.
