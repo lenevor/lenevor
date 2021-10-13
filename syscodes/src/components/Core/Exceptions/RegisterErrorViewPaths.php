be9437911262a3f83b1fe425bde661164d5b88b9
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:22:50 -0500

    Changed namespaces

--- syscodes/src/components/Core/Exceptions/RegisterErrorViewPaths.php
+++ syscodes/src/components/Core/Exceptions/RegisterErrorViewPaths.php
@@ -20,9 +20,9 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Core\Exceptions;
+namespace Syscodes\Components\Core\Exceptions;
 
-use Syscodes\Support\Facades\View;
+use Syscodes\Components\Support\Facades\View;
 
 /**
  * Allows register the errors in view paths.
