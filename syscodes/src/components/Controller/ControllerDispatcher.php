5d6b282bb2baaa3e44e9ff0bdf9d02867de7ded8
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:20:35 -0500

    Changed namespaces

--- syscodes/src/components/Controller/ControllerDispatcher.php
+++ syscodes/src/components/Controller/ControllerDispatcher.php
@@ -20,12 +20,12 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
  
-namespace Syscodes\Controller;
+namespace Syscodes\Components\Controller;
 
-use Syscodes\Routing\Route;
-use Syscodes\Contracts\Container\Container;
-use Syscodes\Routing\Concerns\RouteDependencyResolver;
-use Syscodes\Controller\Contracts\ControllerDispatcher as ControllerDispatcherContract;
+use Syscodes\Components\Routing\Route;
+use Syscodes\Components\Contracts\Container\Container;
+use Syscodes\Components\Routing\Concerns\RouteDependencyResolver;
+use Syscodes\Components\Controller\Contracts\ControllerDispatcher as ControllerDispatcherContract;
 
 /**
  * Dispatch requests when called a given controller and method.
