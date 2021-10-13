ecf8c67935bac5478c09676401786955df3e90b8
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:16:22 -0500

    Changed namespace

--- syscodes/src/components/Contracts/Dotenv/Adapter.php
+++ syscodes/src/components/Contracts/Dotenv/Adapter.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Contracts\Dotenv; 
+namespace Syscodes\Components\Contracts\Dotenv; 
 
 /**
  * Read, write and delete an environment variable.
