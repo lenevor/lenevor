7ab974f46f6a8e563f438054ea715d7a75d5663b
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:13:15 -0500

    Changed namespaces

--- syscodes/src/components/Console/Util/Table.php
+++ syscodes/src/components/Console/Util/Table.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Console\Util;
+namespace Syscodes\Components\Console\Util;
 
 /**
  * @author Alexander Campo <jalexcam@gmail.com>
