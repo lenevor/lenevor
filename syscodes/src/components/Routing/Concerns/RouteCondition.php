a0e12afb786d475218ce628682fcbe50668b17bf
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:45:16 -0500

    Changed namespace

--- syscodes/src/components/Routing/Concerns/RouteCondition.php
+++ syscodes/src/components/Routing/Concerns/RouteCondition.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Routing\Concerns;
+namespace Syscodes\Components\Routing\Concerns;
 
 /**
  * Describe the parameters of the routes according to the user condition 
