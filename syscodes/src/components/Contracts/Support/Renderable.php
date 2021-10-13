a9dea2c8eb01d0d1119b3b14666145a32b4a367e
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:18:39 -0500

    Changed namespace

--- syscodes/src/components/Contracts/Support/Renderable.php
+++ syscodes/src/components/Contracts/Support/Renderable.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Contracts\Support;
+namespace Syscodes\Components\Contracts\Support;
 
 /**
  * Gets a string Renderable.
