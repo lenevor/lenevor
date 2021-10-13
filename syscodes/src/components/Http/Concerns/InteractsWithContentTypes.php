ebd182b1d83962312e5a2af700352f39ce88817f
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:39:57 -0500

    Changed namespaces

--- syscodes/src/components/Http/Concerns/InteractsWithContentTypes.php
+++ syscodes/src/components/Http/Concerns/InteractsWithContentTypes.php
@@ -20,9 +20,9 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Http\Concerns;
+namespace Syscodes\Components\Http\Concerns;
 
-use Syscodes\Support\Str;
+use Syscodes\Components\Support\Str;
 
 /**
  * Trait InteractsWithContentTypes.
