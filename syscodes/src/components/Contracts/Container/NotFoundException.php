be16fa0432e5dcdafeb3979c45e41e4736b8dd69
Alexander Campo <jalexcam@gmail.com>
2021-10-13 04:15:24 -0500

    Changed namespace

--- syscodes/src/components/Contracts/Container/NotFoundException.php
+++ syscodes/src/components/Contracts/Container/NotFoundException.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Contracts\Container;
+namespace Syscodes\Components\Contracts\Container;
 
 use InvalidArgumentException;
 use Psr\Container\NotFoundExceptionInterface;
