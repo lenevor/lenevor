759235677eb84259ea3acf686f1e3e1ac07ecc5c
Alexander Campo <jalexcam@gmail.com>
2021-10-25 02:52:32 -0500

    Spacing

--- syscodes/src/components/View/ViewName.php
+++ syscodes/src/components/View/ViewName.php
@@ -41,13 +41,13 @@ class ViewName
     public static function normalize($name)
     {
         $delimiter = ViewFinder::HINT_PATH_DELIMITER;
-		
-		if (strpos($name, $delimiter) === false) {
-			return str_replace('/', '.', $name);
-		}
-		
-		[$namespace, $name] = explode($delimiter, $name);
-		
-		return $namespace.$delimiter.str_replace('/', '.', $name);
+        
+        if (strpos($name, $delimiter) === false) {
+            return str_replace('/', '.', $name);
+        }
+        
+        [$namespace, $name] = explode($delimiter, $name);
+        
+        return $namespace.$delimiter.str_replace('/', '.', $name);
     }
 }
