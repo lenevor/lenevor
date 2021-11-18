0fba3bd1591a7a77a9c53f453b31f7b6f38809ef
Alexander Campo <jalexcam@gmail.com>
2021-11-18 06:20:21 -0500

    Changed namespace

--- syscodes/src/components/Database/Connectors/SqlServerConnector.php
+++ syscodes/src/components/Database/Connectors/SqlServerConnector.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Database\Connectors;
+namespace Syscodes\Components\Database\Connectors;
 
 use PDO;
 
