48f3d03fc52fd998b7ae03169bdea5e2ecab8fa1
Alexander Campo <jalexcam@gmail.com>
2021-11-18 06:23:24 -0500

    Changed namespace

--- syscodes/src/components/Database/Exceptions/ConnectionException.php
+++ syscodes/src/components/Database/Exceptions/ConnectionException.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Database\Exceptions;
+namespace Syscodes\Components\Database\Exceptions;
 
 use Exception;
 use PDOException;
