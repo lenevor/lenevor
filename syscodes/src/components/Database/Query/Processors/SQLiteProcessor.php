74e740b5880d392ed65130b32a5cd3fcb617f5fd
Alexander Campo <jalexcam@gmail.com>
2021-11-18 06:29:50 -0500

    Changed namespace

--- syscodes/src/components/Database/Query/Processors/SQLiteProcessor.php
+++ syscodes/src/components/Database/Query/Processors/SQLiteProcessor.php
@@ -20,9 +20,9 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
  
-namespace Syscode\Database\Query\Processors;
+namespace Syscodes\Components\Database\Query\Processors;
 
-use Syscode\Database\Query\Processor;
+use Syscodes\Components\Database\Query\Processor;
 
 /**
  * Allows show the results of a column listing query for SQLite Database.
