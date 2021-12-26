7993d544fe2940b0e356689d3a1d52d877847e70
Alexander Campo <jalexcam@gmail.com>
2021-12-25 21:34:38 -0500

    Added new methods

--- syscodes/src/components/Contracts/View/Factory.php
+++ syscodes/src/components/Contracts/View/Factory.php
@@ -22,6 +22,8 @@
 
 namespace Syscodes\Components\Contracts\View;
 
+use Syscodes\Components\View\View;
+
 /**
  * Gets the contents of a view.
  * 
@@ -36,15 +38,97 @@ interface Factory
 	 *
 	 * @return bool
 	 */
-    public function viewExists($view);
+    public function viewExists($view): bool;
 
     /**
-     * Get the contents of a view.
-     * 
-     * @param  string  $view
-     * @param  array  $data
-     * 
-     * @return \Syscodes\Components\Contracts\View\View
+     * Global and local data are merged and extracted to create local variables within the view file.
+	 * Renders the view object to a string.
+	 *
+	 * @example $output = $view->make();
+	 *
+	 * @param  string  $view  View filename
+	 * @param  array  $data  Array of values
+	 *
+	 * @return \Syscodes\Components\Contracts\View\View
      */
     public function make($view, $data = []);
+
+    /**
+	 * Call the creator for a given view.
+	 * 
+	 * @param  \Syscodes\View\View  $view
+	 * 
+	 * @return void
+	 */
+	public function callCreator(View $view): void;
+
+    /**
+	 * Get the extension to engine bindings.
+	 * 
+	 * @return array
+	 */
+	public function getExtensions(): array;
+
+
+    /**
+	 * Add a piece of shared data to the environment.
+	 * 
+	 * @param  array|string  $key
+	 * @param  mixed|null  $value  
+	 * 
+	 * @return mixed
+	 */
+	public function share($key, $value = null);
+
+    /**
+	 * Replace the namespace hints for the given namespace.
+	 * 
+	 * @param  string  $namespace
+	 * @param  string|array  $hints
+	 * 
+	 * @return self
+	 */
+	public function replaceNamespace($namespace, $hints): self;
+
+    /**
+	 * Increment the rendering counter.
+	 * 
+	 * @return int
+	 */
+	public function increment(): int;
+
+    /**
+	 * Decrement the rendering counter.
+	 * 
+	 * @return int
+	 */
+	public function decrement(): int;
+
+    /**
+	 * Check if there are no active render operations.
+	 * 
+	 * @return bool
+	 */
+	public function doneRendering(): bool;
+
+    /**
+	 * Flush all of the parser state like sections.
+	 * 
+	 * @return void
+	 */
+	public function flushState(): void;
+
+    /**
+	 * Flush all of the section contents if done rendering.
+	 * 
+	 * @return void
+	 */
+	public function flushStateIfDoneRendering(): void;
+
+    /**
+	 * Get all of the shared data for the environment.
+	 * 
+	 * @return array
+	 */
+	public function getShared(): array;
 }
