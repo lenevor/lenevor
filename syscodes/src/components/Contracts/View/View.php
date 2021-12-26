a9d503f52f8286f78a9d2a5ffd05f06ad3fe9081
Alexander Campo <jalexcam@gmail.com>
2021-12-25 21:41:22 -0500

    Added new methods

--- syscodes/src/components/Contracts/View/View.php
+++ syscodes/src/components/Contracts/View/View.php
@@ -32,6 +32,22 @@ use Syscodes\Components\Contracts\Support\Renderable;
 interface View extends Renderable
 {
 	/**
+	 * The view data will be extracted.
+	 * 
+	 * @return array
+	 */
+	public function getArrayData(): array;
+
+	/**
+	 * Get the sections of the rendered view.
+	 * 
+	 * @return array
+	 * 
+	 * @throws \Throwable
+	 */
+	public function renderSections();
+
+	/**
 	 * Add a piece of data to the view.
 	 * 
 	 * @example  $view->assign($content, $data);
@@ -39,9 +55,9 @@ interface View extends Renderable
 	 * @param  string|array  $key
 	 * @param  mixed  $value
 	 * 
-	 * @return $this
+	 * @return self
 	 */
-	public function assign($key, $value = null);
+	public function assign($key, $value = null): self;
 	
 	/**
 	 * Assigns a value by reference. The benefit of binding is that values can be altered
@@ -53,7 +69,60 @@ interface View extends Renderable
 	 * @param  string  $key  Variable name
 	 * @param  mixed  $value  Referenced variable
 	 * 
-	 * @return $this
+	 * @return self
 	 */
-	public function bind($key, & $value);
+	public function bind($key, & $value): self;
+
+	/**
+	 * Get the array of view data.
+	 * 
+	 * @return array
+	 */
+	public function getData(): array;
+
+	/**
+	 * Get the name of the view.
+	 * 
+	 * @return string
+	 */
+	public function getView(): string;
+
+	/**
+	 * Get the path to the view file.
+	 * 
+	 * @return string
+	 */
+	public function getPath(): string;
+
+	/**
+	 * Set the path to the view file.
+	 * 
+	 * @param  string  $path
+	 * 
+	 * @return void
+	 */
+	public function setPath($path): void;
+
+	/**
+	 * Assigns a variable by name. Assigned values will be available as a
+	 * variable within the view file:
+	 *
+	 * This value can be accessed as $var within the view
+	 * @example $view->set(array('food' => 'bread', 'beverage' => 'water'));
+	 *
+	 * @param  string|array  $key    Variable name
+	 * @param  mixed         $value  Value
+	 *
+	 * @return self
+	 *
+	 * @uses   instanceof \Traversable
+	 */
+	public function set($key, $value = null): self;
+
+	/**
+	 * Get content as a string of HTML.
+	 * 
+	 * @return string
+	 */
+	public function toHtml();
 }
