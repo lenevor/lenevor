7c5fff0ad703416e07afac061d3f686874314e34
Alexander Campo <jalexcam@gmail.com>
2021-12-28 00:25:37 -0500

    Changed namespace

--- syscodes/src/components/Database/Capsule/Manager.php
+++ syscodes/src/components/Database/Capsule/Manager.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Components\Database\Elusen\Capsule;
+namespace Syscodes\Components\Database\Capsule;
 
 /**
  * Capsules the manager database.
