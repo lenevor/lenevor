c0adfdec616f0a1c8828b45cb998d57d49445ad9
Alexander Campo <jalexcam@gmail.com>
2022-01-01 17:24:21 -0500

    Create CapsuleManager.php

---
+++ syscodes/src/components/Database/Concerns/CapsuleManager.php
@@ -0,0 +1,96 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2021 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Database\Concerns;
+
+use Syscodes\Components\Support\Flowing;
+use Syscodes\Components\Contracts\Container\Container;
+
+/**
+ * Trait CapsuleManager.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+trait CapsuleManager
+{
+    /**
+     * The current globally used instance.
+     * 
+     * @var object $instance
+     */
+    protected static $instance;
+    
+    /**
+     * The container instance.
+     * 
+     * @var \Syscodes\Components\Container\Container $container
+     */
+    protected $container;
+
+    /**
+     * Get the container instance.
+     * 
+     * @param  \Syscodes\Components\Container\Container  $container
+     * 
+     * @return void
+     */
+    protected function getContainerManager(Container $container)
+    {
+        $this->container = $container;
+
+        if ( ! $this->container->bound('config')) {
+            $this->container->instance('config', new Flowing);
+        }
+    }
+
+    /**
+     * Set this capsule instance available global.
+     * 
+     * @return void
+     */
+    public function setCapsuleGlobal()
+    {
+        static::$instance = $this;
+    }
+
+    /**
+	 * Get the container instance.
+	 * 
+	 * @return  \Syscodes\Components\Container\Container
+	 */
+	public function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
+	 * Set the container instance.
+	 * 
+	 * @param  \Syscodes\Components\Container\Container  $container
+	 * 
+	 * @return void
+	 */
+	public function setContainer(Container $container): void
+	{
+		$this->container = $container;
+	}    
+}
