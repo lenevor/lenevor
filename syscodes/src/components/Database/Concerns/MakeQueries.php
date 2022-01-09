7ddeac20818943f653f0b80f357d75d87a16b50d
Alexander Campo <jalexcam@gmail.com>
2022-01-08 23:57:16 -0500

    Create MakeQueries.php

---
+++ syscodes/src/components/Database/Concerns/MakeQueries.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Database\Concerns;
+
+/**
+ * Trait MakeQueries.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+trait MakeQueries
+{
+    /**
+     * Execute the query and get the first result.
+     * 
+     * @param  array|string  $column
+     * 
+     * @return \Syscodes\Components\Database\Eloquent\Model|object|static|null
+     */
+    public function first($column = ['*'])
+    {
+        return $this->limit(1)->first($column);
+    }
+}
