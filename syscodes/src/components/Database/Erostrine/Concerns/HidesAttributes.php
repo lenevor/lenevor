6bc0bb0f997132eec0cc3a459544cae3799df2e8
Alexander Campo <jalexcam@gmail.com>
2022-01-22 00:58:17 -0500

    Create HidesAttributes.php

---
+++ syscodes/src/components/Database/Erostrine/Concerns/HidesAttributes.php
@@ -0,0 +1,93 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Database\Erostrine\Concerns;
+
+/**
+ * Trait HidesAttributes.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+trait HidesAttributes
+{
+    /**
+     * The attributes that should be hidden for arrays.
+     * 
+     * @var array $hidden
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be visible for arrays.
+     * 
+     * @var array $visible
+     */
+    protected $visible = [];
+
+    /**
+     * Get the hidden attributes for the model.
+     * 
+     * @return array
+     */
+    public function getHidden(): array
+    {
+        return $this->hidden;
+    }
+
+    /**
+     * Set the hidden attributes for the model.
+     * 
+     * @param  array  $hidden
+     * 
+     * @return self
+     */
+    public function setHidden(array $hidden): self
+    {
+        $this->hidden = $hidden;
+
+        return $this;
+    }
+
+    /**
+     * Get the visible attributes for the model.
+     * 
+     * @return array
+     */
+    public function getVisible(): array
+    {
+        return $this->visible;
+    }
+
+    /**
+     * Set the visible attributes for the model.
+     * 
+     * @param  array  $visible
+     * 
+     * @return self
+     */
+    public function setVisible(array $visible): self
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+}
