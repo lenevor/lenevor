4ee15196faa6dcece773572ccab7d05823a7c489
Alexander Campo <jalexcam@gmail.com>
2022-01-25 00:00:16 -0500

    Update Macroable.php

--- syscodes/src/components/Macroable/Traits/Macroable.php
+++ syscodes/src/components/Macroable/Traits/Macroable.php
@@ -22,6 +22,11 @@
 
 namespace Syscodes\Components\Support\Traits;
 
+use Closure;
+use ReflectionClass;
+use ReflectionMethod;
+use BadMethodCallException;
+
 /**
  * Trait Macroable.
  * 
@@ -29,5 +34,116 @@ namespace Syscodes\Components\Support\Traits;
  */
 trait Macroable
 {
+    /**
+     * The registered string macros.
+     * 
+     * @var array $macros
+     */
+    protected static $macros = [];
+    
+    /**
+     * Register a custom macro.
+     * 
+     * @param  string  $name
+     * @param  object|callable  $macro
+     * 
+     * @return void
+     */
+    public static function macro(string $name, $macro): void
+    {
+        static::$macros[$name] = $macro;
+    }
 
+    /**
+     * Checks if macro is registered.
+     * 
+     * @param  string  $name
+     * 
+     * @return bool
+     */
+    public static function hasMacro(string $name): bool
+    {
+        return isset(static::$macros[$name]);
+    }
+
+    /**
+     * Mix another object into the class.
+     * 
+     * @param  object|string  $mixin
+     * @param  bool  $bool
+     * 
+     * @return void
+     */
+    public static function mixin($mixin, bool $bool = true): void
+    {
+        $methods = (new ReflectionClass($mixin))->getMethods(
+            ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED
+        );
+
+        foreach ($methods as $method) {
+            if ($bool || ! static::hasMacro($method)) {
+                $method->setAccesible(true);
+
+                static::macro($method->name, $method->invoke($mixin));
+            }
+        }
+    }
+
+    /**
+     * Magic method.
+     * 
+     * Dynamically handle calls to the class.
+     * 
+     * @param  string  $method
+     * @param  array  $parameters
+     * 
+     * @return mixed
+     * 
+     * @throws \BadMethodCallException
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        if ( ! static::hasMacro($method)) {
+            throw new BadMethodCallException(sprintf(
+                'Method %s::%s() does not exist', static::class, $method 
+            ));
+        }
+
+        $macro = static::$macros[$method];
+
+        if ($macro instanceof Closure) {
+            $macro = $macro->bindTo(null, static::class);
+        }
+
+        return call_user_func_array($macro, $parameters);
+    }
+
+    /**
+     * Magic method.
+     * 
+     * Dynamically handle calls to the class.
+     * 
+     * @param  string  $method
+     * @param  array  $parameters
+     * 
+     * @return mixed
+     * 
+     * @throws \BadMethodCallException
+     */
+    public function __call($method, $parameters)
+    {
+        if ( ! static::hasMacro($method)) {
+            throw new BadMethodCallException(sprintf(
+                'Method %s::%s() does not exist', static::class, $method 
+            ));
+        }
+
+        $macro = static::$macros[$method];
+
+        if ($macro instanceof Closure) {
+            $macro = $macro->bindTo($this, static::class);
+        }
+
+        return call_user_func_array($macro, $parameters);
+    }
 }
