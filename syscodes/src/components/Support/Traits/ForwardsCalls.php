7b623d016073d1ffb8d585cdfc02bc6995f6b6b0
Alexander Campo <jalexcam@gmail.com>
2022-01-26 00:56:32 -0500

    Create ForwardsCalls.php

---
+++ syscodes/src/components/Support/Traits/ForwardsCalls.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Support\Traits;
+
+use Error;
+use BadMethodCallException;
+
+/**
+ * Trait ForwardsCalls.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+trait ForwardsCalls
+{
+    /**
+     * Forward a method call to the given object.
+     * 
+     * @param  mixed  $object
+     * @param  string  $method
+     * @param  array  $parameters
+     * 
+     * @return mixed
+     * 
+     * @throws \BadMethodCallException
+     */
+    protected function forwardCallTo($object, $method, $parameters)
+    {
+        try {
+            return $object->{$method}(...$parameters);
+        } catch (Error|BadMethodCallException $e) {
+            static::BadMethodCallException($method);
+        }
+    }
+
+    /**
+     * Throw a bad method call exception for the given method.
+     * 
+     * @param  string  $method
+     * 
+     * @return void
+     * 
+     * @throws \BadMethodCallException
+     */
+    protected static function badMethodCallException($method): void
+    {
+        throw new BadMethodCallException(sprintf(
+            'Call to undefined method %s::%s()', static::class, $method
+        ));
+    }
+}
