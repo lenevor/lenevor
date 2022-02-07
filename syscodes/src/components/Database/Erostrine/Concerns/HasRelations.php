f6e1437ff6a47b96baece322ff0140ed19bb34f6
Alexander Campo <jalexcam@gmail.com>
2022-02-06 23:56:20 -0500

    Added methods for generate relationship in the model

--- syscodes/src/components/Database/Erostrine/Concerns/HasRelations.php
+++ syscodes/src/components/Database/Erostrine/Concerns/HasRelations.php
@@ -22,6 +22,11 @@
 
 namespace Syscodes\Components\Database\Erostrine\Concerns;
 
+use Syscodes\Components\Support\Str;
+use Syscodes\Components\Database\Erostrine\Model;
+use Syscodes\Components\Database\Erostrine\Builder;
+use Syscodes\Components\Database\Erostrine\Relations\BelongsTo;
+
 /**
  * HasRelations.
  * 
@@ -39,4 +44,66 @@ trait HasRelations
      * 
      * @return \Syscodes\Components\Database\Erostrine\Relations\BelongsTo
      */
+    public function belongsTo($related, $foreignKey = null, $ownerKey = null, $relation = null)
+    {
+        if (is_null($relation)) {
+            $relation = $this->callBelongsToRelation();
+        }
+
+        $instance = $this->newRelatedInstance($related);
+
+        if (is_null($foreignKey)) {
+            $foreignKey = Str::snake($relation).'_'.$instance->getKeyName();
+        }
+
+        $ownerKey = $ownerKey ?: $instance->getKeyName();
+
+        return $this->newBelongsTo(
+            $instance->newQuery(), $this, $foreignKey, $ownerKey, $relation
+        );
+    }
+    
+    /**
+     * Instantiate a new BelongsTo relationship.
+     * 
+     * @param  \Syscodes\Components\Database\Erostrine\Builder  $builder
+     * @param  \Syscodes\Components\Database\Erostrine\Model  $child
+     * @param  string  $foreignKey
+     * @param  string  $ownerKey
+     * @param  string  $relation
+     * 
+     * @return \Syscodes\Components\Database\Erostrine\Relations\BelongsTo
+     */
+    protected function newBelongsTo(Builder $builder, Model $child, $foreignKey, $ownerKey, $relation)
+    {
+        return new BelongsTo($builder, $child, $foreignKey, $ownerKey, $relation);
+    }
+    
+    /**
+     * Calls the "belongs to" relationship name.
+     * 
+     * @return string
+     */
+    protected function callBelongsToRelation(): string
+    {
+        [$one, $two, $caller] = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3);
+        
+        return $caller['function'];
+    }
+    
+    /**
+     * Create a new model instance for a related model.
+     * 
+     * @param  string  $class
+     * 
+     * @return mixed
+     */
+    protected function newRelatedInstance($class)
+    {
+        return take(new $class, function ($instance) {
+            if ( ! $instance->getConnectionName()) {
+                $instance->setConnection($this->connection);
+            }
+        });
+    }
 }
