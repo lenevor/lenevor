1463ff6804af5f3a7598696283dad0b653798d25
Alexander Campo <jalexcam@gmail.com>
2022-02-10 08:21:49 -0500

    Changed namespace

--- syscodes/src/components/Container/Exceptions/UnknownIdentifierException.php
+++ syscodes/src/components/Container/Exceptions/UnknownIdentifierException.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Container\Exceptions;
+namespace Syscodes\Components\Container\Exceptions;
 
 use InvalidArgumentException;
 use Psr\Container\ContainerExceptionInterface;
