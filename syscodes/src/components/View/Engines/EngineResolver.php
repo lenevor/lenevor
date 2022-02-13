1cd41e1d44525da7b050f612eb9ff44c8937a2b0
Alexander Campo <jalexcam@gmail.com>
2022-02-13 02:20:44 -0500

    Removed dot in comment

--- syscodes/src/components/View/Engines/EngineResolver.php
+++ syscodes/src/components/View/Engines/EngineResolver.php
@@ -80,6 +80,6 @@ class EngineResolver
             return $this->resolved[$engine] = call_user_func($this->resolvers[$engine]);
         }
 
-        throw new InvalidArgumentException("Engine [{$engine}] not found.");
+        throw new InvalidArgumentException("Engine [{$engine}] not found");
     }
 }
