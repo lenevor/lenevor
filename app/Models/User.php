da8d569c19e1d9456c71a7393c776dfc236e4bca
Alexander Campo <jalexcam@gmail.com>
2022-02-27 00:17:52 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,5 @@ use Syscodes\Components\Database\Erostrine\Model;
 
 class User extends Model
 {
-    public function roles() 
-    {
-        return $this->hasOne(Role::class, 'role_id');
-    }
+    protected $fillable = ['name', 'email'];
 }
