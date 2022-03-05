b2956ac4d4553384229459690eab4984bf8a196a
Alexander Campo <jalexcam@gmail.com>
2022-03-04 23:56:25 -0500

    [0.7.x] Added new methods for returns items of a collection

--- syscodes/src/components/Database/Erostrine/Collection.php
+++ syscodes/src/components/Database/Erostrine/Collection.php
@@ -51,16 +51,250 @@ class Collection extends BaseCollection
             $key = $key->toArray();
         }
         
-        if (is_array($key)) {
-            if ($this->isEmpty()) {
-                return new static;
-            }
-            
-            return $this->whereIn($this->first()->getKeyName(), $key);
-        }
-        
-        return Arr::first($this->items, function($itemKey, $model) use ($key) {
+        return Arr::first($this->items, function($model) use ($key) {
             return $model->getKey() == $key;
         }, $default);
     }
+    
+    /**
+     * Load a set of relationships onto the collection.
+     * 
+     * @param  mixed  $relations
+     * 
+     * @return self
+     */
+    public function load($relations): self
+    {
+        if (count($this->items) > 0) {
+            if (is_string($relations)) $relations = func_get_args();
+            
+            $query = $this->first()->newQuery()->with($relations);
+            
+            $this->items = $query->eagerLoadRelations($this->items);
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Add an item to the collection.
+     * 
+     * @param  mixed  $item
+     * 
+     * @return self
+     */
+    public function add($item): self
+    {
+        $this->items[] = $item;
+        
+        return $this;
+    }
+    
+    /**
+     * Determine if a key exists in the collection.
+     * 
+     * @param  mixed  $key
+     * 
+     * @return bool
+     */
+    public function contains($key): bool
+    {
+        return ! is_null($this->find($key));
+    }
+    
+    /**
+     * Fetch a nested element of the collection.
+     * 
+     * @param  string  $key
+     * 
+     * @return static
+     */
+    public function fetch($key)
+    {
+        return new static(Arr::fetch($this->toArray(), $key));
+    }
+    
+    /**
+     * Get the max value of a given key.
+     * 
+     * @param  string  $key
+     *
+     * @return mixed
+     */
+    public function max($key)
+    {
+        return $this->reduce(function($result, $item) use ($key) {
+            return (is_null($result) || $item->{$key} > $result) ? $item->{$key} : $result;
+        });
+    }
+    
+    /**
+     * Get the min value of a given key.
+     * 
+     * @param  string  $key
+     * 
+     * @return mixed
+     */
+    public function min($key)
+    {
+        return $this->reduce(function($result, $item) use ($key) {
+            return (is_null($result) || $item->{$key} < $result) ? $item->{$key} : $result;
+        });
+    }
+    
+    /**
+     * Get the array of primary keys.
+     * 
+     * @return array
+     */
+    public function modelKeys(): array
+    {
+        return array_map(function($m) { return $m->getKey(); }, $this->items);
+    }
+    
+    /**
+     * Merge the collection with the given items.
+     * 
+     * @param  \ArrayAccess|array  $items
+     * 
+     * @return static
+     */
+    public function merge($items)
+    {
+        $dictionary = $this->getDictionary();
+        
+        foreach ($items as $item) {
+            $dictionary[$item->getKey()] = $item;
+        }
+        
+        return new static(array_values($dictionary));
+    }
+    
+    /**
+     * Diff the collection with the given items.
+     * 
+     * @param  \ArrayAccess|array  $items
+     * 
+     * @return static
+     */
+    public function diff($items)
+    {
+        $diff = new static;
+        
+        $dictionary = $this->getDictionary($items);
+        
+        foreach ($this->items as $item) {
+            if ( ! isset($dictionary[$item->getKey()])) {
+                $diff->add($item);
+            }
+        }
+        
+        return $diff;
+    }
+    
+    /**
+     * Intersect the collection with the given items.
+     * 
+     * @param  \ArrayAccess|array  $items
+     * 
+     * @return static
+     */
+    public function intersect($items)
+    {
+        $intersect = new static;
+        
+        $dictionary = $this->getDictionary($items);
+        
+        foreach ($this->items as $item) {
+            if (isset($dictionary[$item->getKey()])) {
+                $intersect->add($item);
+            }
+        }
+        
+        return $intersect;
+    }
+    
+    /**
+     * Return only unique items from the collection.
+     * 
+     * @return static
+     */
+    public function unique()
+    {
+        $dictionary = $this->getDictionary();
+        
+        return new static(array_values($dictionary));
+    }
+    
+    /**
+     * Returns only the models from the collection with the specified keys.
+     * 
+     * @param  mixed  $keys
+     * 
+     * @return static
+     */
+    public function only($keys)
+    {
+        $dictionary = Arr::only($this->getDictionary(), $keys);
+        
+        return new static(array_values($dictionary));
+    }
+    
+    /**
+     * Returns all models in the collection except the models with specified keys.
+     * 
+     * @param  mixed  $keys
+     * 
+     * @return static
+     */
+    public function except($keys)
+    {
+        $dictionary = Arr::except($this->getDictionary(), $keys);
+        
+        return new static(array_values($dictionary));
+    }
+    
+    /**
+     * Get a dictionary keyed by primary keys.
+     * 
+     * @param  \ArrayAccess|array  $items
+     * 
+     * @return array
+     */
+    public function getDictionary($items = null): array
+    {
+        $items = is_null($items) ? $this->items : $items;
+        
+        $dictionary = [];
+        
+        foreach ($items as $value) {
+            $dictionary[$value->getKey()] = $value;
+        }
+        
+        return $dictionary;
+    }
+    
+    /**
+     * Get an array with the values of a given key.
+     * 
+     * @param  string  $value
+     * @param  string|null  $key
+     * 
+     * @return \Syscodes\Components\Collections\Collection
+     */
+    public function pluck($value, $key = null)
+    {
+        return $this->toBase()->pluck($value, $key);
+    }
+    
+    /**
+     * Get a base Support collection instance from this collection.
+     * 
+     * @return \Syscodes\Components\Collections\Collection
+     */
+    public function toBase()
+    {
+        return new BaseCollection($this->items);
+    }
+
 }
