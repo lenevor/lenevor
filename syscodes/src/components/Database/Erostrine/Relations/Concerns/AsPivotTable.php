186ed5e5c382a37a6444843c910979214a516aae
Alexander Campo <jalexcam@gmail.com>
2022-03-20 12:31:07 -0500

    Created new trait for interacts with pivot table

--- syscodes/src/components/Database/Erostrine/Relations/Concerns/AsPivotTable.php
+++ syscodes/src/components/Database/Erostrine/Relations/Concerns/AsPivotTable.php
@@ -22,7 +22,9 @@
 
 namespace Syscodes\Components\Database\Erostrine\Relations\Concerns;
 
+use Syscodes\Components\Support\Str;
 use Syscodes\Components\Database\Erostrine\Model;
+use Syscodes\Components\Database\Erostrine\Builder;
 
 /**
  * Trait AsPivotTable.
@@ -80,5 +82,135 @@ trait AsPivotTable
         $instance->exists = $exists;
 
         return $instance;
+    }
+
+    /**
+     * Create a new pivot model from raw values.
+     * 
+     * @param  \Syscodes\Components\Database\Erostrine\Model  $model
+     * @param  array  $attributes
+     * @param  string  $table
+     * @param  bool  $exists
+     * 
+     * @return static
+     */
+    public static function fromRawAttributtes(
+        Model $parent, 
+        $attributes, 
+        $table,
+        bool $exists = false
+    ) {
+        $instance = static::fromAttributes($parent, [], $table, $exists);
+
+        $instance->setRawAttributes($attributes, true);
+
+        return $instance;
+    }
+
+    /**
+     * Set the keys for a save update query.
+     * 
+     * @param  \Syscodes\Components\Database\Erostrine\Builder  $builder
+     * 
+     * @return \Syscodes\Components\Database\Erostrine\Builder
+     */
+    public function setKeysForSaveQuery(Builder $builder)
+    {
+        if (isset($this->attributes[$this->getKeyName()])) {
+            return parent::setKeysForSaveQuery($builder);
+        }
+        
+        $builder->where($this->foreignKey, $this->getOriginal(
+            $this->foreignKey, $this->getAttribute($this->foreignKey)
+        ));
+        
+        return $builder->where($this->relatedKey, $this->getOriginal(
+            $this->relatedKey, $this->getAttribute($this->relatedKey)
+        ));
+    }
+
+    /**
+     * Delete the pivot model record from the database.
+     * 
+     * @return int
+     */
+    public function delete(): int
+    {
+        if (isset($this->attributes[$this->getKeyName()])) {
+            return (int) parent::delete();
+        }
+        
+        if ($this->fireModelEvent('deleting') === false) {
+            return 0;
+        }
+        
+        return take($this->getDeleteQuery()->delete(), function () {
+            $this->exists = false;
+            
+            $this->fireModelEvent('deleted', false);
+        });
+    }
+
+    /**
+     * Get the query builder for a delete operation on the pivot.
+     * 
+     * @return \Syscodes\Components\Database\Erostrine\Builder
+     */
+    protected function getDeleteQuery()
+    {
+        return $this->newQuery()->where([
+            $this->foreignKey => $this->getOriginal($this->foreignKey, $this->getAttribute($this->foreignKey)),
+            $this->relatedKey => $this->getOriginal($this->relatedKey, $this->getAttribute($this->relatedKey)),
+        ]);
+    }
+
+    /**
+     * Get the table associated with the model.
+     * 
+     * @return string
+     */
+    public function getTable(): string
+    {
+        $class = str_replace(
+            '\\', '', Str::snake(Str::singular(class_basename($this)))
+        );
+
+        return $this->table ?? $this->setTable($class);
+    }
+
+    /**
+     * Get the foreign key column name.
+     * 
+     * @return string
+     */
+    public function getForeignkey(): string
+    {
+        return $this->foreignKey;
+    }
+
+    /**
+     * Get the "related key" column name.
+     * 
+     * @return string
+     */
+    public function getRelatedKey(): string
+    {
+        return $this->relatedkey;
+    }
+
+    /**
+     * Set the key names for the pivot model instance.
+     * 
+     * @param  string  $foreignKey
+     * @param  string  $relatedKey
+     * 
+     * @return self
+     */
+    public function setPivotKeys($foreignKey, $relatedKey): self
+    {
+        $this->foreignKey = $foreignKey;
+        $this->relatedkey = $relatedKey;
+
+        return $this;
     }
 }
