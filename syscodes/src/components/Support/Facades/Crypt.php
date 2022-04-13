795b3eefcd43532b1b16dd33c20ce09c6a191ec8
Alexander Campo <jalexcam@gmail.com>
2022-04-13 12:10:27 -0500

    [0.7.8] Added the type output of method

--- syscodes/src/components/Support/Facades/Crypt.php
+++ syscodes/src/components/Support/Facades/Crypt.php
@@ -43,8 +43,10 @@ class Crypt extends Facade
      * Get the registered name of the component.
      * 
      * @return string
+     * 
+     * @throws \RuntimeException
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'encrypter';
     }
