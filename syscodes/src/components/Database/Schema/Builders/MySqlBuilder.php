6877d154de2153f17507eb49f7ed89e323d19aeb
Alexander Campo <jalexcam@gmail.com>
2022-04-14 10:55:25 -0500

    Added new methods for manipulates database, tables and columns

--- syscodes/src/components/Database/Schema/Builders/MySqlBuilder.php
+++ syscodes/src/components/Database/Schema/Builders/MySqlBuilder.php
@@ -30,5 +30,141 @@ namespace Syscodes\Components\Database\Schema\Builders;
  */
 class MySqlBuilder extends Builder
 {
+    /**
+     * Create a database in the schema.
+     * 
+     * @param  string  $name
+     * 
+     * @return bool
+     */
+    public function createDatabase($name): bool
+    {
+        return $this->connection->statement(
+           $this->grammar->compileCreateDatabase($name, $this->connection) 
+        );
+    }
+    
+    /**
+     * Drop a database from the schema if the database exists.
+     * 
+     * @param  string  $name
+     * 
+     * @return bool
+     */
+    public function dropDatabaseIfExists($name): bool
+    {
+        return $this->connection->statement(
+            $this->grammar->compileDropDatabaseIfExists($name)
+        );
+    }
+    
+    /**
+     * Determine if the given table exists.
+     * 
+     * @param  string  $table
+     * 
+     * @return bool
+     */
+    public function hasTable($table): bool
+    {
+        $table = $this->connection->getTablePrefix().$table;
+        
+        return count($this->connection->select(
+            $this->grammar->compileTableListing(), [$this->connection->getDatabase(), $table]
+        )) > 0;
+    }
+    
+    /**
+     * Get the column listing for a given table.
+     * 
+     * @param  string  $table
+     * 
+     * @return array
+     */
+    public function getColumnListing($table): array
+    {
+        $table = $this->connection->getTablePrefix().$table;
+        
+        $results = $this->connection->select(
+            $this->grammar->compileColumnListing(), [$this->connection->getDatabase(), $table]
+        );
+        
+        return $this->connection->getPostProcessor()->processColumnListing($results);
+    }
 
+    /**
+     * Drop all tables from the database.
+     * 
+     * @return void
+     */
+    public function dropAllTables(): void
+    {
+        $tables = [];
+        
+        foreach ($this->getAllTables() as $row) {
+            $row = (array) $row;
+            
+            $tables[] = headItem($row);
+        }
+        
+        if (empty($tables)) {
+            return;
+        }
+        
+        $this->disableForeignKeyConstraints();
+        
+        $this->connection->statement(
+            $this->grammar->compileDropAllTables($tables)
+        );
+        
+        $this->enableForeignKeyConstraints();
+    }
+    
+    /**
+     * Drop all views from the database.
+     * 
+     * @return void
+     */
+    public function dropAllViews(): void
+    {
+        $views = [];
+        
+        foreach ($this->getAllViews() as $row) {
+            $row = (array) $row;
+            
+            $views[] = reset($row);
+        }
+        
+        if (empty($views)) {
+            return;
+        }
+        
+        $this->connection->statement(
+            $this->grammar->compileDropAllViews($views)
+        );
+    }
+    
+    /**
+     * Get all of the table names for the database.
+     * 
+     * @return array
+     */
+    public function getAllTables()
+    {
+        return $this->connection->select(
+            $this->grammar->compileGetAllTables()
+        );
+    }
+    
+    /**
+     * Get all of the view names for the database.
+     * 
+     * @return array
+     */
+    public function getAllViews()
+    {
+        return $this->connection->select(
+            $this->grammar->compileGetAllViews()
+        );
+    }
 }
