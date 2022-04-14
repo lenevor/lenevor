14011e3e2bf008709efe2f330f31a5ea8473ba5f
Alexander Campo <jalexcam@gmail.com>
2022-04-14 18:51:18 -0500

    [0.7.x] Added the type output of method

--- syscodes/src/components/Support/Facades/Request.php
+++ syscodes/src/components/Support/Facades/Request.php
@@ -61,8 +61,10 @@ class Request extends Facade
      * Get the registered name of the component.
      * 
      * @return string
+     * 
+     * @throws \RuntimeException
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'request';
     }
