2149afcb3e03fdd2a0a54ca4720b4ea9316290ea
Alexander Campo <jalexcam@gmail.com>
2022-04-14 18:50:37 -0500

    [0.7.x] Added the type output of method

--- syscodes/src/components/Support/Facades/Redis.php
+++ syscodes/src/components/Support/Facades/Redis.php
@@ -37,8 +37,10 @@ class Redis extends Facade
      * Get the registered name of the component.
      * 
      * @return string
+     * 
+     * @throws \RuntimeException
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'redis';
     }
