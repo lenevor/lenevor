b340dc6d7bad77488c8817e1c9e19ad3ded8fde1
Alexander Campo <jalexcam@gmail.com>
2022-04-14 19:21:40 -0500

    [0.7.x] Added the type output of method

--- syscodes/src/components/Support/Facades/View.php
+++ syscodes/src/components/Support/Facades/View.php
@@ -44,8 +44,10 @@ class View extends Facade
      * Get the registered name of the component.
      * 
      * @return string
+     * 
+     * @throws \RuntimeException
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'view';
     }
