7b9229d3f65a5ccd9f939e72ea9dba13ac65b866
Alexander Campo <jalexcam@gmail.com>
2022-04-14 19:06:33 -0500

    [0.7.x] Added the type output of method

--- syscodes/src/components/Support/Facades/Route.php
+++ syscodes/src/components/Support/Facades/Route.php
@@ -55,8 +55,10 @@ class Route extends Facade
      * Get the registered name of the component.
      * 
      * @return string
+     * 
+     * @throws \RuntimeException
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'router';
     }
