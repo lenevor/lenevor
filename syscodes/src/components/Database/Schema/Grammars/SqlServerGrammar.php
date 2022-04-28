5fe8e6173a7fbc111ab576d276ff8e36e5ec23a1
Alexander Campo <jalexcam@gmail.com>
2022-04-28 12:08:48 -0500

    Added new methods for compile commands in the database

--- syscodes/src/components/Database/Schema/Grammars/SqlServerGrammar.php
+++ syscodes/src/components/Database/Schema/Grammars/SqlServerGrammar.php
@@ -22,6 +22,10 @@
 
 namespace Syscodes\Components\Database\Schema\Grammars;
 
+use RuntimeException;
+use Syscodes\Components\Support\Flowing;
+use Syscodes\Components\Database\Schema\Dataprint;
+
 /**
  * Allows the compilation of sql sentences for the
  * SqlServer database.
@@ -30,5 +34,667 @@ namespace Syscodes\Components\Database\Schema\Grammars;
  */
 class SqlServerGrammar extends Grammar
 {
+    /**
+     * The possible column modifiers.
+     * 
+     * @var string[] $modifiers
+     */
+    protected $modifiers = ['Increment', 'Collate', 'Nullable', 'Default', 'Persisted'];
+    
+    /**
+     * The possible column serials.
+     * 
+     * @var string[] $serials
+     */
+    protected $serials = ['tinyInteger', 'smallInteger', 'mediumInteger', 'integer', 'bigInteger'];
 
+    /**
+     * Compile a create database command.
+     * 
+     * @param  string  $name
+     * @param  \Syscodes\Components\Database\Connections\Connection  $connection
+     * 
+     * @return string
+     */
+    public function compileCreateDatabase($name, $connection)
+    {
+        return sprintf('create database %s',
+            $this->wrapValue($name)
+        );
+    }
+
+    /**
+     * Compile a drop database if exists command.
+     * 
+     * @param  string  $name
+     * 
+     * @return string
+     */
+    public function compileDropDatabaseIfExists($name)
+    {
+        return sprintf('drop database if exists %s',
+            $this->wrapValue($name)
+        );
+    }
+
+    /**
+     * Compile the query to determine the list of tables.
+     *
+     * @return string
+     */
+    public function compileTableListing(): string
+    {
+        return "select * from sys.sysobjects where id = object_id(?) and xtype in ('U', 'V')";
+    }
+
+    /**
+     * Compile the query to determine the list of columns.
+     * 
+     * @param  string  $table
+     *
+     * @return string
+     */
+    public function compileColumnListing($table): string
+    {
+        return "select name from sys.columns where object_id = object_id('$table')";
+    }
+
+    /**
+     * Compile a create table command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileCreate(Dataprint $dataprint, Flowing $command): string
+    {
+        $columns = implode(', ', $this->getColumns($dataprint));
+        
+        return 'create table '.$this->wrapTable($dataprint)." ($columns)";
+    }
+    
+    /**
+     * Compile an add column command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileAdd(Dataprint $dataprint, Flowing $command): string
+    {
+        return sprintf('alter table %s add %s',
+            $this->wrapTable($dataprint),
+            implode(', ', $this->getColumns($dataprint))
+        );        
+    }
+
+    /**
+     * Compile a primary key command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compilePrimary(Dataprint $dataprint, Flowing $command): string
+    {
+        return sprintf('alter table %s add constraint %s primary key (%s)',
+            $this->wrapTable($dataprint),
+            $this->wrap($command->index),
+            $this->columnize($command->columns)
+        );
+    }
+    
+    /**
+     * Compile a unique key command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileUnique(Dataprint $dataprint, Flowing $command): string
+    {
+        return sprintf('create unique index %s on %s (%s)',
+            $this->wrap($command->index),
+            $this->wrapTable($dataprint),
+            $this->columnize($command->columns)
+        );
+    }
+    
+    /**
+     * Compile a plain index key command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileIndex(Dataprint $dataprint, Flowing $command): string
+    {
+        return sprintf('create index %s on %s (%s)',
+            $this->wrap($command->index),
+            $this->wrapTable($dataprint),
+            $this->columnize($command->columns)
+        );        
+    }
+    
+    /**
+     * Compile a spatial index key command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileSpatialIndex(Dataprint $dataprint, Flowing $command): string
+    {
+        return sprintf('create spatial index %s on %s (%s)',
+            $this->wrap($command->index),
+            $this->wrapTable($dataprint),
+            $this->columnize($command->columns)
+        );
+    }
+    
+    /**
+     * Compile a drop table command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileDrop(Dataprint $dataprint, Flowing $command): string
+    {
+        return 'drop table '.$this->wrapTable($dataprint);
+    }
+    
+    /**
+     * Compile a drop table (if exists) command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileDropIfExists(Dataprint $dataprint, Flowing $command): string
+    {
+        return 'drop table if exists '.$this->wrapTable($dataprint);
+    }
+    
+    /**
+     * Compile a drop column command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileDropColumn(Dataprint $dataprint, Flowing $command): string
+    {
+        $columns = $this->prefixArray('drop column', $this->wrapArray($command->columns));
+        
+        return 'alter table '.$this->wrapTable($dataprint).' '.implode(', ', $columns);
+    }
+    
+    /**
+     * Compile a drop unique key command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileDropUnique(Dataprint $dataprint, Flowing $command): string
+    {
+        $index = $this->wrap($command->index);
+        
+        return "drop index {$index}";
+    }
+    
+    /**
+     * Compile a drop index command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileDropIndex(Dataprint $dataprint, Flowing $command): string
+    {
+        $index = $this->wrap($command->index);
+        
+        return "drop index {$index}";
+    }
+    
+    /**
+     * Compile a drop spatial index command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return void
+     * 
+     * @throws \RuntimeException
+     */
+    public function compileDropSpatialIndex(Dataprint $dataprint, Flowing $command): void
+    {
+        throw new RuntimeException('The database driver in use does not support spatial indexes');
+    }
+    
+    /**
+     * Compile a rename table command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileRename(Dataprint $dataprint, Flowing $command): string
+    {
+        $from = $this->wrapTable($dataprint);
+        
+        return "alter table {$from} rename to ".$this->wrapTable($command->to);
+    }
+    
+    /**
+     * Compile a rename index command.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $command
+     * 
+     * @return string
+     */
+    public function compileRenameIndex(Dataprint $dataprint, Flowing $command): string
+    {
+        return sprintf('alter table %s rename to %s',
+            $this->wrap($command->from),
+            $this->wrap($command->to)
+        );
+    }
+    
+    /**
+     * Compile the SQL needed to drop all tables.
+     * 
+     * @param  array  $tables
+     * 
+     * @return string
+     */
+    public function compileDropAllTables($tables): string
+    {
+        return "delete from sqlite_master where type in ('table', 'index', 'trigger')";
+    }
+    
+    /**
+     * Compile the SQL needed to drop all views.
+     * 
+     * @param  array  $views
+     * 
+     * @return string
+     */
+    public function compileDropAllViews($views): string
+    {
+        return "delete from sqlite_master where type in ('view')";
+    }
+    
+    /**
+     * Compile the SQL needed to retrieve all table names.
+     * 
+     * @param  array|string  $schema
+     * 
+     * @return string
+     */
+    public function compileGetAllTables($schema): string
+    {
+        return 'select type, name from sqlite_master where type = \'table\' and name not like \'sqlite_%\'';
+    }
+    
+    /**
+     * Compile the SQL needed to retrieve all view names.
+     * 
+     * @param  array|string  $schema
+     * 
+     * @return string
+     */
+    public function compileGetAllViews($schema): string
+    {
+        return 'select type, name from sqlite_master where type = \'view\'';
+    }
+    
+    /**
+     * Compile the command to enable foreign key constraints.
+     * 
+     * @return string
+     */
+    public function compileEnableForeignKeyConstraints(): string
+    {
+        return 'PRAGMA foreign_keys = ON;';
+    }
+    
+    /**
+     * Compile the command to disable foreign key constraints.
+     * 
+     * @return string
+     */
+    public function compileDisableForeignKeyConstraints(): string
+    {
+        return 'PRAGMA foreign_keys = OFF;';
+    }
+    
+    /**
+     * Compile the SQL needed to enable a writable schema.
+     * 
+     * @return string
+     */
+    public function compileEnableWriteableSchema(): string
+    {
+        return 'PRAGMA writable_schema = 1;';
+    }
+    
+    /**
+     * Compile the SQL needed to disable a writable schema.
+     * 
+     * @return string
+     */
+    public function compileDisableWriteableSchema(): string
+    {
+        return 'PRAGMA writable_schema = 0;';
+    }
+    
+    /**
+     * Create the column definition for a char type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeChar(Flowing $column): string
+    {
+        return "nchar({$column->length})";
+    }
+    
+    /**
+     * Create the column definition for a string type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeString(Flowing $column): string
+    {
+        return "nvarchar({$column->length})";
+    }
+    
+    /**
+     * Create the column definition for a tiny text type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeTinyText(Flowing $column): string
+    {
+        return 'nvarchar(max)';
+    }
+    
+    /**
+     * Create the column definition for a text type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeText(Flowing $column): string
+    {
+        return 'nvarchar(max)';
+    }
+    
+    /**
+     * Create the column definition for a medium text type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeMediumText(Flowing $column): string
+    {
+        return 'nvarchar(max)';
+    }
+    
+    /**
+     * Create the column definition for a long text type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeLongText(Flowing $column): string
+    {
+        return 'nvarchar(max)';
+    }
+    
+    /**
+     * Create the column definition for a big integer type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeBigInteger(Flowing $column): string
+    {
+        return 'bigint';
+    }
+    
+    /**
+     * Create the column definition for an integer type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeInteger(Flowing $column): string
+    {
+        return 'int';
+    }
+    
+    /**
+     * Create the column definition for a medium integer type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeMediumInteger(Flowing $column): string
+    {
+        return 'int';
+    }
+    
+    /**
+     * Create the column definition for a tiny integer type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeTinyInteger(Flowing $column): string
+    {
+        return 'tinyint';
+    }
+    
+    /**
+     * Create the column definition for a small integer type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeSmallInteger(Flowing $column): string
+    {
+        return 'smallint';
+    }
+    
+    /**
+     * Create the column definition for a float type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeFloat(Flowing $column): string
+    {
+        return 'float';
+    }
+    
+    /**
+     * Create the column definition for a double type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeDouble(Flowing $column): string
+    {
+        return 'float';
+    }
+   
+    /**
+     * Create the column definition for a decimal type.
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeDecimal(Flowing $column): string
+    {
+        return "decimal({$column->total}, {$column->places})";
+    }
+    
+    /**
+     * Create the column definition for a boolean type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeBoolean(Flowing $column): string
+    {
+        return 'bit';
+    }
+    
+    /**
+     * Create the column definition for an enum type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeEnum(Flowing $column): string
+    {
+        return 'nvarchar(255)';
+    }
+    
+    /**
+     * Create the column definition for a date type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeDate(Flowing $column): string
+    {
+        return 'date';
+    }
+    
+    /**
+     * Create the column definition for a date-time type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeDateTime(Flowing $column): string
+    {
+        return 'datetime';
+    }
+
+    /**
+     * Create the column definition for a time type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeTime(Flowing $column): string
+    {
+        return 'time';
+    }
+    
+    /**
+     * Create the column definition for a timestamp type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeTimestamp(Flowing $column): string
+    {
+        return $column->useCurrent ? 'datetime default CURRENT_TIMESTAMP' : 'datetime';
+    }
+    
+    /**
+     * Create the column definition for a binary type.
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string
+     */
+    protected function typeBinary(Flowing $column): string
+    {
+        return 'varbinary(max)';
+    }
+    
+    /**
+     * Get the SQL for a nullable column modifier.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string|null
+     */
+    protected function modifyNullable(Dataprint $dataprint, Flowing $column): string
+    {
+        return $column->nullable ? ' null' : ' not null';
+    }
+    
+    /**
+     * Get the SQL for a default column modifier.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * 
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string|null
+     */
+    protected function modifyDefault(Dataprint $dataprint, Flowing $column)
+    {
+        if ( ! is_null($column->default))  {
+            return ' default '.$this->getDefaultValue($column->default);
+        }
+    }
+    
+    /**
+     * Get the SQL for an auto-increment column modifier.
+     * 
+     * @param  \Syscodes\Components\Database\Schema\Dataprint  $dataprint
+     * @param  \Syscodes\Components\Support\Flowing  $column
+     * 
+     * @return string|null
+     */
+    protected function modifyIncrement(Dataprint $dataprint, Flowing $column)
+    {
+        if (in_array($column->type, $this->serials) && $column->autoIncrement) {
+            return ' identity primary key';
+        }
+    }
 }
