0ed376877c38bc465a977e67ed4df0401d9d60e6
Alexander Campo <jalexcam@gmail.com>
2022-04-30 01:07:08 -0500

    Update SqlServerBuilder.php

--- syscodes/src/components/Database/Schema/Builders/SqlServerBuilder.php
+++ syscodes/src/components/Database/Schema/Builders/SqlServerBuilder.php
@@ -30,5 +30,77 @@ namespace Syscodes\Components\Database\Schema\Builders;
  */
 class SqlServerBuilder extends Builder
 {
-
+    /**
+     * Create a database in the schema.
+     * 
+     * @param  string  $name
+     * 
+     * @return bool
+     */
+    public function createDatabase($name): bool
+    {
+        return $this->connection->statement(
+            $this->grammar->compileCreateDatabase($name, $this->connection)
+        );
+    }
+    
+    /**
+     * Drop a database from the schema if the database exists.
+     * 
+     * @param  string  $name
+     * 
+     * @return bool
+     */
+    public function dropDatabaseIfExists($name): bool
+    {
+        return $this->connection->statement(
+            $this->grammar->compileDropDatabaseIfExists($name)
+        );
+    }
+    
+    /**
+     * Drop all tables from the database.
+     * 
+     * @return void
+     */
+    public function dropAllTables()
+    {
+        $this->connection->statement($this->grammar->compileDropAllForeignKeys());
+        
+        $this->connection->statement($this->grammar->compileDropAllTables());
+    }
+    
+    /**
+     * Drop all views from the database.
+     * 
+     * @return void
+     */
+    public function dropAllViews()
+    {
+        $this->connection->statement($this->grammar->compileDropAllViews());
+    }
+    
+    /**
+     * Drop all tables from the database.
+     * 
+     * @return array
+     */
+    public function getAllTables()
+    {
+        return $this->connection->select(
+            $this->grammar->compileGetAllTables()
+        );
+    }
+    
+    /**
+     * Get all of the view names for the database.
+     * 
+     * @return array
+     */
+    public function getAllViews()
+    {
+        return $this->connection->select(
+            $this->grammar->compileGetAllViews()
+        );
+    }
 }
