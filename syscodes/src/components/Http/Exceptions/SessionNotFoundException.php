12e68506b306fe77d224ba1224d68de247057e60
Alexander Campo <jalexcam@gmail.com>
2022-06-04 22:45:28 -0500

    Create SessionNotFoundException.php

---
+++ syscodes/src/components/Http/Exceptions/SessionNotFoundException.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Http\Exceptions;
+
+use Throwable;
+use LogicException;
+
+/**
+ * SessionNotFoundException.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+class SessionNotFoundException extends LogicException
+{
+    /**
+     * Initialize constructor.
+     * 
+     * @param  string  $message
+     * @param  int  $code
+     * @param  \Throwable  $previous
+     * 
+     * @return void 
+     */    
+    public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
+    {
+        if ($message === '') {
+            $message = 'There is currently no session available';
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
