3cb990d3991382bdbbea1d2cc10f453a67b586a5
Alexander Campo <jalexcam@gmail.com>
2022-06-06 23:56:19 -0500

    Create Utils.php

---
+++ syscodes/src/components/Http/Contributors/Utils.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Http\Contributors;
+
+/**
+ * Generate utils for attributes of request.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+final class Utils
+{
+    /**
+     * Normalizes a query string.
+     * 
+     * @param  string  $queryString
+     * 
+     * @return string
+     */
+    public static function normalizeQueryString(?string $queryString): string
+    {
+        if ('' === ($queryString ?? '')) {
+            return '';
+        }
+        
+        return http_build_query([$queryString], '', '&', \PHP_QUERY_RFC3986);
+    }
+}
