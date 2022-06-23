dbcb1cac85f5e81ff06f4b9eafd65186685bd91f
Alexander Campo <jalexcam@gmail.com>
2022-06-23 11:27:36 -0500

    Changed namespace

--- syscodes/src/components/Collections/Arr.php
+++ syscodes/src/components/Collections/Arr.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Components\Collections;
+namespace Syscodes\Components\Support;
 
 use ArrayAccess;
 
