4dbc4f771938bde8171b4b917c4c3fda17625a94
Alexander Campo <jalexcam@gmail.com>
2022-06-23 12:03:39 -0500

    Changed namespace

--- syscodes/src/components/Routing/RouteAction.php
+++ syscodes/src/components/Routing/RouteAction.php
@@ -24,8 +24,8 @@ namespace Syscodes\Components\Routing;
 
 use LogicException;
 use UnexpectedValueException;
+use Syscodes\Components\Support\Arr;
 use Syscodes\Components\Support\Str;
-use Syscodes\Components\Collections\Arr;
 
 /**
  * Solve the actions obtained from a route.
