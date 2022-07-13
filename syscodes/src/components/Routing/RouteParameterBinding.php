c6a1db290a8d2dd57c9017a05272b23163128080
Alexander Campo <jalexcam@gmail.com>
2022-07-12 23:38:25 -0500

    Changed is the class name

---
+++ syscodes/src/components/Routing/RouteParameterBinding.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Routing;
+
+use Syscodes\Components\Support\Arr;
+
+/**
+ * Allows the parameter matches for the path portion of the URI.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+class RouteParameterBinding
+{
+    /**
+     * The route instance.
+     * 
+     * @var \Syscodes\Components\Routing\Route $route
+     */
+    protected $route;
+
+    /**
+     * Constructor. Create a new Route parameter binder instance.
+     * 
+     * @param  \Syscodes\Components\Routing\Route  $route
+     * 
+     * @return void
+     */
+    public function __construct($route)
+    {
+        $this->route = $route;
+    }
+
+    /**
+     * Get the parameter for the route.
+     * 
+     * @param  \Syscodes\Components\Http\Request  $request
+     * 
+     * @return array
+     */
+    public function parameters($request): array
+    {
+        $parameters = $this->bindParameters($request);
+
+        return $this->replaceDefaults($parameters);
+    }
+
+    /**
+     * Get the parameter matches for the path portion of the URI.
+     * 
+     * @param  \Syscodes\Components\Http\Request  $request
+     * 
+     * @return array
+     */
+    protected function bindParameters($request): array
+    {
+        $path = '/'.ltrim($request->decodedPath(), '/');
+
+        preg_match('~[^/'.$this->route->getRoute().'$]+~sDu', $path, $matches);
+        
+        return $this->matchToKeys(array_slice($matches, 0));
+    }
+    
+    /**
+     * Combine a set of parameter matches with the route's keys.
+     * 
+     * @param  array  $matches
+     * 
+     * @return array
+     */
+    protected function matchToKeys(array $matches): array
+    {
+        if (empty($parameterNames = $this->route->parameterNames())) {
+            return [];
+        }
+
+        $parameters = array_intersect_key($matches, array_values($parameterNames));
+        
+        return array_filter($parameters, function ($value) {
+            return is_string($value) && strlen($value) > 0;
+        });
+    }
+    
+    /**
+     * Replace null parameters with their defaults.
+     * 
+     * @param  array  $parameters
+     * 
+     * @return array
+     */
+    protected function replaceDefaults(array $parameters): array
+    {
+        foreach ($parameters as $key => $value) {
+            $parameters[$key] = $value ?? Arr::get($this->route->defaults, $key);
+        }
+        
+        foreach ($this->route->defaults as $key => $value) {
+            if ( ! isset($parameters[$key])) {
+                $parameters[$key] = $value;
+            }
+        }
+        
+        return $parameters;
+    }
+}
