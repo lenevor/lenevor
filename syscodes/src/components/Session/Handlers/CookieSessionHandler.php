c56893d36d5d445f0de41c59cbca17db84d7d183
Alexander Campo <jalexcam@gmail.com>
2022-08-06 11:07:46 -0500

    Added methods of SessionHandlerInterface

--- syscodes/src/components/Session/Handlers/CookieSessionHandler.php
+++ syscodes/src/components/Session/Handlers/CookieSessionHandler.php
@@ -31,5 +31,63 @@ use SessionHandlerInterface;
  */
 class CookieSessionHandler implements SessionHandlerInterface
 {
-    //
+    /**
+     * {@inheritdoc}
+     * 
+     * @return bool
+     */
+    public function open($savePath, $sessionName): bool
+    {
+        return true;
+    }
+    
+    /**
+     * {@inheritdoc}
+     * 
+     * @return bool
+     */
+    public function close(): bool
+    {
+        return true;
+    }
+    
+    /**
+     * {@inheritdoc}
+     * 
+     * @return string
+     */
+    public function read($sessionId): string
+    {
+        return '';
+    }
+    
+    /**
+     * {@inheritdoc}
+     * 
+     * @return bool
+     */
+    public function write($sessionId, $data): bool
+    {
+        return true;
+    }
+    
+    /**
+     * {@inheritdoc}
+     * 
+     * @return bool
+     */
+    public function destroy($sessionId): bool
+    {
+        return true;
+    }
+    
+    /**
+     * {@inheritdoc}
+     * 
+     * @return int
+     */
+    public function gc(int $lifetime): int
+    {
+        return 0;
+    }
 }
