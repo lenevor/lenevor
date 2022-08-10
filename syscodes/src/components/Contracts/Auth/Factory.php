abdb78a9a3bc1d911050a92becfb1e6e63a24752
Alexander Campo <jalexcam@gmail.com>
2022-08-10 12:07:28 -0500

    Create Factory.php

---
+++ syscodes/src/components/Contracts/Auth/Factory.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Contracts\Auth;
+
+/**
+ * Determine a guard by name.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+interface Factory
+{
+    /**
+     * Get a guard instance by name.
+     * 
+     * @param  string|null  $name
+     * 
+     * @return \Syscodes\Components\Contracts\Auth\Guard|\Syscodes\Components\Contracts\Auth\StatefulGuard
+     */
+    public function guard($name = null);
+    
+    /**
+     * Set the default guard the factory should serve.
+     * 
+     * @param  string  $name
+     * 
+     * @return void
+     */
+    public function shouldUse($name): void;
+}
