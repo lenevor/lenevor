281fc5a1cb4399960c03a333c3267b7cf3e3838b
Alexander Campo <jalexcam@gmail.com>
2022-08-17 22:06:55 -0500

    Create TokenGuard.php

---
+++ syscodes/src/components/Auth/Guards/TokenGuard.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Auth\Guards;
+
+use Syscodes\Components\Contracts\Auth\Guard;
+use Syscodes\Components\Auth\Concerns\GuardAuthenticationUser;
+
+/**
+ * Capture the user data using a token header. 
+ * 
+ * @author Alexander Campo <jalexam@gmail.com> 
+ */
+class TokenGuard implements Guard
+{
+    use GuardAuthenticationUser;
+}
+
