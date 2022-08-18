0756252fb78bbc58f71b78f882d56f2d491ca301
Alexander Campo <jalexcam@gmail.com>
2022-08-18 01:38:07 -0500

    Spacing

--- syscodes/src/components/Auth/Guards/TokenGuard.php
+++ syscodes/src/components/Auth/Guards/TokenGuard.php
@@ -22,7 +22,9 @@
 
 namespace Syscodes\Components\Auth\Guards;
 
+use Syscodes\Components\Http\Request;
 use Syscodes\Components\Contracts\Auth\Guard;
+use Syscodes\Components\Contracts\Auth\UserProvider;
 use Syscodes\Components\Auth\Concerns\GuardAuthenticationUser;
 
 /**
@@ -33,5 +35,89 @@ use Syscodes\Components\Auth\Concerns\GuardAuthenticationUser;
 class TokenGuard implements Guard
 {
     use GuardAuthenticationUser;
+    
+    /**
+     * The request instance.
+     * 
+     * @var \Syscodes\Components\Http\Request $request
+     */
+    protected $request;
+    
+    /**
+     * The name of the query string item from the request containing the API token.
+     * 
+     * @var string $inputKey
+     */
+    protected $inputKey;
+    
+    /**
+     * The name of the token "column" in persistent storage.
+     * 
+     * @var string $storageKey
+     */
+    protected $storageKey;
+    
+    /**
+     * Indicates if the API token is hashed in storage.
+     * 
+     * @var bool $hash
+     */
+    protected $hash = false;
+    
+    /**
+     * Constructor. Create a new authentication guard.
+     * 
+     * @param  \Syscodes\Components\Contracts\Auth\UserProvider  $provider
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  string  $inputKey
+     * @param  string  $storageKey
+     * @param  bool  $hash
+     * 
+     * @return void
+     */
+    public function __construct(
+        UserProvider $provider,
+        Request $request,
+        $inputKey = 'api_token',
+        $storageKey = 'api_token',
+        $hash = false
+    ) {
+        $this->hash = $hash;
+        $this->request = $request;
+        $this->provider = $provider;
+        $this->inputKey = $inputKey;
+        $this->storageKey = $storageKey;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function user()
+    {
+        if ( ! is_null($this->user)) {
+            return $this->user;
+        }
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function validate(array $credentials = []): bool
+    {
+        return false;
+    }
+    
+    /**
+     * Set the current request instance.
+     * 
+     * @param  \Syscodes\Components\Http\Request  $request
+     * 
+     * @return self
+     */
+    public function setRequest(Request $request): self
+    {
+        $this->request = $request;
+        
+        return $this;
+    }
 }
-
