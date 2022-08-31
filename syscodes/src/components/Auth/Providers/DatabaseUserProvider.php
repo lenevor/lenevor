472295bf1fb50cad12ec479377e882d8b23e603a
Alexander Campo <jalexcam@gmail.com>
2022-08-30 23:39:12 -0500

    Update DatabaseUserProvider.php

--- syscodes/src/components/Auth/Providers/DatabaseUserProvider.php
+++ syscodes/src/components/Auth/Providers/DatabaseUserProvider.php
@@ -22,14 +22,17 @@
 
 namespace Syscodes\Components\Auth\Providers;
 
+use Closure;
+use Syscodes\Components\Support\Str;
 use Syscodes\Components\Auth\GenericUser;
 use Syscodes\Components\Contracts\Auth\UserProvider;
-use Syscodes\Components\Contracts\Auth\Authenticatable;
+use Syscodes\Components\Contracts\Support\Arrayable;
 use Syscodes\Components\Database\Connections\ConnectionInterface;
 use Syscodes\Components\Contracts\Hashing\Hasher as HasherContract;
+use Syscodes\Components\Contracts\Auth\Authenticatable as UserContract;
 
 /**
- * 
+ * Allows the validation of credentials using the connection with the database.
  * 
  * @author Alexander Campo <jalexcam@gmail.com>
  */
@@ -87,15 +90,22 @@ class DatabaseUserProvider implements UserProvider
      */
     public function retrieveByToken($identifier, string $token)
     {
-        
+        $user = $this->getGenericUser(
+            $this->connection->table($this->table)->find($identifier)
+        );
+
+        return $user && $user->getRememberToken() && hash_equals($user->getRememberToken(), $token)
+                    ? $user : null;
     }
     
     /**
      * {@inheritdoc}
      */
-    public function updateRememberToken(Authenticatable $user, string $token): void
+    public function updateRememberToken(UserContract $user, string $token): void
     {
-        
+        $this->connection->table($this->table)
+             ->where($user->getAuthIdentifierName(), $user->getAuthIdentifier())
+             ->update([$user->getRememberTokenName() => $token]);
     }
     
     /**
@@ -103,13 +113,35 @@ class DatabaseUserProvider implements UserProvider
      */
     public function retrieveByCredentials(array $credentials)
     {
-        
+        $credentials = array_filter($credentials, function ($key) {
+            return ! Str::contains($key, 'password');
+        }, ARRAY_FILTER_USE_KEY);
+
+        if (empty($credentials)) {
+            return;
+        }
+
+        $query = $this->connection->table($this->table);
+
+        foreach ($credentials as $key => $value) {
+            if (is_array($value) || $value instanceof Arrayable) {
+                $query->whereIn($key, $value);
+            } elseif ($value instanceof Closure) {
+                $value($query);
+            } else {
+                $query->where($key, $value);
+            }
+        }
+
+        $user = $query->first();
+
+        return $this->getGenericUser($user);
     }
     
     /**
      * {@inheritdoc}
      */
-    public function validateCredentials(Authenticatable $user, array $credentials): bool
+    public function validateCredentials(UserContract $user, array $credentials): bool
     {
         return $this->hasher->check(
             $credentials['password'], $user->getAuthPassword()
@@ -121,7 +153,7 @@ class DatabaseUserProvider implements UserProvider
      * 
      * @param  mixed  $user
      * 
-     * @return \Syscodes\Components\Auth\GenericUser
+     * @return \Syscodes\Components\Auth\GenericUser|null
      */
     private function getGenericUser($user)
     {
