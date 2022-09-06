ab433adfe3240da8a490064b81688196da8b7ce8
Alexander Campo <jalexcam@gmail.com>
2022-09-05 23:05:15 -0500

    Update layout.plaze.php

--- syscodes/src/components/Core/Exceptions/views/layout.plaze.php
+++ syscodes/src/components/Core/Exceptions/views/layout.plaze.php
@@ -28,7 +28,7 @@
 				<div class="flex items-center justify-center sm:flex-col md:flex-col sm:p-right md:p-right sm:p-full md:p-full">
 					<h1 class="text-color-title font-bold sm:text-size-title md:text-size-title md:m-bottom text-gradient"><@give('code')</h1>
 					<p class="text-color-subtitle font-bold uppercase letter-spacing sm:text-size-subtitle md:text-size-subtitle"><@give('message')</p>
-					<a href='{{ url('/') }}' class="round p-full sm:m-top md:m-top md:p-full sm:text-size-button md:text-size-button"><@give('button')</a>
+					<a href="{{ url('/') }}" class="round p-full sm:m-top md:m-top md:p-full sm:text-size-button md:text-size-button"><@give('button')</a>
 				</div>
 
 			</div>
