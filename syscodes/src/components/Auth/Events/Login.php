53f9177577269ec7cc5d2e499931500ca86e579f
Alexander Campo <jalexcam@gmail.com>
2022-09-27 13:50:28 -0500

    Spacing

--- syscodes/src/components/Auth/Events/Login.php
+++ syscodes/src/components/Auth/Events/Login.php
@@ -61,8 +61,8 @@ class Login
      */
     public function __construct($guard, $user, $remember)
     {
-        $this->user = $user;
-        $this->guard = $guard;
+        $this->user     = $user;
+        $this->guard    = $guard;
         $this->remember = $remember;
     }
 }
