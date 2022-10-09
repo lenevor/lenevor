6c02c587b5441c3797678c7652eb7f2567083dbe
Alexander Campo <jalexcam@gmail.com>
2022-10-09 15:13:14 -0500

    Update DatabaseStore.php

--- syscodes/src/components/Cache/Store/DatabaseStore.php
+++ syscodes/src/components/Cache/Store/DatabaseStore.php
@@ -24,6 +24,7 @@ namespace Syscodes\Components\Cache\Store;
 
 use Syscodes\Components\Contracts\Cache\Store;
 use Syscodes\Components\Support\InteractsWithTime;
+use Syscodes\Components\Cache\concerns\CacheMultipleKeys;
 
 /**
  * Database cache handler.
@@ -32,5 +33,86 @@ use Syscodes\Components\Support\InteractsWithTime;
  */
 class DatabaseStore implements Store
 {
+    use CacheMultipleKeys,
+        InteractsWithTime;
     
+    /**
+     * {@inheritdoc}
+     */
+    public function get($key)
+    {
+
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function many(array $keys): array
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function put($key, $value, $seconds): bool
+    {
+
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function putMany(array $values, $seconds): bool
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function increment($key, $value = 1)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function decrement($key, $value = 1)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($key)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function forever($key, $value): bool
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function flush(): bool
+    {
+
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getPrefix(): string
+    {
+
+    }
 }
