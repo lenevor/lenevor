b0a992567e4d7cd17f213709d93df4cb0292a197
Alexander Campo <jalexcam@gmail.com>
2022-10-11 12:38:10 -0500

    Update AuthManager.php

--- syscodes/src/components/Auth/AuthManager.php
+++ syscodes/src/components/Auth/AuthManager.php
@@ -22,6 +22,7 @@
 
 namespace Syscodes\Components\Auth;
 
+use Closure;
 use Syscodes\Components\Contracts\Auth\Factory;
 use Syscodes\Components\Auth\Concerns\CreatesUserProviders;
 
@@ -35,11 +36,79 @@ class AuthManager implements Factory
     use CreatesUserProviders;
 
     /**
+     * The applicaction instance.
+     * 
+     * @var \Syscodes\Components\Contracts\Core\Application $app
+     */
+    protected $app;
+
+    /**
+     * The registered custom driver creators.
+     * 
+     * @var array $customCreators
+     */
+    protected $customCreators = [];
+
+    /**
+     * The array of created "drivers".
+     * 
+     * @var array $guards
+     */
+    protected $guards = [];
+
+    /**
+     * The user resolver shared by various services.
+     * 
+     * @var \Closure $userResolver
+     */
+    protected $userResolver;
+
+    /**
+     * Constructor. Create a new AuthManager class instance.
+     * 
+     * @param  \Syscodes\Components\Contracts\Core\Application  $app
+     * 
+     * @return void
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+
+        $this->userResolver = function ($guard = null) {
+            return $this->guard($guard)->user();
+        };
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function guard(string $name = null)
     {
-
+        $name = $name ?: $this->getDefaultDriver();
+        
+        return $this->guards[$name] ?? $this->guards[$name] = $this->resolve($name);
+    }
+    
+    /**
+     * Get the default authentication driver name.
+     * 
+     * @return string
+     */
+    public function getDefaultDriver(): string
+    {
+        return $this->app['config']['auth.defaults.guard'];
+    }
+    
+    /**
+     * Get the guard configuration.
+     * 
+     * @param  string  $name
+     * 
+     * @return array
+     */
+    protected function getConfig($name): array
+    {
+        return $this->app['config']["auth.guards.{$name}"];
     }
     
     /**
@@ -47,6 +116,93 @@ class AuthManager implements Factory
      */
     public function shouldUse(string $name): void
     {
+        $name = $name ?: $this->getDefaultDriver();
+        
+        $this->setDefaultDriver($name);
+        
+        $this->userResolver = function ($name = null) {
+            return $this->guard($name)->user();
+        };
+    }
 
+    /**
+     * set the default authentication driver name.
+     * 
+     * @param  string  $name
+     * 
+     * @return void
+     */
+    public function setDefaultDriver($name): void
+    {
+        $this->app['config']['auth.defaults.guard'] = $name;
+    }
+    
+    /**
+     * Get the user resolver callback.
+     * 
+     * @return \Closure
+     */
+    public function userResolver()
+    {
+        return $this->userResolver;
+    }
+    
+    /**
+     * Register a custom driver creator Closure.
+     * 
+     * @param  string  $driver
+     * @param  \Closure  $callback
+     * 
+     * @return self
+     */
+    public function extend($driver, Closure $callback): self
+    {
+        $this->customCreators[$driver] = $callback;
+        
+        return $this;
+    }
+    
+    /**
+     * Register a custom provider creator Closure.
+     * 
+     * @param  string  $name
+     * @param  \Closure  $callback
+     * 
+     * @return self
+     */
+    public function provider($name, Closure $callback): self
+    {
+        $this->customProviderCreators[$name] = $callback;
+        
+        return $this;
+    }
+    
+    /**
+     * Set the application instance used by the manager.
+     * 
+     * @param  \Syscodes\Components\Contracts\Core\Application  $app
+     * 
+     * @return self
+     */
+    public function setApplication($app): self
+    {
+        $this->app = $app;
+        
+        return $this;
+    }
+    
+    /**
+     * Magic method.
+     * 
+     * Dynamically call the default driver instance.
+     * 
+     * @param  string  $method
+     * @param  array  $parameters
+     * 
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        return $this->guard()->{$method}(...$parameters);
     }
 }
