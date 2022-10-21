2121992c86bb397065f493160bf8194ed2f0cf49
Alexander Campo <jalexcam@gmail.com>
2022-10-21 01:26:03 -0500

    Create UrlGeneratorException.php

---
+++ syscodes/src/components/Routing/Exceptions/UrlGeneratorException.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Routing\Exceptions;
+
+use Exception;
+use Syscodes\Components\Support\Str;
+use Syscodes\Components\Routing\Route;
+
+/**
+ * UrlGeneratorException.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+class UrlGeneratorException extends Exception
+{
+    /**
+     * Create a new exception for missing route parameters.
+     * 
+     * @param  \Syscodes\Components\Routing\Route  $route
+     * @param  array  $parameters
+     * 
+     * @return static
+     */
+    public static function missingParameters(Route $route, array $parameters = [])
+    {
+        $parameter = Str::plural('parameter', count($parameters));
+        
+        $message = sprintf('Missing required %s for [Route: %s] [URI: %s]',
+            $parameter,
+            $route->getName(),
+            $route->getRoute()
+        );
+        
+        if (count($parameters) > 0) {
+            $message .= sprintf(' [Missing %s: %s]', $parameter, implode(', ', $parameters));
+        }
+        
+        $message .= '.';
+        
+        return new static($message);
+    }
+}
