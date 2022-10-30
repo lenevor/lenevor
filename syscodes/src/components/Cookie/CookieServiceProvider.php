a83d176bd436fe21c466df370b97e4fb0dd776d1
Alexander Campo <jalexcam@gmail.com>
2022-10-30 16:40:20 -0500

    Spacing

--- syscodes/src/components/Cookie/CookieServiceProvider.php
+++ syscodes/src/components/Cookie/CookieServiceProvider.php
@@ -29,16 +29,16 @@ use Syscodes\Components\Support\ServiceProvider;
  * @author Alexander Campo <jalexcam@gmail.com>
  */
 class CookieServiceProvider extends ServiceProvider
-{/**
+{
+    /**
      * Register the service provider.
-     *
+     * 
      * @return void
      */
     public function register()
     {
         $this->app->singleton('cookie', function ($app) {
             $config = $app->make('config')->get('session');
-            
             return (new CookieManager)->setDefaultPathAndDomain(
                 $config['path'], $config['domain'], $config['secure'], $config['sameSite'] ?? null
             );
