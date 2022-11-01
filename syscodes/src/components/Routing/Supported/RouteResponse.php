388d0fd5f666c4dd10f28734250daf71bec28bf3
Alexander Campo <jalexcam@gmail.com>
2022-11-01 00:25:18 -0500

    Added new methods for redirected response

--- syscodes/src/components/Routing/Supported/RouteResponse.php
+++ syscodes/src/components/Routing/Supported/RouteResponse.php
@@ -65,7 +65,13 @@ class RouteResponse implements ResponseContract
     }
 
     /**
-     * {@inheritdoc}
+     *  Return a new response from the application.
+     *
+     * @param  string  $body
+     * @param  int  $status
+     * @param  array  $headers
+     * 
+     * @return \Syscodes\Components\Http\Response
      */
     public function make($body = '', $status = 200, array $headers = []): Response
     {
@@ -73,7 +79,12 @@ class RouteResponse implements ResponseContract
     }
 
     /**
-     * {@inheritdoc}
+     * Creates a new 'no content' response.
+     * 
+     * @param  int  $status
+     * @param  array  $headers
+     * 
+     * @return \Syscodes\Components\Http\Response
      */
     public function noContent($status = 204, array $headers = []): Response
     {
@@ -81,7 +92,14 @@ class RouteResponse implements ResponseContract
     }
 
     /**
-     * {@inheritdoc}
+     * Return a new View Response from the application.
+     *
+     * @param  string  $view
+     * @param  array  $data
+     * @param  int  $status
+     * @param  array  $headers
+     * 
+     * @return  \Syscodes\Components\Http\Response
      */
     public function view(
         $view, 
@@ -95,7 +113,14 @@ class RouteResponse implements ResponseContract
     }
 
     /**
-     * {@inheritdoc}
+     * Create a new JSON response instance.
+     * 
+     * @param  mixed  $data
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  int  $options
+     * 
+     * @return \Syscodes\Components\Http\JsonResponse
      */
     public function json(
         $data = [], 
@@ -107,7 +132,14 @@ class RouteResponse implements ResponseContract
     }
 
     /**
-     * {@inheritdoc}
+     * Create a new redirect response to the given path.
+     * 
+     * @param  string  $path
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * 
+     * @return \Syscodes\Components\Http\RedirectResponse
      */
     public function redirectTo(
         $path, 
@@ -116,5 +148,81 @@ class RouteResponse implements ResponseContract
         $secure = null
     ) {
         return $this->redirector->to($path, $status, $headers, $secure);
+    }
+
+    /**
+     * Create a new redirect response to a named route.
+     * 
+     * @param  string  $route
+     * @param  mixed  $parameters
+     * @param  int  $status
+     * @param  array  $headers
+     * 
+     * @return \Syscodes\Components\Http\RedirectResponse
+     */
+    public function redirectToRoute(
+        $route,
+        $parameters = [],
+        $status = 302,
+        $headers = []
+    ) {
+        return $this->redirector->route($route, $parameters = [], $status, $headers);   
+    }
+    
+    /**
+     * Create a new redirect response to a controller action.
+     * 
+     * @param  string  $action
+     * @param  mixed  $parameters
+     * @param  int  $status
+     * @param  array  $headers
+     * 
+     * @return \Syscodes\Components\Http\RedirectResponse
+     */
+    public function redirectToAction(
+        $action,
+        $parameters = [],
+        $status = 302,
+        $headers = []
+    ) {
+        return $this->redirector->action($action, $parameters, $status, $headers);
+    }
+
+    /**
+     * Create a new redirect response, while putting the current URL in the session.
+     * 
+     * @param  string  $path
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * 
+     * @return \Syscodes\Components\Http\RedirectResponse
+     */
+    public function redirectGuest(
+        $path,
+        $status = 302,
+        $headers = [],
+        $secure = null
+    ) {
+        return $this->redirector->guest($path, $status, $headers, $secure);
+    }
+
+    /**
+     * Create a new redirect response to the previously intended location.
+     * 
+     * @param  string  $default
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * 
+     * @return \Syscodes\Components\Http\RedirectResponse
+     */
+    public function redirectToIntended(
+        $default = '/',
+        $status = 302,
+        $headers = [],
+        $secure = null
+    ) {
+        return $this->redirector->intended($default, $status, $headers, $secure);
     }
 }
