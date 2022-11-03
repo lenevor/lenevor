35e78961604d488cd053a5a0a28994e449d1abdb
Alexander Campo <jalexcam@gmail.com>
2022-11-02 23:38:59 -0500

    Update details_panel.php

--- syscodes/src/components/Debug/Exceptions/Resources/views/details_panel.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/details_panel.php
@@ -1,14 +1,14 @@
 <section class="panel details-panel">
 	<?= $template->render($code_source) ?>
-	<div class="details-description details-description-application">
+	<!-- <div class="details-description details-description-application">
 
 		<span><?= e(__('exception.environment'))?></span>
 
-	</div>
+	</div> -->
 
 	<div class="content">
 		
-		<?= $template->render($details_content) ?>
+		<!-- <?= $template->render($details_content) ?> -->
 		
 	</div> 
 
