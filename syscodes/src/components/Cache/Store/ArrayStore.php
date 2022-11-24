6fac3bab252b258cbec2f5e4f487dd18d08c2b1c
Alexander Campo <jalexcam@gmail.com>
2022-11-23 22:20:32 -0500

    Changed the comments in the methods

--- syscodes/src/components/Cache/Store/ArrayStore.php
+++ syscodes/src/components/Cache/Store/ArrayStore.php
@@ -63,7 +63,11 @@ class ArrayStore implements Store
     }
 
     /**
-     * {@inheritdoc}
+     * Gets an item from the cache by key.
+     * 
+     * @param  string  $key
+     * 
+     * @return mixed
      */
     public function get($key)
     {
@@ -83,7 +87,13 @@ class ArrayStore implements Store
     }
 
     /**
-     * {@inheritdoc}
+     * Store an item in the cache for a given number of seconds.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * @param  int     $seconds
+     * 
+     * @return bool
      */
     public function put($key, $value, $seconds): bool
     {
@@ -96,7 +106,12 @@ class ArrayStore implements Store
     }
 
     /**
-     * {@inheritdoc}
+     * Increment the value of an item in the cache.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * 
+     * @return int|bool
      */
     public function increment($key, $value = 1)
     {
@@ -114,7 +129,12 @@ class ArrayStore implements Store
     }
 
     /**
-     * {@inheritdoc}
+     * Decrement the value of an item in the cache.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * 
+     * @return int|bool
      */
     public function decrement($key, $value = 1)
     {
@@ -122,7 +142,11 @@ class ArrayStore implements Store
     }
 
     /**
-     * {@inheritdoc}
+     * Deletes a specific item from the cache store.
+     * 
+     * @param  string  $key
+     * 
+     * @return mixed
      */
     public function delete($key)
     {
@@ -136,7 +160,12 @@ class ArrayStore implements Store
     }
 
     /**
-     * {@inheritdoc}
+     * Stores an item in the cache indefinitely.
+     * 
+     * @param  string  $key
+     * @param  mixed   $value
+     * 
+     * @return bool
      */
     public function forever($key, $value): bool
     {
@@ -144,7 +173,9 @@ class ArrayStore implements Store
     }
 
     /**
-     * {@inheritdoc}
+     * Remove all items from the cache.
+     * 
+     * @return bool
      */
     public function flush(): bool
     {
@@ -154,7 +185,9 @@ class ArrayStore implements Store
     }
 
     /**
-     * {@inheritdoc}
+     * Gets the cache key prefix.
+     *
+     * @return string
      */
     public function getPrefix(): string
     {
