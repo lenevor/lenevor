1960986c865e83e84d7d4b2fd03defa7bb585a8c
Alexander Campo <jalexcam@gmail.com>
2022-11-25 10:22:59 -0500

    Update InputOption.php

--- syscodes/src/components/Contracts/Console/Input/InputOption.php
+++ syscodes/src/components/Contracts/Console/Input/InputOption.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Components\Contracts\Console;
+namespace Syscodes\Components\Contracts\Console\Input;
 
 /**
  * InputOption Interface.
