b53ef6d5dafe02a97fbaaae280af7881fcb2662e
Alexander Campo <jalexcam@gmail.com>
2022-12-07 11:56:12 -0500

    Create HandlesAuthorization.php

---
+++ syscodes/src/components/Auth/Access/Concerns/HandlesAuthorization.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2022 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Auth\Concerns;
+
+use Syscodes\Components\Auth\Access\Response;
+
+/**
+ * Allows and denied the authorizations to show a message HTTP status code.
+ * 
+ * @author Alexander Campo <jalexcam@gmail.com>
+ */
+trait HandlesAuthorization
+{
+    /**
+     * Create a new access response.
+     * 
+     * @param  string|null  $message
+     * @param  mixed  $code
+     * 
+     * @return \Syscodes\Components\Auth\Access\Response
+     */
+    protected function allow($message = null, $code = null)
+    {
+        return Response::allow($message, $code);
+    }
+    
+    /**
+     * Throws an unauthorized exception.
+     * 
+     * @param  string|null  $message
+     * @param  mixed|null  $code
+     * 
+     * @return \Syscodes\Components\Auth\Access\Response
+     */
+    protected function deny($message = null, $code = null)
+    {
+        return Response::deny($message, $code);
+    }
+}
