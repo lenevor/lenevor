658c90df3603bb996c8ae96dcd0afce5cc48cb7f
Alexander Campo <jalexcam@gmail.com>
2022-12-12 12:15:11 -0500

    Update Gate.php

--- syscodes/src/components/Auth/Access/Gate.php
+++ syscodes/src/components/Auth/Access/Gate.php
@@ -22,7 +22,9 @@
 
 namespace Syscodes\Components\Auth\Access;
 
+use Exception;
 use InvalidArgumentException;
+use Syscodes\Components\Support\Arr;
 use Syscodes\Components\Support\Str;
 use Syscodes\Components\Contracts\Container\Container;
 use Syscodes\Components\Auth\Concerns\HandlesAuthorization;
@@ -137,6 +139,18 @@ class Gate implements GateContract
      */
     public function define($ability, $callback): self
     {
+        if (is_array($callback) && isset($callback[0]) && is_string($callback[0])) {
+            $callback = $callback[0].'@'.$callback[1];
+        }
+        
+        if (is_callable($callback)) {
+            $this->abilities[$ability] = $callback;
+        } elseif (is_string($callback) && Str::contains($callback, '@')) {            
+            $this->abilities[$ability] = $this->buildAbilityCallback($ability, $callback);
+        } else {
+            throw new InvalidArgumentException("Callback must be a callable, callback array, or a 'Class@method' string.");
+        }
+        
         return $this;
     }
     
@@ -151,7 +165,45 @@ class Gate implements GateContract
      */
     public function resource($name, $class, array $abilities = null): self
     {
+        $abilities = $abilities ?: [
+            'view' => 'view',
+            'create' => 'create',
+            'update' => 'update',
+            'delete' => 'delete',
+        ];
+        
+        foreach ($abilities as $ability => $method) {
+            $this->define($name.'.'.$ability, $class.'@'.$method);
+        }
+        
         return $this;
+    }
+    
+    /**
+     * Create the ability callback for a callback string.
+     * 
+     * @param  string  $ability
+     * @param  string  $callback
+     * 
+     * @return \Closure
+     */
+    protected function buildAbilityCallback($ability, $callback)
+    {
+        return function () use ($ability, $callback) {
+            if (Str::contains($callback, '@')) {
+                [$class, $method] = Str::parseCallback($callback);
+            } else {
+                $class = $callback;
+            }
+            
+            $policy = $this->resolvePolicy($class);
+
+            $arguments = func_get_args();
+            
+            return isset($method)
+                    ? $policy->{$method}(...$arguments)
+                    : $policy(...$arguments);
+        };
     }
     
     /**
@@ -178,6 +230,8 @@ class Gate implements GateContract
      */
     public function before(callable $callback): self
     {
+        $this->beforeCallbacks[] = $callback;
+        
         return $this;
     }
     
@@ -190,6 +244,8 @@ class Gate implements GateContract
      */
     public function after(callable $callback): self
     {
+        $this->afterCallbacks[] = $callback;
+        
         return $this;
     }
     
@@ -229,7 +285,13 @@ class Gate implements GateContract
      */
     public function check($ability, $arguments = []): bool
     {
-
+        try {
+            $result = $this->inspect($ability, $arguments);
+        } catch (Exception $e) {
+            return false;
+        }
+        
+        return (bool) $result;
     }
     
     /**
@@ -242,7 +304,9 @@ class Gate implements GateContract
      */
     public function any($abilities, $arguments = []): bool
     {
-
+        return collect($abilities)->contains(function ($ability) use ($arguments) {
+            return $this->check($ability, $arguments);
+        });
     }
     
     /**
@@ -256,7 +320,7 @@ class Gate implements GateContract
      */
     public function authorize($ability, $arguments = [])
     {
-
+        return $this->inspect($ability, $arguments)->authorize();
     }
     
     /**
@@ -269,22 +333,149 @@ class Gate implements GateContract
      */
     public function inspect($ability, $arguments = [])
     {
-
+        try {
+            $result = $this->raw($ability, $arguments);
+            
+            if ($result instanceof Response) {
+                return $result;
+            }
+            
+            return $result ? Response::allow() : Response::deny();
+        } catch (InvalidArgumentException $e) {
+            throw $e;
+        }
     }
     
     /**
      * Get the raw result from the authorization callback.
      * 
      * @param  string  $ability
-     * @param  array|mixed  $arguments
+     * @param  array  $arguments
      * 
      * @return mixed
      * 
      * @throws \Syscodes\Components\Auth\Access\AuthorizationException
      */
-    public function raw($ability, $arguments = [])
+    public function raw($ability, array $arguments)
     {
+        $arguments = Arr::wrap($arguments);
+        
+        if (is_null($user = $this->resolveUser())) {
+            return false;
+        }
+        
+        $result = $this->callBeforeCallbacks($user, $ability, $arguments);
+        
+        if (is_null($result)) {
+            $result = $this->callAuthCallback($user, $ability, $arguments);
+        }
+        
+        $this->callAfterCallbacks($user, $ability, $arguments, $result);
+        
+        return $result;
+    }
+    
+    /**
+     * Resolve and call the appropriate authorization callback.
+     * 
+     * @param  \Syscodes\Components\Contracts\Auth\Authenticatable|null  $user
+     * @param  string  $ability
+     * @param  array  $arguments
+     * 
+     * @return bool
+     */
+    protected function callAuthCallback($user, $ability, array $arguments): bool
+    {
+        $callback = $this->resolveAuthCallback($user, $ability, $arguments);
+        
+        return $callback($user, ...$arguments);
+    }
+    
+    /**
+     * Call all of the before callbacks and return if a result is given.
+     * 
+     * @param  \Syscodes\Components\Contracts\Auth\Authenticatable|null  $user
+     * @param  string  $ability
+     * @param  array  $arguments
+     * 
+     * @return bool|null
+     */
+    protected function callBeforeCallbacks($user, $ability, array $arguments)
+    {
+        $arguments = array_merge([$user, $ability], $arguments);
+        
+        foreach ($this->beforeCallbacks as $callback) {
+            if ( ! is_null($result = call_user_func_array($callback, $arguments))) {
+                return $result;
+            }
+        }
+    }
+    
+    /**
+     * Call all of the after callbacks with check result.
+     * 
+     * @param  \Syscodes\Components\Contracts\Auth\Authenticatable|null  $user
+     * @param  string  $ability
+     * @param  array  $arguments
+     * @param  bool  $result
+     * 
+     * @return void
+     */
+    protected function callAfterCallbacks($user, $ability, array $arguments, $result): void
+    {
+        $arguments = array_merge([$user, $ability, $result], $arguments);
+        
+        foreach ($this->afterCallbacks as $callback) {
+            call_user_func_array($callback, $arguments);
+        }
+    }
+    
+    /**
+     * Resolve the callable for the given ability and arguments.
+     * 
+     * @param  \Syscodes\Components\Contracts\Auth\Authenticatable|null  $user
+     * @param  string  $ability
+     * @param  array  $arguments
+     * 
+     * @return \callable
+     */
+    protected function resolveAuthCallback($user, $ability, array $arguments)
+    {
+        if ($this->firstArgumentToPolicy($arguments)) {
+            return $this->resolvePolicyCallback($user, $ability, $arguments);
+        }
+        elseif (isset($this->abilities[$ability])) {
+            return $this->abilities[$ability];
+        }
 
+        return function ()
+        {
+            return false;
+        };
+    }
+    
+    /**
+     * Determine if the first argument in the array corresponds to a policy.
+     * 
+     * @param  array  $arguments
+     * 
+     * @return bool
+     */
+    protected function firstArgumentToPolicy(array $arguments): bool
+    {
+        if ( ! isset($arguments[0])) {
+            return false;
+        }
+        
+        $argument = $arguments[0];
+        
+        if (is_object($argument)) {
+            $class = getClass($argument, true);
+            
+            return isset($this->policies[$class]);
+        }
+        
+        return is_string($argument) && isset($this->policies[$argument]);
     }
     
     /**
