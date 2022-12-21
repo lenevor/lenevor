6188ae6835db97fc5e49a1d58fc0c270fbc02b5d
Alexander Campo <jalexcam@gmail.com>
2022-12-21 11:21:35 -0500

    Removed the properties phpdoc of author in the heading of class

--- syscodes/src/components/Contracts/Http/Lenevor.php
+++ syscodes/src/components/Contracts/Http/Lenevor.php
@@ -24,8 +24,6 @@ namespace Syscodes\Components\Contracts\Http;
 
 /**
  * Handles a Request to convert it to a Response.
- * 
- * @author Alexander Campo <jalexcam@gmail.com>
  */
 interface Lenevor
 {
