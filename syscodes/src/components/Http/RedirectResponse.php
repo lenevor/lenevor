68e64ebdc93529f3d016facbb1235819da1caeb4
Alexander Campo <jalexcam@gmail.com>
2023-01-10 16:48:16 -0500

    Update RedirectResponse.php

--- syscodes/src/components/Http/RedirectResponse.php
+++ syscodes/src/components/Http/RedirectResponse.php
@@ -22,14 +22,26 @@
 
 namespace Syscodes\Components\Http;
 
-use InvalidArgumentException;
+use Syscodes\Components\Support\Str;
+use Syscodes\Components\Http\Request;
+use Syscodes\Components\Support\Traits\Macroable;
+use Syscodes\Components\Http\Concerns\HttpResponse;
+use Syscodes\Components\Support\Traits\ForwardsCalls;
+use Syscodes\Components\Session\Store as SessionStore;
+use Syscodes\Components\Http\Response\RedirectResponseHeader as BaseRedirectResponse;
 
 /**
  * Redirects to another URL. Sets the redirect header, sends the headers and exits.
  * Can redirect via a Location header or using a Refresh header.
  */
-class RedirectResponse extends Response
+class RedirectResponse extends BaseRedirectResponse
 {
+    use ForwardsCalls,
+        HttpResponse,
+        Macroable {
+            __call as macroCall;
+        }
+
     /**
      * The request instance.
      * 
@@ -38,104 +50,46 @@ class RedirectResponse extends Response
     protected $request;
 
     /**
-     * The target URL.
+     * The session store implementation.
      * 
-     * @var string $targetUrl
+     * @var \Syscodes\Components\Session\Store $session
      */
-    protected $targetUrl;
-
-    /**
-     * Constructor. Creates a redirect response so that it conforms to the rules 
-     * defined for a redirect status code.
-     * 
-     * @param  string  $url  The URL to redirect to
-     * @param  int  $status  The redirect status code  
-     * @param  array  $headers  The header array
-     * 
-     * @return void
-     * 
-     * @throws \InvalidArgumentException
-     */
-    public function __construct(?string $url, int $status = 302, array $headers = [])
-    {
-        if (null === $url) {
-            @trigger_error(sprintf('Passing a null url when instantiating a "%s"', __CLASS__), E_USER_DEPRECATED);
-            $url = '';
-        }
-
-        parent::__construct('', $status, $headers);
-
-        $this->setTargetUrl($url);
-
-        if ( ! $this->isRedirect()) {
-            throw new InvalidArgumentException(sprintf('The HTTP status code is not a redirect ("%s" given).', $status));
-        }
-
-        // Loaded the headers and status code
-        $this->send(true);
-
-        // Terminate the current script 
-        exit;
-    }
-
-    /**
-     * Creates an instance of the same redirect class for rendering URL's to the url, method rules defined
-     * status code and headers.
-     * 
-     * @param  mixed  $url  The URL to redirect to
-     * @param  int  $status  The HTTP response status for this response  
-     * @param  array  $headers  Array of HTTP headers for this response
-     * 
-     * @return static
-     */
-    public static function render($url = '', $status = 302, $headers = [])
-    {
-        return new static($url, $status, $headers);
-    }
-
-    /**
-     * Returns the target URL.
-     * 
-     * @return string
-     */
-    public function getTargetUrl(): string
-    {
-        return $this->targetUrl;
-    }
-
-    /**
-    * Redirects to another url. Sets the redirect header, sends the headers and exits.
-    * Can redirect via a Location header.
-    *
-    * @param  string  $url  The url
-    *
-    * @return self
-    */
-    public function setTargetUrl($url): self
-    {
-        if ('' === ($url ?? '')) {
-            throw new InvalidArgumentException('Cannot redirect to an empty URL');
-        }
-        
-        $this->targetUrl = $url;
-        
-        $this->setContent(sprintf('<!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="UTF-8" />
-            <meta http-equiv="refresh" content="0;url=%1$s" />
-            <title>Redirecting to %1$s</title>
-        </head>
-        <body>
-            Redirecting to <a href="%1$s">%1$s</a>.
-        </body>
-    </html>', htmlspecialchars($url, ENT_QUOTES, 'UTF-8')));
+    protected $session;
     
-        $this->headers->set("Location", $url);
+    /**
+     * Flash a piece of data to the session.
+     * 
+     * @param  string|array  $key
+     * @param  mixed  $value
+     * 
+     * @return self
+     */
+    public function with($key, $value = null): self
+    {
+        $key = is_array($key) ? $key : [$key => $value];
+        
+        foreach ($key as $k => $v) {
+            $this->session->flash($k, $v);
+        }
         
         return $this;
     }
     
+    /**
+     * Add multiple cookies to the response.
+     * 
+     * @param  array  $cookies
+     * 
+     * @return self
+     */
+    public function withCookies(array $cookies): self
+    {
+        foreach ($cookies as $cookie) {
+            $this->headers->setCookie($cookie);
+        }
+        
+        return $this;
+    }
     
     /**
      * Gets the Request instance.
@@ -157,5 +111,52 @@ class RedirectResponse extends Response
     public function setRequest(Request $request): void
     {
         $this->request = $request;
+    }
+    
+    /**
+     * Get the session store instance.
+     * 
+     * @return \Syscodes\Components\Session\Store|null
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
+    
+    /**
+     * Set the session store instance.
+     * 
+     * @param  \Syscodes\Components\Session\Store  $session
+     * 
+     * @return void
+     */
+    public function setSession(SessionStore $session)
+    {
+        $this->session = $session;
+    }
+    
+    /**
+     * Magic method.
+     * 
+     * Dynamically bind flash data in the session.
+     * 
+     * @param  string  $method
+     * @param  array  $parameters
+     * 
+     * @return mixed
+     * 
+     * @throws \BadMethodCallException
+     */
+    public function __call($method, $parameters)
+    {
+        if (static::hasMacro($method)) {
+            return $this->macroCall($method, $parameters);
+        }
+        
+        if (Str::startsWith($method, 'with')) {
+            return $this->with(Str::snake(substr($method, 4)), $parameters[0]);
+        }
+        
+        static::throwBadMethodCallException($method);
     }
 }
