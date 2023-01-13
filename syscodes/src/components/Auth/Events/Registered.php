694a86e11e2799294b6352fc7c884d91d9402b12
Alexander Campo <jalexcam@gmail.com>
2023-01-13 18:08:58 -0500

    Create Registered.php

---
+++ syscodes/src/components/Auth/Events/Registered.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Auth\Events;
+
+/**
+ * The registered from the user provider.
+ */
+class Registered
+{
+    /**
+     * The authenticated user.
+     * 
+     * @var \Syscodes\Components\Contracts\Auth\Authenticatable $user
+     */
+    public $user;
+
+    /**
+     * Constructor. Create a new Registered class instance.
+     * 
+     * @param  \Syscodes\Components\Contracts\Auth\Authenticatable  $user
+     * 
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+}
