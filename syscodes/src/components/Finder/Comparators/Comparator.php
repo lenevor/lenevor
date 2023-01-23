e8cf5223fca1b51b165d7457cf2c89bb4bf4335e
Alexander Campo <jalexcam@gmail.com>
2023-01-23 12:23:56 -0500

    Create Comparator.php

---
+++ syscodes/src/components/Finder/Comparators/Comparator.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Finder\Comparators;
+
+use InvalidArgumentException;
+
+/**
+ * Allows realize the comparison operator.
+ */
+class Comparator
+{
+    /**
+     * The type of comparison operator.
+     * 
+     * @var string $operator
+     */
+    private string $operator;
+
+    /**
+     * Gets the target.
+     * 
+     * @var string $target
+     */
+    private string $target;
+
+    /**
+     * Constructor. Create a new Comparator class instance.
+     * 
+     * @return void
+     */
+    public function __construct(string $target, string $operator = '==')
+    {
+        if ( ! in_array($operator, ['>', '<', '>=', '<=', '==', '!='])) {
+            throw new InvalidArgumentException(sprintf('Invalid operator "%s"', $operator));
+        }
+
+        $this->target   = $target;
+        $this->operator = $operator;        
+    }
+
+    /**
+     * Gets the type of comparison target.
+     * 
+     * @return string
+     */
+    public function getOperator(): string
+    {
+        return $this->operator;
+    }
+
+    /**
+     * Gets the target value.
+     * 
+     * @return string
+     */
+    public function getTarget(): string
+    {
+        return $this->target;
+    }
+}
