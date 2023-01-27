f5bea361200edad0cfd3ef27c6e5359c9779b5d9
Alexander Campo <jalexcam@gmail.com>
2023-01-26 23:44:50 -0500

    Create DirectoryFilterIterator.php

---
+++ syscodes/src/components/Finder/Filters/DirectoryFilterIterator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Syscodes\Components\Finder\Filters;
+
+use RecursiveFilterIterator;
+use Syscodes\Components\Finder\SplFileInfo;
+
+class DirectoryFilterIterator extends RecursiveFilterIterator
+{
+    /**
+     * Constructor. Create a new DirectoryFilterIterator class instance.
+     * 
+     * @param  string|\RecursiveIterator  $path
+     * @param  int  $flags
+     * @param  bool  $ignoreDirs
+     */
+    public function __construct(string $path, int $flags, bool $ignoreDirs = false)
+    {
+        parent::__construct($path, $flags);
+    }
+
+    public function accept(): bool 
+    {
+        return true;
+    }
+
+    /**
+     * Return an instance of SplFileInfo with support for relative paths.
+     */
+    public function current(): SplFileInfo
+    {
+        return new SplFileInfo('', '', '');
+    }
+}
