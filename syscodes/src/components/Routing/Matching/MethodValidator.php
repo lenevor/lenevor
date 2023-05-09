b0675326f3280f9043ba1448ea97112bfe36bd28
Alexander Campo <jalexcam@gmail.com>
2023-05-09 00:52:55 -0500

    Created a new class for validate rules

---
+++ syscodes/src/components/Routing/Matching/MethodValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Routing\Matching;
+
+use Syscodes\Components\Http\Request;
+use Syscodes\Components\Routing\Route;
+
+/**
+ * Allows validate the method with given rules.
+ */
+class MethodValidator implements ValidatorInterface
+{
+    /**
+     * Validate a given rule against a route and request.
+     * 
+     * @param  \Syscodes\Components\Routing\Route  $route
+     * @param  \Syscodes\Components\Http\Request  $request
+     * 
+     * @return bool
+     */
+    public function matches(Route $route, Request $request): bool
+    {
+        return in_array($request->getMethod(), $route->getMethod());
+    }
+}
