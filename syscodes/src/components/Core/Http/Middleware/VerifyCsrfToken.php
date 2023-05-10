150db3766a4cfb9c658fbd57630a8d8eda95d8ac
Alex McArrow <alex@mcarrow.ru>
2023-05-11 00:32:49 +0500

    Update VerifyCsrfToken.php
    Fix namespace lowercase

--- syscodes/src/components/Core/Http/Middleware/VerifyCsrfToken.php
+++ syscodes/src/components/Core/Http/Middleware/VerifyCsrfToken.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Components\core\Http\Middleware;
+namespace Syscodes\Components\Core\Http\Middleware;
 
 use Closure;
 use Syscodes\Components\Http\Cookie;
