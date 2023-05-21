32e1601ca5de628295c3d859d0845b1f8c67c8a4
Alexander Campo <jalexcam@gmail.com>
2023-05-20 23:58:26 -0500

    Update BaseRouteCollection.php

--- syscodes/src/components/Routing/Collections/BaseRouteCollection.php
+++ syscodes/src/components/Routing/Collections/BaseRouteCollection.php
@@ -28,6 +28,7 @@ use ArrayIterator;
 use IteratorAggregate;
 use Syscodes\Components\Http\Request;
 use Syscodes\Components\Contracts\Support\Arrayable;
+use Syscodes\Components\Routing\Matching\UriMatches;
 use Syscodes\Components\Core\Http\Exceptions\NotFoundHttpException;
 
 /**
@@ -39,21 +40,49 @@ abstract class BaseRouteCollection implements Countable, IteratorAggregate
      * Handle the matched route.
      * 
      * @param  \Syscodes\Components\Http\Request  $request
-     * @param  \Syscodes\Components\Routing\Route|null  $route
+     * @param  \Syscodes\Components\Routing\Route|array|null  $routes
      * 
      * @return \Syscodes\Components\Routing\Route
      * 
      * @throws \Syscodes\Components\Core\Http\Exceptions\NotFoundHttpException
      */
-    public function handleMatchedRoute(Request $request, $route)
+    public function handleMatchedRoute(Request $request, $routes)
     {
-        if ( ! is_null($route)) {
-            return $route->bind($request);
+        if ( ! is_null($routes)) {
+             // Loop trough the possible routes
+            foreach ($routes as $route) {   
+                $host = $route->getHost();
+
+                if ($host !== null && $host != $request->getHost()) {
+                    continue;
+                }
+                
+                $scheme = $route->getScheme();
+                
+                if ($scheme !== null && $scheme !== $request->getScheme()) {
+                    continue;
+                }
+                
+                $port = $route->getPort();
+                
+                if ($port !== null && $port !== $request->getPort()) {
+                    continue;
+                }
+
+                $parameters = [];
+
+                $path = rtrim($request->path(), '/');
+                
+                // If the requested route one of the defined routes
+                if (UriMatches::compareUri($route->uri, $path, $parameters, $route->wheres)) {
+                    return $route->bind($request);
+                }
+            }
         }
 
         throw new NotFoundHttpException(sprintf(
             'The route "%s" could not be found', $request->path()
-            ));
+        ));
     }
 
     /*
