7f997e25c30c25a3de10e141fd9956aa7938b090
Alexander Campo <jalexcam@gmail.com>
2023-05-23 23:26:09 -0500

    Changed the namespace

--- syscodes/src/components/Auth/Access/Concerns/HandlesAuthorization.php
+++ syscodes/src/components/Auth/Access/Concerns/HandlesAuthorization.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Components\Auth\Concerns;
+namespace Syscodes\Components\Auth\Access\Concerns;
 
 use Syscodes\Components\Auth\Access\Response;
 
