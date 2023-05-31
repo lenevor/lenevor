3e2db5bdd2bd5b871f1ef507afb7fba3b6df1b71
Alexander Campo <jalexcam@gmail.com>
2023-05-31 01:31:00 -0500

    Added new methods for returns the compilation of pattern in the routes

--- syscodes/src/components/Routing/Generator/RouteCompiler.php
+++ syscodes/src/components/Routing/Generator/RouteCompiler.php
@@ -24,9 +24,11 @@ namespace Syscodes\Components\Routing\Generator;
 
 use LogicException;
 use DomainException;
+use InvalidArgumentException;
 use Syscodes\Components\Support\Arr;
 use Syscodes\Components\Support\Str;
 use Syscodes\Components\Routing\Route;
+use Syscodes\Components\Routing\Collections\CompiledRouteCollection;
 
 /**
  * Allows compile the route patterns.
@@ -51,59 +53,73 @@ class RouteCompiler
      * 
      * @return string
      * 
-     * @throws \LogicException|\DomainException
+     * @throws \LogicException|\DomainException|\InvalidArgumentException
      */
-    public static function compile(Route $route): string
+    public static function compile(Route $route): CompiledRouteCollection
     {
-        $uri       = $route->getRoute();
-        $patterns  = $route->getPatterns();
-        $optionals = 0;
+        $hostVariables = [];
         $variables = [];
-        echo static::compilePattern($route, $uri, true);
-        $pattern = preg_replace_callback('~/\{(.*?)(\?)?\}~', function ($matches) use ($uri, $patterns, &$optionals, &$variables) {
-            list(, $name, $optional) = array_pad($matches, 3, false);
-            
-            if (in_array($name, $variables)) {
-                throw new LogicException("Route pattern [{$uri}] cannot reference variable name [{$name}] more than once");
-            } elseif (strlen($name) > 32) {
-                throw new DomainException("Variable name [{$name}] cannot be longer than 32 characters in route pattern [{$uri}]");
-            } elseif (preg_match('/^\d/', $name) === 1) {
-                throw new DomainException("Variable name [{$name}] cannot start with a digit in route pattern [{$uri}]");
-            }
-            
-            $variables[] = $name;
+        $hostRegex = null;
+        $hostTokens = [];
 
-            $pattern = Arr::get($patterns, $name, '[^/]+');
-            
-            if ($optional) {
-                $optionals++;
-                
-                return sprintf('(?:/(?P<%s>%s)', $name, $pattern);
-            } elseif ($optionals > 0) {
-                throw new LogicException("Route pattern [{$pattern}] cannot reference standard variable [{$name}] after optionals");
+        if ('' !== $host = $route->getHost()) {
+            $result = self::compilePattern($route, $host, true);
+
+            $hostVariables = $result['variables'];
+            $variables = $hostVariables;
+
+            $hostTokens = $result['tokens'];
+            $hostRegex = $result['regex'];
+        }
+
+        $path = $route->getRoute();
+
+        $result = static::compilePattern($route, $path, false);
+
+        $staticPrefix = $result['staticPrefix'];
+
+        $pathVariables = $result['variables'];
+
+        foreach ($pathVariables as $pathParam) {
+            if ('_fragment' === $pathParam) {
+                throw new InvalidArgumentException(
+                    sprintf('Route pattern "%s" cannot contain "_fragment" as a path parameter.', $route->getRoute())
+                );
             }
-            
-            return sprintf('/(?P<%s>%s)', $name, $pattern);
-        }, $uri);
-       
-        return sprintf('~^%s%s~sDu', $pattern, str_repeat(')?', $optionals));
+        }
+
+        $variables = array_merge($variables, $pathVariables);
+
+        $tokens = $result['tokens'];
+        $regex = $result['regex'];
+
+        return new CompiledRouteCollection(
+            $staticPrefix,
+            $regex,
+            $tokens,
+            $pathVariables,
+            $hostRegex,
+            $hostTokens,
+            $hostVariables,
+            array_unique($variables)
+        );
     }
 
     /**
      * The compile pattern for iterate over variables in the routes.
      * 
      * @param  \Syscodes\Components\Routing\Route  $route
-     * @param  string  $pattern
+     * @param  string|null  $pattern
      * @param  bool  $isHost
      * 
      * @return array
      */
-    private static function compilePattern(Route $route, string $pattern, bool $ishost)
+    private static function compilePattern(Route $route, ?string $pattern, bool $isHost): array
     {
         $tokens           = [];
         $variables        = [];
         $pos              = 0;
-        $dafaultSeparator = $ishost ? '.' : '/';
+        $defaultSeparator = $isHost ? '.' : '/';
         $useUtf8          = preg_match('//u', $pattern);
 
         if ($useUtf8 && preg_match('~[\x80-\xFF]~', $pattern)) {
@@ -131,12 +147,12 @@ class RouteCompiler
             } elseif ($useUtf8) {
                 preg_match('/.$/u', $precedingText, $precedingChar);
 
-                $preceddingChar = $precedingChar[0];
+                $precedingChar = $precedingChar[0];
             } else {
-                $preceddingChar = substr($precedingText, -1);
+                $precedingChar = substr($precedingText, -1);
             }
 
-            $separator = '' != $preceddingChar && Str::contains(static::SEPARATOR, $preceddingChar);
+            $separator = '' != $precedingChar && Str::contains(static::SEPARATOR, $precedingChar);
 
             if (preg_match('~^\d~', $varName) === 1) {
                 throw new DomainException(
@@ -162,7 +178,156 @@ class RouteCompiler
                 $tokens[] = ['text', $precedingText];
             }
             
-            $regex = $route->setPattern($varName);         
+            $regex = $route->setPattern($varName);
+            
+            if (null === $regex) {
+                $following = (string) substr($pattern, $pos);
+
+                $nextSeparator = static::findNextStaticSeparator($following, $useUtf8);
+
+                $regex = sprintf(
+                    '[^%s%s]+',
+                    preg_quote($defaultSeparator),
+                    $defaultSeparator !== $nextSeparator && '' !== $nextSeparator ? preg_quote($nextSeparator) : ''
+                );
+
+                if (('' !== $nextSeparator && ! preg_match('~^\{[\w\x80-\xFF]+\}~', $following)) || '' === $following) {
+                    $regex .= '+';
+                }
+            } else {
+                if ( ! preg_match('//u', $regex)) {
+                    $useUtf8 = false;
+                }
+
+                $regex = static::groupsToNonCapturings($regex);
+            }
+            
+            if ($main) {
+                $token = ['variable', $separator ? $precedingChar : '', $regex, $varName, false, true];
+            } else {
+                $token = ['variable', $separator ? $precedingChar : '', $regex, $varName];
+            }
+            
+            $tokens[]    = $token;
+            $variables[] = $varName;
         }
+        
+        if ($pos < strlen($pattern)) {
+            $tokens[] = ['text', substr($pattern, $pos)];
+        }
+        
+        // find the first optional token
+        $firstOptional = PHP_INT_MAX;
+        
+        // compute the matching regexp
+        $regex = '';
+
+        for ($i = 0, $nbToken = count($tokens); $i < $nbToken; ++$i) {
+            $regex .= static::CheckTokenRegex($tokens, $i, $firstOptional);
+        }
+
+        $regex = '{^/'.$regex.'$}sD'.($isHost ? 'i' : '');
+
+        return [
+            'staticPrefix' => $regex,
+            'regex' => $regex,
+            'tokens' => array_reverse($tokens),
+            'variables' => $variables,
+        ];
+    }
+
+    /**
+     * Returns the next static character in the Route pattern that will serve as a separator.
+     * 
+     * @param  string  $pattern
+     * @param  bool  $useUtf8
+     * 
+     * @return string
+     */
+    private static function findNextStaticSeparator(string $pattern, bool $useUtf8): string
+    {
+        if ('' === $pattern) {
+            return '';
+        }
+
+        if ('' === $pattern = preg_replace('~\{[\w\x80-\xFF]+\}~', '', $pattern)) {
+            return '';
+        }
+
+        if ($useUtf8) {
+            preg_match('/^./u', $pattern, $pattern); 
+        }
+
+        return Str::contains(self::SEPARATOR, $pattern[0]) ? $pattern[0] : '';
+    }
+    
+    /**
+     * Checks the regex used to match a specific token. It can be static text or a subpattern.
+     * 
+     * @param  array  $tokens  The route tokens
+     * @param  int  index  The index of the current token
+     * @param  int  $firstOptional  The index of the first optional token
+     * 
+     * @return string
+     */
+    private static function CheckTokenRegex(array $tokens, int $index, int $firstOptional): string
+    {
+        $token = $tokens[$index];
+        
+        if ('text' === $token[0]) {
+            // Text tokens
+            return preg_quote($token[1]);
+        } else {
+            // Variable tokens
+            if (0 === $index && 0 === $firstOptional) {
+                // When the only token is an optional variable token, the separator is required
+                return sprintf('%s(?P<%s>%s)?', preg_quote($token[1]), $token[3], $token[2]);
+            } else {
+                $regexp = sprintf('%s(?P<%s>%s)', preg_quote($token[1]), $token[3], $token[2]);
+                
+                if ($index >= $firstOptional) {
+                    $regexp = "(?:$regexp";
+                    $nbTokens = count($tokens);
+                    
+                    if ($nbTokens - 1 == $index) {
+                        // Close the optional subpatterns
+                        $regexp .= str_repeat(')?', $nbTokens - $firstOptional - (0 === $firstOptional ? 1 : 0));
+                    }
+                }
+                
+                return $regexp;
+            }
+        }
+    }
+
+    /**
+     * The groups non capturings of regex.
+     * 
+     * @param  string  $regex
+     * 
+     * @return string
+     */    
+    private static function groupsToNonCapturings(string $regex): string
+    {
+        for ($i = 0; $i < strlen($regex); ++$i) {
+            if ('\\' === $regex[$i]) {
+                ++$i;
+                continue;
+            }
+            
+            if ('(' !== $regex[$i] || ! isset($regex[$i + 2])) {
+                continue;
+            }
+            
+            if ('*' === $regex[++$i] || '?' === $regex[$i]) {
+                ++$i;
+                continue;
+            }
+            
+            $regex = substr_replace($regex, '?:', $i, 0);
+            ++$i;
+        }
+        
+        return $regex;
     }
 }
