2e953d3ab56bb41dfd8f4994558a3f9d8ea28118
Alexander Campo <jalexcam@gmail.com>
2023-06-08 19:46:08 -0500

    Create UrlGenerator.php

---
+++ syscodes/src/components/Contracts/Routing/UrlGenerator.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Contracts\Routing;
+
+/**
+ * Returns the URL generated.
+ */
+interface UrlGenerator
+{
+    /**
+     * Get the current URL for the request.
+     * 
+     * @return string
+     */
+    public function current(): string;
+    
+    /**
+     * Get the URL for the previous request.
+     * 
+     * @param  mixed  $fallback  
+     * 
+     * @return string
+     */
+    public function previous($fallback = false): string;
+
+    /**
+     * Generate a absolute URL to the given path.
+     * 
+     * @param  string  $path
+     * @param  mixed  $options
+     * @param  bool|null  $secure
+     * 
+     * @return string
+     */
+    public function to($path, $options = [], $secure = null): string;
+
+    /**
+     * Generate a secure, absolute URL to the given path.
+     * 
+     * @param  string  $path
+     * @param  array  $parameters
+     * 
+     * @return string
+     */
+    public function secure($path, $parameters = []): string;
+
+    /**
+     * Generate a URL to an application asset.
+     * 
+     * @param  string  $path
+     * @param  bool|null  $secure  
+     * 
+     * @return string
+     */
+    public function asset($path, $secure = null): string;
+
+    /**
+     * Get the URL to a named route.
+     * 
+     * @param  string  $name
+     * @param  array  $parameters
+     * @param  bool  $forced 
+     * 
+     * @return string
+     * 
+     * @throws \InvalidArgumentException
+     */
+    public function route($name, array $parameters = [], $forced = true): string;
+
+    /**
+     * Get the URL to a controller action.
+     * 
+     * @param  string  $action
+     * @param  mixed  $parameters
+     * @param  bool  $forced  
+     * 
+     * @return string
+     * 
+     * @throws \InvalidArgumentException
+     */
+    public function action($action, $parameters = [], $forced = true): string;
+}
