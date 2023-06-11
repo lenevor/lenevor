6a0c845ddb9578c3f5d345510dd288aaa6759f59
Alexander Campo <jalexcam@gmail.com>
2023-06-11 03:13:57 -0500

    Create Util.php

---
+++ syscodes/src/components/Container/Util.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Container;
+
+use ReflectionNamedType;
+
+/**
+ * @internal
+ */
+class Util
+{
+    /**
+     * Get the class name of the given parameter's type.
+     * 
+     * @param  \ReflectionParameter  $parameter
+     * 
+     * @return string|null
+     */
+    public static function getParameterClassName($parameter): string|null
+    {
+        $type = $parameter->getType();
+        
+        if ( ! $type instanceof ReflectionNamedType || $type->isBuiltin()) {
+            return null;
+        }
+        
+        $name = $type->getName();
+        
+        if (! is_null($class = $parameter->getDeclaringClass())) {
+            if ($name === 'self') {
+                return $class->getName();
+            }
+            
+            if ($name === 'parent' && $parent = $class->getParentClass()) {
+                return $parent->getName();
+            }
+        }
+        
+        return $name;
+    }
+}
