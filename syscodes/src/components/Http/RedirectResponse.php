cdf77d04437cf98c1c79553edb15502f352e6314
Alexander Campo <jalexcam@gmail.com>
2023-06-24 17:01:31 -0500

    Update RedirectResponse.php

--- syscodes/src/components/Http/RedirectResponse.php
+++ syscodes/src/components/Http/RedirectResponse.php
@@ -22,22 +22,22 @@
 
 namespace Syscodes\Components\Http;
 
+use InvalidArgumentException;
 use Syscodes\Components\Support\Str;
 use Syscodes\Components\Http\Request;
 use Syscodes\Components\Support\MessageBag;
 use Syscodes\Components\Support\ViewErrorBag;
 use Syscodes\Components\Support\Traits\Macroable;
-use Syscodes\Components\Http\Concerns\HttpResponse;
+use Syscodes\Components\Http\Resources\HttpResponse;
 use Syscodes\Components\Support\Traits\ForwardsCalls;
 use Syscodes\Components\Session\Store as SessionStore;
 use Syscodes\Components\Contracts\Support\MessageProvider;
-use Syscodes\Components\Http\Response\RedirectResponseHeader as BaseRedirectResponse;
 
 /**
  * Redirects to another URL. Sets the redirect header, sends the headers and exits.
  * Can redirect via a Location header or using a Refresh header.
  */
-class RedirectResponse extends BaseRedirectResponse
+class RedirectResponse extends Response
 {
     use ForwardsCalls,
         HttpResponse,
@@ -58,6 +58,106 @@ class RedirectResponse extends BaseRedirectResponse
      * @var \Syscodes\Components\Session\Store $session
      */
     protected $session;
+
+    /**
+     * The target URL.
+     * 
+     * @var string $targetUrl
+     */
+    protected $targetUrl;
+
+    /**
+     * Constructor. Creates a redirect response so that it conforms to the rules 
+     * defined for a redirect status code.
+     * 
+     * @param  string  $url  The URL to redirect to
+     * @param  int  $status  The redirect status code  
+     * @param  array  $headers  The header array
+     * 
+     * @return void
+     * 
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(?string $url, int $status = 302, array $headers = [])
+    {
+        if (null === $url) {
+            @trigger_error(sprintf('Passing a null url when instantiating a "%s"', __CLASS__), E_USER_DEPRECATED);
+            
+            $url = '';
+        }
+
+        parent::__construct('', $status, $headers);
+
+        $this->setTargetUrl($url);
+
+        if ( ! $this->isRedirect()) {
+            throw new InvalidArgumentException(sprintf('The HTTP status code is not a redirect ("%s" given).', $status));
+        }
+
+        // Loaded the headers and status code
+        $this->send(true);
+
+        // Terminate the current script 
+        exit;
+    }
+
+    /**
+     * Creates an instance of the same redirect class for rendering URL's to the url, method rules defined
+     * status code and headers.
+     * 
+     * @param  mixed  $url  The URL to redirect to
+     * @param  int  $status  The HTTP response status for this response  
+     * @param  array  $headers  Array of HTTP headers for this response
+     * 
+     * @return static
+     */
+    public static function render($url = '', $status = 302, $headers = []): static
+    {
+        return new static($url, $status, $headers);
+    }
+
+    /**
+     * Returns the target URL.
+     * 
+     * @return string
+     */
+    public function getTargetUrl(): string
+    {
+        return $this->targetUrl;
+    }
+
+    /**
+    * Redirects to another url. Sets the redirect header, sends the headers and exits.
+    * Can redirect via a Location header.
+    *
+    * @param  string  $url  The url
+    *
+    * @return static
+    */
+    public function setTargetUrl($url): static
+    {
+        if ('' === ($url ?? '')) {
+            throw new InvalidArgumentException('Cannot redirect to an empty URL');
+        }
+        
+        $this->targetUrl = $url;
+        
+        $this->setContent(sprintf('<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="refresh" content="0;url=%1$s" />
+            <title>Redirecting to %1$s</title>
+        </head>
+        <body>
+            Redirecting to <a href="%1$s">%1$s</a>.
+        </body>
+    </html>', htmlspecialchars($url, ENT_QUOTES, 'UTF-8')));
+    
+        $this->headers->set("Location", $url);
+        
+        return $this;
+    }
     
     /**
      * Flash a piece of data to the session.
