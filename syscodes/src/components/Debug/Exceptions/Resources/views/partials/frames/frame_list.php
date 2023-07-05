ba99c8c56f0e32fe50c9d418f6da3839c334854b
Alexander Campo <jalexcam@gmail.com>
2023-07-04 22:55:09 -0500

    Spacing

--- syscodes/src/components/Debug/Exceptions/Resources/views/partials/frames/frame_list.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/partials/frames/frame_list.php
@@ -1,14 +1,10 @@
 <aside class="frame-container-scroll scrollbar">
 <?php foreach ($frames as $index => $frame) : ?>
-	<div class="frame frame-application <?= ($index == 0) ? 'active' : '' ?>" data-index="<?= $index ?>">
-		
+	<div class="frame frame-application <?= ($index == 0) ? 'active' : '' ?>" data-index="<?= $index ?>">		
 		<div class="frame-main-index">
 			<div class="frame-index"><?= (count($frames) - $index - 1) + 1 ?></div>
-		</div>
-			
-		<div class="frame-method-info">
-			<span class="frame-file"><?= $frame->getFile() ? $frame->getFile() : '<#unknown>'?></span>
-			
+		</div>			
+		<div class="frame-method-info">			
 			<div class="frame-info-class">
 			<?php if ($frame->getClass() == '') : ?>
 				<span class="frame-function"><?= e($frame->getFunction()) ?></span>
@@ -20,11 +16,9 @@
 			<?php endif ?>
 			</div>
 		</div>
-
 		<div class="frame-line-number">
 			<span class="frame-line"><?= (int) $frame->getLine() ?></span>
 		</div>		
-		
 	</div>		
 <?php endforeach; ?>	
 </aside>
