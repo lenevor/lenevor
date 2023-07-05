f3563491abeeec804708c7869fbc9153e634ef5f
Alexander Campo <jalexcam@gmail.com>
2023-07-04 22:59:23 -0500

    Added new item of menu

--- syscodes/src/components/Debug/Exceptions/Resources/views/partials/header.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/partials/header.php
@@ -1,11 +1,20 @@
 <header>
 	<div class="container">
-		<ul>
-			<li>
-				<a href="#stack" target="_self">
-					<?= e(__('exception.stack')) ?>
-				</a>
-			</li>
-		</ul>
+		<nav>
+			<ul>
+				<li>
+					<span class="icon-stack"></span>
+					<a href="#stack" target="_self" class="">
+						<?= e(__('exception.stack')) ?>
+					</a>
+				</li>
+				<li>
+					<span class="icon-detail"></span>
+					<a href="#details" target="_self" class="">
+						<?= e(__('exception.details')) ?>
+					</a>
+				</li>
+			</ul>
+		</nav>
 	</div>
 </header>
