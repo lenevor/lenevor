ba6c57c4786ae5f505fbc7d1dba596809757510c
Alexander Campo <jalexcam@gmail.com>
2023-07-12 02:36:26 -0500

    Formatting

--- syscodes/src/components/Debug/Exceptions/Resources/views/partials/info/info_exception.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/partials/info/info_exception.php
@@ -1,12 +1,14 @@
 <section class="exception"> 
 	<div class="exception-title">
+		<div class="exception-background-title">
 		<?php foreach ($class as $i => $name) : ?>
-			<?php if ($i == count($class) - 1): ?>
-		<h2><?= $template->escape($name) ?></h2> 
+			<?php if ($i == count($class) - 1): ?>		
+			<h2><?= $template->escape($name) ?></h2> 
 			<?php else: ?>
-		<h2><?= $template->escape($name).'&nbsp;\\' ?></h2>
+			<h2><?= $template->escape($name).'&nbsp;\\&nbsp;' ?></h2>
 			<?php endif; ?>
 		<?php endforeach; ?>
+		</div>
 		<?php if ($code): ?>
 		<span class="subtitle" title="Exception Code"><?= $template->escape($code) ?></span>
 		<?php endif ?>
