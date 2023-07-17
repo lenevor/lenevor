a6acf8da27203bc7b08059365ea08e9f377bb49c
Alexander Campo <jalexcam@gmail.com>
2023-07-17 03:29:16 -0500

    Created template

---
+++ syscodes/src/components/Debug/Exceptions/Resources/views/partials/request_info.php
@@ -0,0 +1,7 @@
+<section class="">
+    <a id="" class=""></a>
+    <section class="section-details">
+        <?= $template->render($navigation) ?>
+        <?= $template->render($section_detail_context) ?>
+    </section>
+</section>
