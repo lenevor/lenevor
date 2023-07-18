331a24bc8f5d80930c8bced4562a8a512cd44313
Alexander Campo <jalexcam@gmail.com>
2023-07-18 01:45:11 -0500

    Added new template

---
+++ syscodes/src/components/Debug/Exceptions/Resources/views/partials/updown/header.php
@@ -0,0 +1,24 @@
+<header>
+	<div class="container">
+		<nav>
+			<ul>
+				<li>
+					<div class="container-title">
+						<i class="icon-stack"></i>
+						<a href="#stack" target="_self" class="">
+							<?= e(__('exception.stack')) ?>
+						</a>
+					</div>
+				</li>
+				<li>
+					<div class="container-title">
+						<i class="icon-detail"></i>
+						<a href="#details" target="_self" class="">
+							<?= e(__('exception.details')) ?>
+						</a>
+					</div>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</header>
