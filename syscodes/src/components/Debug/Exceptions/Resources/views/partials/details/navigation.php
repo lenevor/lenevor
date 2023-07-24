0b91a3a4f6630b26c7c8ef83074efb861a6ff963
Alexander Campo <jalexcam@gmail.com>
2023-07-24 12:10:32 -0500

    Updated content of the view

--- syscodes/src/components/Debug/Exceptions/Resources/views/partials/details/navigation.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/partials/details/navigation.php
@@ -74,16 +74,16 @@
             </li>
             <li>
                 <a href="#detail-context-data-version">
-                    <svg viewBox="4 0 24 24" xmlns="http://www.w3.org/2000/svg"> 
-                        <path d="M12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14Z" stroke-width="1.5"/>
-                        <path d="M14 6C12.8954 6 12 5.10457 12 4C12 2.89543 12.8954 2 14 2C15.1046 2 16 2.89543 16 4C16 5.10457 15.1046 6 14 6Z" stroke-width="1.5"/>
-                        <path d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z" stroke-width="1.5"/>
-                        <path d="M17.5 20L19 20M12 20L14.75 20" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-                        <path d="M6.5 4L5 4M12 4L9.25 4" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-                        <path d="M19 12H14" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-                        <path d="M19 4L16 4" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-                        <path d="M5 20L7.66667 20" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-                        <path d="M10 12L7.5 12M5.5 12L5 12" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                    <svg viewBox="8 0 24 24" xmlns="http://www.w3.org/2000/svg"> 
+                        <path d="M12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14Z" stroke-width="1"/>
+                        <path d="M14 6C12.8954 6 12 5.10457 12 4C12 2.89543 12.8954 2 14 2C15.1046 2 16 2.89543 16 4C16 5.10457 15.1046 6 14 6Z" stroke-width="1"/>
+                        <path d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z" stroke-width="1"/>
+                        <path d="M17.5 20L19 20M12 20L14.75 20" stroke-width="1" stroke-linecap="round"/>
+                        <path d="M6.5 4L5 4M12 4L9.25 4" stroke-width="1" stroke-linecap="round"/>
+                        <path d="M19 12H14" stroke-width="1" stroke-linecap="round"/>
+                        <path d="M19 4L16 4" stroke-width="1" stroke-linecap="round"/>
+                        <path d="M5 20L7.66667 20" stroke-width="1" stroke-linecap="round"/>
+                        <path d="M10 12L7.5 12M5.5 12L5 12" stroke-width="1" stroke-linecap="round"/>
                     </svg>
                     <?= e(__('exception.versions')) ?>
                 </a>
--- syscodes/src/components/Debug/Exceptions/Resources/views/partials/details/section_detail_context.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/partials/details/section_detail_context.php
@@ -1,8 +1,9 @@
 <section class="">
     <a id="details" class="scroll_detail"></a>
-    <section class="section-detail-context">
+    <section class="section-detail-request">
         <div class="section-info-request">
             <div class="info-request-title">
+                <a id="detail-request" class="scroll-target"></a>
                 <h2><?= e(__('exception.request')) ?></h2>
             </div>
             <div class="info-url-method">
@@ -11,6 +12,7 @@
             </div>
             <div class="info-header-title"> 
             <?php foreach ($tables as $data) : ?>
+                <a id="detail-request-header" class="scroll-target"></a>
                 <h2><?= e(__('exception.headers')) ?></h2>
                 <div class="data-table">
                     <table class="data-table">
@@ -31,6 +33,20 @@
                     </table>
                 </div>
             <?php endforeach; ?>   
+            </div>
+            <div class="info-body-title">
+                <a id="detail-request-body" class="scroll-target"></a>
+                <h2><?= e(__('exception.body')) ?></h2>
+                <pre>
+                    <span>[]</span>
+                </pre>
+            </div>
+        </div>
+    </section>
+    <section class="section-detail-app">
+        <div class="section-info-app">
+            <div class="info-app-title">
+                
             </div>
         </div>
     </section>
