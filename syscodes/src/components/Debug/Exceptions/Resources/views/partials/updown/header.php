794b2ef9314bd343e94ad3fd2c31f314b99ea73c
Alexander Campo <jalexcam@gmail.com>
2023-08-07 12:27:00 -0500

    Update header.php

--- syscodes/src/components/Debug/Exceptions/Resources/views/partials/updown/header.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/partials/updown/header.php
@@ -1,24 +1,33 @@
 <header>
-	<div class="container">
-		<nav>
-			<ul>
-				<li>
-					<div class="container-title">
-						<i class="icon-stack"></i>
-						<a href="#stack" target="_self" class="">
-							<?= e(__('exception.stack')) ?>
-						</a>
-					</div>
-				</li>
-				<li>
-					<div class="container-title">
-						<i class="icon-detail"></i>
-						<a href="#detail-request" target="_self" class="">
-							<?= e(__('exception.details')) ?>
-						</a>
-					</div>
-				</li>
-			</ul>
-		</nav>
+	<div class="margins">
+		<div class="container">
+			<nav>
+				<ul>
+					<li>
+						<div class="container-title">
+							<i class="icon-stack"></i>
+							<a href="#stack" target="_self" class="">
+								<?= e(__('exception.stack')) ?>
+							</a>
+						</div>
+					</li>
+					<li>
+						<div class="container-title">
+							<i class="icon-detail"></i>
+							<a href="#detail-request" target="_self" class="">
+								<?= e(__('exception.details')) ?>
+							</a>
+						</div>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="time">
+			<div class="message">
+				<div class="container">
+					<a href="#"><?= ucfirst($template->escape($message)) ?></a>
+				</div>
+			</div>
+		</div>
 	</div>
 </header>
