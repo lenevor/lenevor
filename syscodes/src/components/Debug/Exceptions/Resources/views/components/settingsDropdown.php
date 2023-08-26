316169f8202a453e5939e43d77d7f06a294d361a
Alexander Campo <jalexcam@gmail.com>
2023-08-26 01:30:41 -0500

    Update settingsDropdown.php

--- syscodes/src/components/Debug/Exceptions/Resources/views/components/settingsDropdown.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/components/settingsDropdown.php
@@ -4,5 +4,19 @@
             <h4>Gdebug Settings</h4>
             <a href="https://github.com/lenevor/docs/blob/master/gdebug.md">docs</a>
         </div>
+        <label for="editor">
+            <span class="editor-title">Editor</span>
+            <select name="editor-select" id="editor">
+                <option class="option-editor" value="sublime">Sublime</option>
+                <option class="option-editor" value="textmate">TextMate</option>
+                <option class="option-editor" value="emacs">Emacs</option>
+                <option class="option-editor" value="macvim">MacVim</option>
+                <option class="option-editor" value="phpstorm">PhpStorm</option>
+                <option class="option-editor" value="idea">Idea</option>
+                <option class="option-editor" value="netbeans">Netbeans</option>
+                <option class="option-editor" value="vscode">VS Code</option>
+                <option class="option-editor" value="atom">Atom</option>
+            </select>
+        </label>
     </div>
 </div>
