953ac30ea7b20b29329fa956b667a2f38fc96914
Alexander Campo <jalexcam@gmail.com>
2023-08-31 12:00:23 -0500

    Update settingsDropdown.php

--- syscodes/src/components/Debug/Exceptions/Resources/views/components/settingsDropdown.php
+++ syscodes/src/components/Debug/Exceptions/Resources/views/components/settingsDropdown.php
@@ -6,31 +6,29 @@
         </div>
         <label for="editor">
             <span class="editor-title">Editor</span>
-            <select name="editor-select" id="editor">
-                <option class="option-editor" value="sublime">Sublime</option>
-                <option class="option-editor" value="textmate">TextMate</option>
-                <option class="option-editor" value="emacs">Emacs</option>
-                <option class="option-editor" value="macvim">MacVim</option>
-                <option class="option-editor" value="phpstorm" selected>PhpStorm</option>
-                <option class="option-editor" value="idea">Idea</option>
-                <option class="option-editor" value="netbeans">Netbeans</option>
-                <option class="option-editor" value="vscode">VS Code</option>
-                <option class="option-editor" value="atom">Atom</option>
-            </select>
+            <div class="select-box">
+                <select name="editor-select" id="editor">
+                    <option class="option-editor" value="sublime">Sublime</option>
+                    <option class="option-editor" value="textmate">TextMate</option>
+                    <option class="option-editor" value="emacs">Emacs</option>
+                    <option class="option-editor" value="macvim">MacVim</option>
+                    <option class="option-editor" value="phpstorm" selected>PhpStorm</option>
+                    <option class="option-editor" value="idea">Idea</option>
+                    <option class="option-editor" value="netbeans">Netbeans</option>
+                    <option class="option-editor" value="vscode">VS Code</option>
+                    <option class="option-editor" value="atom">Atom</option>
+                </select>
+            </div>
         </label>
         <label for="editor">
             <span class="editor-title">Theme</span>
-            <select name="editor-select" id="editor">
-                <option class="option-theme" value="auto" selected>
-                    Auto
-                </option>
-                <option class="option-theme" value="light">
-                    Light
-                </option>
-                <option class="option-theme" value="dark">
-                    Dark
-                </option>
-            </select>
+            <div class="select-box">
+                <select name="editor-select" id="editor">
+                    <option class="option-theme" value="auto" selected>Auto</option>
+                    <option class="option-theme" value="light">Light</option>
+                    <option class="option-theme" value="dark">Dark</option>
+                </select>
+            </div>
         </label>
     </div>
 </div>
