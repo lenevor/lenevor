72d0ad197aad1c547a8ceabb633d6f48ead12b35
Alexander Campo <jalexcam@gmail.com>
2023-09-08 23:57:10 -0500

    Update ApplicationDescription.php

--- syscodes/src/components/Console/Helper/Description/ApplicationDescription.php
+++ syscodes/src/components/Console/Helper/Description/ApplicationDescription.php
@@ -22,26 +22,190 @@
 
 namespace Syscodes\Components\Console\Helper\Description;
 
+use Syscodes\Components\Console\Application;
+use Syscodes\Components\Console\Command\Command;
+use Syscodes\Components\Console\Exceptions\CommandNotFoundException;
+
+/**
+ * @internal
+ */
 class ApplicationDescription
 {
+    public const G_NAMESPACE = '_global';
+
+    /**
+     * Get the aliases of the commands.
+     * 
+     * @var array $aliases
+     */
+    protected $aliases = [];
+
+    /**
+     * The application console.
+     * 
+     * @var \Syscodes\Components\Console\Application $application
+     */
     protected $application;
 
-    protected $commands;
+    /**
+     * Get the commands.
+     * 
+     * @var array $commands
+     */
+    protected array $commands;
 
-    public function __construct($application)
+    /**
+     * Get the namespace of commands.
+     * 
+     * @var string|null $namespace
+     */
+    protected ?string $namespace;
+
+    /**
+     * Get the namespaces.
+     * 
+     * @var array $namespaces
+     */
+    protected array $namespaces;
+
+    /**
+     * Get the hidden data in boolean.
+     * 
+     * @var bool $hidden
+     */
+    protected bool $hidden;
+
+    /**
+     * Constructor. Create a new ApplicationDescription instance.
+     * 
+     * @param  \Syscodes\Components\Console\Application  $application
+     * @param  string|null  $namespace
+     * @param  bool  $hidden
+     * 
+     * @return void
+     */
+    public function __construct(Application $application, string $namespace = null, bool $hidden = false)
     {
         $this->application = $application;
+        $this->namespace = $namespace;
+        $this->hidden = $hidden;
     }
 
-    public function getCommands()
+    /**
+     * Get all the commands of console.
+     * 
+     * @return array|\Syscodes\Components\Console\Command\Command
+     */
+    public function getCommands(): array
     {
-        return $this->initAplication();
+        if ( ! isset($this->commands)) {
+            return $this->initAplication();
+        }
+
+        return $this->commands;
     }
 
-    protected function initAplication()
+    /**
+     * Get a command of console.
+     * 
+     * @param  string  $name
+     * 
+     * @return \Syscodes\Components\Console\Command\Command
+     */
+    public function getCommand(string $name): Command
     {
-        $all = $this->application->all();
+        if ( ! isset($this->commands[$name]) && ! isset($this->aliases[$name])) {
+            throw new CommandNotFoundException(sprintf('Command "%s" does not exist', $name));
+        }
+        
+        return $this->commands[$name] ?? $this->aliases[$name];
+    }
 
-        return $all;
+    /**
+     * Get all the namespaces of commands.
+     * 
+     * @return array
+     */
+    public function getNamespaces(): array
+    {
+        if ( ! isset($this->namespaces)) {
+            return $this->initAplication();
+        }
+
+        return $this->namespaces;
+    }
+
+    /**
+     * Get the initialize to application of console.
+     * 
+     * @return void
+     */
+    private function initAplication(): void
+    {
+        $this->commands = [];
+        $this->namespaces = [];
+        
+        $all = $this->application->all($this->namespace ? $this->application->findNamespace($this->namespace) : null);
+        
+        foreach ($this->sortCommands($all) as $namespace => $commands) {
+            $names = [];
+            
+            /** @var \Syscodes\Components\Console\Command\Command $command */
+            foreach ($commands as $name => $command) {
+                if ( ! $command->getName() || (!$this->hidden && $command->isHidden())) {
+                    continue;
+                }
+                
+                if ($command->getName() === $name) {
+                    $this->commands[$name] = $command;
+                } else {
+                    $this->aliases[$name] = $command;
+                }
+                
+                $names[] = $name;
+            }
+            
+            $this->namespaces[$namespace] = ['id' => $namespace, 'commands' => $names];
+        }
+    }
+
+    /**
+     * Get the sort commands order.
+     * 
+     * @param  array  $commands
+     * 
+     * @return array
+     */
+    private function sortCommands(array $commands): array
+    {
+        $namespacedCommands = [];
+        $globalCommands = [];
+        $sortedCommands = [];
+        
+        foreach ($commands as $name => $command) {
+            $key = $this->application->extractNamespace($name, 1);
+            
+            if (in_array($key, ['', self::G_NAMESPACE], true)) {
+                $globalCommands[$name] = $command;
+            } else {
+                $namespacedCommands[$key][$name] = $command;
+            }
+        }
+        
+        if ($globalCommands) {
+            ksort($globalCommands);
+            $sortedCommands[self::G_NAMESPACE] = $globalCommands;
+        }
+        
+        if ($namespacedCommands) {
+            ksort($namespacedCommands, SORT_STRING);
+            
+            foreach ($namespacedCommands as $key => $commandsSet) {
+                ksort($commandsSet);
+                $sortedCommands[$key] = $commandsSet;
+            }
+        }
+        
+        return $sortedCommands;
     }
 }
