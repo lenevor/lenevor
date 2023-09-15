8419b897ec88d1b2ee38249b9d5574c011006944
Alexander Campo <jalexcam@gmail.com>
2023-09-15 01:05:08 -0500

    Added new methods for manages the format of strings in console

--- syscodes/src/components/Console/Helper/Helper.php
+++ syscodes/src/components/Console/Helper/Helper.php
@@ -30,18 +30,93 @@ class Helper
     /**
      * The width is how many characters positions the string will use.
      * 
-     * @param  string|null  $string
+     * @param  string|null  $value
      * 
      * @return int
      */
-    public static function width(?string $string): int
+    public static function width(?string $value): int
     {
-        $string ?? '';
+        $value ?? '';
         
-        if (false === $encoding = mb_detect_encoding($string, null, true)) {
-            return strlen($string);
+        if (false === $encoding = mb_detect_encoding($value, null, true)) {
+            return strlen($value);
         }
         
-        return mb_strwidth($string, $encoding);
+        return mb_strwidth($value, $encoding);
+    }
+    
+    /**
+     * The length is related to how many bytes the string will use.
+     * 
+     * @param  string|null  $value
+     * 
+     * @return int
+     */
+    public static function length(?string $value): int
+    {
+        $value ?? '';
+        
+        if (false === $encoding = mb_detect_encoding($value, null, true)) {
+            return strlen($value);
+        }
+        
+        return mb_strlen($value, $encoding);
+    }
+
+    /**
+     * Returns the subset of a string, using mb_substr if it is available.
+     * 
+     * @param  string|null  $value
+     * @param  int  $from
+     * @param  int|null  $length
+     * 
+     * @return string
+     */
+    public static function substr(?string $value, int $from, int $length = null): string
+    {
+        $value ?? '';
+        
+        if (false === $encoding = mb_detect_encoding($value, null, true)) {
+            return substr($value, $from, $length);
+        }
+        
+        return mb_substr($value, $from, $length, $encoding);
+    }
+    
+    /**
+     * Get the format for time of an item result to response time in console.
+     * 
+     * @param  int|float  $seconds
+     * 
+     * @return string
+     */
+    public static function formatTime(int|float $secs)
+    {
+        static $timeFormats = [
+            [0, '< 1 sec'],
+            [1, '1 sec'],
+            [2, 'secs', 1],
+            [60, '1 min'],
+            [120, 'mins', 60],
+            [3600, '1 hr'],
+            [7200, 'hrs', 3600],
+            [86400, '1 day'],
+            [172800, 'days', 86400],
+        ];
+
+        foreach ($timeFormats as $index => $format) {
+            if ($secs >= $format[0]) {
+                if ((isset($timeFormats[$index + 1]) && 
+                           $secs < $timeFormats[$index + 1][0]) ||
+                           $index == \count($timeFormats) - 1
+                ) {
+                    if (2 == \count($format)) {
+                        return $format[1];
+                    }
+
+                    return floor($secs / $format[2]).' '.$format[1];
+                }
+            }
+        }
     }
 }
