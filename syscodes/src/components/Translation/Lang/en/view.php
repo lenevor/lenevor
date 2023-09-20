d596696912787138df65f12bc1012cee8bbcfb58
Alexander Campo <jalexcam@gmail.com>
2023-09-20 11:55:23 -0500

    Changed at a new folder to system of translation

---
+++ syscodes/src/components/Translation/Lang/en/view.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | View Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the View class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+   
+    'dataObjectArray' => 'The data parameter only accepts objects and arrays.',    
+    'notFound' => 'View [{file}] not found.',    
+    'rendering' => 'You must set the file to use within your view before rendering.',
+    'variableNotSet' => 'View variable is not set: [{key}].',
+
+];
---
+++ syscodes/src/components/Translation/Lang/pr/view.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | View Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the View class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+   
+    'dataObjectArray' => 'O parâmetro data só aceita objetos e arrays.',    
+    'notFound' => 'Ver [{file}] não encontrado.',    
+    'rendering' => 'Você deve definir o arquivo a ser usado em sua visualização antes de renderizar.',
+    'variableNotSet' => 'A variável de visualização não está definida: [{key}].',
+];
