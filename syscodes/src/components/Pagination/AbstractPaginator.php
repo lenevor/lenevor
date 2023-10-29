bb462e6a5e77798782341e43c0dfc9e1758029b9
Alexander Campo <jalexcam@gmail.com>
2023-10-29 00:47:18 -0500

    Added new methods for get the pagination with URLs

--- syscodes/src/components/Pagination/AbstractPaginator.php
+++ syscodes/src/components/Pagination/AbstractPaginator.php
@@ -22,11 +22,13 @@
 
 namespace Syscodes\Components\Pagination;
 
+use Closure;
 use ArrayAccess;
 use Traversable;
 use ArrayIterator;
 use IteratorAggregate;
 use Syscodes\Components\Support\Arr;
+use Syscodes\Components\Support\Str;
 
 /**
  * Allows get the links of pagination of database data register.
@@ -95,6 +97,13 @@ abstract class AbstractPaginator implements ArrayAccess, IteratorAggregate
      * @var string $pageName
      */
     protected $pageName = 'page';
+
+    /**
+     * The base path to assign to all URLs.
+     * 
+     * @var string $path
+     */
+    protected $path = '/';
     
     /**
      * The number of items to be shown per page.
@@ -104,18 +113,369 @@ abstract class AbstractPaginator implements ArrayAccess, IteratorAggregate
     protected $perPage;
     
     /**
-     * The base path to assign to all URLs.
-     * 
-     * @var string $path
-     */
-    protected $path = '/';
-    
-    /**
      * The query parameters to add to all URLs.
      * 
      * @var array $query
      */
     protected $query = [];
+    
+    /**
+     * Determine if the given value is a valid page number.
+     * 
+     * @param  int  $pageNumber
+     * 
+     * @return bool
+     */
+    protected function isValidPageNumber(int $pageNumber): bool
+    {
+        return ($pageNumber >= 1) && (filter_var($pageNumber, FILTER_VALIDATE_INT) !== false);
+    }
+    
+    /**
+     * Get the URL for the previous page.
+     * 
+     * @return string|null
+     */
+    public function previousPageUrl(): string|null
+    {
+        if ($this->currentPage() > 1) {
+            return $this->url($this->currentPage() - 1);
+        }
+    }
+    
+    /**
+     * Create a range of pagination URLs.
+     * 
+     * @param  int  $start
+     * @param  int  $end
+     * 
+     * @return array
+     */
+    public function getUrlRange(int $start, int $end): array
+    {
+        $results = [];
+        
+        foreach (range($start, $end) as $page) {
+            $results[$page] = $this->url($page);
+        }
+        
+        return $results;
+    }
+    
+    /**
+     * Get the URL for a given page number.
+     * 
+     * @param  int  $page
+     * 
+     * @return string
+     */
+    public function url(int $page): string
+    {
+        if ($page <= 0) {
+            $page = 1;
+        }
+        
+        $path = $this->path;
+        
+        $parameters = array_merge($this->query, [$this->pageName => $page]);
+        
+        if (count($parameters) > 0) {
+            $path .= (Str::contains($path, '?') ? '&' : '?').http_build_query($parameters, '', '&');
+        }
+        
+        return $path .$this->buildFragment();
+    }
+    
+    /**
+     * Get / set the URL fragment to be appended to URLs.
+     * 
+     * @param  string|null  $fragment
+     * 
+     * @return static|string|null
+     */
+    public function fragment($fragment = null): static|string|null
+    {
+        if (is_null($fragment)) {
+            return $this->fragment;
+        }
+        
+        $this->fragment = $fragment;
+        
+        return $this;
+    }
+    
+    /**
+     * Add a set of query string values to the paginator.
+     * 
+     * @param  array|string  $keys
+     * @param  string|null  $value
+     * 
+     * @return static
+     */
+    public function appends($keys, $value = null): static
+    {
+        if ( ! is_array($keys)) {
+            return $this->addQuery($keys, $value);
+        }
+        
+        foreach ($keys as $key => $value) {
+            $this->addQuery($key, $value);
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Add a query string value to the paginator.
+     * 
+     * @param  string  $key
+     * @param  string  $value
+     * 
+     * @return static
+     */
+    protected function addQuery($key, $value): static
+    {
+        if ($key !== $this->pageName) {
+            $this->query[$key] = $value;
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Build the full fragment portion of a URL.
+     * 
+     * @return string
+     */
+    protected function buildFragment(): string
+    {
+        return $this->fragment ? '#' .$this->fragment : '';
+    }
+    
+    /**
+     * Get the slice of items being paginated.
+     * 
+     * @return array
+     */
+    public function items(): array
+    {
+        return $this->items;
+    }
+    
+    /**
+     * Get the number of the first item in the slice.
+     * 
+     * @return int
+     */
+    public function firstItem(): int
+    {
+        if (count($this->items) > 0) {
+            return (($this->currentPage - 1) * $this->perPage) + 1;
+        }
+    }
+    
+    /**
+     * Get the number of the last item in the slice.
+     * 
+     * @return int
+     */
+    public function lastItem(): int
+    {
+        if (count($this->items) > 0) {
+            return $this->firstItem() + $this->count() - 1;
+        }
+    }
+    
+    /**
+     * Get the number of items shown per page.
+     * 
+     * @return int
+     */
+    public function perPage(): int
+    {
+        return $this->perPage;
+    }
+    
+    /**
+     * Determine if there are enough items to split into multiple pages.
+     * 
+     * @return bool
+     */
+    public function hasPages(): bool
+    {
+        return ($this->currentPage() != 1) || $this->hasMorePages();
+    }
+    
+    /**
+     * Determine if the paginator is on the first page.
+     * 
+     * @return bool
+     */
+    public function onFirstPage(): bool
+    {
+        return $this->currentPage() <= 1;
+    }
+    
+    /**
+     * Get the current page.
+     * 
+     * @return int
+     */
+    public function currentPage(): int
+    {
+        return $this->currentPage;
+    }
+    
+    /**
+     * Get the query string variable used to store the page.
+     * 
+     * @return string
+     */
+    public function getPageName(): string
+    {
+        return $this->pageName;
+    }
+    
+    /**
+     * Set the query string variable used to store the page.
+     * 
+     * @param  string  $name
+     * 
+     * @return static
+     */
+    public function setPageName(string $name): static
+    {
+        $this->pageName = $name;
+        
+        return $this;
+    }
+    
+    /**
+     * Get the base path to assign to all URLs.
+     * 
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+    
+    /**
+     * Set the base path to assign to all URLs.
+     * 
+     * @param  string  $path
+     * 
+     * @return static
+     */
+    public function setPath(string $path): static
+    {
+        $this->path = $path;
+        
+        return $this;
+    }
+    
+    /**
+     * Resolve the current request path or return the default value.
+     * 
+     * @param  string  $default
+     * 
+     * @return string
+     */
+    public static function resolveCurrentPath($default = '/')
+    {
+        if (isset(static::$currentPathResolver)) {
+            return call_user_func(static::$currentPathResolver);
+        }
+        
+        return $default;
+    }
+    
+    /**
+     * Set the current request path resolver callback.
+     * 
+     * @param  \Closure  $resolver
+     * 
+     * @return void
+     */
+    public static function currentPathResolver(Closure $resolver): void
+    {
+        static::$currentPathResolver = $resolver;
+    }
+    
+    /**
+     * Resolve the current page or return the default value.
+     * 
+     * @param  string  $pageName
+     * @param  int  $default
+     * 
+     * @return int
+     */
+    public static function resolveCurrentPage($pageName = 'page', $default = 1)
+    {
+        if (isset(static::$currentPageResolver)) {
+            return call_user_func(static::$currentPageResolver, $pageName);
+        }
+        
+        return $default;
+    }
+    
+    /**
+     * Set the current page resolver callback.
+     * 
+     * @param  \Closure  $resolver
+     * 
+     * @return void
+     */
+    public static function currentPageResolver(Closure $resolver): void
+    {
+        static::$currentPageResolver = $resolver;
+    }
+    
+    /**
+     * Get an instance of the view factory from the resolver.
+     * 
+     * @return \Syscodes\Components\Contracts\View\Factory
+     */
+    public static function viewFactory()
+    {
+        return call_user_func(static::$viewFactoryResolver);
+    }
+    
+    /**
+     * Set the view factory resolver callback.
+     * 
+     * @param  \Closure  $resolver
+     * 
+     * @return void
+     */
+    public static function viewFactoryResolver(Closure $resolver): void
+    {
+        static::$viewFactoryResolver = $resolver;
+    }
+    
+    /**
+     * Set the default pagination view.
+     * 
+     * @param  string  $view
+     * 
+     * @return void
+     */
+    public static function defaultView($view): void
+    {
+        static::$defaultView = $view;
+    }
+    
+    /**
+     * Set the default "simple" pagination view.
+     * 
+     * @param  string  $view
+     * 
+     * @return void
+     */
+    public static function defaultSimpleView($view): void
+    {
+        static::$defaultSimpleView = $view;
+    }
     
     /**
      * Get an iterator for the items.
@@ -225,4 +585,16 @@ abstract class AbstractPaginator implements ArrayAccess, IteratorAggregate
 	{
 		Arr::erase($this->items, $offset);
 	}
+    
+    /**
+     * Magic method.
+     * 
+     * Render the contents of the paginator when casting to string.
+     * 
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return (string) $this->render();
+    }
 }
