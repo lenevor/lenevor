5934b2e11957fe11c8ad3821022452e5e940b2d8
Alexander Campo <jalexcam@gmail.com>
2023-11-01 11:47:13 -0500

    Added method for the pagination state using the application container

--- syscodes/src/components/Pagination/PaginationState.php
+++ syscodes/src/components/Pagination/PaginationState.php
@@ -27,5 +27,27 @@ namespace Syscodes\Components\Pagination;
  */
 class PaginationState
 {
-
+    /**
+     * Bind the pagination state resolvers using the given application container as a base.
+     * 
+     * @param  \Syscodes\Components\Contracts\Core\Application  $app
+     * 
+     * @return void
+     */
+    public static function resolvingUsing($app)
+    {
+        Paginator::viewFactoryResolver(fn () => $app['view']);
+        
+        Paginator::currentPathResolver(fn () => $app['request']->url());
+        
+        Paginator::currentPageResolver(function ($pageName = 'page') use ($app) {
+            $page = $app['request']->input($pageName);
+            
+            if (filter_var($page, FILTER_VALIDATE_INT) !== false && (int) $page >= 1) {
+                return (int) $page;
+            }
+            
+            return 1;
+        });
+    }
 }
