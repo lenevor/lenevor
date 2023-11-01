61b1ea81b827565cc4f1ed5a7893444e817bdd76
Alexander Campo <jalexcam@gmail.com>
2023-11-01 11:43:54 -0500

    Create PaginationServiceProvider.php

---
+++ syscodes/src/components/Pagination/PaginationServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+ namespace Syscodes\Components\Pagination;
+
+use Syscodes\Components\Support\ServiceProvider;
+
+/**
+ * For loading the classes from the container of services.
+ */
+class PaginationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsTo(__DIR__.'/resources/views', 'pagination');
+    }
+
+    /**
+     * Register the service provider.
+     * 
+     * @return void
+     */
+    public function register()
+    {
+        PaginationState::resolvingUsing($this->app);
+    }
+}
