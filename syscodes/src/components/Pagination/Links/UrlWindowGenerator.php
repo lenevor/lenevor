d01d20d547f06f70295bb9f8b0ebf844338f37b7
Alexander Campo <jalexcam@gmail.com>
2023-11-04 01:17:42 -0500

    Update UrlWindowGenerator.php

--- syscodes/src/components/Pagination/Links/UrlWindowGenerator.php
+++ syscodes/src/components/Pagination/Links/UrlWindowGenerator.php
@@ -47,4 +47,150 @@ class UrlWindowGenerator
     {
         $this->paginator = $paginator;
     }
+    
+    /**
+     * Create a new URL window instance.
+     * 
+     * @param  \Syscodes\Components\Contracts\Pagination\Paginator  $paginator
+     * 
+     * @return array
+     */
+    public static function make(Paginator $paginator): array
+    {
+        return (new static($paginator))->get();
+    }
+    
+    /**
+     * Get the window of URLs to be shown.
+     * 
+     * @return array
+     */
+    public function get(): array 
+    {
+        if ( ! $this->hasPages()) {
+            return [
+                'first' => null,
+                'slider' => null,
+                'last' => null
+            ];
+        }
+
+        $onEachSide = $this->paginator->onEachSide;
+        
+        $win = $onEachSide * 2;
+        
+        if ($this->lastPage() < ($win + 6)) {
+            return $this->getSmallSlider();
+        }
+        
+        if ($this->currentPage() <= $win) {
+            return $this->getSliderTooCloseToBeginning($win);
+        } else if ($this->currentPage() > ($this->lastPage() - $win)) {
+            return $this->getSliderTooCloseToEnding($win);
+        }
+        
+        return $this->getFullSlider($onEachSide);
+    }
+    
+    /**
+     * Get the slider of URLs there are not enough pages to slide.
+     * 
+     * @return array
+     */
+    protected function getSmallSlider(): array
+    {
+        return [
+            'first'  => $this->paginator->getUrlRange(1, $this->lastPage()),
+            'slider' => null,
+            'last'   => null,
+        ];
+    }
+    
+    /**
+     * Get the slider of URLs when too close to beginning of window.
+     * 
+     * @param  int  $win
+     * 
+     * @return array
+     */
+    protected function getSliderTooCloseToBeginning($win): array
+    {
+        return [
+            'first'  => $this->paginator->getUrlRange(1, $win + 2),
+            'slider' => null,
+            'last'   => $this->paginator->getUrlRange($this->lastPage() - 1, $this->lastPage()),
+        ];
+    }
+    
+    /**
+     * Get the slider of URLs when too close to ending of window.
+     * 
+     * @param  int  $win
+     * 
+     * @return array
+     */
+    protected function getSliderTooCloseToEnding($win): array
+    {
+        $last = $this->paginator->getUrlRange(
+            $this->lastPage() - ($win + 2),
+            $this->lastPage()
+        );
+        
+        return [
+            'first'  => $this->paginator->getUrlRange(1, 2),
+            'slider' => null,
+            'last'   => $last,
+        ];
+    }
+    
+    /**
+     * Get the slider of URLs when a full slider can be made.
+     * 
+     * @param  int  $onEachSide
+     * 
+     * @return array
+     */
+    protected function getFullSlider($onEachSide): array
+    {
+        $slider = $this->paginator->getUrlRange(
+            $this->currentPage() - $onEachSide,
+            $this->currentPage() + $onEachSide
+        );
+        
+        return [
+            'first'  => $this->paginator->getUrlRange(1, 2),
+            'slider' => $slider,
+            'last'   => $this->paginator->getUrlRange($this->lastPage() - 1, $this->lastPage()),
+        ];
+    }
+    
+    /**
+     * Determine if the underlying paginator being presented has pages to show.
+     * 
+     * @return bool
+     */
+    public function hasPages(): bool
+    {
+        return $this->paginator->lastPage() > 1;
+    }
+    
+    /**
+     * Get the current page from the paginator.
+     * 
+     * @return int
+     */
+    protected function currentPage(): int
+    {
+        return $this->paginator->currentPage();
+    }
+    
+    /**
+     * Get the last page from the paginator.
+     * 
+     * @return int
+     */
+    protected function lastPage(): int
+    {
+        return $this->paginator->lastPage();
+    }
 }
