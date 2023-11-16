9f8a5550f881cfc28156cac0b300194797a04917
Alexander Campo <jalexcam@gmail.com>
2023-11-16 14:11:08 -0500

    Added new methods for generate collections of data

--- syscodes/src/components/Collections/Traits/Enumerates.php
+++ syscodes/src/components/Collections/Traits/Enumerates.php
@@ -22,10 +22,14 @@
 
 namespace Syscodes\Components\Support\Traits;
 
+use Exception;
 use Traversable;
 use JsonSerializable;
+use Syscodes\Components\Support\Collection;
 use Syscodes\Components\Contracts\Support\Jsonable;
 use Syscodes\Components\Contracts\Support\Arrayable;
+use Syscodes\Components\Contracts\Support\Collectable;
+use Syscodes\Components\Support\HigherOrderCollectionProxy;
 
 /**
  * Trait Enumerates.
@@ -33,15 +37,57 @@ use Syscodes\Components\Contracts\Support\Arrayable;
 trait Enumerates
 {
     /**
+     * Indicates that the object's string representation should be escaped when __toString is invoked.
+     * 
+     * @var bool $escapeWhenLoadingToString
+     */
+    protected $escapeWhenLoadingToString = false;
+
+    /**
+     * The methods that can be proxied.
+     * 
+     * @var array<int, string> $proxies
+     */
+    protected static $proxies = [
+        'contains',
+        'filter',
+        'first',
+        'flip',
+        'intersect',
+        'keys',
+        'map',
+        'merge',
+        'pad',
+        'pop',
+        'reduce',
+        'reject',
+        'replace',
+        'reverse',
+        'shift',
+        'unique',
+        'values',
+    ];
+
+    /**
      * Create a new collection instance if the value isn't one already.
      * 
      * @param  mixed  $items
      * 
      * @return static
      */
-    public static function make(mixed $items = []): static
+    public static function make($items = []): static
     {
         return new static($items);
+    }
+
+    /**
+     * Collect the values into a collection.
+     *
+     * @return \Syscodes\Components\Support\Collection<TKey, TValue>
+     */
+    public function collect()
+    {
+        return new Collection($this->all());
     }
 
     /**
@@ -119,6 +165,16 @@ trait Enumerates
     }
     
     /**
+     * Get the collection of items as a plain array.
+     * 
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return $this->map(fn ($value) => $value instanceof Arrayable ? $value->toArray() : $value)->all();
+    }
+    
+    /**
      * Determine if the collection is not empty.
      * 
      * @return bool
@@ -136,7 +192,7 @@ trait Enumerates
      * 
      * @return static
      */
-    public function forPage($page, $perPage): static
+    public function forPage(int $page, int $perPage): static
     {
         $offset = max(0, ($page - 1) * $perPage);
         
@@ -150,7 +206,7 @@ trait Enumerates
      * 
      * @return string
      */
-    public function toJson(int $options = 0): string
+    public function toJson($options = 0): string
     {
         return json_encode($this->jsonSerialize(), $options);
     }
@@ -165,12 +221,40 @@ trait Enumerates
         return array_map(function ($value) {
             if ($value instanceof JsonSerializable) {
                 return $value->jsonSerialize();
+            } elseif ($value instanceof Jsonable) {
+                return json_decode($value->toJson(), true);
             } elseif ($value instanceof Arrayable) {
                 return $value->toArray();
             }
             
             return $value;
         }, $this->all());
+    }
+    
+    /**
+     * Indicate that the model's string representation should be escaped when __toString is invoked.
+     * 
+     * @param  bool  $escape
+     * 
+     * @return stati
+     */
+    public function escapeWhenLoadingToString($escape = true): static
+    {
+        $this->escapeWhenLoadingToString = $escape;
+        
+        return $this;
+    }
+    
+    /**
+     * Add a method to the list of proxied methods.
+     * 
+     * @param  string  $method
+     * 
+     * @return void
+     */
+    public static function proxy($method): void
+    {
+        static::$proxies[] = $method;
     }
 
     /**
@@ -180,22 +264,51 @@ trait Enumerates
      * 
      * @return array
      */
-    private function getArrayItems(mixed $items): array
+    protected function getArrayItems($items)
     {
         if (is_array($items)) {
             return $items;
-        } elseif ($items instanceof static) {
-            return $items->all();
-        } elseif ($items instanceof Arrayable) {
-            return $items->toArray();
-        } elseif ($items instanceof JsonSerializable) {
-            return (array) $items->jsonSerialize();
-        } elseif ($items instanceof Jsonable) {
-            return json_decode($items->toJson(), true);
-        } elseif ($items instanceof Traversable) {
-            return iterator_to_array($items);
         }
-
-        return (array) $items;
+        
+        return match (true) {
+            $items instanceof Collectable => $items->all(),
+            $items instanceof Arrayable => $items->toArray(),
+            $items instanceof Traversable => iterator_to_array($items),
+            $items instanceof Jsonable => json_decode($items->toJson(), true),
+            $items instanceof JsonSerializable => (array) $items->jsonSerialize(),
+            default => (array) $items,
+        };
     }
+
+    /**
+     * Magic method.
+     * 
+     * Dynamically access collection proxies.
+     * 
+     * @param  string  $key
+     * 
+     * @return mixed
+     * 
+     * @throws \Exception
+     */
+    public function __get($key)
+    {
+        if ( ! in_array($key, static::$proxies)) {
+            throw new Exception("Property [{$key}] does not exist on this collection instance");
+        }
+        
+        return new HigherOrderCollectionProxy($this, $key);
+    }
+    
+    /**
+     * Convert the collection to its string representation.
+     * 
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->escapeWhenLoadingToString
+                    ? e($this->toJson())
+                    : $this->toJson();
+    }    
 }
