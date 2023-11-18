6a721d7dec473436a5441edad6df65f461ae0862
Alexander Campo <jalexcam@gmail.com>
2023-11-17 20:21:37 -0500

    Create HigherOrderTakeProxy.php

---
+++ syscodes/src/components/Support/HigherOrderTakeProxy.php
@@ -0,0 +1,63 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Support;
+
+/**
+ * Allows a new take proxy instance.
+ */
+class HigherOrderTakeProxy
+{
+    /**
+     * The target being taked.
+     * 
+     * @var mixed $target
+     */
+    public $target;
+
+    /**
+     * Constructor. Create a new take proxy instance.
+     * 
+     * @param  mixed  $target
+     */
+    public function __construct($target)
+    {
+        $this->target = $target;
+    }
+
+    /**
+     * Magic method. 
+     * 
+     * Dynamically pass method calls to the target.
+     * 
+     * @param  string  $method
+     * @param  array  $parameters
+     * 
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        $this->target->{$method}(...$parameters);
+
+        return $this->target;
+    }
+}
