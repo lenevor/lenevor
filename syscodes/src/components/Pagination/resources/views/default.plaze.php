596c2a7ce9a2c7a840a3009cd33ff4c001f6ede2
Alexander Campo <jalexcam@gmail.com>
2023-11-21 01:32:20 -0500

    Update default.plaze.php

--- syscodes/src/components/Pagination/resources/views/default.plaze.php
+++ syscodes/src/components/Pagination/resources/views/default.plaze.php
@@ -4,11 +4,11 @@
             {{-- Previous Page Link --}}
             <@if ($paginator->onFirstPage())
                 <li class="disabled" aria-disabled="true" aria-label="<@lang('pagination.previous')">
-                    <span aria-hidden="true">&lsaquo;</span>
+                    <span aria-hidden="true">&laquo;</span>
                 </li>
             <@else
                 <li>
-                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="<@lang('pagination.previous')">&lsaquo;</a>
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="<@lang('pagination.previous')">&laquo;</a>
                 </li>
             <@endif
 
@@ -34,11 +34,11 @@
             {{-- Next Page Link --}}
             <@if ($paginator->hasMorePages())
                 <li>
-                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="<@lang('pagination.next')">&rsaquo;</a>
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="<@lang('pagination.next')">&raquo;</a>
                 </li>
             <@else
                 <li class="disabled" aria-disabled="true" aria-label="<@lang('pagination.next')">
-                    <span aria-hidden="true">&rsaquo;</span>
+                    <span aria-hidden="true">&raquo;</span>
                 </li>
             <@endif
         </ul>
