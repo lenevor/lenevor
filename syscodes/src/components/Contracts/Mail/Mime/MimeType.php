ec497e0a9337c8295dbdb912ac92f78e38017f13
Alexander Campo <jalexcam@gmail.com>
2023-12-15 11:58:48 -0500

    Added new interface for get the MIME types

---
+++ syscodes/src/components/Contracts/Mail/Mime/MimeType.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Contracts\Mail\Mime;
+
+/**
+ * Allows get the extensions of the MIME types.
+ */
+interface MimeType extends MimeTypeGuesser
+{
+    /**
+     * Gets the extensions for the given MIME type.
+     * 
+     * @param  string  $mimeType
+     * 
+     * @return string[]
+     */
+    public function getExtensions(string $mimeType): array;
+    
+    /**
+     * Gets the MIME types for the given extension.
+     * 
+     * @param  string  $extension
+     * 
+     * @return string[]
+     */
+    public function getMimeTypes(string $extension): array;
+}
---
+++ syscodes/src/components/Contracts/Mail/Mime/MimeTypeGuesser.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2023 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Contracts\Mail\Mime;
+
+/**
+ * Guesses the MIME type of a file.
+ */
+interface MimeTypeGuesser
+{
+    /**
+     * Returns true if this guesser is supported.
+     * 
+     * @return bool
+     */
+    public function isGuesserSupported(): bool;
+    
+    /**
+     * Guesses the MIME type of the file with the given path.
+     * 
+     * @param  string  $path
+     * 
+     * @return string
+     */
+    public function guessMimeType(string $path): ?string;
+}
