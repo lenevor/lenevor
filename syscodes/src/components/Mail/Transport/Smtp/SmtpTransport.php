4d0738ed8ae9f9e14e2740b13e44d922604f98fd
Alexander Campo <jalexcam@gmail.com>
2023-12-29 10:50:06 -0500

    Update SmtpTransport.php

--- syscodes/src/components/Mail/Transport/Smtp/SmtpTransport.php
+++ syscodes/src/components/Mail/Transport/Smtp/SmtpTransport.php
@@ -22,10 +22,81 @@
 
 namespace Syscodes\components\Mail\Transport\Smtp;
 
-/**
- * 
- */
-class SmtpTransport
-{
+use Psr\Log\LoggerInterface;
+use Syscodes\Components\Mail\Helpers\SentMessage;
+use Syscodes\Components\Contracts\Events\Dispatcher;
+use Syscodes\Components\Mail\Transport\AbstractTransport;
 
+/**
+ * Sends emails over SMTP.
+ */
+class SmtpTransport extends AbstractTransport
+{
+    /**
+     * The abstract stream instance.
+     * 
+     * @var AbstractStream $stream
+     */
+    protected AbstractStream $stream;
+
+    /**
+     * Constructor. Create a new SmtpTransport class instance.
+     * 
+     * @param  AbstractStream|null  $stream
+     * @param  Dispatcher|null  $dispatcher
+     * @param  LoggerInterface|null  $logger
+     * 
+     * @return void
+     */
+    public function __construct(AbstractStream $stream = null, Dispatcher $dispatcher = null, LoggerInterface $logger = null)
+    {
+        parent::__construct($dispatcher, $logger);
+        
+        $this->stream = $stream ?? new SocketStream();
+    }
+
+    /**
+     * Get the stream connection for send of messages.
+     * 
+     * @return AbstractStream
+     */
+    public function getStream(): AbstractStream
+    {
+        return $this->stream;
+    }
+
+    /**
+     * Do send to mail.
+     * 
+     * @param  SentMessage  $message
+     * 
+     * @return void
+     */
+    protected function doSend(SentMessage $message): void
+    {
+        
+    }
+
+    /**
+     * Magic method.
+     * 
+     * Returns the protocol of connection.
+     * 
+     * @return string
+     */
+    public function __toString(): string
+    {
+        if ($this->stream instanceof SocketStream) {
+            $name = sprintf('smtp%s://%s', ($tls = $this->stream->isTLS()) ? 's' : '', $this->stream->getHost());
+            $port = $this->stream->getPort();
+            
+            if ( ! (25 === $port || ($tls && 465 === $port))) {
+                $name .= ':'.$port;
+            }
+            
+            return $name;
+        }
+
+        return 'smtp://sendmail';
+    }
 }
