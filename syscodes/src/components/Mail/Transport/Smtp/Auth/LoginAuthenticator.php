2a1a8062b016b3b4a0bfb3897974205449c53549
Alexander Campo <jalexcam@gmail.com>
2024-01-08 11:31:46 -0500

    Changed the namespace

--- syscodes/src/components/Mail/Transport/Smtp/Auth/LoginAuthenticator.php
+++ syscodes/src/components/Mail/Transport/Smtp/Auth/LoginAuthenticator.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\components\Mail\Transport\Smtp;
+namespace Syscodes\Components\Mail\Transport\Smtp;
 
 use Syscodes\components\Constracts\Mail\Auth\Authenticator;
 
