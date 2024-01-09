493c6ebe004396e495724f56e85ff7d210090abf
Alexander Campo <jalexcam@gmail.com>
2024-01-09 09:38:35 -0500

    Added the SmtpTransport class as an extends and the __construct() method

--- syscodes/src/components/Mail/Transport/Smtp/EsmtpTransport.php
+++ syscodes/src/components/Mail/Transport/Smtp/EsmtpTransport.php
@@ -20,14 +20,17 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\components\Mail\Transport\Smtp;
+namespace Syscodes\Components\Mail\Transport\Smtp;
 
+use Psr\Log\LoggerInterface;
+use Syscodes\Components\Contracts\Events\Dispatcher;
+use Syscodes\Components\Mail\Transport\Smtp\SocketStream;
 use Syscodes\components\Constracts\Mail\Auth\Authenticator;
 
 /**
  * Sends Emails over SMTP with ESMTP support.
  */
-class EsmtpTransport
+class EsmtpTransport extends SmtpTransport
 {
     /**
      * Get the authenticators.
@@ -49,6 +52,62 @@ class EsmtpTransport
      * @var string $username
      */
     protected string $username = '';
+    
+    /**
+     * Constructor. Create a new EsmtpTransport class instance.
+     * 
+     * @param  string  $host
+     * @param  int  $port
+     * @param  bool|null  $tls
+     * @param  Dispatcher|null  $dispatcher
+     * @param  LoggerInterface|null  $logger
+     * @param  AbstractStream|null  $stream
+     * @param  array|null  $authenticators
+     * 
+     * @return void
+     */
+    public function __construct(
+        string $host = 'localhost',
+        int $port = 0,
+        bool $tls = null,
+        Dispatcher $dispatcher = null,
+        LoggerInterface $logger = null,
+        AbstractStream $stream = null,
+        array $authenticators = null
+    ) {
+        parent::__construct($stream, $dispatcher, $logger);
+        
+        if (null === $authenticators) {
+            $authenticators = [
+                new LoginAuthenticator(),
+                new PlainAuthenticator(),
+            ];
+        }
+        
+        $this->setAuthenticators($authenticators);
+        
+        /** @var SocketStream $stream */
+        $stream = $this->getStream();
+        
+        if (null === $tls) {
+            if (465 === $port) {
+                $tls = true;
+            } else {
+                $tls = defined('OPENSSL_VERSION_NUMBER') && 0 === $port && 'localhost' !== $host;
+            }
+        }
+        
+        if ( ! $tls) {
+            $stream->disableTls();
+        }
+        
+        if (0 === $port) {
+            $port = $tls ? 465 : 25;
+        }
+        
+        $stream->setHost($host);
+        $stream->setPort($port);
+    }
 
     /**
      * Sets the autenticators.
