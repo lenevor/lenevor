939c87d442d969759b0ccf458ff1dccbb00ab558
Alexander Campo <jalexcam@gmail.com>
2024-01-22 11:02:37 -0500

    Update ArrayTransport.php

--- syscodes/src/components/Mail/Transport/ArrayTransport.php
+++ syscodes/src/components/Mail/Transport/ArrayTransport.php
@@ -22,10 +22,71 @@
 
 namespace Syscodes\Components\Mail\Transport;
 
-/**
- * 
- */
-class ArrayTransport
-{
+use Syscodes\Components\Support\Collection;
+use Syscodes\Components\Mail\Helpers\Envelope;
+use Syscodes\Components\Contracts\Mail\Transport;
+use Syscodes\Components\Mail\Helpers\SentMessage;
+use Syscodes\Components\Mail\Mailables\RawMessage;
 
+/**
+ * ArrayTransport for sending mail through a array data.
+ */
+class ArrayTransport implements Transport
+{
+    /**
+     * The collection of messages.
+     * 
+     * @var \Syscodes\Components\Support\Collection $messages
+     */
+    protected $messages;
+    
+    /**
+     * Constructor. Create a new array transport class instance.
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->messages = new Collection;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function send(RawMessage $message, Envelope $envelope = null): ?SentMessage
+    {
+        return $this->messages[] = new SentMessage($message, $envelope ?? Envelope::create($message));
+    }
+    
+    /**
+     * Retrieve the collection of messages.
+     * 
+     * @return \Syscodes\Components\Support\Collection
+     */
+    public function messages()
+    {
+        return $this->messages;
+    }
+    
+    /**
+     * Clear all of the messages from the local collection.
+     * 
+     * @return \Syscodes\Components\Support\Collection
+     */
+    public function flush()
+    {
+        return $this->messages = new Collection;
+    }
+    
+    /**
+     * Method magic.
+     * 
+     * Get the string representation of the transport.
+     * 
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return 'array';
+    }
 }
