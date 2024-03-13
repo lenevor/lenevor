7c4bff16a4dd3f4de520218741eff79da96e6147
Alexander Campo <jalexcam@gmail.com>
2024-03-13 12:19:37 -0500

    Update PendingMail.php

--- syscodes/src/components/Mail/PendingMail.php
+++ syscodes/src/components/Mail/PendingMail.php
@@ -22,10 +22,161 @@
 
 namespace Syscodes\Components\Mail;
 
+use Syscodes\Components\Contracts\Mail\Mailer as MailerContract;
+use Syscodes\Components\Contracts\Translation\HasLocalePreferred;
+use Syscodes\Components\Contracts\Mail\Mailbox as MailboxContract;
+
 /**
- * 
+ * Gets from the mailbox of the user data.
  */
 class PendingMail
 {
+    /**
+     * The "bcc" recipients of the message.
+     * 
+     * @var array $bcc
+     */
+    protected $bcc = [];
     
+    /**
+     * The "cc" recipients of the message.
+     * 
+     * @var array $cc
+     */
+    protected $cc = [];
+    
+    /**
+     * The locale of the message.
+     * 
+     * @var string $locale
+     */
+    protected $locale;
+    
+    /**
+     * The mailer instance.
+     * 
+     * @var \Syscodes\Components\Contracts\Mail\Mailer $mailer
+     */
+    protected $mailer;
+    
+    /**
+     * The "to" recipients of the message.
+     * 
+     * @var array $to
+     */
+    protected $to = [];
+    
+    /**
+     * Constructor. Create a new mailable mailer instance.
+     * 
+     * @param  \Syscodes\Components\Contracts\Mail\Mailer  $mailer
+     * 
+     * @return void
+     */
+    public function __construct(MailerContract $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+    
+    /**
+     * Set the locale of the message.
+     * 
+     * @param  string  $locale
+     * 
+     * @return static
+     */
+    public function locale($locale): static
+    {
+        $this->locale = $locale;
+        
+        return $this;
+    }
+    
+    /**
+     * Set the recipients of the message.
+     * 
+     * @param  mixed  $users
+     * 
+     * @return static
+     */
+    public function to($users): static
+    {
+        $this->to = $users;
+        
+        if ( ! $this->locale && $users instanceof HasLocalePreferred) {
+            $this->locale($users->preferredLocale());
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Set the recipients of the message.
+     * 
+     * @param  mixed  $users
+     * 
+     * @return static
+     */
+    public function cc($users): static
+    {
+        $this->cc = $users;
+        
+        return $this;
+    }
+    
+    /**
+     * Set the recipients of the message.
+     * 
+     * @param  mixed  $users
+     * 
+     * @return static
+     */
+    public function bcc($users): static
+    {
+        $this->bcc = $users;
+        
+        return $this;
+    }
+    
+    /**
+     * Send a new mailable message instance.
+     * 
+     * @param  \Syscodes\Components\Contracts\Mail\Mailbox  $mailable
+     * 
+     * @return \Syscodes\Components\Mail\Helpers\SentMessage|null
+     */
+    public function send(MailboxContract $mailable)
+    {
+        return $this->mailer->send($this->fill($mailable));
+    }
+    
+    /**
+     * Send a new mailable message instance synchronously.
+     * 
+     * @param  \Syscodes\Components\Contracts\Mail\Mailbox  $mailable
+     * 
+     * @return \Syscodes\Components\Mail\Helpers\SentMessage|null
+     */
+    public function sendNow(MailboxContract $mailable)
+    {
+        return $this->mailer->sendNow($this->fill($mailable));
+    }
+    
+    /**
+     * Populate the mailable with the addresses.
+     * 
+     * @param  \Syscodes\Components\Contracts\Mail\Mailbox $mailable
+     * 
+     * @return \Syscodes\Components\Mail\Mailbox
+     */
+    protected function fill(MailboxContract $mailable)
+    {
+        return take($mailable->to($this->to)
+                    ->cc($this->cc)
+                    ->bcc($this->bcc), function (MailboxContract $mailable) {
+                        if ($this->locale) {
+                            $mailable->locale($this->locale);
+                        }
+               });
+    }
 }
