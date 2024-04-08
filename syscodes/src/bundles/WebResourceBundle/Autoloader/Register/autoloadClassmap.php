4c4032d95ebf56aa666fb80275bc6997b94749a8
Alexander Campo <jalexcam@gmail.com>
2024-04-08 07:50:03 -0500

    Added new namespace

--- syscodes/src/bundles/WebResourceBundle/Autoloader/Register/autoloadClassmap.php
+++ syscodes/src/bundles/WebResourceBundle/Autoloader/Register/autoloadClassmap.php
@@ -207,6 +207,7 @@ return [
     'Syscodes\\Components\\Core\\Bootstrap\\BootRegisterFacades' => SYS_PATH.'src/components/Core/Bootstrap/BootRegisterFacades.php', 
     'Syscodes\\Components\\Core\\Bootstrap\\BootRegisterProviders' => SYS_PATH.'src/components/Core/Bootstrap/BootRegisterProviders.php',
     'Syscodes\\Components\\Core\\Configuration\\ApplicationBootstrap' => SYS_PATH.'src/components/Core/Configuration/ApplicationBootstrap.php',
+    'Syscodes\\Components\\Core\\Configuration\\ExceptionBootstrap' => SYS_PATH.'src/components/Core/Configuration/ExceptionBootstrap.php',
     'Syscodes\\Components\\Core\\Console\\Commands\\ListCommand' => SYS_PATH.'src/components/Core/Console/Commands/ListCommand.php',
     'Syscodes\\Components\\Core\\Console\\Lenevor' => SYS_PATH.'src/components/Core/Console/Lenevor.php',
     'Syscodes\\Components\\Core\\Exceptions\\Debugging\\DebugExceptionRender' => SYS_PATH.'src/components/Core/Exceptions/Debugging/DebugExceptionRender.php',
