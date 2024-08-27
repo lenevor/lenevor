cee6aed1eecce846f78d392bc298e0edc4b69a5d
Alexander Campo <jalexcam@gmail.com>
2024-08-26 23:36:02 -0500

    Create Required.php

---
+++ syscodes/src/components/Validation/Rules/Required.php
@@ -0,0 +1,50 @@
+<?php
+
+use Syscodes\Components\Validation\Rules;
+use Syscodes\Components\Validation\Rules\Traits\File;
+
+Final class Required extends Rules
+{
+    use File;
+
+    /** @var bool */
+    protected $implicit = true;
+
+    /** @var string */
+    protected $message = "The :attribute is required";
+
+    /**
+     * Check the $value is valid
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public function check($value): bool
+    {
+        $this->setAttributeAsRequired();
+
+        if ($this->attribute and $this->attribute->hasRule('uploaded_file')) {
+            return $this->isValueFromUploadedFiles($value) and $value['error'] != UPLOAD_ERR_NO_FILE;
+        }
+
+        if (is_string($value)) {
+            return mb_strlen(trim($value), 'UTF-8') > 0;
+        }
+        if (is_array($value)) {
+            return count($value) > 0;
+        }
+        return !is_null($value);
+    }
+
+    /**
+     * Set attribute is required if $this->attribute is set
+     *
+     * @return void
+     */
+    protected function setAttributeAsRequired(): void
+    {
+        if ($this->attribute) {
+            $this->attribute->setRequired(true);
+        }
+    }
+}
