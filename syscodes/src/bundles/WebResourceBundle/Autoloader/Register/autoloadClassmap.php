27a5804d0c5ef0cdbc103690636f66b2296f87dc
Alexander Campo <jalexcam@gmail.com>
2024-10-29 23:34:28 -0500

    Added new namespace

--- syscodes/src/bundles/WebResourceBundle/Autoloader/Register/autoloadClassmap.php
+++ syscodes/src/bundles/WebResourceBundle/Autoloader/Register/autoloadClassmap.php
@@ -617,6 +617,7 @@ return [
     'Syscodes\\Components\\Validation\\Rules\\Integer' => SYS_PATH.'src/components/Validation/Rules/Integer.php',
     'Syscodes\\Components\\Validation\\Rules\\Ip' => SYS_PATH.'src/components/Validation/Rules/Ip.php',
     'Syscodes\\Components\\Validation\\Rules\\Ipv4' => SYS_PATH.'src/components/Validation/Rules/Ipv4.php',
+    'Syscodes\\Components\\Validation\\Rules\\Ipv6' => SYS_PATH.'src/components/Validation/Rules/Ipv6.php',
     'Syscodes\\Components\\Validation\\Rules\\Required' => SYS_PATH.'src/components/Validation/Rules/Required.php',
     'Syscodes\\Components\\Validation\\Traits\\Messages' => SYS_PATH.'src/components/Validation/Traits/Messages.php',
     'Syscodes\\Components\\Validation\\Attribute' => SYS_PATH.'src/components/Validation/Attribute.php',
