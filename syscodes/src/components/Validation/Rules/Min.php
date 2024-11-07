cf97c3d6e3740df4b9468407514ea51b0191ce48
Alexander Campo <jalexcam@gmail.com>
2024-11-06 23:44:45 -0500

    Create Min.php

---
+++ syscodes/src/components/Validation/Rules/Min.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2024 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Validation\Rules;
+
+use Syscodes\Components\Validation\Rules;
+  
+/**
+ * Gets the attribute must be maximun max.
+ */
+class Max extends Rules
+{
+    use Traits\Size;
+
+    /** 
+     * The message depends of attribute.
+     * 
+     * @var string $message
+     */
+    protected $message = "The :attribute minimum is :min";
+
+    /**
+     * The fillable params.
+     * 
+     * @var array $fillableParams
+     */
+    protected $fillableParams = ['min'];
+
+    /**
+     * Check the value is valid.
+     * 
+     * @param  mixed  $value
+     * 
+     * @return bool
+     */
+    public function check($value): bool
+    {
+        $this->requireParameters($this->fillableParams);
+        
+        $max       = $this->getBytesSize($this->parameter('min'));
+        $valueSize = $this->getValueSize($value);
+        
+        if ( ! is_numeric($valueSize)) {
+            return false;
+        }
+        
+        return $valueSize >= $max;
+    }
+}
