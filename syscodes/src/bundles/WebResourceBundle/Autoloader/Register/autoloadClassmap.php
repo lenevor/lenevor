f30f9fdb4ed4d7ea4744939f6a24b106f6550ddf
Alexander Campo <jalexcam@gmail.com>
2024-11-10 23:04:40 -0500

    Added new namespace

--- syscodes/src/bundles/WebResourceBundle/Autoloader/Register/autoloadClassmap.php
+++ syscodes/src/bundles/WebResourceBundle/Autoloader/Register/autoloadClassmap.php
@@ -623,6 +623,7 @@ return [
     'Syscodes\\Components\\Validation\\Rules\\Max' => SYS_PATH.'src/components/Validation/Rules/Max.php',
     'Syscodes\\Components\\Validation\\Rules\\Min' => SYS_PATH.'src/components/Validation/Rules/Min.php',
     'Syscodes\\Components\\Validation\\Rules\\Nullable' => SYS_PATH.'src/components/Validation/Rules/Nullable.php',
+    'Syscodes\\Components\\Validation\\Rules\\Numeric' => SYS_PATH.'src/components/Validation/Rules/Numeric.php',
     'Syscodes\\Components\\Validation\\Rules\\Required' => SYS_PATH.'src/components/Validation/Rules/Required.php',
     'Syscodes\\Components\\Validation\\Traits\\Messages' => SYS_PATH.'src/components/Validation/Traits/Messages.php',
     'Syscodes\\Components\\Validation\\Attribute' => SYS_PATH.'src/components/Validation/Attribute.php',
