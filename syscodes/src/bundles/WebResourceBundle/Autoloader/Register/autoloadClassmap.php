266d474d71f1c16678750942d1c618a53ff9a78d
Alexander Campo <jalexcam@gmail.com>
2024-11-18 02:18:28 -0500

    Added new namespace

--- syscodes/src/bundles/WebResourceBundle/Autoloader/Register/autoloadClassmap.php
+++ syscodes/src/bundles/WebResourceBundle/Autoloader/Register/autoloadClassmap.php
@@ -627,6 +627,7 @@ return [
     'Syscodes\\Components\\Validation\\Rules\\Present' => SYS_PATH.'src/components/Validation/Rules/Present.php',
     'Syscodes\\Components\\Validation\\Rules\\Regex' => SYS_PATH.'src/components/Validation/Rules/Regex.php',
     'Syscodes\\Components\\Validation\\Rules\\Required' => SYS_PATH.'src/components/Validation/Rules/Required.php',
+    'Syscodes\\Components\\Validation\\Rules\\Same' => SYS_PATH.'src/components/Validation/Rules/Same.php',
     'Syscodes\\Components\\Validation\\Rules\\Uppercase' => SYS_PATH.'src/components/Validation/Rules/Uppercase.php',
     'Syscodes\\Components\\Validation\\Traits\\Messages' => SYS_PATH.'src/components/Validation/Traits/Messages.php',
     'Syscodes\\Components\\Validation\\Attribute' => SYS_PATH.'src/components/Validation/Attribute.php',
