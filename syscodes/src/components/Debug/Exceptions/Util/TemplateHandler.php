e12aa51a3c460ff8756b57ceb285295d0f1484b7
Alexander Campo <jalexcam@gmail.com>
2025-03-05 01:58:23 -0500

    Changed color of the comments of source code

--- syscodes/src/components/Debug/Exceptions/Util/TemplateHandler.php
+++ syscodes/src/components/Debug/Exceptions/Util/TemplateHandler.php
@@ -165,7 +165,7 @@ class TemplateHandler
 		// Set our highlight colors:
 		if (function_exists('ini_set')) {
 			ini_set('highlight.bg', '#000');
-			ini_set('highlight.comment', '#959595');
+			ini_set('highlight.comment', '#757575');
 			ini_set('highlight.default', '#717DE8');
 			ini_set('highlight.html', '#06B');
 			ini_set('highlight.keyword', '#F47E3A');
