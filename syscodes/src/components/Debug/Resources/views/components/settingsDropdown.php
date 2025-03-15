9879eefd941f478115eb11e95845f9c5cc86c0c7
Alexander Campo <jalexcam@gmail.com>
2025-03-15 03:14:16 -0500

    Update settingsDropdown.php

--- syscodes/src/components/Debug/Resources/views/components/settingsDropdown.php
+++ syscodes/src/components/Debug/Resources/views/components/settingsDropdown.php
@@ -6,7 +6,7 @@
         </div>
         <label for="editor">
             <span class="editor-title">Editor</span>
-            <div class="select-box">
+            <div class="group-option">
                 <select name="editor-select" id="editor">
                     <option class="option-editor" value="sublime">Sublime</option>
                     <option class="option-editor" value="textmate">TextMate</option>
@@ -18,16 +18,22 @@
                     <option class="option-editor" value="vscode">VS Code</option>
                     <option class="option-editor" value="atom">Atom</option>
                 </select>
-            </div>
+                <div class="identify">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-down" class="svg-inline--fa fa-angle-down group-hover:text-indigo-500 text-sm" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"></path></svg>
+                </div>
+            <div>
         </label>
         <label for="editor">
             <span class="editor-title">Theme</span>
-            <div class="select-box">
+            <div class="group-option">
                 <select name="editor-select" id="editor">
-                    <option class="option-theme" value="auto" selected>Auto</option>
-                    <option class="option-theme" value="light">Light</option>
-                    <option class="option-theme" value="dark">Dark</option>
+                        <option class="option-theme" value="auto" selected>Auto</option>
+                        <option class="option-theme" value="light">Light</option>
+                        <option class="option-theme" value="dark">Dark</option>
                 </select>
+                <div class="identify">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-down" class="svg-inline--fa fa-angle-down group-hover:text-indigo-500 text-sm" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"></path></svg>
+                </div>
             </div>
         </label>
     </div>
