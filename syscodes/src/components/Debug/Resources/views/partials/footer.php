98c43380f5063cf39b12864cd9515f379c4058fe
Alexander Campo <jalexcam@gmail.com>
2025-03-29 01:43:13 -0500

    Update footer.php

--- syscodes/src/components/Debug/Resources/views/partials/footer.php
+++ syscodes/src/components/Debug/Resources/views/partials/footer.php
@@ -1,9 +1,16 @@
 <footer>
-		<div class="copy">
-			<?= e($handler->getBrand()) ?>
-			<p class="version">
-				<span><?= \Syscodes\Components\Version::RELEASE ?></span>
-			</p>&nbsp;-&nbsp;<p>The Lenevor error reporting service.</p>
-		</div>
+	<nav>
+		<li>
+			<a href="">Docs</a>
+		</li>
+		<li>
+			<a href="">Lenevor</a>
+		</li>
+	</nav>
+	<div class="copy">
+		<?= e($handler->getBrand()) ?>
+		<p class="version">
+			<span><?= \Syscodes\Components\Version::RELEASE ?></span>
+		</p>&nbsp;-&nbsp;<p>The Lenevor error reporting service.</p>
 	</div>
 </footer>
