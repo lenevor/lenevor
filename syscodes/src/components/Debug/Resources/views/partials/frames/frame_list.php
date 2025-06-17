84b717f0b0eb1d77f74e2aecf8ed90c39db1ad34
Alexander Campo <jalexcam@gmail.com>
2025-06-17 01:15:16 -0500

    Removed the tags html

--- syscodes/src/components/Debug/Resources/views/partials/frames/frame_list.php
+++ syscodes/src/components/Debug/Resources/views/partials/frames/frame_list.php
@@ -1,9 +1,6 @@
 <aside class="frame-container-scroll scrollbar">
 <?php foreach ($frames as $index => $frame) : ?>
-	<div class="frame frame-application <?= ($index == 0) ? 'active' : '' ?>" data-index="<?= $index ?>">		
-		<div class="frame-main-index">
-			<div class="frame-index"><?= (count($frames) - $index - 1) + 1 ?></div>
-		</div>			
+	<div class="frame frame-application <?= ($index == 0) ? 'active' : '' ?>" data-index="<?= $index ?>">				
 		<div class="frame-method-info">			
 			<div class="frame-info-class">
 			<?php if ($frame->getClass() == '') : ?>
