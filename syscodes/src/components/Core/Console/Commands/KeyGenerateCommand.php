86487807d643ecc1c0ffe0cfea31ea76aefd1cc4
Alexander Campo <jalexcam@gmail.com>
2025-10-05 21:16:49 -0500

    Added new methods for generate a random key

--- syscodes/src/components/Core/Console/Commands/KeyGenerateCommand.php
+++ syscodes/src/components/Core/Console/Commands/KeyGenerateCommand.php
@@ -22,15 +22,28 @@
 
 namespace Syscodes\Components\Core\Console\Commands;
 
-use Syscodes\Components\Console\Command\Command;
+use Syscodes\Components\Console\Command;
+use Syscodes\Components\Encryption\Encrypter;
 use Syscodes\Components\Console\Input\InputOption;
+use Syscodes\Components\Console\Concerns\ConfirmProcess;
+use Syscodes\Components\Console\Attribute\AsCommandAttribute;
 use Syscodes\Components\Contracts\Console\Input\InputOption as InputOptionInterface;
 
 /**
  * This class displays the key generate for a given command.
  */
+#[AsCommandAttribute(name: 'key:generate')]
 class KeyGenerateCommand extends Command
 {
+    use ConfirmProcess;
+
+    /**
+     * The console command description.
+     * 
+     * @var string $description
+     */
+    protected string $description = 'Set the application key';
+
     /**
      * Gets input definition for command.
      * 
@@ -38,11 +51,10 @@ class KeyGenerateCommand extends Command
      */
     protected function define()
     {
-        $this->setName('key:generate')
-             ->setDefinition([
+        $this->setDefinition([
                     new InputOption('show', null, InputOptionInterface::VALUE_REQUIRED, 'Display the key instead of modifying files'),
-               ])
-             ->setDescription('Set the application key');
+                    new InputOption('force', null, InputOptionInterface::VALUE_OPTIONAL, 'Force the operation to run when in production'),
+        ]);
     }
 
     /**
@@ -52,10 +64,90 @@ class KeyGenerateCommand extends Command
      * 
      * @throws \LogicException
      */
-    protected function handle()
+    public function handle()
     {
-        echo 'hola mundo';
+        $key = $this->generateRandomKey();
 
-        return 0;
+        if ($this->option('show')) {
+            return $this->note($key);
+        }
+
+        if ( ! $this->setKeyInEnvironmentFile($key)) {
+            return;
+        }
+        
+        $this->lenevor['config']['security.key'] = $key;
+
+        $this->commandline('<bg=blue;fg=white> INFO </> Application key set successfully.');
+    }
+    
+    /**
+     * Generate a random key for the application.
+     * 
+     * @return string
+     */
+    protected function generateRandomKey(): string
+    {
+        return 'base64:'.base64_encode(
+            Encrypter::generateRandomKey($this->lenevor['config']['security.cipher'])
+        );
+    }
+
+    /**
+     * Set the application key in the environment file.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    protected function setKeyInEnvironmentFile($key): bool
+    {
+        $currentKey = $this->lenevor['config']['security.key'];
+
+        if (strlen($currentKey) !== 0 && ( ! $this->confirmToProceed())) {
+            return false;
+        }
+
+        if ( ! $this->writeNewEnvironmentFileWith($key)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Write a new environment file with the given key.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    protected function writeNewEnvironmentFileWith($key): bool
+    {
+        $replaced = preg_replace(
+            $this->keyReplacementPattern(),
+            'APP_KEY = '.$key,
+            $input = file_get_contents($this->lenevor->environmentFilePath())
+        );
+        
+        if ($replaced === $input || $replaced === null) {
+            $this->error('Unable to set application key. No APP_KEY variable was found in the .env file.');
+
+            return false;
+        }
+
+        file_put_contents($this->lenevor->environmentFilePath(), $replaced);
+
+        return true;
+    }
+
+    /**
+     * Get a regex pattern that will match env APP_KEY with any random key.
+     *
+     * @return string
+     */
+    protected function keyReplacementPattern(): string
+    {
+        $escaped = preg_quote(' = '.$this->lenevor['config']['security.key'], '/');
+
+        return "/^APP_KEY{$escaped}/m";
     }
 }
