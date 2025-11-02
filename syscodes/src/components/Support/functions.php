e04ed23f95873c448db70976d785ebbc25f6fd24
Alexander Campo <jalexcam@gmail.com>
2025-11-01 20:56:41 -0500

    Create functions.php

---
+++ syscodes/src/components/Support/functions.php
@@ -0,0 +1,47 @@
+<?php 
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Support;
+
+if ( ! function_exists('Syscodes\Components\Support\php_binary')) {
+    /**
+     * Determine the PHP Binary.
+     * 
+     * @return string
+     */
+    function php_binary(): string
+    {
+        return \PHP_BINARY;
+    }
+}
+
+if ( ! function_exists('Syscodes\Components\Support\prime_binary')) {
+    /**
+     * Determine the Prime executable.
+     * 
+     * @return string
+     */
+    function prime_binary(): string
+    {
+        return defined('PRIME_BINARY') ? PRIME_BINARY : 'prime';
+    }
+}
