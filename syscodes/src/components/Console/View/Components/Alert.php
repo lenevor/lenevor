ed931302ca9a73c5eea28b32f18ba3b5d0532460
Alexander Campo <jalexcam@gmail.com>
2025-11-27 01:27:03 -0500

    Create Alert.php

---
+++ syscodes/src/components/Console/View/Components/Alert.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Console\View\Components;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Renders the alert component.
+ */
+class Alert extends Component
+{
+    /**
+     * Renders the component using the given arguments.
+     *
+     * @param  string  $string
+     * @param  int  $verbosity
+     * 
+     * @return void
+     */
+    public function render($string, $verbosity = OutputInterface::VERBOSITY_NORMAL)
+    {
+        $this->renderView('alert', [
+            'content' => $string,
+        ], $verbosity);
+    }
+}
