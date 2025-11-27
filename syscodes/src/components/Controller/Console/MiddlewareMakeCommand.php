991931bc76205dcf4129d3126d097f710c26d76e
Alexander Campo <jalexcam@gmail.com>
2025-11-27 01:27:37 -0500

    Create MiddlewareMakeCommand.php

---
+++ syscodes/src/components/Controller/Console/MiddlewareMakeCommand.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Routing\Console;
+
+use Syscodes\Components\Console\GeneratorCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+/**
+ * Creates middlewares.
+ */
+#[AsCommand(name: 'make:middleware')]
+class MiddlewareMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:middleware';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new HTTP middleware class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Middleware';
+
+    /**
+     * Get the template file for the generator.
+     *
+     * @return string
+     */
+    protected function getTemplate(): string
+    {
+        return $this->resolveTemplatePath('/templates/middleware.tpl');
+    }
+
+    /**
+     * Resolve the fully-qualified path to the template.
+     *
+     * @param  string  $template
+     * 
+     * @return string
+     */
+    protected function resolveTemplatePath($template)
+    {
+        return file_exists($customPath = $this->lenevor->basePath(trim($template, '/')))
+            ? $customPath
+            : __DIR__.$template;
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * 
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace.'\Http\Middleware';
+    }
+}
