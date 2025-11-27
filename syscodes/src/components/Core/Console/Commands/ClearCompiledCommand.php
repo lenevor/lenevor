a6a1edb4225284c426201000141d365ea9463936
Alexander Campo <jalexcam@gmail.com>
2025-11-27 01:27:53 -0500

    Create ClearCompiledCommand.php

---
+++ syscodes/src/components/Core/Console/Commands/ClearCompiledCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Core\Console\Commands;
+
+use Syscodes\Components\Console\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+/**
+ * Allows clear compiled of commands.
+ */
+#[AsCommand(name: 'clear-compiled')]
+class ClearCompiledCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'clear-compiled';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove the compiled class file';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (is_file($servicesPath = $this->lenevor->getCachedServicesPath())) {
+            @unlink($servicesPath);
+        }
+
+        $this->components->info('Compiled services files removed successfully.');
+    }
+}
