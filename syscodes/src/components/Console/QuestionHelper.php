7c9a341e4865cf6705893b50ef233b148a987062
Alexander Campo <jalexcam@gmail.com>
2025-11-29 20:08:02 -0500

    Create QuestionHelper.php

---
+++ syscodes/src/components/Console/QuestionHelper.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Console;
+
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+use Symfony\Component\Console\Helper\SymfonyQuestionHelper;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class QuestionHelper extends SymfonyQuestionHelper
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @return void
+     */
+    #[\Override]
+    protected function writePrompt(OutputInterface $output, Question $question): void
+    {
+        $text = OutputFormatter::escapeTrailingBackslash($question->getQuestion());
+
+        $text = "  <fg=default;options=bold>$text</></>";
+
+        $default = $question->getDefault();
+
+        if ($question->isMultiline()) {
+            $text .= sprintf(' (press %s to continue)', 'Windows' == PHP_OS_FAMILY
+                ? '<comment>Ctrl+Z</comment> then <comment>Enter</comment>'
+                : '<comment>Ctrl+D</comment>');
+        }
+
+        switch (true) {
+            case null === $default:
+                $text = sprintf('<info>%s</info>', $text);
+
+                break;
+
+            case $question instanceof ConfirmationQuestion:
+                $text = sprintf('<info>%s (yes/no)</info> [<comment>%s</comment>]', $text, $default ? 'yes' : 'no');
+
+                break;
+
+            case $question instanceof ChoiceQuestion:
+                $choices = $question->getChoices();
+                $text = sprintf('<info>%s</info> [<comment>%s</comment>]', $text, OutputFormatter::escape($choices[$default] ?? $default));
+
+                break;
+
+            default:
+                $text = sprintf('<info>%s</info> [<comment>%s</comment>]', $text, OutputFormatter::escape($default));
+
+                break;
+        }
+
+        $output->writeln($text);
+
+        $output->write('<options=bold>❯ </>');
+    }
+}
