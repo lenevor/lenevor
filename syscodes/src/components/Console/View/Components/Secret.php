947ae015c27e3a9c669cfdee28c7595da19bcdbf
Alexander Campo <jalexcam@gmail.com>
2025-12-02 02:17:37 -0500

    Create Secret.php

---
+++ syscodes/src/components/Console/View/Components/Secret.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Console\View\Components;
+
+use Symfony\Component\Console\Question\Question;
+
+/**
+ * Renders the secret component.
+ */
+class Secret extends Component
+{
+    /**
+     * Renders the component using the given arguments.
+     *
+     * @param  string  $question
+     * @param  bool  $fallback
+     * 
+     * @return mixed
+     */
+    public function render($question, $fallback = true)
+    {
+        $question = new Question($question);
+
+        $question->setHidden(true)->setHiddenFallback($fallback);
+
+        return $this->usingQuestionHelper(fn () => $this->output->askQuestion($question));
+    }
+}
