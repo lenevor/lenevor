a3be9d307505310e22c56b8a331f20faae19c3d1
Alexander Campo <jalexcam@gmail.com>
2025-12-06 01:57:59 -0500

    Create ViewMakeCommand.php

---
+++ syscodes/src/components/Core/Console/Commands/ViewMakeCommand.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Syscodes\Components\Core\Console\Commands;
+
+use Syscodes\Components\Core\Inspiration;
+use Syscodes\Components\Console\GeneratorCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Allows create view file 
+ */
+#[AsCommand(name: 'make:view')]
+class ViewMakeCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $name = 'make:view';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new view';
+
+    /**
+     * The type of file being generated.
+     *
+     * @var string
+     */
+    protected $type = 'View';
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * 
+     * @return string
+     *
+     * @throws \Syscodes\Components\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function buildClass($name): string
+    {
+        $contents = parent::buildClass($name);
+
+        return str_replace(
+            '{{ quote }}',
+            Inspiration::quotes()->random(),
+            $contents,
+        );
+    }
+
+    /**
+     * Get the destination view path.
+     *
+     * @param  string  $name
+     * 
+     * @return string
+     */
+    protected function getPath($name): string
+    {
+        return $this->viewPath(
+            $this->getNameInput().'.'.$this->option('extension'),
+        );
+    }
+
+    /**
+     * Get the desired view name from the input.
+     *
+     * @return string
+     */
+    protected function getNameInput(): string
+    {
+        $name = trim($this->argument('name'));
+
+        $name = str_replace(['\\', '.'], '/', $name);
+
+        return $name;
+    }
+
+    /**
+     * Get the template file for the generator.
+     *
+     * @return string
+     */
+    protected function getTemplate(): string
+    {
+        return $this->resolveTemplatePath(
+            '/templates/view.tpl',
+        );
+    }
+
+    /**
+     * Resolve the fully-qualified path to the template.
+     *
+     * @param  string  $template
+     * 
+     * @return string
+     */
+    protected function resolveTemplatePath($template): string
+    {
+        return file_exists($customPath = $this->lenevor->basePath(trim($template, '/')))
+            ? $customPath
+            : __DIR__.$template;
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['extension', null, InputOption::VALUE_OPTIONAL, 'The extension of the generated view', 'plaze.php'],
+            ['force', 'f', InputOption::VALUE_NONE, 'Create the view even if the view already exists'],
+        ];
+    }
+}
