8e39e8f18b281e51b068d60fd49e811a2508a96d
Alexander Campo <jalexcam@gmail.com>
2025-12-06 01:57:14 -0500

    Create InterfaceMakeCommand.php

---
+++ syscodes/src/components/Core/Console/Commands/InterfaceMakeCommand.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Core\Console\Commands;
+
+use Syscodes\Components\Console\GeneratorCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Create an interface for console.
+ */
+#[AsCommand(name: 'make:interface')]
+class InterfaceMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:interface';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new interface';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Interface';
+
+    /**
+     * Get the template file for the generator.
+     *
+     * @return string
+     */
+    protected function getTemplate(): string
+    {
+        return __DIR__.'/templates/interface.tpl';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * 
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return match (true) {
+            is_dir(app_path('Contracts')) => $rootNamespace.'\\Contracts',
+            is_dir(app_path('Interfaces')) => $rootNamespace.'\\Interfaces',
+            default => $rootNamespace,
+        };
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['force', 'f', InputOption::VALUE_NONE, 'Create the interface even if the interface already exists'],
+        ];
+    }
+}
