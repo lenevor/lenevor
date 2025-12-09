b353eb44a1ad1bd1cf4ad28b1dc09040803ecc32
Alexander Campo <jalexcam@gmail.com>
2025-12-08 19:38:16 -0500

    Create ConfigClearCommand.php

---
+++ syscodes/src/components/Core/Console/Commands/ConfigClearCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Core\Console\Commands;
+
+use Syscodes\Components\Console\Command;
+use Syscodes\Components\Filesystem\Filesystem;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+/**
+ * Allows the cleaning of the configuration file.
+ */
+#[AsCommand(name: 'config:clear')]
+class ConfigClearCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'config:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove the configuration cache file';
+
+    /**
+     * The filesystem instance.
+     *
+     * @var \Syscodes\Components\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * Constructor. Create a new config clear command instance.
+     *
+     * @param  \Syscodes\Components\Filesystem\Filesystem  $files
+     * 
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->files->delete($this->lenevor->getCachedConfigPath());
+
+        $this->components->info('Configuration cache cleared successfully.');
+    }
+}
