23f7592233ed5f4a273eba80ebdba749445fc413
Alexander Campo <jalexcam@gmail.com>
2025-12-12 22:03:28 -0500

    Create FormRequestServiceProvider.php

---
+++ syscodes/src/components/Core/Providers/FormRequestServiceProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Core\Providers;
+
+use Syscodes\Components\Core\Http\FormRequest;
+use Syscodes\Components\Support\ServiceProvider;
+use Syscodes\Components\Routing\Generators\Redirector;
+use Syscodes\Components\Contracts\Validation\ValidatesResolved;
+
+/**
+ * Gets form request service provider.
+ */
+class FormRequestServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->afterResolving(ValidatesResolved::class, function ($resolved) {
+            $resolved->validateResolved();
+        });
+
+        $this->app->resolving(FormRequest::class, function ($request, $app) {
+            $request = FormRequest::createFrom($app['request'], $request);
+
+            $request->setContainer($app)->setRedirector($app->make(Redirector::class));
+        });
+    }
+}
