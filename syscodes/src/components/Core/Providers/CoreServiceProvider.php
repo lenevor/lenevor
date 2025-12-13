2a47b119680f1c102c1533223f748ae067a90e24
Alexander Campo <jalexcam@gmail.com>
2025-12-12 22:03:25 -0500

    Create CoreServiceProvider.php

---
+++ syscodes/src/components/Core/Providers/CoreServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Core\Providers;
+
+use Syscodes\Components\Support\AggregateServiceProvider;
+
+/**
+ * Allows is register aggregate service providers use to core system.
+ */
+class CoreServiceProvider extends AggregateServiceProvider
+{
+    /**
+     * The provider class names.
+     * 
+     * @var array $providers
+     */
+    protected $providers = [
+        FormRequestServiceProvider::class,
+    ];
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        parent::register();
+    }
+}
