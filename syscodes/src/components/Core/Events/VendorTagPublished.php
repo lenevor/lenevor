ab724a06d7687447cde8f02bf52797c4f0fe2ce3
Alexander Campo <jalexcam@gmail.com>
2025-12-17 01:38:26 -0500

    Create VendorTagPublished.php

---
+++ syscodes/src/components/Core/Events/VendorTagPublished.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Core\Events;
+
+/**
+ * Allows publish paths registered with tags.
+ */
+class VendorTagPublished
+{
+    /**
+     * The vendor tag that was published.
+     *
+     * @var string
+     */
+    public $tag;
+
+    /**
+     * The publishable paths registered by the tag.
+     *
+     * @var array
+     */
+    public $paths;
+
+    /**
+     * Construtor. Create a new event instance.
+     *
+     * @param  string  $tag
+     * @param  array  $paths
+     * 
+     * @return void
+     */
+    public function __construct($tag, $paths)
+    {
+        $this->tag = $tag;
+        $this->paths = $paths;
+    }
+}
