847143050690f7ea8bcfeaf2e48c1413467ebb24
Alexander Campo <jalexcam@gmail.com>
2025-12-22 02:21:28 -0500

    Spacing

--- syscodes/src/components/Collections/helpers.php
+++ syscodes/src/components/Collections/helpers.php
@@ -57,7 +57,7 @@ if ( ! function_exists('data_get')) {
             if ($segment === '*') {
                 if ($target instanceof Collection) {
                     $target = $target->all();
-                } elseif (! is_array($target)) {
+                } elseif ( ! is_array($target)) {
                     return value($default);
                 }
                 
