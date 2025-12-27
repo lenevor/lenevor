15a2ef86b1ca23b8945a396071820f53ea5543ca
Alexander Campo <jalexcam@gmail.com>
2025-12-26 23:57:45 -0500

    Create ThrottleLimiter.php

---
+++ syscodes/src/components/Support/Facades/ThrottleLimiter.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Support\Facades;
+
+/**
+ * Initialize the ThrottleLimiter class facade.
+ * 
+ * @method static \Syscodes\Components\ThrottleLimiter\ThrottleLimiter register(\BackedEnum|\UnitEnum|string $name, \Closure $callback)
+ * @method static \Closure|null limiter(\BackedEnum|\UnitEnum|string $name)
+ * @method static mixed attempt(string $key, int $maxAttempts, \Closure $callback, \DateTimeInterface|\DateInterval|int $decaySeconds = 60)
+ * @method static bool tooManyAttempts(string $key, int $maxAttempts)
+ * @method static int hit(string $key, \DateTimeInterface|\DateInterval|int $decaySeconds = 60)
+ * @method static int increment(string $key, \DateTimeInterface|\DateInterval|int $decaySeconds = 60, int $amount = 1)
+ * @method static int decrement(string $key, \DateTimeInterface|\DateInterval|int $decaySeconds = 60, int $amount = 1)
+ * @method static mixed attempts(string $key)
+ * @method static bool resetAttempts(string $key)
+ * @method static int remaining(string $key, int $maxAttempts)
+ * @method static int retriesLeft(string $key, int $maxAttempts)
+ * @method static void clear(string $key)
+ * @method static int availableIn(string $key)
+ * @method static string cleanRateLimiterKey(string $key)
+ * 
+ * @see \Syscodes\Components\ThrottleLimiter\ThrottleLimiter
+ */
+class ThrottleLimiter extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     * 
+     * @return string
+     * 
+     * @throws \RuntimeException
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return \Syscodes\Components\ThrottleLimiter\ThrottleLimiter::class;
+    }
+}
