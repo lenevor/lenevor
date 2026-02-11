3f03a16d96e8bdda60aac2f30fa7462744875908
Alexander Campo <jalexcam@gmail.com>
2026-02-11 00:06:58 -0500

    Update PostgresConnector.php

--- syscodes/src/components/Database/Connectors/PostgresConnector.php
+++ syscodes/src/components/Database/Connectors/PostgresConnector.php
@@ -23,12 +23,15 @@
 namespace Syscodes\Components\Database\Connectors;
 
 use PDO;
+use Syscodes\Components\Database\Concerns\ParsesSearchPath;
 
 /**
  * A PDO based PostgreSQL Database Connector.
  */
 class PostgresConnector extends Connector implements ConnectorInterface
 {
+    use ParsesSearchPath;
+
     /**
      * The default PDO connection options.
      * 
@@ -54,13 +57,15 @@ class PostgresConnector extends Connector implements ConnectorInterface
 
         $options = $this->getOptions($config);
 
+        // First we'll create the basic DSN and connection instance connecting to the
+        // using the configuration option specified by the developer.
         $connection = $this->createConnection($dsn, $config, $options);
 
         $this->configureEncoding($connection, $config);
 
         $this->configureTimezone($connection, $config);
 
-        $this->configureSchema($connection, $config);
+        $this->configureSearchPath($connection, $config);
 
         return $connection;
     }
@@ -74,18 +79,28 @@ class PostgresConnector extends Connector implements ConnectorInterface
      */
     protected function getDsn(array $config): string
     {
+        // First we will create the basic DSN setup as well as the port if it is in
+        // in the configuration options.
         extract($config, EXTR_SKIP);
 
-        $host = isset($config['host']) ? "host={$config['host']}" : '';
+        $host = isset($host) ? "host={$host};" : '';
 
-        $dsn = "pgsql:{$host}";
+        // Sometimes - users may need to connect to a database that has a different
+        // name than the database used for "information_schema" queries.
+        $database = $connect_via_database ?? $database ?? null;
 
-        if (isset($config['database'])) {
-            $dsn .= ";dbname={$config['database']}";
+        $port = $connect_via_port ?? $port ?? null;
+
+        $dsn = "pgsql:{$host}dbname='{$database}'";
+
+        // If a port was specified, we will add it to this Postgres DSN 
+        // connections format.
+        if ( ! is_null($port)) {
+            $dsn .= ";port={$port}";
         }
 
-        if (isset($config['port'])) {
-            $dsn .= ";port={$config['port']}";
+        if (isset($charset)) {
+            $dsn .= ";client_encoding='{$charset}'";
         }
 
         return $this->addSslOptions($dsn, $config);
@@ -101,8 +116,7 @@ class PostgresConnector extends Connector implements ConnectorInterface
      */
     protected function addSslOptions($dsn, array $config): string
     {
-        foreach (['sslmode', 'sslcert', 'sslkey', 'sslrootcert'] as $option)
-        {
+        foreach (['sslmode', 'sslcert', 'sslkey', 'sslrootcert'] as $option) {
             if (isset($config[$option])) {
                 $dsn .= ";{$option}={$config[$option]}";
             }
@@ -144,17 +158,33 @@ class PostgresConnector extends Connector implements ConnectorInterface
     }
 
     /**
-     * Get the schema on the connection.
+     * Get the "search_path" on the connection.
      * 
      * @param  \PDO  $connection
      * @param  array  $config
      * 
      * @return void
      */
-    protected function configureSchema($connection, array $config): void
+    protected function configureSearchPath($connection, array $config): void
     {
-        if (isset($config['schema'])) {
-            $connection->prepare("set search_path to {$config['schema']}")->execute();
+        if (isset($config['search_path']) || isset($config['schema'])) {
+            $searchPath = $this->quoteSearchPath(
+                $this->parseSearchPath($config['search_path'] ?? $config['schema'])
+            );
+
+            $connection->prepare("set search_path to {$searchPath}")->execute();
         }
+    }
+
+    /**
+     * Format the search path for the DSN.
+     *
+     * @param  array  $searchPath
+     * 
+     * @return string
+     */
+    protected function quoteSearchPath($searchPath): string
+    {
+        return count($searchPath) === 1 ? '"'.$searchPath[0].'"' : '"'.implode('", "', $searchPath).'"';
     }
 }
