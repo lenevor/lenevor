77d0489ec96cdde6e9574b07e76d86cc4e2c4e88
Alexander Campo <jalexcam@gmail.com>
2026-02-21 02:19:20 -0500

    Update SqlServerGrammar.php

--- syscodes/src/components/Database/Query/Grammars/SqlServerGrammar.php
+++ syscodes/src/components/Database/Query/Grammars/SqlServerGrammar.php
@@ -23,6 +23,8 @@
 namespace Syscodes\Components\Database\Query\Grammars;
 
 use Syscodes\Components\Database\Query\Builder;
+use Syscodes\Components\Support\Arr;
+use Syscodes\Components\Support\Str;
 
 /**
  * Allows make the grammar's for get results of the database
@@ -119,6 +121,24 @@ class SqlServerGrammar extends Grammar
     }
 
     /**
+     * Compile a delete statement without joins into SQL.
+     *
+     * @param  \Syscodes\Components\Database\Query\Builder  $builder
+     * @param  string  $table
+     * @param  string  $where
+     * 
+     * @return string
+     */
+    protected function compileDeleteWithoutJoins(Builder $builder, $table, $where): string
+    {
+        $sql = parent::compileDeleteWithoutJoins($builder, $table, $where);
+
+        return ! is_null($builder->limit) && $builder->limit > 0 && $builder->offset <= 0
+            ? Str::replaceFirst('delete', 'delete top ('.$builder->limit.')', $sql)
+            : $sql;
+    }
+
+    /**
      * Compile the random statement into SQL.
      * 
      * @param  string  $seed
@@ -140,6 +160,12 @@ class SqlServerGrammar extends Grammar
      */
     protected function compileLimit(Builder $builder, $limit): string
     {
+        $limit = (int) $limit;
+
+        if ($limit && $builder->offset > 0) {
+            return "fetch next {$limit} rows only";
+        }
+
         return '';
     }
 
@@ -153,6 +179,12 @@ class SqlServerGrammar extends Grammar
      */
     protected function compileOffset(Builder $builder, $offset): string
     {
+        $offset = (int) $offset;
+
+        if ($offset) {
+            return "offset {$offset} rows";
+        }
+
         return '';
     }
 
@@ -181,6 +213,22 @@ class SqlServerGrammar extends Grammar
         return 'select * from ('.$sql.') as '.$this->wrapTable('temp_table');
     }
 
+    /**
+     * Compile an exists statement into SQL.
+     *
+     * @param  \Syscodes\Components\Database\Query\Builder  $builder
+     * 
+     * @return string
+     */
+    public function compileExists(Builder $builder): string
+    {
+        $existsQuery = clone $builder;
+
+        $existsQuery->columns = [];
+
+        return $this->compileSelect($existsQuery->selectRaw('1 [exists]')->limit(1));
+    }
+
      /**
      * Compile a truncate table statement into SQL.
      * 
@@ -194,13 +242,75 @@ class SqlServerGrammar extends Grammar
     }
 
     /**
+     * Compile an update statement with joins into SQL.
+     *
+     * @param  \Syscodes\Components\Database\Query\Builder  $builder
+     * @param  string  $table
+     * @param  string  $columns
+     * @param  string  $where
+     * @return string
+     */
+    protected function compileUpdateWithJoins(Builder $builder, $table, $columns, $where): string
+    {
+        $alias = last(explode(' as ', $table));
+
+        $joins = $this->compileJoins($builder, $builder->joins);
+
+        return "update {$alias} set {$columns} from {$table} {$joins} {$where}";
+    }
+
+    /**
+     * Prepare the bindings for an update statement.
+     *
+     * @param  array  $bindings
+     * @param  array  $values
+     * 
+     * @return array
+     */
+    #[\Override]
+    public function prepareBindingsForUpdate(array $bindings, array $values): array
+    {
+        $cleanBindings = Arr::except($bindings, 'select');
+
+        $values = Arr::flatten(array_map(fn ($value) => value($value), $values));
+
+        return array_values(
+            array_merge($values, Arr::flatten($cleanBindings))
+        );
+    }
+
+    /**
+     * Compile the SQL statement to define a savepoint.
+     *
+     * @param  string  $name
+     * 
+     * @return string
+     */
+    public function compileSavepoint($name): string
+    {
+        return 'SAVE TRANSACTION '.$name;
+    }
+
+    /**
+     * Compile the SQL statement to execute a savepoint rollback.
+     *
+     * @param  string  $name
+     * 
+     * @return string
+     */
+    public function compileSavepointRollBack($name): string
+    {
+        return 'ROLLBACK TRANSACTION '.$name;
+    }
+
+    /**
      * Get the format for database stored dates.
      * 
      * @return string
      */
     public function getDateFormat(): string
     {
-        return 'Y-m-d H:i:s.000';
+        return 'Y-m-d H:i:s.v';
     }
 
     /**
@@ -231,6 +341,8 @@ class SqlServerGrammar extends Grammar
 
         $select = $builder->distinct ? 'select distinct ' : 'select ';
 
+        // If there is a limit on the query, but not an offset, we will add the top
+        // clause to the query, which serves as a "limit" type clause.
         if ($builder->limit > 0 && $builder->offset <= 0) {
             $select .= 'top '.$builder->limit.' ';
         }
@@ -257,5 +369,142 @@ class SqlServerGrammar extends Grammar
         if ( ! is_null($builder->lock)) {
             return $from.' with(rowlock,'.($builder->lock ? 'uplock,' : '').'holdlock)';
         }
+    }
+
+    /**
+     * Compile a "where date" clause.
+     *
+     * @param  \Syscodes\Components\Database\Query\Builder  $builder
+     * @param  array  $where
+     * 
+     * @return string
+     */
+    protected function whereDate(Builder $builder, $where): string
+    {
+        $value = $this->parameter($where['value']);
+
+        return 'cast('.$this->wrap($where['column']).' as date) '.$where['operator'].' '.$value;
+    }
+
+    /**
+     * Compile a "where time" clause.
+     *
+     * @param  \Syscodes\Components\Database\Query\Builder  $builder
+     * @param  array  $where
+     * 
+     * @return string
+     */
+    protected function whereTime(Builder $builder, $where): string
+    {
+        $value = $this->parameter($where['value']);
+
+        return 'cast('.$this->wrap($where['column']).' as time) '.$where['operator'].' '.$value;
+    }
+
+    /**
+     * Compile a "JSON contains" statement into SQL.
+     *
+     * @param  string  $column
+     * @param  string  $value
+     * 
+     * @return string
+     */
+    protected function compileJsonContains($column, $value): string
+    {
+        [$field, $path] = $this->wrapJsonFieldAndPath($column);
+
+        return $value.' in (select [value] from openjson('.$field.$path.'))';
+    }
+
+    /**
+     * Prepare the binding for a "JSON contains" statement.
+     *
+     * @param  mixed  $binding
+     * 
+     * @return string
+     */
+    public function prepareBindingForJsonContains($binding): string
+    {
+        return is_bool($binding) ? json_encode($binding) : $binding;
+    }
+
+    /**
+     * Compile a "JSON contains key" statement into SQL.
+     *
+     * @param  string  $column
+     * 
+     * @return string
+     */
+    protected function compileJsonContainsKey($column): string
+    {
+        $segments = explode('->', $column);
+
+        $lastSegment = array_pop($segments);
+
+        if (preg_match('/\[([0-9]+)\]$/', $lastSegment, $matches)) {
+            $segments[] = Str::beforeLast($lastSegment, $matches[0]);
+
+            $key = $matches[1];
+        } else {
+            $key = "'".str_replace("'", "''", $lastSegment)."'";
+        }
+
+        [$field, $path] = $this->wrapJsonFieldAndPath(implode('->', $segments));
+
+        return $key.' in (select [key] from openjson('.$field.$path.'))';
+    }
+
+    /**
+     * Compile a "JSON length" statement into SQL.
+     *
+     * @param  string  $column
+     * @param  string  $operator
+     * @param  string  $value
+     * 
+     * @return string
+     */
+    protected function compileJsonLength($column, $operator, $value): string
+    {
+        [$field, $path] = $this->wrapJsonFieldAndPath($column);
+
+        return '(select count(*) from openjson('.$field.$path.')) '.$operator.' '.$value;
+    }
+
+    /**
+     * Compile a "JSON value cast" statement into SQL.
+     *
+     * @param  string  $value
+     * 
+     * @return string
+     */
+    public function compileJsonValueCast($value): string
+    {
+        return 'json_query('.$value.')';
+    }
+
+    /**
+     * Wrap the given JSON selector.
+     *
+     * @param  string  $value
+     * 
+     * @return string
+     */
+    protected function wrapJsonSelector($value): string
+    {
+        [$field, $path] = $this->wrapJsonFieldAndPath($value);
+
+        return 'json_value('.$field.$path.')';
+    }
+
+    /**
+     * Wrap the given JSON boolean value.
+     *
+     * @param  string  $value
+     * 
+     * @return string
+     */
+    protected function wrapJsonBooleanValue($value): string
+    {
+        return "'".$value."'";
     }
 }
