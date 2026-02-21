fdca166a901be8a33f51e71291618111205ce342
Alexander Campo <jalexcam@gmail.com>
2026-02-21 02:19:47 -0500

    Update SqlServerProcessor.php

--- syscodes/src/components/Database/Query/Processors/SqlServerProcessor.php
+++ syscodes/src/components/Database/Query/Processors/SqlServerProcessor.php
@@ -22,20 +22,124 @@
  
 namespace Syscodes\Components\Database\Query\Processors;
 
+use Exception;
+use Syscodes\Components\Database\Connections\Connection;
+use Syscodes\Components\Database\Query\Builder;
+
 /**
  * Allows show the results of a column listing query for SqlServer Database.
  */
 class SqlServerProcessor extends Processor
 {
     /**
-     * Process the results of a column listing query.
+     * Process an "insert get ID" query.
+     *
+     * @param  \Syscodes\Components\Database\Query\Builder  $builder
+     * @param  string  $sql
+     * @param  array  $values
+     * @param  string|null  $sequence
      * 
-     * @param  array  $results
-     * 
-     * @return array
+     * @return int
      */
-    public function processColumnListing($results): array
+    public function processInsertGetId(Builder $builder, $sql, $values, $sequence = null): int
     {
-        return array_map(fn ($result) => ((object) $result)->column_name, $results);
+        $connection = $builder->getConnection();
+
+        $connection->insert($sql, $values);
+
+        if ($connection->getConfig('odbc') === true) {
+            $id = $this->processInsertGetIdForOdbc($connection);
+        } else {
+            $id = $connection->getPdo()->lastInsertId();
+        }
+
+        return is_numeric($id) ? (int) $id : $id;
+    }
+
+    /**
+     * Process an "insert get ID" query for ODBC.
+     *
+     * @param  \Syscodes\Components\Database\Connections\Connection  $connection
+     * @return int
+     *
+     * @throws \Exception
+     */
+    protected function processInsertGetIdForOdbc(Connection $connection): int
+    {
+        $result = $connection->selectFromConnection(
+            'SELECT CAST(COALESCE(SCOPE_IDENTITY(), @@IDENTITY) AS int) AS insertid'
+        );
+
+        if ( ! $result) {
+            throw new Exception('Unable to retrieve lastInsertID for ODBC.');
+        }
+
+        $row = $result[0];
+
+        return is_object($row) ? $row->insertid : $row['insertid'];
+    }
+
+    /** @inheritDoc */
+    public function processColumns($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            $type = match ($typeName = $result->type_name) {
+                'binary', 'varbinary', 'char', 'varchar', 'nchar', 'nvarchar' => $result->length == -1 ? $typeName.'(max)' : $typeName."($result->length)",
+                'decimal', 'numeric' => $typeName."($result->precision,$result->places)",
+                'float', 'datetime2', 'datetimeoffset', 'time' => $typeName."($result->precision)",
+                default => $typeName,
+            };
+
+            return [
+                'name' => $result->name,
+                'type_name' => $result->type_name,
+                'type' => $type,
+                'collation' => $result->collation,
+                'nullable' => (bool) $result->nullable,
+                'default' => $result->default,
+                'auto_increment' => (bool) $result->autoincrement,
+                'comment' => $result->comment,
+                'generation' => $result->expression ? [
+                    'type' => $result->persisted ? 'stored' : 'virtual',
+                    'expression' => $result->expression,
+                ] : null,
+            ];
+        }, $results);
+    }
+
+    /** @inheritDoc */
+    public function processIndexes($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            return [
+                'name' => strtolower($result->name),
+                'columns' => $result->columns ? explode(',', $result->columns) : [],
+                'type' => strtolower($result->type),
+                'unique' => (bool) $result->unique,
+                'primary' => (bool) $result->primary,
+            ];
+        }, $results);
+    }
+
+    /** @inheritDoc */
+    public function processForeignKeys($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            return [
+                'name' => $result->name,
+                'columns' => explode(',', $result->columns),
+                'foreign_schema' => $result->foreign_schema,
+                'foreign_table' => $result->foreign_table,
+                'foreign_columns' => explode(',', $result->foreign_columns),
+                'on_update' => strtolower(str_replace('_', ' ', $result->on_update)),
+                'on_delete' => strtolower(str_replace('_', ' ', $result->on_delete)),
+            ];
+        }, $results);
     }
 }
