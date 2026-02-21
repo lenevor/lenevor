6a05c257c4fc9f602992d5a540e7d58fa905c4bb
Alexander Campo <jalexcam@gmail.com>
2026-02-21 02:19:23 -0500

    Update MySqlProcessor.php

--- syscodes/src/components/Database/Query/Processors/MySqlProcessor.php
+++ syscodes/src/components/Database/Query/Processors/MySqlProcessor.php
@@ -22,6 +22,8 @@
  
 namespace Syscodes\Components\Database\Query\Processors;
 
+use Syscodes\Components\Database\Query\Builder;
+
 /**
  * Allows show the results of a column listing query for Mysql Database.
  */
@@ -37,5 +39,85 @@ class MySqlProcessor extends Processor
     public function processColumnListing($results): array
     {
         return array_map(fn ($result) => ((object) $result)->column_name, $results);
+    }
+
+    /**
+     * Process an  "insert get ID" query.
+     *
+     * @param  \Syscodes\Components\Database\Query\Builder  $builder
+     * @param  string  $sql
+     * @param  array  $values
+     * @param  string|null  $sequence
+     * 
+     * @return int
+     */
+    public function processInsertGetId(Builder $builder, $sql, $values, $sequence = null): int
+    {
+        $builder->getConnection()->insert($sql, $values, $sequence);
+
+        $id = $builder->getConnection()->getLastInsertId();
+
+        return is_numeric($id) ? (int) $id : $id;
+    }
+
+    /** @inheritDoc */
+    public function processColumns($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            return [
+                'name' => $result->name,
+                'type_name' => $result->type_name,
+                'type' => $result->type,
+                'collation' => $result->collation,
+                'nullable' => $result->nullable === 'YES',
+                'default' => $result->default,
+                'auto_increment' => $result->extra === 'auto_increment',
+                'comment' => $result->comment ?: null,
+                'generation' => $result->expression ? [
+                    'type' => match ($result->extra) {
+                        'STORED GENERATED' => 'stored',
+                        'VIRTUAL GENERATED' => 'virtual',
+                        default => null,
+                    },
+                    'expression' => $result->expression,
+                ] : null,
+            ];
+        }, $results);
+    }
+
+    /** @inheritDoc */
+    public function processIndexes($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            return [
+                'name' => $name = strtolower($result->name),
+                'columns' => $result->columns ? explode(',', $result->columns) : [],
+                'type' => strtolower($result->type),
+                'unique' => (bool) $result->unique,
+                'primary' => $name === 'primary',
+            ];
+        }, $results);
+    }
+
+    /** @inheritDoc */
+    public function processForeignKeys($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            return [
+                'name' => $result->name,
+                'columns' => explode(',', $result->columns),
+                'foreign_schema' => $result->foreign_schema,
+                'foreign_table' => $result->foreign_table,
+                'foreign_columns' => explode(',', $result->foreign_columns),
+                'on_update' => strtolower($result->on_update),
+                'on_delete' => strtolower($result->on_delete),
+            ];
+        }, $results);
     }
 }
