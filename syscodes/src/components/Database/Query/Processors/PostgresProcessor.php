771a598e0773ffe4126d8b372e9821ff238532b2
Alexander Campo <jalexcam@gmail.com>
2026-02-21 02:19:26 -0500

    Update PostgresProcessor.php

--- syscodes/src/components/Database/Query/Processors/PostgresProcessor.php
+++ syscodes/src/components/Database/Query/Processors/PostgresProcessor.php
@@ -41,7 +41,11 @@ class PostgresProcessor extends Processor
      */
     public function processInsertGetId(Builder $builder, $sql, $values, $sequence = null): int
     {
-        $result = $builder->getConnection()->getFromConnection($sql, $values)[0];
+        $connection = $builder->getConnection();
+
+        $connection->recordsHaveBeenModified();
+
+        $result = $connection->selectFromConnection($sql, $values)[0];
 
         $sequence = $sequence ?: 'id';
 
@@ -49,16 +53,125 @@ class PostgresProcessor extends Processor
 
         return is_numeric($id) ? (int) $id : $id;
     }
-    
-    /**
-     * Process the results of a column listing query.
-     * 
-     * @param  array  $results
-     * 
-     * @return array
-     */
-    public function processColumnListing($results): array
+
+    /** @inheritDoc */
+    public function processTypes($results): array
     {
-        return array_map(fn ($result) => ((object) $result)->column_name, $results);
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            return [
+                'name' => $result->name,
+                'schema' => $result->schema,
+                'schema_qualified_name' => $result->schema.'.'.$result->name,
+                'implicit' => (bool) $result->implicit,
+                'type' => match (strtolower($result->type)) {
+                    'b' => 'base',
+                    'c' => 'composite',
+                    'd' => 'domain',
+                    'e' => 'enum',
+                    'p' => 'pseudo',
+                    'r' => 'range',
+                    'm' => 'multirange',
+                    default => null,
+                },
+                'category' => match (strtolower($result->category)) {
+                    'a' => 'array',
+                    'b' => 'boolean',
+                    'c' => 'composite',
+                    'd' => 'date_time',
+                    'e' => 'enum',
+                    'g' => 'geometric',
+                    'i' => 'network_address',
+                    'n' => 'numeric',
+                    'p' => 'pseudo',
+                    'r' => 'range',
+                    's' => 'string',
+                    't' => 'timespan',
+                    'u' => 'user_defined',
+                    'v' => 'bit_string',
+                    'x' => 'unknown',
+                    'z' => 'internal_use',
+                    default => null,
+                },
+            ];
+        }, $results);
+    }
+
+    /** @inheritDoc */
+    public function processColumns($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            $autoincrement = $result->default !== null && str_starts_with($result->default, 'nextval(');
+
+            return [
+                'name' => $result->name,
+                'type_name' => $result->type_name,
+                'type' => $result->type,
+                'collation' => $result->collation,
+                'nullable' => (bool) $result->nullable,
+                'default' => $result->generated ? null : $result->default,
+                'auto_increment' => $autoincrement,
+                'comment' => $result->comment,
+                'generation' => $result->generated ? [
+                    'type' => match ($result->generated) {
+                        's' => 'stored',
+                        'v' => 'virtual',
+                        default => null,
+                    },
+                    'expression' => $result->default,
+                ] : null,
+            ];
+        }, $results);
+    }
+
+    /** @inheritDoc */
+    public function processIndexes($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            return [
+                'name' => strtolower($result->name),
+                'columns' => $result->columns ? explode(',', $result->columns) : [],
+                'type' => strtolower($result->type),
+                'unique' => (bool) $result->unique,
+                'primary' => (bool) $result->primary,
+            ];
+        }, $results);
+    }
+
+    /** @inheritDoc */
+    public function processForeignKeys($results): array
+    {
+        return array_map(function ($result) {
+            $result = (object) $result;
+
+            return [
+                'name' => $result->name,
+                'columns' => explode(',', $result->columns),
+                'foreign_schema' => $result->foreign_schema,
+                'foreign_table' => $result->foreign_table,
+                'foreign_columns' => explode(',', $result->foreign_columns),
+                'on_update' => match (strtolower($result->on_update)) {
+                    'a' => 'no action',
+                    'r' => 'restrict',
+                    'c' => 'cascade',
+                    'n' => 'set null',
+                    'd' => 'set default',
+                    default => null,
+                },
+                'on_delete' => match (strtolower($result->on_delete)) {
+                    'a' => 'no action',
+                    'r' => 'restrict',
+                    'c' => 'cascade',
+                    'n' => 'set null',
+                    'd' => 'set default',
+                    default => null,
+                },
+            ];
+        }, $results);
     }
 }
