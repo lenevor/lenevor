c1be555227556db25e88e0f1c016f28ea1b5f3f1
Alexander Campo <jalexcam@gmail.com>
2026-02-23 21:43:30 -0500

    Update SqlServerConnection.php

--- syscodes/src/components/Database/Connections/SqlServerConnection.php
+++ syscodes/src/components/Database/Connections/SqlServerConnection.php
@@ -22,10 +22,13 @@
 
 namespace Syscodes\Components\Database\Connections;
 
+use Closure;
+use Exception;
 use Syscodes\Components\Database\Query\Grammars\SqlServerGrammar as QueryGrammar;
 use Syscodes\Components\Database\Query\Processors\SqlServerProcessor as QueryProcessor;
 use Syscodes\Components\Database\Schema\Builders\SqlServerBuilder;
 use Syscodes\Components\Database\Schema\Grammars\SqlServerGrammar as SchemaGrammar;
+use Throwable;
 
 /**
  * SqlServer connection.
@@ -33,13 +36,86 @@ use Syscodes\Components\Database\Schema\Grammars\SqlServerGrammar as SchemaGramm
 class SqlServerConnection extends Connection
 {
     /**
+     * {@inheritdoc}
+     */
+    public function getDriverTitle()
+    {
+        return 'SQL Server';
+    }
+
+    /**
+     * Execute a Closure within a transaction.
+     *
+     * @param  \Closure  $callback
+     * @param  int  $attempts
+     * 
+     * @return mixed
+     *
+     * @throws \Throwable
+     */
+    public function transaction(Closure $callback, $attempts = 1)
+    {
+        for ($a = 1; $a <= $attempts; $a++) {
+            if ($this->getDriverName() === 'sqlsrv') {
+                return parent::transaction($callback, $attempts);
+            }
+
+            $this->getPdo()->exec('BEGIN TRAN');
+
+            // We'll simply execute the given callback within a try / catch block
+            // and if we catch any exception we can rollback the transaction
+            // so that none of the changes are persisted to the database.
+            try {
+                $result = $callback($this);
+
+                $this->getPdo()->exec('COMMIT TRAN');
+            }
+            // If we catch an exception, we will rollback so nothing gets messed
+            // up in the database.
+            catch (Throwable $e) {
+                $this->getPdo()->exec('ROLLBACK TRAN');
+
+                throw $e;
+            }
+
+            return $result;
+        }
+    }
+
+    /**
+     * Escape a binary value for safe SQL embedding.
+     *
+     * @param  string  $value
+     * 
+     * @return string
+     */
+    protected function escapeBinary($value): string
+    {
+        $hex = bin2hex($value);
+
+        return "0x{$hex}";
+    }
+
+    /**
+     * Determine if the given database exception was caused by a unique constraint violation.
+     *
+     * @param  \Exception  $exception
+     * 
+     * @return bool
+     */
+    protected function isUniqueConstraintError(Exception $exception): bool
+    {
+        return (bool) preg_match('#Cannot insert duplicate key row in object#i', $exception->getMessage());
+    }
+
+    /**
      * Get the default query grammar instance.
      * 
      * @return Syscodes\Components\Database\Query\SqlServerGrammar
      */
     public function getDefaultQueryGrammar()
     {
-        return $this->withTablePrefix(new QueryGrammar);
+        return $this->withTablePrefix(new QueryGrammar($this));
     }
 
     /**
@@ -73,6 +149,6 @@ class SqlServerConnection extends Connection
      */
     protected function getDefaultSchemaGrammar()
     {
-        return $this->withTablePrefix(new SchemaGrammar);
+        return $this->withTablePrefix(new SchemaGrammar($this));
     }
 }
