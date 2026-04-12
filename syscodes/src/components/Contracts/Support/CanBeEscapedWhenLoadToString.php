aa3fa3e6523cda2b92c28baaa77987b818a05b8c
Alexander Campo <jalexcam@gmail.com>
2026-04-12 02:46:53 -0500

    [0.8.x] Added new interface

---
+++ syscodes/src/components/Contracts/Support/CanBeEscapedWhenLoadToString.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2026 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Contracts\Support;
+
+/**
+ * Gets the indication for the paginator's string expressed be escaped.
+ */
+interface CanBeEscapedWhenLoadToString
+{
+    /**
+     * Indicate that the paginator's string representation should be escaped when __toString is invoked.
+     *
+     * @param  bool
+     * 
+     * @return static
+     */
+    public function escapeWhenLoadingToString($escape = true): static;
+}
