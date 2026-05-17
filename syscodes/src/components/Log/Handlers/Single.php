89fe6c1b2b16763e314951c50c579322484d049b
Alexander Campo <jalexcam@gmail.com>
2026-05-16 23:41:58 -0500

    [0.9.x] Changed the filename FileLogger by Single

---
+++ syscodes/src/components/Log/Handlers/Single.php
@@ -0,0 +1,285 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2026 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Log\Handlers;
+
+use DateTime;
+use Syscodes\Components\Contracts\Log\Handler;
+use Syscodes\Components\Log\Concerns\ParseLogEnvironment;
+use Syscodes\Components\Log\Exceptions\LogException;
+use Syscodes\Components\Support\Chronos;
+use Throwable;
+
+/**
+ * The Lenevor Single logger of errors.
+ */
+class Single implements Handler
+{
+    use ParseLogEnvironment;
+    
+    /**
+     * The application implementation.
+     * 
+     * @var \Syscodes\Components\Contracts\Core\Application
+     */
+    protected $app;
+
+    /**
+     * Format of the timestamp for log files.
+     * 
+     * @var string
+     */
+    protected $logDateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Path to the log file.
+     * 
+     * @var string
+     */
+    protected $logFilePath;
+
+    /**
+     * Octal notation for default permissions of the log file.
+     * 
+     * @var int
+     */
+    protected $logFilePermissions;
+
+    /**
+     * Caches instances of the handlers.
+     * 
+     * @var string
+     */
+    protected $logHandler;
+
+    /**
+     * Write message in log file.
+     * 
+     * @var string
+     */
+    protected $logMessage;
+
+    /**
+     * Constructor. Create new Single class instance.
+     * 
+     * @param  array  $config
+     * @param  \Syscodes\Components\Contracts\Core\Application  $app
+     * 
+     * @return void
+     */
+    public function __construct(array $config, $app)
+    {
+        $this->app = $app;
+        $this->logFilePath = $config['path'] ?? $this->app->storagePath().'/logs/lenevor.log';
+        $this->logFilePermissions = $config['permission'] ?? 0644;
+    }
+
+    /**
+     * Magic method.
+     * 
+     * Destructor. Close.
+     * 
+     * @return bool
+     */
+    public function __destruct()
+    {
+        if ($this->logHandler) {
+            fclose($this->logHandler);
+        }
+    }
+    
+    /**
+     * Logs with an arbitrary level.
+     * 
+     * @param  mixed  $level
+     * @param  string  $message
+     * @param  array  $context
+     * 
+     * @return bool
+     */
+    public function log($level, $message, array $context = []): bool
+    {
+        $message = $this->exchangeProcess($message, $context);
+
+        $this->handle($level, $message);
+
+        return true;
+    }
+
+    /**
+     * Replaces any placeholders in the message with variables
+     * from the context.
+     * 
+     * @param  string  $message
+     * @param  array  $context
+     * 
+     * @return mixed
+     */
+    protected function exchangeProcess($message, array $context = [])
+    {
+        if ( ! is_string($message)) {
+            return $message;
+        }
+        
+        $replace = [];
+        
+        foreach ($context as $key => $value) {
+            if ($key === 'exception' && $value instanceof Throwable) {
+                $value = $value->getMessage().' '.$this->cleanFileNames($value->getFile()).':'.$value->getLine();
+                // Todo - sanitize input before writing to file?
+                $replace["{{$key}}"] = $value;
+            } elseif (null === $value || is_scalar($value) || (is_object($value) && method_exists($value, '__toString'))) {
+                $replace["{{$key}}"] = $value;
+            } elseif ($value instanceof DateTime) {
+                $replace["{{$key}}"] = $value->format(DateTime::RFC3339);
+            } elseif (is_object($value)) {
+                $replace["{{$key}}"] = '[object '.get_class($value).']';
+            } else {
+                $replace["{{$key}}"] = '['.gettype($value).']';
+            }
+        }
+        
+        // Add special placeholders
+        $replace['{postVars}'] = '$_POST: '.print_r($_POST, true);
+        $replace['{getVars}'] = '$_GET: '.print_r($_GET, true);
+        $replace['{env}'] = '['.$this->getLogEnvironment().']';
+        
+        return strtr($message, $replace);
+    }
+
+    /**
+     * Cleans the paths of filenames by replacing APPPATH, SYSPATH
+     * with the actual var. i.e.
+     * 
+     * /var/www/site/app/Http/Controllers/Home.php
+     * 
+     * becomes:
+     * 
+     * APPPATH/Http/Controllers/Home.php
+     * 
+     * @param  string  $file
+     * 
+     * @return string
+     */
+    protected function cleanFileNames(string $file)
+    {
+        $file = str_replace(APP_PATH, 'APPPATH/', $file);
+        $file = str_replace(SYS_PATH, 'SYSPATH/', $file);
+        
+        return $file;
+    }
+
+    /**
+     * Handles logging the message.
+     * 
+     * @param  string  $level
+     * @param  string  $message
+     * 
+     * @return bool
+     */
+    public function handle($level, $message): bool
+    {        
+        $result = '';
+
+        $path = $this->logFilePath;
+        
+        if ( ! is_file($path)) {
+            $newFile = true;
+        }
+
+        $this->open($path);
+
+        $this->logMessage($level, $message);
+        
+        flock($this->logHandler, LOCK_EX);
+        
+        for ($written = 0, $length = strlen($this->logMessage); $written < $length; $written += $result) {
+            if (($result = fwrite($this->logHandler, substr($this->logMessage, $written))) === false) {
+                // if we get this far, we'll never see this during travis-ci
+                // @codeCoverageIgnoreStart
+                break;
+                // @codeCoverageIgnoreEnd
+            }
+        }
+        
+        flock($this->logHandler, LOCK_UN);
+        
+        if (isset($newFile) && $newFile === true) {
+            chmod($path, $this->logFilePermissions);
+        }
+        
+        return is_int($result);
+    }
+
+    /**
+     * Opens the current file.
+     * 
+     * @param  string  $path
+     * 
+     * @return bool
+     */
+    private function open($path): bool
+    {
+        if (false === $this->logHandler = is_resource($path) ? $path : @fopen($path, 'ab')) {
+            throw new LogException(sprintf('Unable to open "%s".', $path));
+        }
+
+        return true;
+    }
+
+    /**
+     * Write message of log file.
+     * 
+     * @param  mixed  $level
+     * @param  string  $message
+     * 
+     * @return static
+     */
+    private function logMessage($level, $message): static
+    {
+        $level = $this->getLogEnvironment().'.'.strtolower($level);
+
+        $message = ucfirst($message);
+
+        $this->logMessage .= "[{$this->getTimestamp()}] [{$level}] {$message}\n";
+
+        return $this;
+    }
+
+    /**
+     * Gets the correctly formatted Date/Time for the log entry.
+     * 
+     * PHP DateTime is dump, and you have to resort to trickery to get microseconds
+     * to work correctly, so here it is.
+     * 
+     * @return string
+     */
+    private function getTimestamp(): string
+    {
+        $logDateFormat = $this->app['config']['logger.dateFormat'] ?? $this->logDateFormat;
+        $originalTime = microtime(true);
+        $micro = sprintf("%06d", ($originalTime - floor($originalTime)) * 1000000);
+        $date = new Chronos(date('Y-m-d H:i:s.'.$micro, (int) $originalTime));
+        
+        return $date->format($logDateFormat);
+    }   
+}
