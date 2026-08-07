4ccaec69472c6480ebb17af18c41e1626ff83917
Alexander Campo <jalexcam@gmail.com>
2026-08-06 22:31:40 -0500

    [0.9.x] Added new methods

--- syscodes/src/components/Validation/Concerns/ValidatesAttributes.php
+++ syscodes/src/components/Validation/Concerns/ValidatesAttributes.php
@@ -22,6 +22,7 @@
 
 namespace Syscodes\Components\Validation\Concerns;
 
+use Symfony\Component\HttpFoundation\File\File;
 use Syscodes\Components\Support\Arr;
 
 /**
@@ -39,5 +40,70 @@ trait ValidatesAttributes
     public function validatePresent($attribute, $value): bool
     {
         return Arr::has($this->data, $attribute);
+    }
+
+    /**
+     * Validate that a required attribute exists.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function validateRequired($attribute, $value): bool
+    {
+        if (is_null($value)) {
+            return false;
+        } elseif (is_string($value) && trim($value) === '') {
+            return false;
+        } elseif (is_countable($value) && count($value) < 1) {
+            return false;
+        } elseif ($value instanceof File) {
+            return (string) $value->getPath() !== '';
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the size of an attribute.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function getSize($attribute, $value)
+    {
+        $hasNumeric = $this->hasRule($attribute, $this->numericRules);
+
+        // This method will determine if the attribute is a number, string, or file and
+        // return the proper size accordingly. If it is a number, then number itself
+        // is the size. If it is a file, we take kilobytes, and for a string the
+        // entire length of the string will be considered the attribute size.
+        if (is_numeric($value) && $hasNumeric) {
+            return $value;
+        } elseif (is_array($value)) {
+            return count($value);
+        } elseif ($value instanceof File) {
+            return $value->getSize() / 1024;
+        }
+
+        return mb_strlen($value);
+    }
+
+    /**
+     * Parse named parameters to $key => $value items.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    protected function parseNamedParameters($parameters): array
+    {
+        return array_reduce($parameters, function ($result, $item) {
+            [$key, $value] = array_pad(explode('=', $item, 2), 2, null);
+
+            $result[$key] = $value;
+
+            return $result;
+        });
     }
 }
