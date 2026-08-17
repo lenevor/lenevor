d61eaa8228808c0725a6888c5bb7061767130725
Alexander Campo <jalexcam@gmail.com>
2026-08-16 23:05:04 -0500

    [0.9.x] Added new interface

---
+++ syscodes/src/components/Contracts/Validation/UncompromisedCheck.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2026 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Contracts\Validation;
+
+/**
+ * Get the check the given data.
+ */
+interface UncompromisedCheck
+{
+    /**
+     * Checks that the given data has not been compromised in data leaks.
+     *
+     * @param  array  $data
+     * @return bool
+     */
+    public function check($data);
+}
