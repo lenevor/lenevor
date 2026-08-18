13e0ec7f9a972650a70cc6a8cab503a7cc7afe6b
Alexander Campo <jalexcam@gmail.com>
2026-08-17 23:22:05 -0500

    Update FormRequest.php

--- syscodes/src/components/Core/Http/FormRequest.php
+++ syscodes/src/components/Core/Http/FormRequest.php
@@ -22,15 +22,21 @@
 
 namespace Syscodes\Components\Core\Http;
 
+use Syscodes\Components\Auth\Access\Response;
 use Syscodes\Components\Contracts\Container\Container;
 use Syscodes\Components\Contracts\Validation\ValidatesResolved;
+use Syscodes\Components\Contracts\Validation\Factory as ValidationFactory;
 use Syscodes\Components\Contracts\Validation\Validator;
+use Syscodes\Components\Core\Http\Attributes\ErrorBag;
+use Syscodes\Components\Core\Http\Attributes\StopOnFirstFailure;
+use Syscodes\Components\Core\Http\Attributes\RedirectTo;
+use Syscodes\Components\Core\Http\Attributes\RedirectToRoute;
 use Syscodes\Components\Http\Exceptions\HttpResponseException;
 use Syscodes\Components\Http\JsonResponse;
 use Syscodes\Components\Http\Request;
-use Syscodes\Components\Http\Response;
 use Syscodes\Components\Routing\Generators\Redirector;
-use Syscodes\Components\Validation\Traits\ValidationWhenResolved;
+use Syscodes\Components\Validation\Concerns\ValidationWhenResolved;
+use ReflectionClass;
 
 /**
  * Gets the form request.
@@ -87,6 +93,20 @@ class FormRequest extends Request implements ValidatesResolved
      * @var string
      */
     protected $redirectRoute;
+
+    /**
+     * Indicates whether validation should stop after the first rule failure.
+     *
+     * @var bool
+     */
+    protected $stopOnFirstFailure = false;
+
+    /**
+     * The validator instance.
+     *
+     * @var \Syscodes\Components\Contracts\Validation\Validator
+     */
+    protected $validator;
     
     /**
      * Get the validator instance for the request.
@@ -95,28 +115,115 @@ class FormRequest extends Request implements ValidatesResolved
      */
     protected function getValidatorInstance()
     {
-        $validation = $this->container->make(Validator::class);
+        if ($this->validator) {
+            return $this->validator;
+        }
+
+        $this->configureFromAttributes();
+
+        $factory = $this->container->make(ValidationFactory::class);
         
         if (method_exists($this, 'validator')) {
-            return $this->container->call([$this, 'validator'], compact('factory'));
+            $validator = $this->container->call($this->validator(...), ['factory' => $factory]);
+        } else {
+            $validator = $this->createDefaultValidator($factory);
+        }
+
+        if (method_exists($this, 'withValidator')) {
+            $this->withValidator($validator);
         }
         
-        $rules = $this->container->call([$this, 'rules']);
+        $this->setValidator($validator);
         
-        return $validation->make($this->all(), $rules, $this->messages(), $this->attributes());
+        return $this->validator;
+    }
+
+    /**
+     * Configure the form request from class attributes.
+     *
+     * @return void
+     */
+    protected function configureFromAttributes(): void
+    {
+        $reflection = new ReflectionClass($this);
+
+        if ($reflection->getAttributes(StopOnFirstFailure::class) !== []) {
+            $this->stopOnFirstFailure = true;
+        }
+
+        $redirectTo = $reflection->getAttributes(RedirectTo::class);
+
+        if ($redirectTo !== []) {
+            $this->redirect = $redirectTo[0]->newInstance()->url;
+        }
+
+        $redirectToRoute = $reflection->getAttributes(RedirectToRoute::class);
+
+        if ($redirectToRoute !== []) {
+            $this->redirectRoute = $redirectToRoute[0]->newInstance()->route;
+        }
+
+        $errorBag = $reflection->getAttributes(ErrorBag::class);
+
+        if ($errorBag !== []) {
+            $this->errorBag = $errorBag[0]->newInstance()->name;
+        }
+    }
+
+    /**
+     * Create the default validator instance.
+     *
+     * @param  \Syscodes\Components\Contracts\Validation\Factory  $factory
+     * @return \Syscodes\Components\Contracts\Validation\Validator
+     */
+    protected function createDefaultValidator(ValidationFactory $factory)
+    {
+        $rules = $this->validationRules();
+
+        $validator = $factory->make(
+            $this->validationData(),
+            $rules,
+            $this->messages(),
+            $this->attributes(),
+        )
+        ->stopOnFirstFailure($this->stopOnFirstFailure);
+
+        return $validator;
+    }
+
+    /**
+     * Get data to be validated from the request.
+     *
+     * @return array
+     */
+    public function validationData(): array
+    {
+        return $this->all();
+    }
+
+    /**
+     * Get the validation rules for this form request.
+     *
+     * @return array
+     */
+    protected function validationRules()
+    {
+        return method_exists($this, 'rules') ? $this->container->call([$this, 'rules']) : [];
     }
     
     /**
      * Handle a failed validation attempt.
      * 
-     * @param  \Syscodes\Components\Contracts\Validation\Validator  $validator 
+     * @param  \Syscodes\Components\Validation\Validator  $validator 
      * @return mixed
      */
     protected function failedValidation(Validator $validator)
     {
-        throw new HttpResponseException($this->response(
-            $this->formatErrors($validator)
-        ));
+        $exception = $validator->getException();
+
+        throw (new $exception($validator))
+            ->errorBag($this->errorBag)
+            ->redirectTo($this->getRedirectUrl());
     }
     
     /**
@@ -127,7 +234,9 @@ class FormRequest extends Request implements ValidatesResolved
     protected function passesAuthorization()
     {
         if (method_exists($this, 'authorize')) {
-            return $this->container->call([$this, 'authorize']);
+            $result = $this->container->call([$this, 'authorize']);
+
+            return $result instanceof Response ? $result->authorize() : $result;
         }
         
         return false;
@@ -171,17 +280,6 @@ class FormRequest extends Request implements ValidatesResolved
     }
     
     /**
-     * Format the errors from the given Validator instance.
-     * 
-     * @param  \Syscodes\Components\Contracts\Validation\Validator  $validator 
-     * @return array
-     */
-    protected function formatErrors(Validator $validator): array
-    {
-        return $validator->getMessageBag()->toArray();
-    }
-    
-    /**
      * Get the URL to redirect to on a validation error.
      * 
      * @return string
@@ -189,16 +287,13 @@ class FormRequest extends Request implements ValidatesResolved
     protected function getRedirectUrl(): string
     {
         $url = $this->redirector->getUrlGenerator();
-        
-        if ($this->redirect) {
-            return $url->to($this->redirect);
-        } else if ($this->redirectRoute) {
-            return $url->route($this->redirectRoute);
-        } else if ($this->redirectAction) {
-            return $url->action($this->redirectAction);
-        }
-        
-        return $url->previous();
+
+        return match (true) {
+            ! empty($this->redirect) => $url->to($this->redirect),
+            ! empty($this->redirectRoute) => $url->route($this->redirectRoute),
+            ! empty($this->redirectAction) => $url->action($this->redirectAction),
+            default => $url->previous(),
+        };
     }
     
     /**
@@ -219,6 +314,19 @@ class FormRequest extends Request implements ValidatesResolved
     public function attributes(): array
     {
         return [];
+    }
+
+     /**
+     * Set the Validator instance.
+     *
+     * @param  \Syscodes\Components\Contracts\Validation\Validator  $validator
+     * @return static
+     */
+    public function setValidator(Validator $validator): static
+    {
+        $this->validator = $validator;
+
+        return $this;
     }
     
     /**
