ee1a1071d1bbb33298b70acf6d5039d9c273da9c
Alexander Campo <jalexcam@gmail.com>
2026-08-18 23:25:24 -0500

    [0.9.x] Added new methods

--- syscodes/src/components/Http/Concerns/CanBePrecognitive.php
+++ syscodes/src/components/Http/Concerns/CanBePrecognitive.php
@@ -22,11 +22,52 @@
 
 namespace Syscodes\Components\Http\Concerns;
 
+use Syscodes\Components\Support\Collection;
+
 /**
  * Lets know if it is precognitive.
  */
 trait CanBePrecognitive
 {
+    /**
+     * Filter the given array of rules into an array of rules that are included in precognitive headers.
+     *
+     * @param  array  $rules
+     * @return array
+     */
+    public function filterPrecognitiveRules($rules)
+    {
+        if ( ! $this->headers->has('Precognition-Validate-Only')) {
+            return $rules;
+        }
+
+        $validateOnly = explode(',', $this->header('Precognition-Validate-Only'));
+
+        return (new Collection($rules))
+            ->filter(fn ($rule, $attribute) => $this->shouldValidatePrecognitiveAttribute($attribute, $validateOnly))
+            ->all();
+    }
+
+    /**
+     * Determine if the given attribute should be validated.
+     *
+     * @param  string  $attribute
+     * @param  array  $validateOnly
+     * @return bool
+     */
+    protected function shouldValidatePrecognitiveAttribute($attribute, $validateOnly): bool
+    {
+        foreach ($validateOnly as $pattern) {
+            $regex = '/^'.str_replace('\*', '[^.]+', preg_quote($pattern, '/')).'$/';
+
+            if (preg_match($regex, $attribute)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     /**
      * Determine if the request is attempting to be precognitive.
      * 
