d35a960619f85c30c9d9363825015cf2b55fe8bd
Alexander Campo <jalexcam@gmail.com>
2026-08-25 23:38:59 -0500

    Update Handler.php

--- syscodes/src/components/Core/Exceptions/Handler.php
+++ syscodes/src/components/Core/Exceptions/Handler.php
@@ -40,13 +40,16 @@ use Syscodes\Components\Core\Http\Exceptions\HttpException;
 use Syscodes\Components\Core\Http\Exceptions\NotFoundHttpException;
 use Syscodes\Components\Core\Http\Exceptions\AccessDeniedHttpException;
 use Syscodes\Components\Database\Erostrine\Exceptions\ModelNotFoundException;
+use Syscodes\Components\Database\Exceptions\RecordNotFoundException;
+use Syscodes\Components\Database\Exceptions\RecordsNotFoundException;
 use Syscodes\Components\Http\Exceptions\HttpResponseException;
 use Syscodes\Components\Http\RedirectResponse;
 use Syscodes\Components\Http\Response;
 use Syscodes\Components\Routing\Router;
 use Syscodes\Components\Session\Exceptions\TokenMismatchException;
 use Syscodes\Components\Support\Arr;
-use Syscodes\Components\Support\Str;
+use Syscodes\Components\Support\Collection;
+use Syscodes\Components\Support\Stringable;
 use Syscodes\Components\Support\ViewErrorBag;
 use Syscodes\Components\Validation\Exceptions\ValidationException;
 use Throwable;
@@ -64,6 +67,17 @@ class Handler implements ExceptionHandlerContract
     protected $container;
 
     /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
      * A list of the exception types that should not be reported.
      * 
      * @var array 
@@ -77,12 +91,23 @@ class Handler implements ExceptionHandlerContract
      */
     protected $coreDontReport = [
         AuthenticationException::class,
+        AuthorizationException::class,
         HttpException::class,
         HttpResponseException::class,
         ModelNotFoundException::class,
+        RecordNotFoundException::class,
+        RecordsNotFoundException::class,
+        RequestExceptionInterface::class,
         TokenMismatchException::class,
         ValidationException::class,
     ];
+
+    /**
+     * The callback that prepares responses to be returned to the browser.
+     *
+     * @var callable|null
+     */
+    protected $finalizeResponseCallback;
 
     /**
      * The callbacks that should be used during reporting.
@@ -97,6 +122,13 @@ class Handler implements ExceptionHandlerContract
      * @var array 
      */
     protected $renderCallbacks = [];
+
+    /**
+     * The callback that determines if the exception handler response should be JSON.
+     *
+     * @var callable|null
+     */
+    protected $shouldRenderJsonWhenCallback;
 
     /**
      * Constructor. Create a new exception handler instance.
@@ -211,32 +243,53 @@ class Handler implements ExceptionHandlerContract
     public function render($request, Throwable $e)
     {
         if (method_exists($e, 'render') && $response = $e->render($request)) {
-            return Router::toResponse($request, $response);
+            return $this->finalizeRenderedResponse(
+                $request,
+                Router::toResponse($request, $response),
+                $e
+            );
         }
         
         $e = $this->prepareException($e);
         
-        foreach ($this->renderCallbacks as $renderCallback) {
-            $response = $renderCallback($e, $request);
-            
-            if ( ! is_null($response)) {
-                return $response;
-            }
+        if ($response = $this->renderViaCallbacks($request, $e)) {
+            return $this->finalizeRenderedResponse($request, $response, $e);
         }
 
-        foreach ($this->renderCallbacks as $renderCallback) {
-            $response = $renderCallback($e, $request);
+        return $this->finalizeRenderedResponse($request, match (true) {
+            $e instanceof HttpResponseException => $e->getResponse(),
+            $e instanceof AuthenticationException => $this->unauthenticated($request, $e),
+            $e instanceof ValidationException => $this->convertValidationExceptionToResponse($e, $request),
+            default => $this->renderExceptionResponse($request, $e),
+        }, $e);
+    }
 
-            if ( ! is_null($response)) {
-                return $response;
-            }
-        }
+    /**
+     * Prepare the final, rendered response to be returned to the browser.
+     *
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @param  \Throwable  $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function finalizeRenderedResponse($request, $response, Throwable $e)
+    {
+        return $this->finalizeResponseCallback
+            ? call_user_func($this->finalizeResponseCallback, $response, $e, $request)
+            : $response;
+    }
 
-        if ($e instanceof HttpResponseException) {
-            $e->getResponse();
-        }
+    /**
+     * Prepare the final, rendered response for an exception using the given callback.
+     *
+     * @param  callable  $callback
+     * @return static
+     */
+    public function respondUsing($callback): static
+    {
+        $this->finalizeResponseCallback = $callback;
 
-        return $this->prepareResponse($request, $e);
+        return $this;
     }
 
     /**
@@ -255,10 +308,146 @@ class Handler implements ExceptionHandlerContract
             $e instanceof AuthorizationException && ! $e->hasStatus() => new AccessDeniedHttpException($e->getMessage(), $e),
             $e instanceof TokenMismatchException => new HttpException(419, $e->getMessage(), $e),
             $e instanceof RequestExceptionInterface => new BadRequestHttpException('Bad request.', $e),
+            $e instanceof RecordNotFoundException => new NotFoundHttpException('Not found.', $e),
+            $e instanceof RecordsNotFoundException => new NotFoundHttpException('Not found.', $e),
             default => $e,
         };
     }
-     
+
+    /**
+     * Try to render a response from request and exception via render callbacks.
+     *
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return mixed
+     *
+     * @throws \ReflectionException
+     */
+    protected function renderViaCallbacks($request, Throwable $e)
+    {
+        foreach ($this->renderCallbacks as $renderCallback) {
+            foreach ($this->firstClosureParameterTypes($renderCallback) as $type) {
+                if (is_a($e, $type)) {
+                    $response = $renderCallback($e, $request);
+
+                    if ( ! is_null($response)) {
+                        return $response;
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     * Render a default exception response if any.
+     *
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return \Syscodes\Components\Http\Response|\Syscodes\Components\Http\JsonResponse|\Syscodes\Components\Http\RedirectResponse
+     */
+    protected function renderExceptionResponse($request, Throwable $e)
+    {
+        return $this->shouldReturnJson($request, $e)
+            ? $this->prepareJsonResponse($request, $e)
+            : $this->prepareResponse($request, $e);
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  \Syscodes\Components\Auth\Exceptions\AuthenticationException  $exception
+     * @return \Syscodes\Components\Http\Response|\Syscodes\Components\Http\JsonResponse|\Syscodes\Components\Http\RedirectResponse
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($this->shouldReturnJson($request, $exception)) {
+            return response()->json(['message' => $exception->getMessage()], 401);
+        }
+
+        $redirectTo = $exception->redirectTo($request);
+
+        if ( ! $redirectTo) {
+            return response()->noContent(401);
+        }
+
+        return redirect()->guest($redirectTo);
+    }
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Syscodes\Components\Validation\Exceptions\ValidationException  $e
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        if ($e->response) {
+            return $e->response;
+        }
+
+        return $this->shouldReturnJson($request, $e)
+            ? $this->invalidJson($request, $e)
+            : $this->invalid($request, $e);
+    }
+
+    /**
+     * Convert a validation exception into a response.
+     *
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  \Syscodes\Components\Validation\Exceptions\ValidationException  $exception
+     * @return \Syscodes\Components\Http\Response|\Syscodes\Components\Http\JsonResponse|\Syscodes\Components\Http\RedirectResponse
+     */
+    protected function invalid($request, ValidationException $exception)
+    {
+        return redirect($exception->redirectTo ?? url()->previous())
+            ->withInput(Arr::except($request->input(), $this->dontFlash))
+            ->withErrors($exception->errors(), $request->input('_error_bag', $exception->errorBag));
+    }
+
+    /**
+     * Convert a validation exception into a JSON response.
+     *
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  \Syscodes\Components\Validation\Exceptions\ValidationException  $exception
+     * @return \Syscodes\Components\Http\JsonResponse
+     */
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'message' => $exception->getMessage(),
+            'errors' => $exception->errors(),
+        ], $exception->status);
+    }
+
+    /**
+     * Determine if the exception handler response should be JSON.
+     *
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return bool
+     */
+    protected function shouldReturnJson($request, $e)
+    {
+        return $this->shouldRenderJsonWhenCallback
+            ? call_user_func($this->shouldRenderJsonWhenCallback, $request, $e)
+            : $request->expectsJson();
+    }
+
+    /**
+     * Register the callable that determines if the exception handler response should be JSON.
+     *
+     * @param  callable(\Syscodes\Components\Http\Request $request, \Throwable): bool  $callback
+     * @return static
+     */
+    public function shouldRenderJsonWhen($callback): static
+    {
+        $this->shouldRenderJsonWhenCallback = $callback;
+
+        return $this;
+    }
+
     /**
      * Prepare a response for the given exception.
      * 
@@ -297,8 +486,8 @@ class Handler implements ExceptionHandlerContract
         if ($view = $this->getHttpExceptionView($e)) {
             try {
                 return response()->view($view, [
-                        'errors' => new ViewErrorBag,
-                        'exception' => $e,
+                    'errors' => new ViewErrorBag,
+                    'exception' => $e,
                 ], $e->getStatusCode(), $e->getHeaders());
             } catch (Throwable $th) {
                 config('app.debug') && throw $th;
@@ -423,6 +612,42 @@ class Handler implements ExceptionHandlerContract
     }
 
     /**
+     * Prepare a JSON response for the given exception.
+     *
+     * @param  \Syscodes\Components\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return \Syscodes\Components\Http\JsonResponse
+     */
+    protected function prepareJsonResponse($request, Throwable $e)
+    {
+        return response()->json(
+            $this->convertExceptionToArray($e),
+            $this->isHttpException($e) ? $e->getStatusCode() : 500,
+            $this->isHttpException($e) ? $e->getHeaders() : [],
+            JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
+    }
+
+    /**
+     * Convert the given exception to an array.
+     *
+     * @param  \Throwable  $e
+     * @return array
+     */
+    protected function convertExceptionToArray(Throwable $e): array
+    {
+        return config('app.debug') ? [
+            'message' => $e->getMessage(),
+            'exception' => get_class($e),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'trace' => (new Collection($e->getTrace()))->map(fn ($trace) => Arr::except($trace, ['args']))->all(),
+        ] : [
+            'message' => $this->isHttpException($e) ? $e->getMessage() : 'Server Error',
+        ];
+    }
+
+    /**
      * Render an exception to the console.
      * 
      * @param  \Symfony\Component\Console\Output\OutputInterface  $output
@@ -432,7 +657,7 @@ class Handler implements ExceptionHandlerContract
     public function renderForConsole($output, Throwable $e)
     {
         if ($e instanceof CommandNotFoundException) {
-            $message = Str::of($e->getMessage());
+            $message = (new Stringable($e->getMessage()))->explode('.')->first();
 
             if ( ! empty($alternatives = $e->getAlternatives())) {
                 $message .= '. Did you mean one of these?';
