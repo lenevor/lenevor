930212de8957eadf05ee4869430511be2a1ee711
Alexander Campo <jalexcam@gmail.com>
2026-08-25 23:39:32 -0500

    Update FormRequest.php

--- syscodes/src/components/Core/Http/FormRequest.php
+++ syscodes/src/components/Core/Http/FormRequest.php
@@ -22,21 +22,22 @@
 
 namespace Syscodes\Components\Core\Http;
 
+use Syscodes\Components\Auth\Access\Exceptions\AuthorizationException;
 use Syscodes\Components\Auth\Access\Response;
 use Syscodes\Components\Contracts\Container\Container;
 use Syscodes\Components\Contracts\Validation\ValidatesResolved;
 use Syscodes\Components\Contracts\Validation\Factory as ValidationFactory;
 use Syscodes\Components\Contracts\Validation\Validator;
 use Syscodes\Components\Core\Http\Attributes\ErrorBag;
+use Syscodes\Components\Core\Http\Attributes\FailOnUnknownFields;
 use Syscodes\Components\Core\Http\Attributes\StopOnFirstFailure;
 use Syscodes\Components\Core\Http\Attributes\RedirectTo;
 use Syscodes\Components\Core\Http\Attributes\RedirectToRoute;
-use Syscodes\Components\Http\Exceptions\HttpResponseException;
-use Syscodes\Components\Http\JsonResponse;
 use Syscodes\Components\Http\Request;
 use Syscodes\Components\Routing\Generators\Redirector;
 use Syscodes\Components\Validation\Concerns\ValidationWhenResolved;
 use ReflectionClass;
+use Syscodes\Components\Support\Arr;
 
 /**
  * Gets the form request.
@@ -107,6 +108,13 @@ class FormRequest extends Request implements ValidatesResolved
      * @var \Syscodes\Components\Contracts\Validation\Validator
      */
     protected $validator;
+
+    /**
+     * Indicates if unknown fields should be rejected for all form requests.
+     *
+     * @var bool
+     */
+    protected static bool $globalFailOnUnknownFields = false;
     
     /**
      * Get the validator instance for the request.
@@ -131,6 +139,13 @@ class FormRequest extends Request implements ValidatesResolved
 
         if (method_exists($this, 'withValidator')) {
             $this->withValidator($validator);
+        }
+
+        if (method_exists($this, 'after')) {
+            $validator->after($this->container->call(
+                $this->after(...),
+                ['validator' => $validator]
+            ));
         }
         
         $this->setValidator($validator);
@@ -185,8 +200,13 @@ class FormRequest extends Request implements ValidatesResolved
             $rules,
             $this->messages(),
             $this->attributes(),
-        )
-        ->stopOnFirstFailure($this->stopOnFirstFailure);
+        )->stopOnFirstFailure($this->stopOnFirstFailure);
+
+        if ($this->isPrecognitive()) {
+            $validator->setRules(
+                $this->filterPrecognitiveRules($validator->getRulesWithoutPlaceholders())
+            );
+        }
 
         return $validator;
     }
@@ -210,6 +230,72 @@ class FormRequest extends Request implements ValidatesResolved
     {
         return method_exists($this, 'rules') ? $this->container->call([$this, 'rules']) : [];
     }
+
+    /**
+     * Determine if fields not present in rules should fail validation.
+     *
+     * @return bool
+     */
+    protected function shouldFailOnUnknownFields(): bool
+    {
+        $failOnUnknownFields = (new ReflectionClass($this))->getAttributes(FailOnUnknownFields::class);
+
+        return $failOnUnknownFields !== []
+            ? $failOnUnknownFields[0]->newInstance()->value
+            : static::$globalFailOnUnknownFields;
+    }
+
+    /**
+     * Validate that no unknown fields were sent as input.
+     *
+     * @param  \Syscodes\Components\Contracts\Validation\Validator  $validator
+     * @return void
+     */
+    protected function validateNoUnknownFields(Validator $validator)
+    {
+        $allowedKeys = array_keys($this->validationRules());
+
+        $input = $this->isJson() ? $this->json()->all() : $this->request->all();
+
+        foreach (array_keys(Arr::dot($input)) as $inputKey) {
+            if ( ! $this->isKnownField($inputKey, $allowedKeys)) {
+                $validator->errors()->add($inputKey, trans('validation.prohibited', [
+                    'attribute' => str_replace('_', ' ', $inputKey),
+                ]));
+            }
+        }
+    }
+
+    /**
+     * Determine if the given input key is an allowed key based on the validation rules.
+     *
+     * @param  string  $inputKey
+     * @param  array  $allowedKeys
+     * @return bool
+     */
+    protected function isKnownField(string $inputKey, array $allowedKeys): bool
+    {
+        foreach ($allowedKeys as $ruleKey) {
+            if ($ruleKey === $inputKey) {
+                return true;
+            }
+
+            if (str_ends_with($inputKey, '_confirmation') &&
+                $ruleKey === substr($inputKey, 0, -13)) {
+                return true;
+            }
+
+            if (str_contains($ruleKey, '*')) {
+                $pattern = '/^'.str_replace('\*', '[^.]+', preg_quote($ruleKey, '/')).'$/';
+
+                if (preg_match($pattern, $inputKey)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
     
     /**
      * Handle a failed validation attempt.
@@ -224,49 +310,6 @@ class FormRequest extends Request implements ValidatesResolved
         throw (new $exception($validator))
             ->errorBag($this->errorBag)
             ->redirectTo($this->getRedirectUrl());
-    }
-    
-    /**
-     * Determine if the request passes the authorization check.
-     * 
-     * @return bool
-     */
-    protected function passesAuthorization()
-    {
-        if (method_exists($this, 'authorize')) {
-            $result = $this->container->call([$this, 'authorize']);
-
-            return $result instanceof Response ? $result->authorize() : $result;
-        }
-        
-        return false;
-    }
-    
-    /**
-     * Handle a failed authorization attempt.
-     * 
-     * @return mixed
-     */
-    protected function failedAuthorization()
-    {
-        throw new HttpResponseException($this->forbiddenResponse());
-    }
-    
-    /**
-     * Get the proper failed validation response for the request.
-     * 
-     * @param  array  $errors 
-     * @return \Syscodes\Components\Http\Response
-     */
-    public function response(array $errors)
-    {
-        if ($this->ajax() || $this->wantsJson()) {
-            return new JsonResponse($errors, 422);
-        }
-        
-        return $this->redirector->to($this->getRedirectUrl())
-            ->withInput($this->except($this->dontFlash))
-            ->withErrors($errors, $this->errorBag);
     }
     
     /**
@@ -294,6 +337,63 @@ class FormRequest extends Request implements ValidatesResolved
             ! empty($this->redirectAction) => $url->action($this->redirectAction),
             default => $url->previous(),
         };
+    }
+
+     /**
+     * Determine if the request passes the authorization check.
+     *
+     * @return bool
+     *
+     * @throws \Syscodes\Components\Auth\Access\Exceptions\AuthorizationException
+     */
+    protected function passesAuthorization()
+    {
+        if (method_exists($this, 'authorize')) {
+            $result = $this->container->call([$this, 'authorize']);
+
+            return $result instanceof Response ? $result->authorize() : $result;
+        }
+
+        return true;
+    }
+
+    /**
+     * Handle a failed authorization attempt.
+     *
+     * @return void
+     *
+     * @throws \Syscodes\Components\Auth\Access\Exceptions\AuthorizationException
+     */
+    protected function failedAuthorization()
+    {
+        throw new AuthorizationException;
+    }
+
+    /**
+     * Get a validated input container for the validated input.
+     *
+     * @param  array<int, string>|null  $keys
+     * @return ($keys is array ? array<string, mixed> : \Syscodes\Components\Support\ValidatedInput)
+     *
+     * @throws \Syscodes\Components\Validation\Exceptions\ValidationException
+     */
+    public function safe(?array $keys = null)
+    {
+        return is_array($keys)
+            ? $this->validator->safe()->only($keys)
+            : $this->validator->safe();
+    }
+
+    /**
+     * Get the validated data from the request.
+     *
+     * @param  array|int|string|null  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    public function validated($key = null, $default = null)
+    {
+        return data_get($this->validator->validated(), $key, $default);
     }
     
     /**
