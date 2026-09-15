2220ee83193daa962cc874b1a9d919b684b77977
Alexander Campo <jalexcam@gmail.com>
2026-09-15 00:38:23 -0500

    [0.9.x] Added new event class

---
+++ syscodes/src/components/Cache/Events/CacheFlushing.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2026 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Cache\Events;
+
+/**
+ * Allows the cache flushing.
+ */
+class CacheFlushing
+{
+    /**
+     * The name of the cache store.
+     *
+     * @var string|null
+     */
+    public $storeName;
+
+    /**
+     * The tags that were assigned to the key.
+     *
+     * @var array
+     */
+    public $tags;
+
+    /**
+     * Constructor. Create a new event instance.
+     *
+     * @param  string|null  $storeName
+     * @param  array  $tags
+     * @return void
+     */
+    public function __construct($storeName, array $tags = [])
+    {
+        $this->storeName = $storeName;
+        $this->tags = $tags;
+    }
+
+    /**
+     * Set the tags for the cache event.
+     *
+     * @param  array  $tags
+     * @return $this
+     */
+    public function setTags($tags): static
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+}
