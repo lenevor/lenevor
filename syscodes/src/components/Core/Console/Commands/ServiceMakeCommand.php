6fe09b82bb92ac815efde7d421c9693fcbc47083
Alexander Campo <jalexcam@gmail.com>
2026-09-26 00:44:57 -0500

    [0.9.x] Created a new service class from the console

---
+++ syscodes/src/components/Core/Console/Commands/ServiceMakeCommand.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2026 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Core\Console\Commands;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Syscodes\Components\Console\GeneratorCommand;
+
+/**
+ * Create a service for console.
+ */
+#[AsCommand(name: 'make:service')]
+class ServiceMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:service';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new service class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
+    /**
+     * Get the template file for the generator.
+     *
+     * @return string
+     */
+    protected function getTemplate(): string
+    {
+        return $this->resolvetemplatePath('/templates/service.tpl');
+    }
+
+    /**
+     * Resolve the fully-qualified path to the template.
+     *
+     * @param  string  $template 
+     * @return string
+     */
+    protected function resolvetemplatePath($template): string
+    {
+        return file_exists($customPath = $this->lenevor->basePath(trim($template, '/')))
+            ? $customPath
+            : __DIR__.$template;
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace 
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+         return $rootNamespace.'\Services';
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the service already exists'],
+        ];
+    }
+}
